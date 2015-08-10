9c49f7acc4f2a19417b1b3b69ad8bb4590782432
Justas Azna <ja@fadeit.dk>
2015-08-10 13:47:23 +0200

    Moving more graph logic into graph component [WIP]

--- src/js/react/graph.jsx
+++ src/js/react/graph.jsx
@@ -1,5 +1,4 @@
 import React from 'react/addons'
-import Chat from './chat.jsx'
 
 import url from 'url'
 import d3 from 'd3'
@@ -7,6 +6,11 @@ import N3 from 'n3'
 import D3Converter from '../lib/d3-converter.js'
 import WebAgent from '../lib/web-agent.js'
 
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function(){
+    this.parentNode.appendChild(this);
+  });
+}
 
 // some style variables - should move them out of here
 let jolocom = {};
@@ -31,8 +35,7 @@ jolocom.style.large_node_size = jolocom.style.width * .5;
 
 
 class D3Graph {
-  // used to be called "prepareVars"
-  constructor() {
+  prepareVars() {
     // indicates whether it's the first graph initialization
     this.firstInit = true;
 
@@ -88,6 +91,150 @@ class D3Graph {
 
     //group links (so they don't overlap nodes)
     this.link_group = this.vis.append( "g" ).attr( "class", "link_group");
+
+    // shit from mobile.js
+    self = this
+    // plus button
+    this.plus = {
+      drawer: false,
+      toggleDrawer: function(){
+        if( self.plus.drawer ){
+          document.getElementsByTagName('body')[0].className = 'closed-drawer';
+          d3.select("#plus_drawer")
+            .transition()
+            .style( "top", jolocom.style.height+"px" );
+          self.zoom.reset();
+        } else {
+          document.getElementsByTagName('body')[0].className = 'open-drawer';
+          d3.select("#plus_drawer")
+            .transition()
+            .style( "top", ( jolocom.style.height / 2 )+"px" );
+          self.zoom.to( 0.5,
+                   jolocom.style.width / 2,
+                   0 );
+        }
+        self.plus.drawer = !self.plus.drawer;
+      },
+      toInbox: function(){
+        // @Justas: this adds the (fake) node to the inbox
+        self.inbox.add({ title: $( "#plus_drawer .title" ).val(),
+                    description: $( "#plus_drawer .description" ).val() });
+        self.plus.toggleDrawer();
+        self.inbox.spring();
+      },
+      directConnect: function(){
+        // @Justas: this adds the (fake) node to the d3.js graph
+        // (find `addNode` in `./VisualRDF-js/main.js`)
+        self.addNode({ title: $( "#plus_drawer .title" ).val(),
+                  description: $( "#plus_drawer .description" ).val() });
+        self.plus.toggleDrawer();
+      }
+    }
+    
+    // inbox
+    this.inbox = {
+      isOpen: false,
+      count: 0,
+      nodes: [],
+      enlarge: function(){
+        $( "#inbox_large" ).css( "display", "block" );
+        zoom.to( 0.5,
+                 jolocom.style.width / 2,
+                 jolocom.style.height );
+      },
+      reduce: function(){
+        $( "#inbox_large" ).css( "display", "none" );
+        zoom.reset();
+      },
+      open: function(){
+        if( !inbox.isOpen ){
+          d3.select( "#inbox" )
+            .transition()
+            .style( "right", ( -jolocom.style.width / 2 )+"px");
+          inbox.isOpen = true;
+        }
+      },
+      close: function(){
+        if( inbox.isOpen ){
+          var size = (inbox.count==0)
+            ?(-jolocom.style.width)
+            :(-jolocom.style.width+(jolocom.style.width/5));
+          d3.select( "#inbox" )
+            .transition()
+            .style( "right", size+"px" );
+          inbox.isOpen = false;
+        }
+      },
+      spring: function(){
+        // a hack for opening it up a bit
+        inbox.isOpen = true;
+        inbox.close();
+      },
+      add: function( d ){
+        inbox.nodes.push( d );
+        inbox.count++;
+        // @Justas: this creates the DOM for the nodes in the inbox
+        $( "#inbox .counter .number" ).text( inbox.count ); // update counter
+        if( inbox.count == 1 ){ // fade in on first node
+          d3.select( "#inbox .counter" )
+            .transition()
+            .style( "opacity", 1 );
+        }
+        var div = "<div></div>";
+        var node = $( div ).addClass( "node" ).attr( "draggable", "true" );
+        node.__index__ = inbox.count - 1;
+        var title = $( div ).addClass( "title" ).text( d.title );
+        var element = $( div ).addClass( "element" ).append( node ).append( title );
+        $( "#inbox_large" ).prepend( element );
+        node.on( "click", function(){
+          addNode( inbox.nodes[ node.__index__ ] ); // `addNode` in `./visualRDF-js/main.js`
+          inbox.reduce();
+        });
+      }
+    };
+    // chat
+    this.chat = {
+      afterLoad: function () {
+    
+    	  d3.select( "#chat" )
+    		.transition()
+    		.style( "top", ( jolocom.style.height / 3 )+ "px");
+    	  zoom.to( 0.5,
+          jolocom.style.width / 2,
+          jolocom.style.height / -6 );
+          console.log('chat after load');
+          return false;
+      }
+    }
+    this.chat.show = function(){
+    	// draw react component for chat by communicating the state to react
+    	$("#chat-state").val("loaded").trigger("click");
+      $("#chat_container").addClass('visible')
+    	console.log("Chat load");
+    };
+    
+    this.chat.hide = function(){
+        d3.select( "#chat" )
+          .transition()
+          .style( "top", jolocom.style.height + "px");
+    
+        self.zoom.reset();
+        $("#chat-state").val("unloaded").trigger("click");
+        $("#chat_container").removeClass('visible')
+        console.log("Chat unload");
+      };
+    
+    // graph zoom/scale
+    this.zoom = {
+      to: function( scale, x, y ){
+        self.vis.transition()
+          .attr("transform", "scale( "+ scale + " ) translate(" + x + "," + y + ")");
+      },
+      reset: function(){
+        self.vis.transition()
+          .attr("transform", "scale( 1 ) translate( 0, 0 )" );
+      }
+    }
   }
 
   mergeGraphs(newNodes, newLinks) {
@@ -664,8 +811,7 @@ class D3Graph {
   }
 
   enablePreview( node ){
-    // TODO: not available
-    //node.moveToFront();
+    node.moveToFront();
 
     node
       .select("circle") // only current circle
@@ -721,8 +867,58 @@ class D3Graph {
 
 }
 
+
+let d3g = new D3Graph()
+console.log(d3g)
+
 let agent = new WebAgent()
 let testData = '{"nodes":[{"name":"https://sister.jolocom.com/reederz/profile/card#me","type":"uri","uri":"https://sister.jolocom.com/reederz/profile/card#me","title":"no title","description":"no description"},{"name":"https://sister.jolocom.com/reederz/profile/card#key","type":"uri","uri":"https://sister.jolocom.com/reederz/profile/card#key","title":"no title","description":"no description"}],"links":[{"source":"0","target":"1","name":" http://www.w3.org/ns/auth/cert#key","value":"10"}],"literals":{"https://sister.jolocom.com/reederz/profile/card":[{"p":"http://purl.org/dc/terms/title","o":"WebID profile of Justas Azna","l":"None","d":"http://www.w3.org/2001/XMLSchema#string"}],"https://sister.jolocom.com/reederz/profile/card#key":[{"p":"http://www.w3.org/ns/auth/cert#modulus","o":"a726cd1e3eddccb4c12ebd3d8581ac83bc2913dfb88cf9d939b5eedff88878e95b9bfa278dc4fbcd5dfe7d80ff866844b69af31f30a79822e32841f3e694d75feeaba92ed91b02be46fd3f86bb800d3be222c4cb480ffa05f87f67949f41324cacc4c34ee9d133d19eb61af71ab6d97048af91d357904b1cdec8333affe1f4098cdd6f6ca465bdbae56e0b006557396dc0abb7ed2c4f3a41dd76ef07cfeb8309e80804b0c288e6e847c0b446d112403d77c939c67b30cc99a96cd695c8dbc53a5afe42bf81c53214ace49c2ce6b5dc35a0c5301dca386d9735b6d7f39c5185a6811459446940ec7b5a5050fc22c6f99844a1b9580ff0972049e898d1e16a3373","l":"None","d":"http://www.w3.org/2001/XMLSchema#hexBinary"},{"p":"http://www.w3.org/ns/auth/cert#exponent","o":"65537","l":"None","d":"http://www.w3.org/2001/XMLSchema#int"}],"https://sister.jolocom.com/reederz/profile/card#me":[{"p":"http://xmlns.com/foaf/0.1/mbox","o":"mailto:justas.azna@gmail.com","l":"None","d":"http://www.w3.org/2001/XMLSchema#string"},{"p":"http://xmlns.com/foaf/0.1/name","o":"Justas Azna","l":"None","d":"http://www.w3.org/2001/XMLSchema#string"}]}}';
+
+let Chat = React.createClass({
+  chatHide: function(e) {
+    //TODO
+    console.log('chat hide')
+    d3g.chat.hide()
+  },
+  render: function() {
+    return (
+      <div id="chat">
+        { /* TODO: structure, ids and class names suck*/ }
+        <div className="close" onClick={this.chatHide}>x</div>
+        <div className="head">
+          <h1 className="title">TODO: chat topic goes here</h1>
+          <p className="origin">TODO: chat origin goes here</p>
+          <div className="message">
+            <div className="content">
+              <h2>TODO: author goes here</h2>
+              <p>TODO: content goes here</p>
+            </div>
+            <div className="node link"/>
+          </div>
+          <div className="message">
+            <div className="content">
+              <h2>TODO: author goes here</h2>
+              <p>TODO: content goes here</p>
+            </div>
+            <div className="node link"/>
+          </div>
+          <div className="message">
+            <div className="content">
+              <h2>TODO: author goes here</h2>
+              <p>TODO: content goes here</p>
+            </div>
+            <div className="node link"/>
+          </div>
+        </div>
+        <div className="message new">
+          <textarea className="content" value="TODO: Current message"/>
+          <div className="node link"/>
+          <div className="button" onClick={this.chatHide}>tell</div>
+        </div>
+      </div>
+    )
+  }
+})
 
 
 let Graph = React.createClass({
@@ -733,8 +929,7 @@ let Graph = React.createClass({
     console.log(d3graph)
   },
   componentDidMount: function() {
-    let d3g = new D3Graph()
-    console.log(d3g)
+    d3g.prepareVars()
     d3g.init(testData)
     return
     console.log('graph component did mount')
@@ -769,6 +964,40 @@ let Graph = React.createClass({
       })
   },
 
+  toggleDrawer: function(e) {
+    //TODO
+    console.log('toggleDrawer')
+    d3g.plus.toggleDrawer()
+  },
+
+  directConnect: function(e) {
+    //TODO
+    console.log('directConnect')
+    d3g.plus.directConnect()
+  },
+
+  toInbox: function(e) {
+    //TODO
+    console.log('toInbox')
+    d3g.plus.toInbox()
+
+    // inbox
+    $( "#inbox" ).on( "click", this.inbox.enlarge );
+    $( "#inbox_large .close" ).on( "click", this.inbox.reduce );
+  },
+
+  inboxEnlarge: function(e) {
+    //TODO
+    console.log('inboxEnlarge')
+    d3g.inbox.enlarge()
+  },
+
+  inboxReduce: function(e) {
+    //TODO
+    console.log('inboxReduce')
+    d3g.inbox.reduce()
+  },
+
   render: function() {
     return (
       <div id="graph-outer">
@@ -778,8 +1007,8 @@ let Graph = React.createClass({
           { /* TODO: probably don't need this hidden stuff anymore */ }
           <input id="chat-state" type="text" hidden="hidden"/>
 
-          <div id="plus_button"/>
-          <div id="plus_drawer">
+          <div id="plus_button" onClick={this.toggleDrawer}/>
+          <div id="plus_drawer" onClick={this.toggleDrawer}>
             <div className="close">x</div>
             <div>
               <textarea className="title" placeholder="Node title" rows="1" cols="50"/>
@@ -787,27 +1016,24 @@ let Graph = React.createClass({
             <div>
               <textarea className="description" placeholder="Node description" rows="5" cols="50"/>
             </div>
-            <div className="button direct">Connect Now</div>
-            <div className="button inbox">Put Into Inbox</div>
+            <div className="button direct" onClick={this.directConnect}>Connect Now</div>
+            <div className="button inbox" onClick={this.toInbox}>Put Into Inbox</div>
           </div>
-          <div id="inbox" draggable="true">
+          <div id="inbox" draggable="true" onClick={this.inboxEnlarge}>
             <div className="counter">
               <div className="number">1</div>
             </div>
           </div>
-          <div id="inbox_large">
+          <div id="inbox_large" onClick={this.inboxReduce}>
             <div className="close">x</div>
           </div>
           <div id="graph">
             <div id="chart"/>
           </div>
         </div>
-        {
-        /* <div id="chat_container">
+        <div id="chat_container">
           <Chat/>
         </div>
-        */
-        }
       </div>
     )
   }
