0b9f08e3fa0680defaa1478122f46b8f1847b82d
Eelco Wiersma <eelco@pagebakers.nl>
2015-10-08 17:53:45 +0200

    Removed unused handler

--- src/js/components/nav.jsx
+++ src/js/components/nav.jsx
@@ -6,7 +6,7 @@ let AppNav = React.createClass({
   render() {
     return (
       <div className="mdl-layout__tab-bar mdl-js-ripple-effect is-casting-shadow">
-        <Link to="/graph" activeClassName="is-active" className="mdl-layout__tab" onClick={this.showComponent}><i className="material-icons">share</i></Link>
+        <Link to="/graph" activeClassName="is-active" className="mdl-layout__tab"><i className="material-icons">share</i></Link>
         <Link to="/chat" activeClassName="is-active" className="mdl-layout__tab"><i className="material-icons">chat</i></Link>
         <Link to="/contacts" activeClassName="is-active" className="mdl-layout__tab"><i className="material-icons">contacts</i></Link>
         <Link to="/projects" activeClassName="is-active" className="mdl-layout__tab"><i className="material-icons">folder</i></Link>
