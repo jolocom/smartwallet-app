12f9defacd841236c4a434b4d3c8222f27d3a457
Eelco Wiersma <eelco@pagebakers.nl>
2015-12-28 18:42:06 +0100

    hook required by lifecycle  mixin

--- src/js/components/accounts/signup-dev.jsx
+++ src/js/components/accounts/signup-dev.jsx
@@ -21,7 +21,10 @@ let SignupDev = React.createClass({
     if (this.state.signedUp)
       this.history.pushState(null, '/graph')
   },
-
+  routerWillLeave() {
+    // if (!this.state.signedUp)
+    //   return false
+  },
   signup() {
     let signupData = {
       username: this.state.username,
