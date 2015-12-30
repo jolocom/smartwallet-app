0e8c7748ac155a8f8c3d68e9503ecd6c731c2941
Justas Azna <justas@reederz.com>
2015-12-30 18:23:54 +0100

    [#38] Enhance the validator to check whether server names and people ids are unique

--- utils/soliddata/soliddata/validators.py
+++ utils/soliddata/soliddata/validators.py
@@ -1,13 +1,17 @@
 from jsonschema import validate
 
 
+class NotUniqueError(Exception):
+    pass
+
+
 def validate_blueprint(blueprint):
     '''Validate test data blueprint
 
     If the blueprintt is valid, nothing happens. Otherwise the
     method raises ValidationError
 
-    :raises: ValidationError
+    :raises: ValidationError, NotUniqueError
     '''
 
     person_schema = {
@@ -41,4 +45,17 @@ def validate_blueprint(blueprint):
             }
         }
     }
+    # Check agains json schema
     validate(blueprint, schema)
+
+    # Check if all the server names are unique
+    server_names = map(lambda s: s['name'], blueprint['servers'])
+    if len(server_names) != len(set(server_names)):
+        raise NotUniqueError('server names are not unique')
+
+    # Check if all the people ids are unique
+    people_ids = reduce(lambda acc_ids, server: acc_ids + map(lambda p: p['id'],
+                                                              server['people']),
+                        blueprint['servers'], [])
+    if len(people_ids) != len(set(people_ids)):
+        raise NotUniqueError('people ids are not unique')
