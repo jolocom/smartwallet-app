2ad4d52c76c545a7119f6a313f467deac192a9f9
Eelco Wiersma <eelco@pagebakers.nl>
2016-07-29 15:30:44 +0200

    #156: Animate fab menu item on open/close

--- src/js/components/graph/graph.jsx
+++ src/js/components/graph/graph.jsx
@@ -1,5 +1,6 @@
 // @see http://nicolashery.com/integrating-d3js-visualizations-in-a-react-app/
-// This file renders the whole graph component. Takes care of all the nuances. It is also stateless,
+// This file renders the whole graph component. Takes care of all the nuances.
+// It is also stateless,
 // Figuring out now how to make it maintain some changes through refreshes.
 import Reflux from 'reflux'
 import React from 'react'
@@ -15,7 +16,7 @@ import Node from '../node/node.jsx'
 
 let Graph = React.createClass({
 
-  mixins : [Reflux.listenTo(GraphStore, 'onStateUpdate')],
+  mixins: [Reflux.listenTo(GraphStore, 'onStateUpdate')],
 
   propTypes: {
     children: React.PropTypes.node
@@ -44,19 +45,16 @@ let Graph = React.createClass({
 
   onStateUpdate: function(data, signal) {
     // Temp. make it more elegant later.
-    if (signal === 'nodeRemove')
-    {
+    if (signal === 'nodeRemove') {
       this.graph.deleteNodeAndRender(data)
       this.setState({activeNode: null})
       // Important to avoid a re-render here.
       graphActions.setState('activeNode', null, false)
-    }
-    else if (signal == 'preview'){
-    }
-    else
-    {
-      if (data) this.setState(data)
-      if (data && data.neighbours){
+    } else if (signal !== 'preview') {
+      if (data) {
+        this.setState(data)
+      }
+      if (data && data.neighbours) {
         this.graph.render(this.state)
         this.graph.updateHistory(this.state.navHistory)
       }
@@ -64,11 +62,12 @@ let Graph = React.createClass({
 
     if (this.state.newNode) {
       // this.graph.addNode(this.state.newNode)
-      // We update the state of the store to be in line with the state of the child
+      // We update the state of the store to be
+      // in line with the state of the child
       this.state.newNode = null
       graphActions.setState('newNode', null, false)
     }
-    if ( signal == 'erase') {
+    if (signal === 'erase') {
       this.graph.eraseGraph()
     }
   },
@@ -89,22 +88,22 @@ let Graph = React.createClass({
     graphActions.getState()
   },
 
-  componentWillUnmount: function(){
+  componentWillUnmount: function() {
     if (this.graph) {
       this.graph.eraseGraph()
       this.graph.removeAllListeners()
     }
   },
 
-  _handleSelectNode(node, svg){
+  _handleSelectNode(node, svg) {
     graphActions.setState('selected', svg)
   },
 
-  _handleChangeRotationIndex(rotationIndex){
-    graphActions.changeRotationIndex(rotationIndex,false)
+  _handleChangeRotationIndex(rotationIndex) {
+    graphActions.changeRotationIndex(rotationIndex, false)
   },
 
-  _handleCenterChange(node){
+  _handleCenterChange(node) {
     graphActions.navigateToNode(node)
   },
 
@@ -130,24 +129,34 @@ let Graph = React.createClass({
   render: function() {
     let styles = this.getStyles()
 
-    if (this.graph)
-    {
+    if (this.graph) {
       this.graph.setRotationIndex(this.state.rotationIndex)
     }
 
     let nodeDetails
 
     if (this.state.activeNode) {
-      nodeDetails = <Node node={this.state.activeNode} center={this.state.center} svg={this.state.selected} state={this.state}/>
+      nodeDetails = (
+        <Node
+          node={this.state.activeNode}
+          center={this.state.center}
+          svg={this.state.selected}
+          state={this.state}
+        />
+      )
     }
 
     let fab
 
     if (!this.context.searchActive) {
       fab = (
-        <FabMenu style={styles.menu}>
-          <FabMenuItem icon="radio_button_unchecked" label="Node" onClick={() => {this.addNode('node')}}/>
-          <FabMenuItem icon="insert_link" label="Link" onClick={() => {this.addNode('link')}}/>
+        <FabMenu style={styles.menu} icon="add" closeIcon="close">
+          <FabMenuItem
+            icon="radio_button_unchecked"
+            label="Node" onTouchTap={this._handleAddNodeTouchTap} />
+          <FabMenuItem
+            icon="insert_link"
+            label="Link" onClick={this._handleLinkNodeTouchTap} />
         </FabMenu>
       )
     }
@@ -167,6 +176,15 @@ let Graph = React.createClass({
 
   _handleViewNode(node) {
     graphActions.viewNode(node)
+  },
+
+  _handleAddNodeTouchTap() {
+    this.addNode('node')
+  },
+
+  _handleLinkNodeTouchTap() {
+    this.addNode('link')
   }
 })
+
 export default Radium(Graph)
