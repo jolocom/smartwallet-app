0cf239af7f93432f1f2923893e5cfe2927051573
Axel Chalon <axel.chalon@hetic.net>
2016-08-18 12:30:38 +0200

    Renaming files

--- src/js/lib/node-types/types/profile.js
+++ src/js/lib/node-types/types/profile.js
@@ -1,5 +1,5 @@
 import {FOAF} from 'lib/namespaces'
-import Profile from 'components/node/types/profile'
+import Profile from 'components/node/fullscreen/profile'
 
 export default {
   // predicate: FOAF,
