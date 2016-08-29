c5725c63dc750b1a018536baabde8f2b434483e5
Axel Chalon <axel.chalon@hetic.net>
2016-08-29 18:09:10 +0200

    WIP contact list from knows pred

--- src/js/stores/contacts.js
+++ src/js/stores/contacts.js
@@ -1,8 +1,11 @@
 import Reflux from 'reflux'
 import _ from 'lodash'
 import ContactsActions from 'actions/contacts'
+import AccountStore from 'stores/account'
+import {PRED} from 'lib/namespaces'
+import GraphAgent from 'lib/agents/graph'
 
-import {contacts} from 'lib/fixtures'
+// import {contacts} from 'lib/fixtures'
 
 export default Reflux.createStore({
   listenables: ContactsActions,
@@ -11,7 +14,54 @@ export default Reflux.createStore({
   },
   onLoad(query) {
     if (!query || query === '') {
-      this.trigger(contacts)
+    
+      
+      if (!this.gAgent) {
+        this.gAgent = new GraphAgent()
+      }
+      
+      // @TOD PRED.knows, too
+     this.gAgent.findObjectsByTerm(AccountStore.state.webId, PRED.knows).then((res) => {
+       console.log('res', res);
+
+       return Promise.all(res.map((node) => {
+         console.log('node.uri',node.uri)
+         return this.gAgent.fetchTriplesAtUri(node.uri)
+       }))
+     }).then((nodes) => {
+       console.log('nodes',nodes)
+       
+       console.table(contacts)
+       
+       let contacts = nodes.filter((node) =>
+        node.triples.some((triple) => {
+         if (triple.predicate.uri == PRED.type.uri)
+          return triple.predicate.uri == PRED.type.uri && triple.object.uri == PRED.Person.uri
+        }
+       ))
+       
+       console.log('contacts',contacts)
+       
+       let formattedContacts = contacts.map((contact) => {
+         
+         let nameTriples = contact.triples.filter((triple) => triple.predicate.uri == PRED.givenName.uri)
+         let usernameTriples = contact.triples.filter((triple) => false)
+         let emailTriples = contact.triples.filter((triple) => triple.predicate.uri == PRED.email.uri)
+         
+         console.log('this contact',contact)
+         
+         return {
+           name: nameTriples && nameTriples[0].object.value || '',
+           username: usernameTriples && usernameTriples[0] || '',
+           webId: contact.uri || '',
+           email: emailTriples && emailTriples[0].object.value || ''
+         }
+       });
+       
+       console.log('formatted contacts',formattedContacts)
+       this.trigger(formattedContacts)
+     })
+     
       return
     }
 
@@ -20,7 +70,7 @@ export default Reflux.createStore({
     let results = _.filter(contacts, (contact) => {
       return contact.name.match(regEx)
     })
-
+    
     this.trigger(results)
   }
 })
