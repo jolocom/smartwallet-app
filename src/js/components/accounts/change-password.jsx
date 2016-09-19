e30dc36f41583d479c898ca401bb724c202eacb8
Dean Cooksey <dean.g.cooksey@gmail.com>
2016-09-19 16:04:01 +0200

    Added the forms: for requesting password reset and saving the new password. Alterations also to the router so that these pages can be located

--- src/js/components/accounts/change-password.jsx
+++ src/js/components/accounts/change-password.jsx
@@ -10,11 +10,124 @@ import {Link} from 'react-router'
 import Account from 'actions/account'
 import AccountStore from 'stores/account'
 
-let ForgotPassword = React.createClass({
+let ChangePassword = React.createClass({
+
+  contextTypes: {
+    muiTheme: React.PropTypes.object,
+    router: React.PropTypes.object
+  },
+
+  errorMessage: {
+    email: 'Please provide a valid email'
+  },
+
+  forgotPassword(){
+    alert('you sir, have forgot your password.')
+  },
+
+  enableSubmit() {
+    this.setState({disabledSubmit: false})
+  },
+
+  disableSubmit() {
+    this.setState({disabledSubmit: true})
+  },
+
+
+  getStyles() {
+    let {muiTheme} = this.context
+    let styles = {
+      container: {
+        textAlign: 'center',
+        background: '#f1f1f1',
+        height: '100%',
+        overflowY: 'auto'
+      },
+      header: {
+        padding: '40px'
+      },
+      logo: {
+        fontSize: '18px',
+        fontWeight: '400',
+        textAlign: 'center',
+        marginTop: '24px',
+        textTransform: 'uppercase'
+      },
+      logoImg: {
+        width: '32px',
+        height: '32px',
+        verticalAlign: 'middle'
+      },
+      title: {
+        fontWeight: 'normal',
+        fontSize: '20px',
+        color: '#4B142B',
+        textAlign: 'left',
+        width: '300px',
+        maxWidth: '90%',
+        padding: '20px',
+        margin: '0 auto 20px auto',
+        boxSizing: 'border-box'
+      },
+      backButton: {
+        textAlign: 'left'
+      },
+      content: {
+        width: '300px',
+        maxWidth: '90%',
+        padding: '20px',
+        margin: '0 auto 20px auto',
+        boxSizing: 'border-box'
+      },
+      button: {
+        width: '100%'
+      },
+      help: {
+        color: muiTheme.jolocom.gray1
+      },
+      link: {
+        color: muiTheme.palette.accent1Color,
+        fontWeight: 'bold'
+      }
+    }
+
+    return styles
+  },
 
   render() {
+
+    let styles = this.getStyles()
+
     return (
-      <div><h1>What the hell?</h1></div>
+      <div style={styles.container}>
+        <div>
+          <div style={styles.title}>Set new password</div>
+         </div>
+        <div style={styles.content}>
+          <Formsy.Form
+            onValid={this.enableSubmit}
+            onInvalid={this.disableSubmit}
+            onValidSubmit={this.forgotPassword}
+          >
+            <div style={{marginBottom: '20px'}}>
+              <FormsyText name="password"
+                          type="password"
+                          floatingLabelText="New password"
+              />
+              <FormsyText name="password"
+                          type="password"
+                          floatingLabelText="Repeat password"
+                          onChange={this._onPasswordChange}
+              />
+            </div>
+
+            <RaisedButton type="submit" secondary={true}
+                          disabled={this.state.disabledSubmit}
+                          style={styles.button}
+                          label="LOG IN"/>
+          </Formsy.Form>
+        </div>
+      </div>
     )
   }
 })
--- src/js/components/accounts/forgot-password.jsx
+++ src/js/components/accounts/forgot-password.jsx
@@ -12,9 +12,117 @@ import AccountStore from 'stores/account'
 
 let ForgotPassword = React.createClass({
 
+  contextTypes: {
+    muiTheme: React.PropTypes.object,
+    router: React.PropTypes.object
+  },
+
+  errorMessage: {
+    email: 'Please provide a valid email'
+  },
+
+  forgotPassword(){
+    alert('you sir, have forgot your password.')
+  },
+
+  enableSubmit() {
+    this.setState({disabledSubmit: false})
+  },
+
+  disableSubmit() {
+    this.setState({disabledSubmit: true})
+  },
+
+
+  getStyles() {
+    let {muiTheme} = this.context
+    let styles = {
+      container: {
+        textAlign: 'center',
+        background: '#f1f1f1',
+        height: '100%',
+        overflowY: 'auto'
+      },
+      header: {
+        padding: '40px'
+      },
+      logo: {
+        fontSize: '18px',
+        fontWeight: '400',
+        textAlign: 'center',
+        marginTop: '24px',
+        textTransform: 'uppercase'
+      },
+      logoImg: {
+        width: '32px',
+        height: '32px',
+        verticalAlign: 'middle'
+      },
+      title: {
+        fontWeight: 'normal',
+        fontSize: '20px',
+        color: '#4B142B',
+        textAlign: 'left',
+        width: '300px',
+        maxWidth: '90%',
+        padding: '20px',
+        margin: '0 auto 20px auto',
+        boxSizing: 'border-box'
+      },
+      backButton: {
+        textAlign: 'left'
+      },
+      content: {
+        width: '300px',
+        maxWidth: '90%',
+        padding: '20px',
+        margin: '0 auto 20px auto',
+        boxSizing: 'border-box'
+      },
+      button: {
+        width: '100%'
+      },
+      help: {
+        color: muiTheme.jolocom.gray1
+      },
+      link: {
+        color: muiTheme.palette.accent1Color,
+        fontWeight: 'bold'
+      }
+    }
+
+    return styles
+  },
+
   render() {
+
+    let styles = this.getStyles()
+
     return (
-      <div><h1>What the hell?</h1></div>
+    <div style={styles.container}>
+      <div style={styles.title}>Forgot password</div>
+      <div style={styles.content}>
+        <Formsy.Form
+          onValid={this.enableSubmit}
+          onInvalid={this.disableSubmit}
+          onValidSubmit={this.forgotPassword}
+        >
+          <div style={{marginBottom: '20px'}}>
+            <FormsyText name="email"
+                        floatingLabelText="Email"
+                        validations="isEmail"
+                        validationError={this.errorMessage.email}
+                        onChange={this._onEmailChange}
+            />
+          </div>
+
+          <RaisedButton type="submit" secondary={true}
+                        disabled={this.state.disabledSubmit}
+                        style={styles.button}
+                        label="REQUEST PASSWORD"/>
+        </Formsy.Form>
+      </div>
+    </div>
     )
   }
 })
