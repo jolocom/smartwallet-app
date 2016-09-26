fdf416e2728dd70f79e004e228dccd5e16cc092f
Axel Chalon <axel.chalon@hetic.net>
2016-09-26 15:15:28 +0200

    Fix for manually changing full-screen view URL

--- src/js/components/node/node.jsx
+++ src/js/components/node/node.jsx
@@ -6,32 +6,36 @@ import graphActions from 'actions/graph-actions'
 import Debug from 'lib/debug'
 let debug = Debug('components:node')
 
-let Node = (props) => {
-  let {node} = props
+let Node = React.createClass({
 
-  if (!node) {
-    debug('Fetching more information on node', props.routeParams.node)
-    graphActions.viewNode(props.routeParams.node)
-    return <div></div>
+  propTypes: {
+    node: PropTypes.object,
+    center: PropTypes.object,
+    navHistory: PropTypes.array,
+    routeParams: PropTypes.object
+  },
+  
+  render: function() {
+    let {node} = this.props
+
+    if (!node || this.props.routeParams.node !== node.uri) {
+      debug('Fetching more information on node', this.props.routeParams.node)
+      graphActions.viewNode(this.props.routeParams.node) // hydrates activeNode
+      return <div></div>
+    }
+
+    const NodeFullScreenComponent = NodeTypes.componentFor(node.type)
+
+    debug('Rendering fullscreen component %s for node type %s',
+      NodeFullScreenComponent.displayName,
+      node.type
+    )
+
+    return (
+      <NodeFullScreenComponent {...this.props} />
+    )
   }
-
-  const NodeFullScreenComponent = NodeTypes.componentFor(node.type)
-
-  debug('Rendering fullscreen component %s for node type %s',
-    NodeFullScreenComponent.displayName,
-    node.type
-  )
-
-  return (
-    <NodeFullScreenComponent {...props} />
-  )
-}
-
-Node.propTypes = {
-  node: PropTypes.object,
-  center: PropTypes.object,
-  navHistory: PropTypes.array,
-  routeParams: PropTypes.object
-}
+  
+})
 
 export default Node
