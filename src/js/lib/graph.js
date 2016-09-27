e470bf0497082f31384a0c214c5e73f444d4015a
deskagebunshin <deskagebunshin@gmail.com>
2016-09-27 11:02:57 -0700

    Moved graph up, clustered history nodes and fixed color of hist nodes

--- src/js/lib/graph.js
+++ src/js/lib/graph.js
@@ -170,7 +170,7 @@ export default class GraphD3 extends EventEmitter {
     this.width = this.graphContainer.offsetWidth || STYLES.width
     this.height = this.graphContainer.offsetHeight || STYLES.height
     this.centerCoordinates = {
-      y: this.height / 2,
+      y: this.height * 3 / 7,
       x: this.width / 2
     }
   }
@@ -204,8 +204,8 @@ export default class GraphD3 extends EventEmitter {
     // Center cicle
     this.svg.select('g.background-layer').append('svg:circle')
       .attr('class', 'center-circle')
-      .attr('cx', this.width * 0.5)
-      .attr('cy', this.height * 0.5)
+      .attr('cx', this.centerCoordinates.x)
+      .attr('cy', this.centerCoordinates.y)
       .attr('r', this.largeNodeSize * 0.57 * 1.1)
       .style('fill', STYLES.lightGrayColor)
 
@@ -250,7 +250,6 @@ export default class GraphD3 extends EventEmitter {
        .outerRadius(this.largeNodeSize * 0)
        .startAngle(0)
 
-
     this.arcEnlarged = d3.arc()
       // enlarged size
       .innerRadius(this.largeNodeSize * 0.57)
@@ -258,7 +257,7 @@ export default class GraphD3 extends EventEmitter {
       .startAngle(0)
 
     let maxRotationIndex = this.numberOfNeighbours - this.MAX_VISIBLE_NODES
-    let trans = this.width * 0.5 + ',' + this.height * 0.5
+    let trans = this.centerCoordinates.x + ',' + this.centerCoordinates.y
     let rot = this.archAngle * (maxRotationIndex - this.rotationIndex)
     // remove "maxRotationIndex -" to reverse the direction
     this.svg.select('.dial')
@@ -508,7 +507,7 @@ export default class GraphD3 extends EventEmitter {
     // Add class hasNodeIcon
     // Adds a Confidential Icon for confidential nodes
     nodeEnter.filter((d) => d.confidential)
-      .classed('hasNodeIcon',true)
+      .classed('hasNodeIcon', true)
       .append('image')
         .attr('class', 'nodeIcon')
         .attr('xlink:href', (d) => {
@@ -844,7 +843,11 @@ export default class GraphD3 extends EventEmitter {
     this.emit('view-node', data, node)
     d3.event.stopPropagation()
   }
+
   resetPos = function () {
+    let histSeparation = (this.smallNodeSize / 2)
+    let historyDist = this.centerCoordinates.y + this.largeNodeSize * 2.1
+
     d3.selectAll('.node')
       .transition('pos').duration(STYLES.nodeTransitionDuration / 5)
       .attr('transform', (d) => {
@@ -854,12 +857,10 @@ export default class GraphD3 extends EventEmitter {
           x = this.nodePositions[d.position].x
           y = this.nodePositions[d.position].y
         } else if (d.rank === 'history') {
-          y += this.largeNodeSize * 2.1 + d.histLevel * this.smallNodeSize
+          y += this.largeNodeSize * 2.1 + d.histLevel * histSeparation
         }
         return 'translate(' + x + ',' + y + ')'
       })
-    //
-    let historyDist = this.centerCoordinates.y + this.largeNodeSize * 2.1
 
     d3.selectAll('.link')
       .transition().duration(STYLES.nodeTransitionDuration / 5)
@@ -868,7 +869,7 @@ export default class GraphD3 extends EventEmitter {
         if (d.target.rank === 'center' || d.source.histLevel === 0) {
           return this.centerCoordinates.y
         } else if (d.target.rank === 'history') {
-          return historyDist + (d.source.histLevel - 1) * this.smallNodeSize
+          return historyDist + (d.source.histLevel - 1) * histSeparation
         }
       })
       .attr('x2', (d) => {
@@ -882,7 +883,7 @@ export default class GraphD3 extends EventEmitter {
         if (d.source.rank === 'neighbour') {
           return this.nodePositions[d.source.position].y
         } else {
-          return historyDist + d.source.histLevel * this.smallNodeSize
+          return historyDist + d.source.histLevel * histSeparation
         }
       })
   }
@@ -947,7 +948,8 @@ export default class GraphD3 extends EventEmitter {
         }
       })
     .attr('opacity', (d) => d.elipsisdepth === 0 ||
-                            d.elipsisdepth === 1 ? 0 : 1)
+                            d.elipsisdepth === 1 ||
+                            d.rank === 'history' ? 0 : 1)
 
     // Reset colour of all circles
     // Tries to interpret the url(#) as a colour @TODO
@@ -955,7 +957,7 @@ export default class GraphD3 extends EventEmitter {
       .select('.nodecircle')
       .style('fill', (d) => {
         if (d.rank === 'history') {
-          return STYLES.grayColor
+          return theme.graph.historyNodeColor
         } else if (d.type === 'passport') {
           return theme.graph.textNodeColor
         } else if (d.elipsisdepth === 0 ||
@@ -1015,11 +1017,11 @@ export default class GraphD3 extends EventEmitter {
       })
       .transition('reset').duration(STYLES.nodeTransitionDuration)
       .attr('y', (d) => {
-          if (d.rank === 'center') {
-            return -55
-          } else {
-            return -50
-          }
+        if (d.rank === 'center') {
+          return -55
+        } else {
+          return -50
+        }
       })
       .style('filter', null)
 
@@ -1039,7 +1041,7 @@ export default class GraphD3 extends EventEmitter {
       .transition('reset').duration(speed)
       .attr('dy', function (d) {
         return d3.select(this.parentNode).classed('hasNodeIcon')
-          ? (d.rank == 'center' ? '0.95em' : '.75em')
+          ? (d.rank === 'center' ? '0.95em' : '.75em')
           : '.35em' })
       .attr('opacity', (d) => {
         return (((d.img && d.type !== 'passport') || d.elipsisdepth >= 0) &&
@@ -1047,7 +1049,6 @@ export default class GraphD3 extends EventEmitter {
                 ? 0
                 : 1
       })
-    
     // Hide the descriptions of all nodes
     d3.selectAll('svg .node')
       .selectAll('.nodedescription')
@@ -1156,23 +1157,25 @@ export default class GraphD3 extends EventEmitter {
         .transition('highlight').duration(STYLES.nodeTransitionDuration)
         .attr('dy', function (d) {
           if (d.description && d.type !== 'bitcoin') {
-            if (d3.select(this.parentNode).classed('hasNodeIcon'))
+            if (d3.select(this.parentNode).classed('hasNodeIcon')) {
               return '.4em'
+            }
             return '-.15em'
-          }
-          else {
-            if (d3.select(this.parentNode).classed('hasNodeIcon'))
-              return (d.rank == 'center' ? '0.95em' : '.75em')
+          } else {
+            if (d3.select(this.parentNode).classed('hasNodeIcon')) {
+              return (d.rank === 'center' ? '0.95em' : '.75em')
+            }
             return '.35em'
-          }})
+          }
+        })
         .attr('opacity', 1)
-      
+
       // Move the icon up if description
       d3.select(node)
         .filter((d) => d.description)
         .select('.nodeIcon')
         .transition('highlight').duration(STYLES.nodeTransitionDuration)
-        .attr('y', (d) => d.rank == 'center' ? -70 : -60)
+        .attr('y', (d) => d.rank === 'center' ? -70 : -60)
       data.highlighted = true
     }
   }.bind(this)
@@ -1285,7 +1288,6 @@ export default class GraphD3 extends EventEmitter {
     if (data.rank !== 'center') {
       let x = this.centerCoordinates.x
       let y = this.centerCoordinates.y
-      let largeSize = STYLES.largeNodeSize
 
       d3.select('.dial')
         .transition().duration(STYLES.nodeTransitionDuration)
