5a2a7a2bdbb6436090936d130c5a4d4d77cf134c
Axel Chalon <axel.chalon@hetic.net>
2016-10-05 15:58:11 +0200

    cleaning up

--- src/js/components/chat/conversations.jsx
+++ src/js/components/chat/conversations.jsx
@@ -8,8 +8,6 @@ import {
   List,
   ListItem,
   Avatar
-  // FloatingActionButton,
-  // FontIcon
 } from 'material-ui'
 import {grey500} from 'material-ui/styles/colors'
 
@@ -64,66 +62,30 @@ let Conversations = React.createClass({
     this.context.router.push(`/conversations/${id}`)
   },
 
-  renderItems(items) {
-    return (
-      <List>
-        {items.map((conversation) => {
-          return <ConversationsListItem
-            key={conversation.id}
-            conversation={conversation}
-            onTouchTap={this.showConversation}
-
-          />
-        })}
-      </List>
-    )
-  },
-
   render: function() {
     let content
 
-    let {loading, items} = this.state.conversations
-    items = items.filter(conv => conv.lastMessage !== null)
-    
-    /*items.sort(
-      (itemA, itemB) => {
-        if (!itemA.lastMessage) {
-          if (!itemB.lastMessage) {
-            return 0
-          } else {
-            return -1
-          }
-        } else if (!itemB.lastMessage) {
-          return 1
-        }
-        return itemA.lastMessage.created.getTime() <
-          itemB.lastMessage.created.getTime()
-      }
-    )*/
-
     if (loading) {
       content = <Loading style={styles.loading} />
-    } else if (!items || !items.length) {
-      content = <div style={styles.empty}>No conversations</div>
-    } else {
-      content = this.renderItems(items)
     }
     
-    items = items.map(
-      (item) => Object.assign({},
-                              item,
-                              {name: item.otherPerson.name || 'Unnamed',
-                               imgUri: item.otherPerson.img,
-                               secondaryText: item.lastMessage.content,
-                               rightText: moment(item.lastMessage.created).fromNow(),
-                               onTouchTap: this.showConversation
-                              })
-    )
-    
-    items.sort(
-      (itemA, itemB) =>
-        itemA.lastMessage.created.getTime() < itemB.lastMessage.created.getTime()
-    )
+    let {loading, items} = this.state.conversations
+    items = items
+      .filter(conv => conv.lastMessage !== null)
+      .map(
+        (item) => Object.assign({},
+                                item,
+                                {name: item.otherPerson.name || 'Unnamed',
+                                 imgUri: item.otherPerson.img,
+                                 secondaryText: item.lastMessage.content,
+                                 rightText: moment(item.lastMessage.created).fromNow(),
+                                 onTouchTap: this.showConversation
+                                })
+      )
+      .sort(
+        (itemA, itemB) =>
+          itemA.lastMessage.created.getTime() < itemB.lastMessage.created.getTime()
+      )
     
     return (
       <div style={styles.container}>
@@ -136,74 +98,12 @@ let Conversations = React.createClass({
             noReordering
             emptyMessage={'No conversations'}
           />
-          {/*{content}*/}
+          {content}
         </div>
-
-        {/* <FloatingActionButton */}
-          {/* secondary */}
-          {/* href="#/chat/new" */}
-          {/* linkButton={true} */}
-          {/* style={styles.actionButton} */}
-        {/* > */}
-          {/* <FontIcon className="material-icons">add</FontIcon> */}
-        {/* </FloatingActionButton> */}
 
         {this.props.children}
       </div>
     )
-  }
-})
-
-let ConversationsListItem = React.createClass({
-
-  propTypes: {
-    conversation: React.PropTypes.object.isRequired,
-    onTouchTap: React.PropTypes.func.isRequired
-  },
-
-  render() {
-    let {conversation} = this.props
-    let {otherPerson, lastMessage} = conversation
-    let {created, content} = lastMessage || {}
-
-    // If otherPerson var is null, then set it to false.
-    // So it wont be used when listing conversations
-    // to avoid errors
-
-    if (otherPerson == null) {
-      otherPerson = false
-    }
-    let nameInitial = Utils.nameInitial(otherPerson)
-    let avatar
-    if (otherPerson.img) {
-      avatar = <Avatar src={Utils.uriToProxied(otherPerson.img)}
-        style={{backgroundSize: 'cover'}} />
-    } else {
-      avatar = <Avatar>{nameInitial}</Avatar>
-    }
-    let date = moment(created).fromNow()
-    
-    
-    
-    
-    /*return (
-      <ListItem
-        key={conversation.id}
-        primaryText={
-          <div>
-            <span>{otherPerson.name || 'Unnamed'}</span>
-            <span style={styles.date}>{date}</span>
-          </div>
-        }
-        secondaryText={content}
-        leftAvatar={avatar}
-        onTouchTap={this._handleListItemTouchTap}
-      />
-    )*/
-  },
-
-  _handleListItemTouchTap() {
-    this.props.onTouchTap(this.props.conversation)
   }
 })
 
