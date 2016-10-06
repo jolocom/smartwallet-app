e46dd3ad256e6581880089a5a2454393d6c9370c
Eugeniu <rusu.eugeniu@gmx.de>
2016-10-06 14:11:11 +0200

    Squash merged develop.
    Squashed commit of the following:
    
    commit 91fcfa3ebe58b91dc4fe1aca80dbf62854c8a99a
    Author: isbl16 <isbl16@gmail.com>
    Date:   Thu Oct 6 10:32:01 2016 +0200
    
        Feature/#258 chat avatars distorted (#328)
    
        * working using separate div on conversations tab
    
        * working on individual conversations
    
        * working on contact list
    
        * now working for avatar in sidemenu
    
        * user avatar new component - wip
    
        * working with abstract UserAvatar component throughout app
    
        * fix avatar size in nav
    
        * commenting UserAvatar component
    
        * linting, nav avatar styling
    
        * changes requested - refactoring
    
        * check for unnamed
    
        * Fix error where contact list doesn't load when Unnamed person there
    
        * Alteration for the request.

---
+++ src/js/components/common/user-avatar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Radium from 'radium'
+
+import {
+  Avatar
+} from 'material-ui'
+
+import UserIcon from 'material-ui/svg-icons/action/face'
+
+import Utils from 'lib/util'
+
+let UserAvatar = React.createClass({
+
+  propTypes: {
+    imgUrl: React.PropTypes.any, // TODO change type to something more precise
+    name: React.PropTypes.string
+  },
+
+  render() {
+    let imgUrl = this.props.imgUrl
+    let name = this.props.name
+
+    let avatar
+
+    if (imgUrl) {
+      // User has display image
+      avatar = (
+        <Avatar
+          style={{
+            backgroundSize: 'cover',
+            backgroundImage: `url(${Utils.uriToProxied(imgUrl)})`
+          }} />
+      )
+    } else if (name && name.trim() && (name !== 'Unnamed')) {
+      // User has no display image but has a non-empty name
+      let nameInitial = name[0].toUpperCase()
+      avatar = <Avatar>{nameInitial}</Avatar>
+    } else {
+      // User has no image and no specified name
+      avatar = <Avatar icon={<UserIcon />} />
+    }
+    return avatar
+  }
+
+})
+
+export default Radium(UserAvatar)
