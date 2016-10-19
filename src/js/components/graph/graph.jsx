jolocom/smartwallet-app

commit e360e87bc3ace15c21e223e40abdf41ce742e3ba

#344: Show loading indicator when app is opening (#365)
* SHow loading indicator when app is opening

* ESlint fixes for Travis

* Added spinner to graph

* Only show spinner in graph on initial load

* Renamed loaded to initialized and updating on failure

src/js/components/graph/graph.jsx
@@ -11,8 +11,8 @@ import FabMenuItem from 'components/common/fab-menu-item.jsx'
 import GraphStore from 'stores/graph-store'
 import graphActions from 'actions/graph-actions'
 import IndicatorOverlay from 'components/graph/indicator-overlay.jsx'
+import Loading from 'components/common/loading.jsx'
 import Radium from 'radium'
-import NodeTypes from 'lib/node-types'
 
 import Debug from 'lib/debug'
 let debug = Debug('components:graph')
@@ -22,7 +22,9 @@ let Graph = React.createClass({
   mixins: [Reflux.listenTo(GraphStore, 'onStateUpdate')],
 
   propTypes: {
-    children: React.PropTypes.node
+    children: React.PropTypes.node,
+    params: React.PropTypes.object,
+    routes: React.PropTypes.object
   },
 
   contextTypes: {
@@ -96,53 +98,48 @@ let Graph = React.createClass({
     this.graph.on('start-scrolling', this.refs.scrollIndicator._handleClick)
 
     if (this.props.params.node) {
-      if (this.props.params.node == account.webId) {
+      if (this.props.params.node === account.webId) {
         debug('Home node (componentDidMount): redirecting to /graph')
-        this.context.router.push(`/graph/`)
-      }
-      else {
+        this.context.router.push('/graph/')
+      } else {
         debug('Navigating to node (componentDidMount)', this.props.params.node)
         graphActions.navigateToNode({uri: this.props.params.node},
                                     {uri: this.context.account.webId,
                                      name: this.context.account.username})
       }
-    }
-    else if (account.webId) {
+    } else if (account.webId) {
       debug('Navigating to default node', account.webId)
       // Load graph when user is logged in
       graphActions.getInitialGraphState(account.webId)
     }
-    
   },
-  
+
   // @TODO combine with componentWillUpdate ?
-  componentDidUpdate(prevProps) {    
+  componentDidUpdate(prevProps) {
     // We do not want to center the graph on the person we're viewing the
     // full-screen profile of.
-    
-    let fullscreenView = this.props.routes.length == 3 // /graph/[uri]/view
-    let viewChanged = prevProps.routes.length != this.props.routes.length
+
+    let fullscreenView = this.props.routes.length === 3 // /graph/[uri]/view
+    let viewChanged = prevProps.routes.length !== this.props.routes.length
     let nodeChanged = prevProps.params.node !== this.props.params.node // .uri?
-      
+
     // In case we disconnected from the node in full-screen view, we want to
     // navigate to the center node again (refresh) if viewChanged, even though
     // the center node will inevitably be the same (nodeChanged == false)
     if (!fullscreenView && (nodeChanged || viewChanged)) {
       if (this.props.params.node &&
-          this.props.params.node == this.context.account.webId) {
+          this.props.params.node === this.context.account.webId) {
         debug('Home node (componentDidUpdate): redirecting to /graph')
-        this.context.router.push(`/graph/`)
-      }
-      else {
+        this.context.router.push('/graph/')
+      } else {
         let nodeUri = this.props.params.node || this.context.account.webId
         debug('Navigating to node (componentDidUpdate)', nodeUri)
-        
+
         if (this.props.params.node) {
           graphActions.navigateToNode({uri: nodeUri},
                                       {uri: this.context.account.webId,
                                        name: this.context.account.username})
-        }
-        else {
+        } else {
           graphActions.navigateToNode({uri: nodeUri})
         }
       }
@@ -163,8 +160,7 @@ let Graph = React.createClass({
         newState.activeNode !== this.state.activeNode) {
       uri = encodeURIComponent(newState.activeNode.uri)
       this.context.router.push(`/graph/${uri}/view`)
-    }
-    else if (newContext.account.webId &&
+    } else if (newContext.account.webId &&
              newContext.account.webId !== this.context.account.webId) {
       graphActions.getInitialGraphState(newContext.account.webId)
     }
@@ -203,6 +199,10 @@ let Graph = React.createClass({
         position: 'absolute',
         bottom: '16px',
         right: '16px'
+      },
+      loading: {
+        backgroundColor: 'transparent',
+        position: 'absolute'
       }
     }
     return styles
@@ -239,10 +239,17 @@ let Graph = React.createClass({
       })
     })
 
+    let loading
+    if (!this.state.initialized) {
+      loading = <Loading style={styles.loading} />
+    }
+
     return (
       <div style={styles.container}>
         <IndicatorOverlay ref="scrollIndicator" />
         <div style={styles.chart} ref="graph"></div>
+
+        {loading}
 
         {fab}
 

src/js/stores/graph-store.js
@@ -1,9 +1,9 @@
 import Reflux from 'reflux'
-import graphAgent from '../lib/agents/graph.js'
+import GraphAgent from '../lib/agents/graph.js'
 import graphActions from '../actions/graph-actions'
 import accountActions from '../actions/account'
 import Utils from 'lib/util'
-import d3Convertor from '../lib/d3-converter'
+import D3Convertor from '../lib/d3-converter'
 
 import Debug from 'lib/debug'
 let debug = Debug('stores:graph')
@@ -11,27 +11,26 @@ let debug = Debug('stores:graph')
 export default Reflux.createStore({
   listenables: [graphActions],
 
-  init: function(){
+  init: function() {
     this.listenTo(accountActions.logout, this.onLogout)
     this.listenTo(accountActions.login.completed, this.onLogin)
-    this.gAgent = new graphAgent()
-    this.convertor = new d3Convertor()
-    this.loaded = false
-
+    this.gAgent = new GraphAgent()
+    this.convertor = new D3Convertor()
 
     this.state = {
-      //These state keys describe the graph
+      // These state keys describe the graph
       webId: null,
       user: null,
       center: null,
       neighbours: null,
-      loaded: false,
+      loading: false,
+      initialized: false,
       newNode: null,
       navHistory: [],
       selected: null,
       rotationIndex: 0,
       previousRenderedNodeUri: null,
-      //These describe the ui
+      // These describe the ui
       showPinned: false,
       showSearch: false,
       plusDrawerOpen: false,
@@ -40,14 +39,15 @@ export default Reflux.createStore({
   },
 
   onLogout() {
-    this.loaded = false
+    this.initialized = false
     this.state = {
       // Graph related
       webId: null,
       user: null,
       center: null,
       neighbours: null,
-      loaded: false,
+      loading: false,
+      initialized: false,
       newNode: null,
       navHistory: [],
       selected: null,
@@ -95,21 +95,20 @@ export default Reflux.createStore({
   },
   */
 
-  dissconnectNode: function(){
+  dissconnectNode: function() {
     // Animation / removing from the neighb array will go here.
   },
 
-
-
-  // This sends Graph.jsx and the Graph.js files a signal to add new ndoes to the graph
-  onDrawNewNode: function(object, predicate){
-    // This fetches the triples at the newly added file, it allows us to draw it
-    // the graph accurately
+  // This sends Graph.jsx and the Graph.js files a signal to add
+  // new ndoes to the graph
+  onDrawNewNode: function(object, predicate) {
+    // This fetches the triples at the newly added file,
+    // it allows us to draw it the graph accurately
     this.gAgent.fetchTriplesAtUri(object).then((result) => {
       // Adding the extra value to the object, the URI, it's usefull later.
       result.triples.uri = object
-        // Now we tell d3 to draw a new adjacent node on the graph, with the info from
-        // the triiple file
+        // Now we tell d3 to draw a new adjacent node on the graph,
+        // with the info from the triiple file
       result.triples.connection = predicate
       return result.triples
     }).then(this.gAgent.hydrateNodeConfidentiality).then((triples) => {
@@ -126,11 +125,17 @@ export default Reflux.createStore({
   },
 
   onGetInitialGraphState: function (webId) {
+    this.state.loading = true
+
+    this.trigger(this.state)
+
     this.state.previousRenderedNodeUri = webId
     this.gAgent.getGraphMapAtWebID(webId).then((triples) => {
       triples[0] = this.convertor.convertToD3('c', triples[0])
       for (let i = 1; i < triples.length; i++) {
-        triples[i] = this.convertor.convertToD3('a', triples[i], i, triples.length - 1)
+        triples[i] = this.convertor.convertToD3(
+          'a', triples[i], i, triples.length - 1
+        )
       }
       graphActions.getInitialGraphState.completed(triples)
     }).catch(graphActions.getInitialGraphState.failed)
@@ -139,26 +144,42 @@ export default Reflux.createStore({
   onGetInitialGraphStateCompleted: function (result) {
     this.state.center = result[0]
     this.state.neighbours = result.slice(1, result.length)
-    this.state.loaded = true
+    this.state.initialized = true
+    this.state.loading = false
     this.state.user = result[0]
     this.trigger(this.state)
   },
 
+  onGetInitialGraphStateFailed: function () {
+    this.state.loading = false
+    this.state.initialized = true
+
+    this.trigger(this.state)
+  },
+
   onRefresh: function() {
-    debug('Refreshing the graph with current center',this.state.center.uri)
+    debug('Refreshing the graph with current center', this.state.center.uri)
     this.drawAtUri(this.state.center.uri)
   },
 
   drawAtUri: function (uri, number) {
-    debug('Drawing at URI',uri)
+    debug('Drawing at URI', uri)
     this.state.previousRenderedNodeUri = uri
-    this.trigger(Object.assign({},this.state,{neighbours: []}))
+    this.state.loading = true
+
+    this.trigger(Object.assign({}, this.state, {
+      neighbours: []
+    }))
+
     return this.gAgent.getGraphMapAtUri(uri).then((triples) => {
+      this.state.loading = false
       this.state.neighbours = []
       triples[0] = this.convertor.convertToD3('c', triples[0])
       this.state.center = triples[0]
       for (let i = 1; i < triples.length; i++) {
-        triples[i] = this.convertor.convertToD3('a', triples[i], i, triples.length - 1)
+        triples[i] = this.convertor.convertToD3(
+          'a', triples[i], i, triples.length - 1
+        )
         this.state.neighbours.push(triples[i])
       }
       for (let i = 0; i < number; i++) {
@@ -169,11 +190,15 @@ export default Reflux.createStore({
   },
 
   onNavigateToNode: function (node, defaultHistoryNode) {
-    this.state.rotationIndex = 0
-    debug('navigateToNode',node.uri,
-    'History was',this.state.navHistory.map(e => e.uri))
+    let {navHistory} = this.state
 
-    node = Object.assign({},node) // Just being cautious
+    this.state.loading = true
+
+    this.trigger(this.state)
+
+    this.state.rotationIndex = 0
+
+    node = Object.assign({}, node) // Just being cautious
     let oldCenter = Object.assign({}, this.state.center)
 
     // Update the center node in the state without waiting for the graph map
@@ -182,10 +207,6 @@ export default Reflux.createStore({
     // this.trigger(this.state,false)
 
     this.gAgent.getGraphMapAtUri(node.uri).then((triples) => {
-
-      debug('navigateToNode',node.uri,
-      'graphMap received ; history was',this.state.navHistory.map(e => e.uri))
-
       this.state.neighbours = []
       triples[0] = this.convertor.convertToD3('c', triples[0])
 
@@ -193,88 +214,97 @@ export default Reflux.createStore({
       // to the node history
 
       let historyCandidate
-      if (oldCenter && oldCenter.uri)
+      if (oldCenter && oldCenter.uri) {
         historyCandidate = oldCenter
-      else
+      } else {
         historyCandidate = defaultHistoryNode
+      }
 
       this.state.center = triples[0]
 
       // We check if we're not navigating to the same node (e.g. went to the
       // full-screen view and then back), in which case we don't want to add
       // the node to the history
+      const prevUri = navHistory.length && navHistory[navHistory.length - 1].uri
+
       if (!this.state.previousRenderedNodeUri ||
-          this.state.previousRenderedNodeUri !== node.uri) {
-
-        if (this.state.navHistory.length > 0 &&
-          this.state.center.uri === this.state.navHistory[this.state.navHistory.length - 1].uri) {
-            debug('Popping navHistory (same as new center node uri)',
-                this.state.navHistory[this.state.navHistory.length - 1].uri)
-            this.state.navHistory.pop()
+        this.state.previousRenderedNodeUri !== node.uri) {
+        if (prevUri && this.state.center.uri === prevUri) {
+          navHistory.pop()
+        } else if (!this.state.previousRenderedNodeUri) {
+          navHistory.push(historyCandidate)
+        } else {
+          navHistory.push(historyCandidate)
         }
-        else if (!this.state.previousRenderedNodeUri) {
-          debug('Pushing to history because no previousRenderedNodeUri',
-            historyCandiate.uri)
-          this.state.navHistory.push(historyCandidate)
-        }
-        else {
-          debug('Pushing to history',historyCandidate.uri,
-            'because previousRenderedNodeUri',this.state.previousRenderedNodeUri,
-            'is different from new node uri',node.uri)
-          this.state.navHistory.push(historyCandidate)
-        }
-
       }
 
+      this.state.navHistory = navHistory
       this.state.previousRenderedNodeUri = node.uri
 
       // if (this.state.navHistory.length > 1) {
       //   // Removed the brackets, one liners.
       //   else if (this.state.navHistory.length > 1)
       //     for (var j = 0; j < this.state.navHistory.length - 1; j++)
-      //       if (this.state.center.uri == this.state.navHistory[this.state.navHistory.length - 2 - j].uri)
+      //       if (this.state.center.uri == this.state.navHistory[
+      //          this.state.navHistory.length - 2 - j].uri)
       //         for (var k = 0; k < j + 2; k++)
       //           this.state.navHistory.pop()
       // }
 
-      for (var i = 1; i < triples.length; i++) {
-        triples[i] = this.convertor.convertToD3('a', triples[i], i, triples.length - 1)
+      for (let i = 1; i < triples.length; i++) {
+        triples[i] = this.convertor.convertToD3(
+          'a', triples[i], i, triples.length - 1
+        )
         this.state.neighbours.push(triples[i])
       }
+
+      this.state.loading = false
+
       this.trigger(this.state)
     })
   },
 
   onViewNode(node) {
+    let activeNodeInfoP
+    let activeNodePermissionsP
+    let centerNodePermissionsP
 
     if (!node) {
-      console.log('Ignoring onViewNode because node is null.')
+      debug('Ignoring onViewNode because node is null.')
       return
     }
 
+    this.state.loading = true
+
+    this.trigger(this.state)
+
     this.state.activeNode = node
 
-    if (typeof node == 'string') {
+    if (typeof node === 'string') {
       debug('Fetching information and user permisions about the node...')
-      var activeNodeInfoP = this.gAgent.fetchTriplesAtUri(node).then((result) => {
-        result.triples.uri = node
-        this.state.activeNode = node = this.convertor.convertToD3('a', result.triples)
-      })
+      activeNodeInfoP = this.gAgent.fetchTriplesAtUri(node)
+        .then((result) => {
+          result.triples.uri = node
+          this.state.activeNode = node = this.convertor.convertToD3(
+            'a', result.triples
+          )
+        })
+
       this.state.activeNode = {uri: node}
-    }
-    else {
+    } else {
       debug('Fetching user permissions about the node...')
-      var activeNodeInfoP = Promise.resolve()
+      activeNodeInfoP = Promise.resolve()
     }
 
     // Check if the cookie is still valid
-    let activeNodePermissionsP = fetch(`${Utils.uriToProxied(this.state.activeNode.uri)}`, {
+    const activeNodeUri = `${Utils.uriToProxied(this.state.activeNode.uri)}`
+    activeNodePermissionsP = fetch(activeNodeUri, {
       method: 'PATCH',
       credentials: 'include',
       headers: {
-         'Content-Type':'application/sparql-update'
+        'Content-Type': 'application/sparql-update'
       }
-    }).then((res)=>{
+    }).then((res) => {
       if (!res.ok) {
         throw new Error(res.statusText)
       }
@@ -286,21 +316,20 @@ export default Reflux.createStore({
     if (!this.state.center) {
       this.state.center = {}
       this.state.center.isOwnedByUser = false
-      var updateCenterNode = Promise.resolve()
-    }
-    else {
-      var centerNodePermissionsP =fetch(
+    } else {
+      centerNodePermissionsP = fetch(
         `${Utils.uriToProxied(this.state.center.uri)}`,
         { method: 'PATCH',
           credentials: 'include',
           headers: {
-            'Content-Type':'application/sparql-update'
+            'Content-Type': 'application/sparql-update'
           }
         }
       )
-      .then((res)=>{
-        if (!res.ok)
+      .then((res) => {
+        if (!res.ok) {
           throw new Error(res.statusText)
+        }
         this.state.center.isOwnedByUser = true
       }).catch(() => {
         this.state.center.isOwnedByUser = false
@@ -308,8 +337,12 @@ export default Reflux.createStore({
     }
 
     activeNodeInfoP
-      .then(() => Promise.all([activeNodePermissionsP,centerNodePermissionsP]))
+      .then(() => Promise.all([activeNodePermissionsP, centerNodePermissionsP]))
+      .catch(() => {
+        debug('onViewNode failed')
+      })
       .then(() => {
+        this.state.loading = false
         this.trigger(this.state)
       })
   }

src/js/components/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Reflux from 'reflux'
 import Radium from 'radium'
-import includes from 'lodash/includes'
 import {bankUri} from 'lib/fixtures'
 
 import {Layout, Content} from 'components/layout'
@@ -23,8 +22,7 @@ import LeftNav from 'components/left-nav/nav.jsx'
 import Profile from 'components/accounts/profile.jsx'
 import Tour from 'components/tour.jsx'
 
-import GraphSearch from 'components/graph/search.jsx'
-import GraphFilters from 'components/graph/filters.jsx'
+import Loading from 'components/common/loading.jsx'
 
 import AccountActions from 'actions/account'
 import AccountStore from 'stores/account'
@@ -36,7 +34,6 @@ import ProfileActions from 'actions/profile'
 import ProfileStore from 'stores/profile'
 
 import SnackbarStore from 'stores/snackbar'
-import SnackbarActions from 'actions/snackbar'
 
 // A pathname is considered public if either "/" or if it starts
 // with any of the following publicRoutes
@@ -112,7 +109,7 @@ let App = React.createClass({
   },
 
   isPublicRoute(path = this.props.location.pathname) {
-    return path == '/' ||
+    return path === '/' ||
       publicRoutes.some((publicRoute) => path.indexOf(publicRoute) === 0)
   },
 
@@ -169,8 +166,9 @@ let App = React.createClass({
     this.refs.leftNav.show()
   },
 
-  handleConfirmClose() {
+  _handleConfirmClose() {
     ConfirmActions.close()
+    this.state.confirm.callback()
   },
 
   getStyles() {
@@ -220,15 +218,14 @@ let App = React.createClass({
     // @TODO render login screen when logging in, also makes sures child
     // components don't get rendered before any user data is available
     if (this.state.account.loggingIn && !this.isPublicRoute()) {
-      return <div />
+      return <Loading />
     }
 
-
     // Deactivating search until we get it working
-    /*<IconButton
-      iconClassName="material-icons"
-      iconStyle={styles.icon}
-      onTouchTap={this._handleSearchTap}>search</IconButton>*/
+    /* <IconButton
+       iconClassName="material-icons"
+       iconStyle={styles.icon}
+       onTouchTap={this._handleSearchTap}>search</IconButton> */
     const nav = (
       <div>
         <IconButton
@@ -257,18 +254,15 @@ let App = React.createClass({
     const confirmActions = [
       <FlatButton
         label="Cancel"
-        primary={true}
-        onTouchTap={this.handleConfirmClose}
+        primary
+        onTouchTap={this._handleConfirmClose}
       />,
       <FlatButton
         label={this.state.confirm.primaryActionText}
-        primary={true}
-        onTouchTap={ () => {
-          this.handleConfirmClose()
-          this.state.confirm.callback()
-        } }
-      />,
-    ];
+        primary
+        onTouchTap={this._handleConfirmClose}
+      />
+    ]
 
     return (
       <div style={styles.container}>
@@ -306,22 +300,23 @@ let App = React.createClass({
           </Layout>
         )}
 
-      <Snackbar
-        open={this.state.snackbar.open}
-        message={this.state.snackbar.message}
-        action={this.state.snackbar.undo && 'undo'}
-        onActionTouchTap={this.state.snackbar.undoCallback}
-      />
+        <Snackbar
+          open={this.state.snackbar.open}
+          message={this.state.snackbar.message}
+          action={this.state.snackbar.undo && 'undo'}
+          onActionTouchTap={this.state.snackbar.undoCallback}
+        />
 
-      <Dialog
-            actions={confirmActions}
-            modal={false}
-            open={this.state.confirm.open}
-            onRequestClose={this.handleClose}
-          >
-            {this.state.confirm.message}
-      </Dialog>
-    </div>
+        <Dialog
+          actions={confirmActions}
+          modal={false}
+          open={this.state.confirm.open}
+          onRequestClose={this.handleClose}
+        >
+          {this.state.confirm.message}
+        </Dialog>
+      </div>
+
     )
   }
 

src/js/components/common/loading.jsx
@@ -5,7 +5,7 @@ import CircularProgress from 'material-ui/CircularProgress'
 const Loading = ({style, ...props}) => {
   return (
     <div style={Object.assign({}, styles.container, style)}>
-      <CircularProgress {...props} />
+      <CircularProgress {...props} style={styles.spinner} />
     </div>
   )
 }
@@ -21,6 +21,10 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  spinner: {
+    margin: 0,
+    transform: 'translate(0, -30px)'
   }
 }
 

commit 2cd52fdc764e2f745b8a9154b38c592876e5e556

Had to undo some modifcations.
Commit e360e87bc3ace15c21e223e40abdf41ce742e3ba introduced some bugs that made the graph hardly usable. I had to comment them out untill we finish them.

src/js/components/graph/graph.jsx
@@ -240,9 +240,11 @@ let Graph = React.createClass({
     })
 
     let loading
+    /*
     if (!this.state.initialized) {
       loading = <Loading style={styles.loading} />
     }
+    */
 
     return (
       <div style={styles.container}>

src/js/stores/graph-store.js
@@ -194,9 +194,10 @@ export default Reflux.createStore({
   onNavigateToNode: function (node, defaultHistoryNode) {
     let {navHistory} = this.state
 
+    /*
     this.state.loading = true
-
     this.trigger(this.state)
+    */
 
     this.state.rotationIndex = 0
 