3165d62043b8a286f9ce87a64a35f682842194b0
Dean Cooksey <dean.g.cooksey@gmail.com>
2016-11-24 15:56:20 +0100

    Linting

--- src/js/actions/graph-actions.js
+++ src/js/actions/graph-actions.js
@@ -1,20 +1,20 @@
 import Reflux from 'reflux'
 
 export default Reflux.createActions({
-  'setState' : {asyncResult: false},
+  'setState': {asyncResult: false},
   'getState': {asyncResult: false},
   'changeRotationIndex': {asyncResult: false},
   // Birth
-  'getInitialGraphState' : {asyncResult: true},
+  'getInitialGraphState': {asyncResult: true},
   // Life
   'drawNewNode': {asyncResult: false},
   'deleteNode': {asyncResult: false},
   'dissconnectNode': {asyncResult: false},
-  'navigateToNode' : {asyncResult: true},
-  'refresh' : {asyncResult: false},
-  'drawAtUri' : {asyncResult: false},
-  'highlight' : {asyncResult : false},
-  'viewNode': {asyncResult : false},
+  'navigateToNode': {asyncResult: true},
+  'refresh': {asyncResult: false},
+  'drawAtUri': {asyncResult: false},
+  'highlight': {asyncResult: false},
+  'viewNode': {asyncResult: false},
   // Death.
-  'eraseGraph' :{asyncResult: false}
+  'eraseGraph': {asyncResult: false}
 })
