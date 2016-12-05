566d6cfd2c67f4cbe5e76d621e7ce0cccae1d639
Dean Cooksey <dean.g.cooksey@gmail.com>
2016-12-05 18:24:18 +0100

    Confirm Email Verification page now working! Awaiting the correct formatted LINK being sent to users, proxy-side.

--- src/js/components/accounts/confirm-email-verification.jsx
+++ src/js/components/accounts/confirm-email-verification.jsx
@@ -15,21 +15,34 @@ let ConfirmEmailVerification = React.createClass({
   },
 
   getStyles() {
-    container: {
-
-    },
-    title: {
-
-    },
-    message: {
-
+    let styles = {
+      container: {
+        textAlign: 'center',
+        background: '#f8f9fb',
+        height: '100%',
+        overflowY: 'auto'
+      },
+      content: {
+        width: '300px',
+        maxWidth: '90%',
+        padding: '20px',
+        margin: '0 auto 20px auto',
+        boxSizing: 'border-box'
+      },
+      button: {
+        width: '100%'
+      }
     }
+    return styles
   },
 
   activateEmailAccount() {
+    let user = encodeURIComponent(this.props.params.username)
+    let code = encodeURIComponent(this.props.params.code)
+
     fetch(`${proxy}/verifyemail`, {
       method: 'POST',
-      body: `username=${user}, code=${code}`,
+      body: `username=${user}&code=${code}`,
       credentials: 'include',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
@@ -40,7 +53,7 @@ let ConfirmEmailVerification = React.createClass({
       }
       SnackbarActions
         .showMessage('Your account has been activated!')
-      this.redirectToLogin
+      this.redirectToLogin()
     }).catch((e) => {
       SnackbarActions.showMessage('An error occured : ' + e)
       // console.error(e)
@@ -49,6 +62,24 @@ let ConfirmEmailVerification = React.createClass({
 
   redirectToLogin() {
     this.context.router.push('/')
+  },
+
+  render() {
+    let styles = this.getStyles()
+    return (
+      <div style={styles.container}>
+        <div style={styles.content}>
+          <h3>Click the button below to activate your Web ID!</h3>
+          <RaisedButton
+            type="submit" secondary
+            disabled={this.state.disabledSubmit}
+            style={styles.button}
+            label="CONFIRM ACTIVATION"
+            onClick={this.activateEmailAccount}
+          />
+        </div>
+      </div>
+    )
   }
 
 })
