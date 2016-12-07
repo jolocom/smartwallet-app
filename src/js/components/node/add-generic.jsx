5141966da5dba98d2f7793f04654273a6a1e2e57
izzystewart <isabel.stewart@uqconnect.edu.au>
2016-12-07 09:25:21 +0100

    adding new img node icon component for header

--- src/js/components/node/add-generic.jsx
+++ src/js/components/node/add-generic.jsx
@@ -16,6 +16,7 @@ import {
 
 import AddNodeIcon from 'components/icons/addNode-icon.jsx'
 import DocIcon from 'components/icons/doc-icon.jsx'
+import ImgIcon from 'components/icons/imgNode-icon.jsx'
 
 import Dialog from 'components/common/dialog.jsx'
 import {Layout, Content} from 'components/layout'
@@ -205,7 +206,7 @@ let NodeAddGeneric = React.createClass({
             {
               this.state.hasFiles
               ? this.state.fileType === 'image'
-                ? <ImageIcon />
+                ? <ImgIcon />
                 : <DocIcon />
               : <AddNodeIcon />
             }
