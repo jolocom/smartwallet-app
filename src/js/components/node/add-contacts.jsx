2044a3f00c26e209ca483166309bb92a95e14d19
izzystewart <isabel.stewart@uqconnect.edu.au>
2016-12-19 13:03:30 +0100

    basic selectable contact list ui in add contacts dialogue

--- src/js/components/node/add-contacts.jsx
+++ src/js/components/node/add-contacts.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 import Reflux from 'reflux'
 import Radium from 'radium'
 
-import {AppBar, IconButton, FlatButton} from 'material-ui'
+import {
+  AppBar,
+  IconButton,
+  FlatButton,
+  List,
+  ListItem,
+  Avatar,
+  Checkbox
+} from 'material-ui'
+import {Tabs, Tab} from 'material-ui/Tabs'
 
 import Dialog from 'components/common/dialog.jsx'
 import {Layout} from 'components/layout'
+import {transparent, pinkA200} from 'material-ui/styles/colors'
+import Util from 'lib/util'
+import UncheckedIcon from 'material-ui/svg-icons/toggle/radio-button-unchecked'
+import CheckedIcon from 'material-ui/svg-icons/action/check-circle'
 
 let AddContact = React.createClass({
 
@@ -18,6 +31,37 @@ let AddContact = React.createClass({
   contextTypes: {
     router: React.PropTypes.any,
     muiTheme: React.PropTypes.object
+  },
+
+  getInitialState() {
+    return {
+      contactArray: [
+        {
+          key: 1,
+          imgUri: 'https://annika.webid.jolocom.de/files/' +
+            'fm86xd-DSC09243-1-Kopie_s.jpg',
+          name: 'Eelco'
+        },
+        {
+          key: 2,
+          imgUri: 'https://annika.webid.jolocom.de/files/' +
+            'fm86xd-DSC09243-1-Kopie_s.jpg',
+          name: 'Isabel'
+        },
+        {
+          key: 3,
+          imgUri: 'https://annika.webid.jolocom.de/files/' +
+            'fm86xd-DSC09243-1-Kopie_s.jpg',
+          name: 'Oldcake'
+        },
+        {
+          key: 4,
+          imgUri: 'https://annika.webid.jolocom.de/files/' +
+            'fm86xd-DSC09243-1-Kopie_s.jpg',
+          name: 'Sabine'
+        }
+      ]
+    }
   },
 
   componentDidMount() {
@@ -36,6 +80,16 @@ let AddContact = React.createClass({
       },
       icon: {
         color: actionAppBar.textColor
+      },
+      listItems: {
+        marginLeft: '40px'
+      },
+      alphaLetter: {
+        position: 'absolute',
+        left: '10px',
+        marginTop: '10px'
+      },
+      checkbox: {
       }
     }
   },
@@ -66,6 +120,45 @@ let AddContact = React.createClass({
               }
               style={styles.bar}
             />
+            <Tabs>
+              <Tab label="Contacts">
+                <List>
+                  <Avatar
+                    style={styles.alphaLetter}
+                    color={pinkA200}
+                    backgroundColor={transparent}>
+                    A
+                  </Avatar>
+                  <div style={styles.listItems}>
+                  {
+                    this.state.contactArray.map((contact) => {
+                      return (
+                        <ListItem
+                          key={contact.key}
+                          leftAvatar={
+                            <Avatar src={
+                            Util.uriToProxied(contact.imgUri)}
+                            />
+                          }
+                          rightToggle={
+                            <Checkbox
+                              checkedIcon={<CheckedIcon />}
+                              uncheckedIcon={<UncheckedIcon />}
+                              style={styles.checkbox}
+                              />
+                          }>
+                          {contact.name}
+                        </ListItem>
+                      )
+                    })
+                  }
+                  </div>
+                </List>
+              </Tab>
+              <Tab label="Groups">
+                <div>GROUPS</div>
+              </Tab>
+            </Tabs>
           </Layout>
         </Dialog>
       </div>
