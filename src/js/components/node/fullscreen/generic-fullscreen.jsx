69ca44a714872b16b0e0aedbd11badf1469f2df0
Eugeniu <rusu.eugeniu@gmx.de>
2017-01-02 19:05:57 +0100

    Fix and confidential node improvement.

--- src/js/components/node/fullscreen/generic-fullscreen.jsx
+++ src/js/components/node/fullscreen/generic-fullscreen.jsx
@@ -148,6 +148,7 @@ let GenericFullScreen = React.createClass({
   },
 
   _handleDisconnect() {
+    console.log(Object.assign({}, this.props))
     if (this.props.rank === 'center') {
       this._handleClose()
     } else {
