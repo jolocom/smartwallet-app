4f6e8c4ed27b52daad59646e75e0f22f8a0b538f
Eugeniu <rusu.eugeniu@gmx.de>
2017-02-09 22:38:35 +0100

    Added proxy endpoint to the test config.

--- config/test.js
+++ config/test.js
@@ -1,7 +1,8 @@
 // @TODO replace values by env settings using gulp
-// endpoint - A String that is the http/s address of the app 
+// endpoint - A String that is the http/s address of the app
 // dev      - A boolean that if true allows to login app without certificate
 module.exports = {
+  proxy: 'https://proxy.jolocom.de',
   endpoint: 'https://localhost:8443',
   dev: false
 }
