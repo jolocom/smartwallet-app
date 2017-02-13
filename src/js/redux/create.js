07702f937a792fba59508b696625f79d940a1167
Vincent den Boer <vincent@shishkabab.net>
2017-02-13 11:10:44 +0100

    Add Redux middleware dealing with Promises

--- src/js/redux/create.js
+++ src/js/redux/create.js
@@ -1,12 +1,16 @@
 import { createStore as _createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { routerMiddleware } from 'react-router-redux'
+import backendMiddleware from './middleware/backend'
 
 export default function createStore(history, client, data) {
   // Sync dispatched route actions to the history
   const reduxRouterMiddleware = routerMiddleware(history)
   const middleware = [
-    // throttleActions([search.SEARCH], 5000),
+    backendMiddleware(
+      'NOT IMPLEMENTED YET: this should be a ' +
+      'single object to access the backend'
+    ),
     reduxRouterMiddleware
   ]
 
