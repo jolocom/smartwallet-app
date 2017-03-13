68ddb42af5ca94a46e5e457a35e56717a4659e84
rawild <annalisa@jolocom.com>
2017-03-13 18:08:33 +0100

    added wording and hooked in confirm-dialog as place holder for 'Why?' UI

--- src/js/components/registration/presentation/user-type.jsx
+++ src/js/components/registration/presentation/user-type.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import Radium from 'radium'
 
 const UserType = (props) => {
+  console.log(props)
   return <div>
-    <h1>Hi !, are you...</h1>
+    <h1>Hi {props.user === '' ? props.user : 'TODO: Connect to Name'}
+    !, are you...</h1>
     <div onClick={() => props.onChange('expert')}>
       {props.value === 'expert' && '*'}
       ...a total tech Geek and want to be in absolute control?
@@ -12,14 +14,20 @@ const UserType = (props) => {
       {props.value === 'layman' && '*'}
       ...the laid-back type, who doesn't want any hassle.
     </div>
-    <div onClick={props.onSubmit}>Next!</div>
-    <div onClick={() => { /*why snackbar*/ }}> WHY?</div>
+    <div onClick={() => props.onSubmit}>Next!</div>
+    <div onClick={() => props.openConfirmDialog('You must be prepared to keep\
+     the passphrase you generated in a safe place where you are not going\
+    to loose it and where others can\'t find it.', 'OK',
+    () => props.closeConfirmDialog)}> WHY?</div>
   </div>
 }
 UserType.propTypes = {
   value: React.PropTypes.string.isRequired,
   onChange: React.PropTypes.func.isRequired,
-  onSubmit: React.PropTypes.func.isRequired
+  onSubmit: React.PropTypes.func.isRequired,
+  openConfirmDialog: React.PropTypes.func,
+  closeConfirmDialog: React.PropTypes.func,
+  user: React.PropTypes.string
 }
 
 export default Radium(UserType)
--- src/js/components/registration/screens/user-type.jsx
+++ src/js/components/registration/screens/user-type.jsx
@@ -4,22 +4,30 @@ import Presentation from '../presentation/user-type'
 
 @connect({
   props: ['registration'],
-  actions: ['registration:goForward', 'registration:setUserType']
+  actions: ['registration:goForward', 'registration:setUserType',
+    'confirmation-dialog:openConfirmDialog',
+    'confirmation-dialog:closeConfirmDialog']
 })
 export default class RegistrationUserTypeScreen extends React.Component {
   static propTypes = {
     registration: React.PropTypes.object.isRequired,
-
     goForward: React.PropTypes.func.isRequired,
-    setUserType: React.PropTypes.func.isRequired
+    setUserType: React.PropTypes.func.isRequired,
+    openConfirmDialog: React.PropTypes.func.isRequired,
+    closeConfirmDialog: React.PropTypes.func.isRequired
   }
 
   render() {
-    return <Presentation
+    return <div>
+    <Presentation
       value={this.props.registration.userType.value}
       valid={this.props.registration.userType.valid}
       onChange={this.props.setUserType}
       onSubmit={this.props.goForward}
+      openConfirmDialog={this.props.openConfirmDialog}
+      closeConfirmDialog={this.props.closeConfirmDialog}
+      user={this.props.registration.humanName}
     />
+    </div>
   }
 }
