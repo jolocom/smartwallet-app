4b4124729b31e323b5474cd11bb0381531241177
rawild <annalisa@jolocom.com>
2017-03-14 10:42:08 +0100

    added unstyled buttons

--- src/js/components/registration/presentation/user-type.jsx
+++ src/js/components/registration/presentation/user-type.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import Radium from 'radium'
+import {RaisedButton} from 'material-ui'
 
 const UserType = (props) => {
   console.log(props)
   return <div>
     <h1>Hi {props.user === '' ? props.user : 'TODO: Connect to Name'}
     !, are you...</h1>
-    <div onClick={() => props.onChange('expert')}>
+    <div>
+    <RaisedButton onClick={() => props.onChange('expert')}>
       {props.value === 'expert' && '*'}
       ...a total tech Geek and want to be in absolute control?
+    </RaisedButton>
     </div>
-    <div onClick={() => props.onChange('layman')}>
+    <div>
+    <RaisedButton onClick={() => props.onChange('layman')}>
       {props.value === 'layman' && '*'}
       ...the laid-back type, who doesn't want any hassle.
+    </RaisedButton>
     </div>
-    <div onClick={() => props.onSubmit}>Next!</div>
+    <div onClick={props.onSubmit}>Next!</div>
     <div onClick={() => props.openConfirmDialog('You must be prepared to keep\
      the passphrase you generated in a safe place where you are not going\
     to loose it and where others can\'t find it.', 'OK',
