f584e32628785c1af5cd3d7ac60d965c88f30792
HamedSaidi <hamedessaidi@gmail.com>
2017-03-15 18:31:40 +0100

    fixing repeated password toggle button and valid field

--- src/js/components/registration/presentation/password.jsx
+++ src/js/components/registration/presentation/password.jsx
@@ -1,23 +1,46 @@
 import React from 'react'
 import Radium from 'radium'
 
-function Password(props){
+function Password(props) {
+	return <div >
+		<input
+			type={props.visibleValue}
+			value={props.value}
+			onChange={
+				e => props.onChangePassword(e.target.value)
+			}
+		/>
 
-    return <div >
-    <h1>Please enter a password:</h1>
-      <input type='password' value={ props.value }  onChange={e => props.onChangePassword(e.target.value)} />
-      <br/>
-	  <input type='password' value={ props.repeatedValue }  onChange={e => props.onChangeRepeatedPassword(e.target.value)} />
-	  <div onClick={props.onSubmit}>Next!</div>
-	</div>;
+		<div onClick={
+			e => props.onTogglePasswordValue(e.target.value)
+		}>
+			toggle!
+		</div>
+
+		<input
+			type={props.visibleRepeatedValue}
+			value={props.repeatedValue}
+			onChange={e => props.onChangeRepeatedPassword(e.target.value)}
+		/>
+		<div onClick={
+			e => props.onTogglePasswordRepeatedValue(e.target.value)
+		}>
+			toggle!
+		</div>
+		<div onClick={props.onSubmit}>Next!</div>
+	</div>
 }
 
 Password.propTypes = {
-  onSubmit: React.PropTypes.func.isRequired,
-  onChangeRepeatedPassword: React.PropTypes.func.isRequired,
-  onChangePassword: React.PropTypes.func.isRequired,
-  value: React.PropTypes.string.isRequired,
-  repeatedValue:  React.PropTypes.string.isRequired,
+	onSubmit: React.PropTypes.func.isRequired,
+	onChangeRepeatedPassword: React.PropTypes.func.isRequired,
+	onChangePassword: React.PropTypes.func.isRequired,
+	onTogglePasswordValue: React.PropTypes.func.isRequired,
+	onTogglePasswordRepeatedValue: React.PropTypes.func.isRequired,
+	value: React.PropTypes.string.isRequired,
+	visibleValue: React.PropTypes.string.isRequired,
+	repeatedValue: React.PropTypes.string.isRequired,
+	visibleRepeatedValue: React.PropTypes.string.isRequired
 }
 
 export default Radium(Password)
