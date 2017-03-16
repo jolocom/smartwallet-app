549b111f95a30459c84d146a906050d4f2fd07f0
izzystewart <isabel.stewart@uqconnect.edu.au>
2017-03-16 18:08:41 +1000

    basic skeleton

--- src/js/components/registration/presentation/write-phrase.jsx
+++ src/js/components/registration/presentation/write-phrase.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
 import Radium from 'radium'
 
+import Checkbox from 'material-ui/Checkbox'
+
 const WritePhrase = (props) => {
-  return <div>
-    <h1>Write down your passphrase</h1>
-    <div onClick={props.onSubmit}>Next!</div>
-  </div>
+  return (
+    <div>
+      <h1>Your secure phrase</h1>
+      <p>
+        {
+          props.value || 'The flying red fox is jumping enthusiastically over\
+          the little brown dog.'
+        }
+      </p>
+      <p>
+        IMPORTANT <br />
+        Write these words down on an analog and secure place. Store it in at
+        least two different places. Without these words you cannot access your
+        wallet again. Anyone with these words can get access to your wallet!
+        By the way! Taking a screenshot is not secure!
+      </p>
+      <Checkbox
+        label="Yes, I have securely written down my phrase."
+      />
+      <div onClick={props.onSubmit}>Next!</div>
+    </div>
+  )
 }
 WritePhrase.propTypes = {
-  onSubmit: React.PropTypes.func.isRequired
+  onSubmit: React.PropTypes.func.isRequired,
+  value: React.PropTypes.string.isRequired
 }
 
 export default Radium(WritePhrase)
