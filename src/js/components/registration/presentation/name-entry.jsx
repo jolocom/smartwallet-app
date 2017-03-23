dfdb517595c360f93537f3e663dad13793798d4b
izzystewart <isabel.stewart@uqconnect.edu.au>
2017-03-23 15:36:30 +1000

    basic name entry styling

--- src/js/components/registration/presentation/name-entry.jsx
+++ src/js/components/registration/presentation/name-entry.jsx
@@ -1,16 +1,39 @@
 import React from 'react'
 import Radium from 'radium'
+import RegistrationStyles from '../styles'
+
+import {
+  RaisedButton,
+  TextField
+} from 'material-ui'
+
+const STYLES = {
+  root: RegistrationStyles
+}
 
 const NameEntry = (props) => {
-  return <div>
-    <h1>Name entry</h1>
-    <input type="text"
-      value={props.value}
-      onChange={(e) => props.onChange(e.target.value)}
-    />
-    <div onClick={props.onSubmit}>Next!</div>
-  </div>
+  return (
+    <div style={STYLES.root.container}>
+      <div style={STYLES.root.elementSpacing}>
+        <h1 style={STYLES.root.header}>Name entry</h1>
+      </div>
+      <div style={STYLES.root.elementSpacing}>
+        <TextField
+          type="text"
+          value={props.value}
+          onChange={(e) => props.onChange(e.target.value)}
+        />
+      </div>
+      <div style={STYLES.root.elementSpacing}>
+        <RaisedButton
+          label="NEXT STEP"
+          secondary
+          onClick={props.onSubmit} />
+      </div>
+    </div>
+  )
 }
+
 NameEntry.propTypes = {
   value: React.PropTypes.string.isRequired,
   onChange: React.PropTypes.func.isRequired,
