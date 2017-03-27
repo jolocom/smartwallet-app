e53245f5711fb6f5ae3b7748c0b3c72383c19415
HamedSaidi <hamedessaidi@gmail.com>
2017-03-27 15:35:25 +0200

    name-entry screen unit test: onSubmit should call goForward with proper params

--- src/js/components/registration/screens/name-entry.test.jsx
+++ src/js/components/registration/screens/name-entry.test.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Immutable from 'immutable'
 import { expect } from 'chai'
 import { shallow } from 'enzyme'
+import { stub } from '../../../../../test/utils'
 import RegistrationNameEntryScreen from './name-entry'
 
 describe('(Component) RegistrationNameEntryScreen', function() {
@@ -11,7 +12,7 @@ describe('(Component) RegistrationNameEntryScreen', function() {
       (<RegistrationNameEntryScreen.WrappedComponent {
         ...RegistrationNameEntryScreen.mapStateToProps(Immutable.fromJS({
           registration: {
-            humanName:  {
+            humanName: {
               value: '',
               valid: false
             }
@@ -23,5 +24,51 @@ describe('(Component) RegistrationNameEntryScreen', function() {
 
     expect(wrapper.find('NameEntry').prop('value')).to.be.empty
     expect(wrapper.find('NameEntry').prop('valid')).to.be.false
+  })
+  it('should call setHumanName onChange', function() {
+    const setHumanName = stub()
+    const goForward = () => {}
+    const wrapper = shallow(
+      (<RegistrationNameEntryScreen.WrappedComponent {
+        ...RegistrationNameEntryScreen.mapStateToProps(Immutable.fromJS({
+          registration: {
+            humanName: {
+              value: '',
+              valid: false
+            }
+          }
+        }))
+      }
+        setHumanName={setHumanName}
+        goForward={goForward}
+     />),
+      { context: { muiTheme: { } } }
+    )
+
+    wrapper.find('NameEntry').prop('onChange')('test')
+    expect(setHumanName.called).to.be.true
+  })
+  it('should call goForward onSubmit with proper params', function() {
+    const setHumanName = () => {}
+    const goForward = stub()
+    const wrapper = shallow(
+      (<RegistrationNameEntryScreen.WrappedComponent {
+        ...RegistrationNameEntryScreen.mapStateToProps(Immutable.fromJS({
+          registration: {
+            humanName: {
+              value: '',
+              valid: false
+            }
+          }
+        }))
+      }
+        setHumanName={setHumanName}
+        goForward={goForward}
+       />),
+      { context: { muiTheme: { } } }
+    )
+    wrapper.find('NameEntry').props().onSubmit()
+    expect(goForward.called).to.be.true
+    expect(goForward.calls).to.deep.equal([{'args': []}])
   })
 })
