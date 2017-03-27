89663bfdaa412ad91426c4852e33d5a7fcc87b9b
HamedSaidi <hamedessaidi@gmail.com>
2017-03-27 14:37:24 +0200

    unit test for onChange prop

--- src/js/components/registration/screens/identifier.test.jsx
+++ src/js/components/registration/screens/identifier.test.jsx
@@ -1,22 +1,20 @@
-/* global describe: true, it: true */
 import React from 'react'
 import Immutable from 'immutable'
 import { expect } from 'chai'
 import { shallow } from 'enzyme'
+import { stub } from '../../../../../test/utils'
 import RegistrationIdentifierScreen from './identifier'
+import Presentation from '../presentation/identifier'
 
-describe('(Component) RegistrationIdentifierScreen', function() {
+describe.only('(Component) RegistrationIdentifierScreen', function() {
   it('should render properly the first time', function() {
     const wrapper = shallow(
       (<RegistrationIdentifierScreen.WrappedComponent {
         ...RegistrationIdentifierScreen.mapStateToProps(Immutable.fromJS({
           registration: {
-            email:  {
+            email: {
               value: '',
               valid: false
-            },
-             humanName:  {
-              value: 'xyz'
             }
           }
         }))
@@ -25,7 +23,32 @@ describe('(Component) RegistrationIdentifierScreen', function() {
     )
 
     expect(wrapper.find('Identifier').prop('value')).to.be.empty
-    expect(wrapper.find('Identifier').prop('userName')).be.equal('xyz')
     expect(wrapper.find('Identifier').prop('valid')).to.be.false
+  })
+  it('should call setEmail onchange', function() {
+    const setEmail = stub()
+    function goForward() { }
+    const wrapper = shallow(
+      (<RegistrationIdentifierScreen.WrappedComponent {
+        ...RegistrationIdentifierScreen.mapStateToProps(Immutable.fromJS({
+          registration: {
+            email: {
+              value: '',
+              valid: false
+            }
+          }
+        }))
+      }
+        setEmail={setEmail}
+        goForward={goForward}
+       />),
+      { context: { muiTheme: { } } }
+    )
+    wrapper.find(Presentation).props().onChange('ha@gmail.com')
+    expect(setEmail.called).to.be.true
+    expect(setEmail.calls).to.deep.equal([{ 'args': [
+      'ha@gmail.com'
+    ]
+    }])
   })
 })
