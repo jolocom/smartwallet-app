58d3e8f195eb49de7224d977c71512a2073dcbd4
Vincent den Boer <vincent@shishkabab.net>
2017-04-03 13:34:38 +0200

    Fixes #605

--- src/js/redux/reducer.js
+++ src/js/redux/reducer.js
@@ -10,5 +10,8 @@ export default combineReducers({
   snackBar: require('./modules/snack-bar').default,
   account: require('./modules/account').default,
   registration: require('./modules/registration').default,
-  leftNav: require('./modules/left-nav').default
+  leftNav: require('./modules/left-nav').default,
+  wallet: combineReducers({
+    tabs: require('./modules/wallet/tabs').default
+  })
 })
