81e6508ec55a2300e337abb8695ae6b24cb73d03
panagiotis.kourouklidis@gmail.com <panagiotis.kourouklidis@gmail.com>
2017-04-03 16:37:47 +0200

    Dummy backend to get account information

--- src/js/lib/agents/wallet.js
+++ src/js/lib/agents/wallet.js
@@ -38,8 +38,21 @@ export default class WalletAgent {
       }, 2000)
     })
   }
-}
 
+  getAccountInformation() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let information = {
+          emails: [{address: 'adress1@example.com', verified: true},
+            {adress: 'adress2@example.com', verified: false}],
+          telNums: [{num: '+4917912345678', type: 'work', verified: true},
+            {num: '+4917923456789', type: 'personal', verified: false}]
+        }
+        resolve(information)
+      }, 2000)
+    })
+  }
+}
 export class Wallet {
   constructor() {
     this.webID = 'https://demo.webid.jolocom.com/profile/card'
---
+++ src/js/redux/modules/wallet/contact.js
@@ -0,0 +1,6 @@
+import * as _ from 'lodash'
+import Immutable from 'immutable'
+import { makeActions } from '../'
+import * as router from '../router'
+
+const actions = module.exports = makeActions('wallet/contact'
