a9ce39e24615d5a3904b01350344745d82728c1d
panagiotis.kourouklidis@gmail.com <panagiotis.kourouklidis@gmail.com>
2017-04-04 08:56:45 +0200

    delete unnecessary contact redux module

--- src/js/redux/modules/wallet/contact.js
+++
@@ -1,6 +0,0 @@
-import * as _ from 'lodash'
-import Immutable from 'immutable'
-import { makeActions } from '../'
-import * as router from '../router'
-
-const actions = module.exports = makeActions('wallet/contact'
