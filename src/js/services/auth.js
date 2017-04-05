edfae5ce45da9231dc18bac0a4a0c69c8656d5c6
Vincent den Boer <vincent@shishkabab.net>
2017-04-05 12:31:57 +0200

    Create auth service

---
+++ src/js/services/auth.js
@@ -0,0 +1,38 @@
+export default class AuthService {
+  constructor(backend) {
+    this.backend = backend
+    this.currentUser = null
+  }
+
+  async registerWithSeedPhrase({userName, seedPhrase}) {
+    this.currentUser = {
+      wallet: await this.backend.registerWithSeedPhrase({userName, seedPhrase})
+    }
+    return this.currentUser
+  }
+
+  async registerWithCredentials({userName, email, password}) {
+    this.currentUser = {
+      wallet: await this.backend.registerWithCredentials({
+        userName, email, password
+      })
+    }
+    return this.currentUser
+  }
+
+  async loginWithSeedPhrase({userName, seedPhrase}) {
+    this.currentUser = {
+      wallet: await this.backend.loginWithSeedPhrase({userName, seedPhrase})
+    }
+    return this.currentUser
+  }
+
+  async loginWithCredentials({userName, email, password}) {
+    this.currentUser = {
+      wallet: await this.backend.loginWithCredentials({
+        userName, email, password
+      })
+    }
+    return this.currentUser
+  }
+}
--- src/js/services/index.js
+++ src/js/services/index.js
@@ -1,7 +1,9 @@
+import AuthService from './auth'
 import EntropyService from './entropy'
 
-export default function create() {
+export default function create(backend) {
   return {
+    auth: new AuthService(backend),
     entropy: new EntropyService()
   }
 }
