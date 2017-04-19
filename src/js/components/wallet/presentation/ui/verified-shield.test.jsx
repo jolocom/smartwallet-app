8d68a6ca32d87f30c730b0a30cf04faa986b1eaa
AW <annalisa@jolocom.com>
2017-04-19 16:34:48 +0200

    refactored contact screen onFocusChange and added test and removed .only

--- src/js/components/wallet/presentation/ui/verified-shield.test.jsx
+++ src/js/components/wallet/presentation/ui/verified-shield.test.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import VerifiedShield from './verified-shield'
 
-describe.only('(Component) VerifiedShield', function() {
+describe('(Component) VerifiedShield', function() {
   it('should render properly the first time', function() {
     shallow((<div><VerifiedShield verified /> </div>),
       { context: { muiTheme: { } } }
