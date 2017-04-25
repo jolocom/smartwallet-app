f9a345e4ffca4d9bbb1540f02bb1e6c833aaab9a
AW <annalisa@jolocom.com>
2017-04-25 10:54:06 +0200

    clean up styling

--- src/js/components/registration/presentation/write-phrase.jsx
+++ src/js/components/registration/presentation/write-phrase.jsx
@@ -30,6 +30,17 @@ const STYLES = {
   },
   uncheckedIcon: {
     fill: theme.jolocom.gray1
+  },
+  checkBox: {
+    width: '40%',
+    margin: 'auto'
+  },
+  labelStyle: {
+    color: theme.jolocom.gray1,
+    fontSize: '13px',
+    marginLeft: '5px',
+    display: 'inline-block',
+    position: 'relative'
   }
 }
 
@@ -59,11 +70,10 @@ const WritePhrase = (props) => {
           By the way! Taking a screenshot is not secure!
         </SideNote>
       </Block>
-      <Block style={{width: '80%', margin: 'auto'}}>
+      <Block style={STYLES.checkBox}>
         <Checkbox
           label="Yes, I have securely written down my phrase."
-          labelStyle={{color: theme.jolocom.gray1, fontSize: '13px',
-            marginLeft: '5px', display: 'inline-block', position: 'relative'}}
+          labelStyle={STYLES.labelStyle}
           checkedIcon={<Checked />}
           uncheckedIcon={<Unchecked style={STYLES.uncheckedIcon} />}
           onClick={(e) => props.onToggle(e.target.checked)}
