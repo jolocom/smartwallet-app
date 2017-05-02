93c38b86ffeec73cd6634d8221067774dddaf0c8
Vincent den Boer <vincent@shishkabab.net>
2017-05-02 16:01:57 +0200

    Add some dependencies and add Jolocom geth config

--- src/js/lib/blockchain/config.js
+++ src/js/lib/blockchain/config.js
@@ -1,11 +1,12 @@
 // ROPSTEN TESTNET
 module.exports = Object.freeze({
-  // Fraunhofer geth node in ropsten testnet
-  GETH_HOST: 'http://verification.jolocom.com:9050',
-  // Lookup Contract has been deployed on the Ropsten Testnet on 2017-04-12
-  // LOOKUP_CONTRACT_ADDRESS: '0x58ab8f7c72b4bec073db317d92aa0a15f09d9a6b'
-  // Lookup contract testrpc
-  LOOKUP_CONTRACT_ADDRESS: '0x1f18b8d96f0a26eea82ce0d4bc202cc429df955f'
+  // Test RPC node
+  // GETH_HOST: 'http://verification.jolocom.com:9050',
+  // LOOKUP_CONTRACT_ADDRESS: '0x1f18b8d96f0a26eea82ce0d4bc202cc429df955f'
+
+  // Ropsten Testnet on 2017-04-12
+  GETH_HOST: 'http://verification.jolocom.com:8545',
+  LOOKUP_CONTRACT_ADDRESS: '0x58ab8f7c72b4bec073db317d92aa0a15f09d9a6b'
 })
 
 // TEST-RPC
