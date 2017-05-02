8e2c69a6ccde50dee7e095c0a52cd7cb1281dbd0
Vincent den Boer <vincent@shishkabab.net>
2017-05-02 14:01:10 +0200

    Test RPC config

--- src/js/lib/blockchain/config.js
+++ src/js/lib/blockchain/config.js
@@ -1,9 +1,11 @@
 // ROPSTEN TESTNET
 module.exports = Object.freeze({
   // Fraunhofer geth node in ropsten testnet
-  GETH_HOST: 'http://193.175.133.233:8545',
+  GETH_HOST: 'http://verification.jolocom.com:9050',
   // Lookup Contract has been deployed on the Ropsten Testnet on 2017-04-12
-  LOOKUP_CONTRACT_ADDRESS: '0x58ab8f7c72b4bec073db317d92aa0a15f09d9a6b'
+  // LOOKUP_CONTRACT_ADDRESS: '0x58ab8f7c72b4bec073db317d92aa0a15f09d9a6b'
+  // Lookup contract testrpc
+  LOOKUP_CONTRACT_ADDRESS: '0x1f18b8d96f0a26eea82ce0d4bc202cc429df955f'
 })
 
 // TEST-RPC
