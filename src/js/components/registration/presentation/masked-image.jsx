bffa301fc54961fcbeb355e82746f7746a1e875e
AW <annalisa@jolocom.com>
2017-05-12 13:45:06 +0200

    fixed DOM error

--- src/js/components/registration/presentation/masked-image.jsx
+++ src/js/components/registration/presentation/masked-image.jsx
@@ -89,7 +89,7 @@ class MaskedImage extends React.Component {
 
       {props.uncoveredPaths.length === 0 && <g>
         <text x="350" y="150" fontSize="21" dy="0"
-          font-weight="lighter" textAnchor="middle" fill={theme.jolocom.gray1}
+          fontWeight="lighter" textAnchor="middle" fill={theme.jolocom.gray1}
         >
           <tspan x="350" dy="0.6em">{props.message1}</tspan>
           <tspan x="350" dy="1.6em">{props.message2}</tspan>
