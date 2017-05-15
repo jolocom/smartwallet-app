d798e5f2c1a3c2d48e09e985ada0908ef9f84d58
Vincent den Boer <vincent@shishkabab.net>
2017-05-15 16:46:24 +0200

    Integrate Smart Wallet contracts lib

--- src/js/lib/agents/wallet.js
+++ src/js/lib/agents/wallet.js
@@ -1,11 +1,15 @@
-import {keystore} from 'eth-lightwallet'
-import SmartWallet from 'lib/blockchain/smartwallet'
+import * as settings from 'settings'
+import {WalletManager} from 'smartwallet-contracts'
 import VerificationAgent from './verification'
 // import SolidAgent from './solid-wallet'
 
 export default class WalletAgent {
+  constructor() {
+    this._manager = new WalletManager(settings.blockchain)
+  }
+
   generateSeedPhrase(entropy) {
-    let seed = keystore.generateRandomSeed(entropy)
+    let seed = this._manager.generateSeedPhrase(entropy)
     // only for testing testSeed has some ether on ropsten testnet
     let testSeed = 'mandate print cereal style toilet hole' +
       ' cave mom heavy fork network indoor'
@@ -16,149 +20,22 @@ export default class WalletAgent {
     return 'blabla blabla blabla blabla blabla blabla blabla blabla'
   }
 
-  registerWithSeedPhrase(
-    {
-      userName,
-      seedPhrase
-    }
-  ) {
-    console.log(
-      'WalletAgent: See Transactions at: https://ropsten.etherscan.io/'
-    )
-    console.log(seedPhrase)
-    let password = '1234'
-    let wallet = new SmartWallet()
-    return new Promise((resolve, reject) => {
-      wallet
-        .init(seedPhrase, password)
-        .then(result => {
-          return wallet.createDigitalIdentity(userName, password)
-        })
-        .then(identityAddress => {
-          wallet.setIdentityAddress(identityAddress)
-          return wallet.addIdentityAddressToLookupContract(identityAddress)
-        })
-        .then(result => {
-          console.log(
-            'WalletAgent: identityAddress addedtoLookupContract Transaction ' +
-              'waiting to be mined txhash -> ' +
-              result.txhash
-          )
-
-          return wallet.waitingToBeMined(result.txhash)
-        })
-        .then(transaction => {
-          console.log(
-            'WalletAgent: Transaction add address to lookup contracted got' +
-              ' mined -> ' +
-              transaction.transactionHash
-          )
-          let privateKeyWebID = wallet.generatePrivateKeyForWebID()
-          wallet.setWebIDPrivateKey(privateKeyWebID)
-          console.log('WalletAgent: privatekey WebID')
-          console.log(privateKeyWebID)
-          let encryptedWebID = wallet.encryptPrivateKeyForWebID(
-            privateKeyWebID
-          )
-          console.log('WalletAgent: privatekey WebID encrypted')
-          console.log(encryptedWebID)
-          return wallet.addProperty('webidkey', encryptedWebID, password)
-        })
-        .then(txhash => {
-          console.log(
-            'WalletAgent: addPropertyTransaction waiting to be mined txhash: ' +
-              txhash
-          )
-          return wallet.waitingToBeMined(txhash)
-        })
-        .then(transaction => {
-          console.log(
-            'WalletAgent: addPropertyTransaction got minded txhash: ' +
-              transaction.transactionHash
-          )
-          resolve(wallet)
-        })
+  registerWithSeedPhrase({userName, seedPhrase, pin}) {
+    return this._manager.registerWithSeedPhrase({
+      userName, seedPhrase, pin
     })
   }
 
-  registerWithCredentials(
-    {
-      userName,
-      email,
-      password
-    }
-  ) {
-    return new Promise((resolve, reject) => {
-      setTimeout(
-        () => {
-          resolve(new SmartWallet())
-        },
-        2000
-      )
-    })
+  registerWithCredentials({userName, email, password}) {
+    return this._manager.registerWithCredentials({userName, email, password})
   }
 
-  // only for demonstration
-  // wallet.addAttributeHash should not be called in the wallet agent
-  addAttributeHash(wallet, attributeId, attribute, definitionUrl, password) {
-    return wallet
-      .addAttributeHashToIdentity(
-        attributeId,
-        attribute,
-        definitionUrl,
-        password,
-        wallet.getIdentityAddress()
-      )
-      .then(transactionHash => {
-        console.log(
-          'WalletAgent: addAttributeHash waiting to be minded ->' +
-            transactionHash
-        )
-        return wallet.waitingToBeMined(transactionHash)
-      })
-  }
-
-  loginWithSeedPhrase(userName, seedPhrase) {
-    let password = '1234'
-    console.log('WalletAgent: Login with Seedphrase')
-    console.log(userName)
-    console.log(seedPhrase)
-    let wallet = new SmartWallet()
-    return new Promise((resolve, reject) => {
-      wallet
-        .init(seedPhrase, password)
-        .then(result => {
-          return wallet.getIdentityAddressFromLookupContract(
-            userName,
-            password
-          )
-        })
-        .then(identityAddress => {
-          console.log('WalletAgent: got identity Address from Lookup Table')
-          console.log(identityAddress)
-          wallet.setIdentityAddress(identityAddress)
-          return wallet.getProperty('webidkey')
-        })
-        .then(webIdEncrypted => {
-          console.log('WalletAgent: private key of the webId')
-          wallet.setWebIDPrivateKey(
-            wallet.decryptPrivateKeyForWebID(webIdEncrypted)
-          )
-          console.log(wallet.getWebIDPrivateKey())
-          resolve(wallet)
-        })
-    })
+  loginWithSeedPhrase({userName, seedPhrase}) {
+    return this._manager.loginWithSeedPhrase({userName, seedPhrase})
   }
 
   loginWithCredentials({userName, email, password}) {
-    return new Promise((resolve, reject) => {
-      setTimeout(
-        () => {
-          resolve(new Wallet())
-        },
-        2000
-      )
-    })
+    return this._manager.loginWithCredentials({userName, email, password})
   }
 
   expertLogin({passphrase, pin}) {
