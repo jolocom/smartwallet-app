4f0af1f0ffa4a65c5af73ffce52e452e696fbb83
HamedSaidi <hamedessaidi@gmail.com>
2017-06-12 12:27:59 +0200

    result screen react components

--- src/js/components/verification/screens/result.jsx
+++ src/js/components/verification/screens/result.jsx
@@ -3,16 +3,28 @@ import {connect} from 'redux/utils'
 import Presentation from '../presentation/result'
 
 @connect({
-  props: ['verification'],
-  actions: []
+  props: ['verification.result'],
+  actions: [
+    'verification/result:finishVerification',
+    'verification/result:startDataCheck '
+  ]
 })
 export default class VerificationResultScreen extends React.Component {
   static propTypes = {
+    result: React.PropTypes.object.isRequired,
+    finishVerification: React.PropTypes.func.isRequired,
+    startDataCheck: React.PropTypes.func.isRequired
   }
 
   render() {
-    return (
-      <Presentation />
+    const {loading, success, numberOfFails} = this.props.result
+    const {startDataCheck, finishVerification} = this.props
+    return (<Presentation
+      loading={loading}
+      success={success}
+      numberOfFails={numberOfFails}
+      finishVerification={finishVerification}
+      startDataCheck={startDataCheck} />
     )
   }
 }
