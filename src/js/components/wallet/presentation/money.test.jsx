2918478a0c0b39517bd13be79602d4d143ac8dd0
HamedSaidi <hamedessaidi@gmail.com>
2017-06-26 17:52:03 +0200

    money presentation unit tests

--- src/js/components/wallet/presentation/money.test.jsx
+++ src/js/components/wallet/presentation/money.test.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import WalletMoney from './money'
 
-describe.only('(Component) Wallet Money', function() {
+describe('(Component) Wallet Money Presentation', function() {
   it('should render properly the first time', function() {
     shallow(
       (<WalletMoney
