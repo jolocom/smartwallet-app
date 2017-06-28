8cff6b3b0a920e87e5802cf11f824961d8110787
nataschaberg <natalja.wittenberg@gmx.de>
2017-06-28 14:26:35 +0200

    small fix spinner

---
+++ src/js/components/wallet/presentation/spinner.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Radium from 'radium'
+
+import {Content, Block, Header} from '../../structure'
+import {
+  SpinnerCircular
+} from './ui'
+
+export default class Spinner extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      message: ''
+    }
+  }
+
+  componentDidMount() {
+    const {message} = this.props
+    const loopMessage = (arr, index) => {
+      if (index === arr.length) {
+        return loopMessage(arr, 0)
+      }
+      if (this.refs.spinner) {
+        this.setState({
+          message: arr[index]
+        })
+      }
+      setTimeout(loopMessage, 4000, arr, index + 1)
+    }
+    loopMessage(message, 0)
+  }
+
+  render() {
+    return (
+      <Content>
+        <Header
+          style={this.props.style}
+          title="Thank you. We are transferring some Ether to your Account."
+          />
+        <Block>
+          <SpinnerCircular />
+        </Block>
+        <Header style={this.props.style}
+          ref="spinner"
+          title={this.state.message} />
+      </Content>
+    )
+  }
+}
