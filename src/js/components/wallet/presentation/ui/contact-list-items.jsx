99ec2537e92f5827202b97a8b7ca996ca5b14e37
HamedSaidi <hamedessaidi@gmail.com>
2017-06-30 18:08:49 +0200

    extracted contact list component

--- src/js/components/wallet/presentation/ui/contact-list-items.jsx
+++ src/js/components/wallet/presentation/ui/contact-list-items.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Radium from 'radium'
 
-import {List, ListItem, FlatButton} from 'material-ui'
-import {StaticListItem} from './'
+import { List } from 'material-ui'
+import { StaticListItem, VerificationButtons } from './'
 
 const STYLES = {
   dialog: {
@@ -19,57 +19,66 @@ const STYLES = {
   }
 }
 
-const ContactList = (props) => {
-  let {fields, onConfirm, labelText, attrType, icon, iconMsg, buttonMsg} = props
-
-  const list = fields.map(({verified, number, address, type = ''}, index) =>
-    (<div key={number || address}>
-      <StaticListItem
-        key={index || address}
-        verified={verified}
-        textValue={address || number}
-        textLabel={labelText}
-        icon={index === 0 ? icon : null}
-        onVerify={() => onConfirm({
-          rightButtonText: 'REQUEST VERIFICATION',
-          leftButtonText: 'CANCEL',
-          message: iconMsg,
-          style: STYLES.dialog,
-          attrType: attrType,
-          attrValue: address || number
-        })}
-        secondaryTextValue={type} />
-      {
-        verified ? null : (<ListItem
-          disabled leftIcon={<div />}>
-          <FlatButton
-            label="Request Verification"
-            secondary
-            style={STYLES.requestBtn}
-            onClick={() => onConfirm({
-              message: buttonMsg,
-              rightButtonText: 'OK',
-              leftButtonText: 'CANCEL',
-              style: STYLES.simpleDialog,
-              attrValue: number || address
-            })} />
-        </ListItem>)
-      }
-    </div>))
-
-  return (<List disabled>
-    {list}
-  </List>)
+const ContactList = ({
+  fields,
+  onConfirm,
+  labelText,
+  attrType,
+  icon,
+  setFocusedPin,
+  changePinValue,
+  iconMsg
+}) => (<List disabled>
+{
+  fields.map(({
+    verified = false,
+    number = '',
+    address = '',
+    smsCode = '',
+    codeIsSent = false,
+    pinFocused = false,
+    type = ''
+  }, index) => (<div key={address || number}>
+    <StaticListItem
+      key={number || address}
+      verified={verified}
+      textValue={address || number}
+      textLabel={labelText}
+      icon={index === 0 ? icon : null}
+      onVerify={() => onConfirm({
+        rightButtonText: 'REQUEST VERIFICATION',
+        leftButtonText: 'CANCEL',
+        message: iconMsg,
+        style: STYLES.dialog,
+        attrType: attrType,
+        attrValue: address || number
+      })}
+      secondaryTextValue={type} />
+    <VerificationButtons
+      attrType={attrType}
+      smsCode={smsCode}
+      setFocusedPin={(value) => { setFocusedPin(value, index) }}
+      changePinValue={(value) => { changePinValue(value, index) }}
+      focused={pinFocused}
+      value={number || address}
+      codeIsSent={codeIsSent}
+      verified={verified}
+      enterCode={onConfirm}
+      missingCodeCallBack={onConfirm}
+      verify={onConfirm} />
+  </div>))
 }
+</List>)
 
 ContactList.propTypes = {
   iconMsg: React.PropTypes.node,
-  buttonMsg: React.PropTypes.node,
   icon: React.PropTypes.any,
   attrType: React.PropTypes.string.isRequired,
   labelText: React.PropTypes.string.isRequired,
   fields: React.PropTypes.array.isRequired,
   onVerify: React.PropTypes.func.isRequired,
+  changePinValue: React.PropTypes.func,
+  setFocusedPin: React.PropTypes.func,
   onConfirm: React.PropTypes.func.isRequired
 }
 
