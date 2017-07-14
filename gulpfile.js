4cb125d224058506b95cfe4f89b559632729ca03
HamedSaidi <hamedessaidi@gmail.com>
2017-07-14 15:25:31 +0200

    changed port 8080 to 8081

--- gulpfile.js
+++ gulpfile.js
@@ -31,9 +31,9 @@ function startDevServer(config, callback) {
 		stats: {
 			colors: true
 		}
-	}).listen(8080, '0.0.0.0', function (err) {
+	}).listen(8081, '0.0.0.0', function (err) {
 		if (err) throw new gutil.PluginError('webpack-dev-server', err);
-		gutil.log('[webpack-dev-server]', 'http://localhost:8080');
+		gutil.log('[webpack-dev-server]', 'http://localhost:8081');
 	});
 }
 
