ad85acf81bfe395ff17ed3ffbf59b5367b77d699
HamedSaidi <hamedessaidi@gmail.com>
2017-07-28 09:56:10 +0200

    connect shared data screen withr redux

---
+++ src/js/components/single-sign-on/screens/shared-data.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import {connect} from 'redux/utils'
+import Presentation from '../presentation/shared-data'
+import ErrorScreen from '../../common/error'
+
+@connect({
+  props: ['singleSignOn.accessRight'],
+  actions: [
+    'single-sign-on/access-right:deleteService',
+    'single-sign-on/access-right:goToAccessRightScreen',
+    'single-sign-on/access-right:showSharedData',
+    'confirmation-dialog:openConfirmDialog',
+    'confirmation-dialog:closeConfirmDialog'
+  ]
+})
+export default class SingleSignOnSharedDatatScreen extends React.Component {
+  static propTypes = {
+    deleteService: React.PropTypes.func,
+    goToAccessRightScreen: React.PropTypes.func,
+    openConfirmDialog: React.PropTypes.func,
+    showSharedData: React.PropTypes.func,
+    closeConfirmDialog: React.PropTypes.func,
+    accessRight: React.PropTypes.object
+  }
+
+  render() {
+    let {serviceNumber, services} = this.props.accessRight
+    if (services.length > 0) {
+      return (<Presentation
+        deleteService={
+        ({title, message, style, rightButtonLabel, leftButtonLabel}) => {
+          this.props.openConfirmDialog(title, message, rightButtonLabel,
+          () => {
+            this.props.deleteService(serviceNumber)
+            this.props.goToAccessRightScreen()
+          }, leftButtonLabel, style)
+        }}
+        serviceUrl={services[serviceNumber].url}
+        sharedData={services[serviceNumber].sharedData}
+        serviceName={services[serviceNumber].label}
+        showDeleteServiceWindow={(
+        {title, message, style, rightButtonLabel, leftButtonLabel}) => {
+          this.props.openConfirmDialog(title, message, rightButtonLabel,
+            () => { this.props.deleteService(serviceNumber) }, leftButtonLabel,
+            style)
+        }}
+        showSharedData={this.props.showSharedData}
+        goToAccessRightScreen={this.props.goToAccessRightScreen} />)
+    }
+    return <ErrorScreen
+      message=""
+      buttonLabel="try again..."
+      onClick={this.render} />
+  }
+}
