ea55a6bd4e85e0f36abeb7633bcb2a3f51591fdf
HamedSaidi <hamedessaidi@gmail.com>
2017-07-28 15:47:37 +0200

    sub menu icon unit tests

---
+++ src/js/components/single-sign-on/presentation/ui/sub-menu-icon.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import SubMenuIcon from './sub-menu-icon'
+
+describe('(Component) SubMenuIcon', () => {
+  it('should render properly the first time', () => {
+    shallow((<SubMenuIcon
+      name=""
+      style={{}}
+      onClick={() => {}}
+      icon={<div/>} />),
+      { context: { muiTheme: { } } }
+    )
+  })
+})
