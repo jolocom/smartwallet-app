fc9965a9328b39470961404d999c7903a242c845
Vincent den Boer <vincent@shishkabab.net>
2017-08-09 10:53:59 +0200

    Remove obsolete code from verification agent

--- src/js/lib/agents/verification.js
+++ src/js/lib/agents/verification.js
@@ -1,5 +1,3 @@
-import WalletCrypto from 'smartwallet-contracts/lib/wallet-crypto'
-import {randomUint8Array} from 'secure-random'
 import * as settings from 'settings'
 import HTTPAgent from './http'
 
@@ -49,47 +47,5 @@ export default class VerificationAgent {
         code
       }
     )
-
-    localStorage.deleteItem(getDataKey({dataType, data}))
   }
-}
-
-function saltAndHash(data) {
-  const randomBuffer = randomUint8Array(10)
-  const decoder = new TextDecoder('utf8')
-  const salt = btoa(decoder.decode(randomBuffer))
-  const hash = (new WalletCrypto()).sha256(data + salt)
-
-  return {salt, hash}
-}
-
-async function sendVerificationEtherIfNeeded({wallet, dataType, data, pin}) {
-  const dataKey = getDataKey({dataType, data})
-  const stored = localStorage.getItem(dataKey)
-  if (stored) {
-    const storedData = JSON.parse(stored)
-    return {
-      salt: storedData.salt,
-      txHash: storedData.txHash
-    }
-  }
-
-  const {salt, hash} = saltAndHash(data)
-
-  const txHash = await wallet.sendEther({
-    receiver: settings.jolocomIdentityWalletAddress,
-    amountEther: 0.005,
-    data: hash,
-    pin
-  })
-
-  localStorage.setItem(dataKey, JSON.stringify({
-    txHash, salt, hash, data
-  }))
-
-  return {salt, txHash}
-}
-
-function getDataKey({dataType, data}) {
-  return `jolocom.verification.${dataType}.${data}`
 }
