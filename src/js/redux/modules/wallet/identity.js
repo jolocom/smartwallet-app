7154ffaf92aa3c20b96ad046e105af294fdbb450
HamedSaidi <hamedessaidi@gmail.com>
2017-08-29 11:37:26 +0200

    added etherurm plus menu to redux

--- src/js/redux/modules/wallet/identity.js
+++ src/js/redux/modules/wallet/identity.js
@@ -156,6 +156,7 @@ const mapBackendToState = ({webId, userName, contact, passports, idCards}) =>
     webId: webId,
     username: {value: userName},
     expandedFields: {
+      ethereum: false,
       contact: false,
       idCards: false,
       passports: false
