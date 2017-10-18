ec17aa43403dc4e37a0dd88bf5c91a80a0bff763
charly <fei.charleen@gmail.com>
2017-10-18 11:44:21 +0200

    webpack migrated to v 2.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path'),
-      webpack = require('webpack'),
-      HtmlWebpackPlugin = require('html-webpack-plugin'),
-      CleanWebpackPlugin = require('clean-webpack-plugin')
+      webpack = require('webpack')
 
 let defaultGatewayUrl = ''
 if (process.env.USE_LOCAL_GATEWAY === 'true') {
@@ -10,31 +8,37 @@ if (process.env.USE_LOCAL_GATEWAY === 'true') {
 
 module.exports = {
 
-  entry: {
-    main: '/src/js/main.jsx',
-    vendor: [
+  entry: [
       'babel-polyfill',
       'whatwg-fetch',
-      'react-hot-loader',
+      'react-hot-loader/patch',
       'webpack-dev-server/client?http://localhost:8080',
-      'webpack/hot/only-dev-server'
-    ]
-  },
+      'webpack/hot/only-dev-server',
+      './src/js/main.jsx',
+      './src/index.html'
+  ],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
+    path: path.resolve(__dirname, 'dist/js'),
+    filename: '[name].js',
+    publicPath: 'js'
   },
   module: {
     rules: [
       { 
         test: /\.jsx?$/, 
         include: [
-          path.resolve(__dirname, '/src/js'),
+          path.resolve(__dirname, 'src/js'),
           path.resolve(__dirname, 'test'),
-          path.resolve(__dirname, '/node_modules/ethereumjs.-tx'),
-          path.resolve(__dirname, 'node_modules')
+          path.resolve(__dirname, 'node_modules/ethereumjs.-tx')
         ],
-        loader: 'babel-loader' 
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+         use: ['file-loader?name=[name].[ext]']
       }
     ]
   },
@@ -45,7 +49,9 @@ module.exports = {
   },
   resolve: {
     modules: [
-      path.resolve(__dirname) + '/src/js'
+      path.resolve(__dirname, 'src/js'),
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(__dirname, 'node_modules/ethereumjs.-tx')
     ],
     extensions: ['*','.js', '.jsx', '.json'],
     alias: {
@@ -54,17 +60,18 @@ module.exports = {
       'stores': 'stores',
       'lib': 'lib',
       'styles': 'styles',
-      'routes': path.resolve(__dirname, '/src/js/routes/default.jsx'),
-      'settings': path.resolve(__dirname) + '/config/development.js'
+      'routes': path.resolve(__dirname, 'src/js/routes/default.jsx'),
+      'settings': path.resolve(__dirname, 'config/development.js')
     }
   },
   externals: [{
-    xmlhttprequest: '{XMLHttpRequest:XMLHttpRequest}'
+    xmlhttprequest: '{XMLHttpRequest:XMLHttpRequest}',
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+    'react/addons': true
   }],
   plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management'
-    }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       'IDENTITY_GATEWAY_URL': process.env.IDENTITY_GATEWAY_URL
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,39 +1,38 @@
 const path = require('path'),
-      webpack = require('webpack'),
-      HtmlWebpackPlugin = require('html-webpack-plugin'),
-      CleanWebpackPlugin = require('clean-webpack-plugin')
-
+      webpack = require('webpack')
+      
 module.exports = {
 
-  entry: {
-    main: './src/js/main.jsx',
-    vendor: [
+  entry: [
       'babel-polyfill',
-      'whatwg-fetch'
-    ]
-  },
+      'whatwg-fetch',
+      './src/js/main.jsx'
+    ],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
+    path: path.resolve(__dirname, 'dist/js'),
+    filename: '[name].js',
+    publicPath: 'js'
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         include: [
-          path.resolve(__dirname, '/src/js'),
+          path.resolve(__dirname, 'src/js'),
           path.resolve(__dirname, 'test'),
+          path.resolve(__dirname, 'node_modules/ethereumjs.-tx')
         ],
-        exclude: [/node_modules/],
-        loader: 'babel-loader'
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
       }
     ]  
   },
   resolve: {
     modules: [
-      path.resolve(__dirname) + '/src/js',
-      // path.resolve('./'),
-      path.resolve('./node_modules')
+      path.resolve(__dirname) + 'src/js',
+      path.resolve(__dirname, 'node_modules')
     ],
     extensions: ['.*', '.js', '.jsx', '.json'],
     alias: {
@@ -42,8 +41,7 @@ module.exports = {
       'stores': 'stores',
       'lib': 'lib',
       'styles': 'styles',
-      'routes': path.resolve(__dirname, '/src/js/routes/default.jsx'),
-      'settings': path.resolve(__dirname) + '/config/production.js'
+      'settings': path.resolve(__dirname, 'config/production.js')
     },
   },
   externals: [{
@@ -51,12 +49,6 @@ module.exports = {
   }],
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-      }),
-    new HtmlWebpackPlugin({
-      title: 'Output Management'
-    }),
     new webpack.DefinePlugin({
       'IDENTITY_GATEWAY_URL': process.env.TIER === 'staging'
       ? '"https://staging.identity.jolocom.com"'
