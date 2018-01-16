d41d0fdbdac4dd9b577fbd00344908b00eb279e6
Exulansis <rusu.eugeniu@gmx.de>
2018-01-16 16:55:48 +0100

    Improve entropy conversion.

--- src/js/redux_state/modules/registration.js
+++ src/js/redux_state/modules/registration.js
@@ -49,7 +49,7 @@ export const actions = makeActions('registration', {
         }))
 
         if (entropy.isReady()) {
-          const randomString = entropy.getRandomString(6)
+          const randomString = entropy.getRandomString(4)
           return dispatch(actions.submitEntropy(randomString))
         }
       }
--- src/js/services/entropy.js
+++ src/js/services/entropy.js
@@ -2,7 +2,7 @@ import sjcl from 'sjcl'
 
 export default class EntropyService {
   constructor() {
-    // eslint-disable-next-line
+    // eslint-disable-next-line new-cap
     this.generator = new sjcl.prng(7)
   }
 
@@ -19,10 +19,10 @@ export default class EntropyService {
   }
 
   getRandomString(wordCount) {
-    return this.generator.randomWords(wordCount)
-      .join('')
-      .replace(/-/g, '') // removing hyphens from result
-      .toString(16)
-      .substring(0, 32)
+    // returns an array of length wordCount filled with random 4 byte words.
+    const intArray = new Int32Array(this.generator.randomWords(wordCount))
+    const buf = new Buffer(intArray.buffer)
+    return buf.toString('hex')
   }
 }
+
