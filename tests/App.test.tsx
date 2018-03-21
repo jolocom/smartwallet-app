a254a40ff298f32a3537bc24e7888bca0b4dea2c
Exulansis <rusu.eugeniu@gmx.de>
2018-03-21 17:22:47 +0100

    Fix test.

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import App from '../App';
+import * as React from 'react'
+import {App} from '../App'
 
-import * as renderer from 'react-test-renderer';
+import * as renderer from 'react-test-renderer'
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
+  const rendered = renderer.create(<App />).toJSON()
+  expect(rendered).toBeTruthy()
 });
