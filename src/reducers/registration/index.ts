5c3fc32a25d3c3946d81bc3e2b19315181dc894d
Exulansis <rusu.eugeniu@gmx.de>
2018-03-27 11:15:19 +0200

    Tests for the reducer and bug fix.

--- src/reducers/registration/index.ts
+++ src/reducers/registration/index.ts
@@ -4,7 +4,7 @@ export const seedPhrase = (state = '', action: AnyAction): string => {
   switch (action.type) {
     case 'SEEDPHRASE_SET':
       return action.value
-    case 'SEEDPHRASE_CLEARED':
+    case 'SEEDPHRASE_CLEAR':
       return ''
     default:
       return state
