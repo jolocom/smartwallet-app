eef2835d8fd5c2861e39a5fc4a1763fba3f488e4
Exulansis <rusu.eugeniu@gmx.de>
2018-03-29 13:02:10 +0200

    Finish tests for seedPhrase screen

--- tests/ui/components/seedPhrase.test.tsx
+++ tests/ui/components/seedPhrase.test.tsx
@@ -6,15 +6,24 @@ const configureStore = require('redux-mock-store')
 const { ThemeProvider } = require ('react-native-material-ui')
 
 describe('seedPhrase component', ()=> {
-  it('matches the snapshot', () => {
+  it('matches the snapshot with checkbox unchecked', () => {
     const renderer = new ShallowRenderer()
-
     const props = {
       seedPhrase: 'mock seedPhrase',
       checked: false,
       onCheck: () => null
     }
+    const rendered = renderer.render(<SeedPhrase {...props}/>)
+    expect(rendered).toMatchSnapshot()
+  })
 
+  it('matches the snapshot with checkbox checked', () => {
+    const renderer = new ShallowRenderer()
+    const props = {
+      seedPhrase: 'mock seedPhrase',
+      checked: true,
+      onCheck: () => null
+    }
     const rendered = renderer.render(<SeedPhrase {...props}/>)
     expect(rendered).toMatchSnapshot()
   })
