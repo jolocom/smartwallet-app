cb8b26bfac7d0dd6d9059a4ad756ffbf95fa60af
charly <fei.charleen@gmail.com>
2018-03-29 13:42:25 +0200

    entropy routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,23 @@
 import { StackNavigator } from 'react-navigation'
 import { Home } from 'src/ui/home/'
-import { SeedPhrase } from 'src/ui/registration/containers/seedPhrase'
+import { SeedPhrase } from 'src/ui/registration'
+import { Entropy } from 'src/ui/registration'
+
 
 export const RootStack = StackNavigator({
     Home: {
       screen: Home
     },
+    Entropy: { 
+      screen: Entropy, 
+      navigationOptions: { header: null} 
+    },
     SeedPhrase: {
-      screen: SeedPhrase
-    }
+      screen: SeedPhrase,
+      navigationOptions: { header: null} 
+    },
+
   }, {
-    initialRouteName: 'SeedPhrase',
-    headerMode: 'none'
+    initialRouteName: 'SeedPhrase'
   },
 )
