df82ca657fb21fb0832d17fc4ce3da8d77097892
Exulansis <rusu.eugeniu@gmx.de>
2018-04-01 18:22:20 +0200

    Bound hardware back button to navigating back. Separated files slightly.

---
+++ src/routes.ts
@@ -0,0 +1,14 @@
+import { StackNavigator, } from 'react-navigation'
+import { Home } from 'src/ui/home/'
+import { Landing } from 'src/ui/landing/'
+import { PasswordEntry, SeedPhrase } from 'src/ui/registration'
+
+export const Routes = StackNavigator({
+    Landing: { screen: Landing },
+    PasswordEntry: { screen: PasswordEntry },
+    SeedPhrase: { screen: SeedPhrase },
+    Home: { screen: Home }
+  }, {
+    headerMode: 'none'
+  }
+)
