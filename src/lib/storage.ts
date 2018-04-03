2acb0a0e5aa4b09ce21843cb4abf5802e94a44a0
Exulansis <rusu.eugeniu@gmx.de>
2018-04-03 15:34:01 +0200

    Some test methods for the storage lib.

---
+++ src/lib/storage.ts
@@ -0,0 +1,37 @@
+const SQLite = require('react-native-sqlite-storage')
+
+export class Storage {
+  private db = SQLite
+
+  async storeData() {
+
+  }
+
+  async getData() {
+
+  }
+
+  async getPersonas() {
+
+  }
+
+  async getAllAttributes(did: string) {
+
+  }
+
+  async getAttributeByType(did: string, type: string) {
+
+  }
+
+  async getAllVerifications(did: string) {
+
+  }
+
+  async getVerificationsByAttribute(did: string, attribute: string) {
+
+  }
+
+  async getVerificationsByType(did : string, attribute: string) {
+
+  }
+}
