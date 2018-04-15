5eb66a110e606fe187f3f4c5f2de918030cd0e7e
Exulansis <rusu.eugeniu@gmx.de>
2018-04-15 14:15:46 +0200

    Not using global variables anymore.

--- tests/ui/components/seedPhrase.test.tsx
+++ tests/ui/components/seedPhrase.test.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { SeedPhrase } from 'src/ui/registration/components/seedPhrase'
+import { shallow } from 'enzyme'
 
 describe('seedPhrase component', ()=> {
   it('matches the snapshot with checkbox unchecked', () => {
