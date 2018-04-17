18101fb8f3e10b3433a6ca11ad8adc313ca49a18
charly <fei.charleen@gmail.com>
2018-04-17 17:01:12 +0200

    test that initial screen is Landing

--- src/ui/home/components/identity.tsx
+++ src/ui/home/components/identity.tsx
@@ -1,19 +1,70 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { Container, Block } from 'src/ui/structure'
+import { JolocomTheme } from 'src/styles/jolocom-theme'
 
 interface Props {
-  // did: string
+  userName: string,
+  phoneNumber: string,
+  emailAddress: string,
+  onUserNameChange: (input: string) => void
+  onPhoneNumberChange: (input: string) => void
+  onEmailAddressChange: (input: string) => void
 }
 
-interface State {}
+interface State {
+
+}
+
+const styles = StyleSheet.create({
+  subHeader: {
+    fontSize: 14,
+    color: JolocomTheme.textStyles.subheadline.color,
+  },
+  infoPassword: {
+    fontSize: 12,
+    color: JolocomTheme.textStyles.labelInputFields.color
+  },
+  textInputField: {
+    width: '80%'
+  },
+  textErrorField: {
+    color: 'red',
+    fontSize: 14
+  },
+  mainContainer: {
+    justifyContent: 'space-between'
+  },
+  nestedContainer: {
+    justifyContent: 'space-around'
+  },
+  buttonContainer: {
+    backgroundColor: JolocomTheme.palette.primaryColor
+  }
+})
 
 export class IdentityComponent extends React.Component<Props, State> {
+
+
   render() {
     return (
-      <View>
-        {/* <Text> {this.props.did} </Text> */}
-        <Text> IDENTITY </Text>
-      </View>
+      <Container>
+        <TextInput
+          style={ styles.textInputField }
+          placeholder='Please enter your name'
+          onChangeText={ this.props.onUserNameChange }
+        />
+        <TextInput
+          style={ styles.textInputField }
+          placeholder='Please enter your phone number'
+          onChangeText={ this.props.onPhoneNumberChange}
+        />
+        <TextInput
+          style={ styles.textInputField }
+          placeholder='Please enter your email address'
+          onChangeText={ this.props.onEmailAddressChange}
+        />
+      </Container>
     )
   }
 }
