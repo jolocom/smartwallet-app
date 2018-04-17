b73a88274ed8ec96e456fbd9d76d1988e995914e
charly <fei.charleen@gmail.com>
2018-04-17 17:01:12 +0200

    test that initial screen is Landing

---
+++ tests/reducers/navigation/index.test.ts
@@ -0,0 +1,9 @@
+import { navigationReducer as reducer } from 'src/reducers/navigation/'
+import { navigate } from 'src/actions/navigation/'
+
+describe('navigation reducer', ()=> {
+  it('should initialize correctly', () => {
+  // const test = reducer(undefined, { type: '@INIT' })
+  // expect(test.routes[0].routeName).toBe('Landing')
+  })
+})
