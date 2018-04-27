51d190a6fc5391cb8e7323bd61359e8482ca74d7
Exulansis <rusu.eugeniu@gmx.de>
2018-04-27 13:49:49 +0200

    Finishing work on the storage agent + typings

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,8 +1,5 @@
 const SQLite = require('react-native-sqlite-storage')
-
-// TODO THROW ERRORS! & ERROR HANDLING
-// FOREIGN KEY / PRIMARY KEY SYNTAX
-// TYPES
+import { Location, ResultSet, Transaction } from 'react-native-sqlite-storage'
 
 interface Field {
   name: string
@@ -15,89 +12,108 @@ interface TableOptions {
   fields: Field[]
 }
 
+interface asyncTransaction {
+  executeSql: (query: string, args?: any[]) => Promise<{result: ResultSet, transaction: Transaction}>
+}
+
+interface SQLiteDatabase {
+  enablePromise: (flag: boolean) => void
+  DEBUG: (flag: boolean) => void
+  openDatabase: (args: {name: string, location: Location}) => Promise<SQLiteDatabase>
+  close: (successCB: Function, errCB: Function) => Promise<void>
+  transaction: (tx: Function) => Promise<ResultSet>
+}
+
 export class Storage {
-  private sqlLite = SQLite
+  private sqlLite: SQLiteDatabase = SQLite
   private dbName = 'LocalSmartWalletData'
+  private location: Location = 'default'
 
   constructor() {
     this.sqlLite.enablePromise(true)
-    // this.db.DEBUG(true)
+    this.sqlLite.DEBUG(true)
   }
 
-  private async getDbInstance() : Promise<any> {
-    return await this.sqlLite.openDatabase({ name: this.dbName })
+  private async getDbInstance() : Promise<SQLiteDatabase> {
+    return await this.sqlLite.openDatabase({
+      name: this.dbName,
+      location: this.location
+    })
   }
 
-  // ERROR ? 
-  closeDB(db: any) : void {
-    db.close()
+  async closeDB(db: SQLiteDatabase) : Promise<{}> {
+    return new Promise((resolve, reject) => {
+      return db.close(resolve, reject)
+    })
   }
 
   async provisionTables() : Promise<void> {
-    const tableData: TableOptions[] = [{
-      name: 'Personas',
-      fields: [{
-        name: 'did',
-        type: 'VARCHAR(20)',
-        options: ['NOT NULL', 'UNIQUE', 'COLLATE NOCASE', 'PRIMARY KEY']
-      }, {
-        name: 'controllingKey',
-        type: 'INTEGER',
-        options: []
-      }, {
-        name: 'FOREIGN KEY(controllingKey)',
-        type: 'REFERENCES Keys(id)',
-        options: []
-      }]
-    }, {
-      name: 'Keys',
-      fields: [{
-        name: 'id',
-        type: 'INTEGER',
-        options: ['PRIMARY KEY', 'NOT NULL', 'UNIQUE']
-      }, {
-        name: 'wif',
-        type: 'VARCHAR(20)',
-        options: ['UNIQUE', 'NOT NULL']
-      }, {
-        name: 'path',
-        type: 'VARCHAR(10)',
-        options: ['NOT NULL']
-      }, {
-        name: 'entropySource',
-        type: 'INTEGER',
-        options: []
-      }, {
-        name: 'algorithm',
-        type: 'TEXT',
-        options: ['NOT NULL']
-      }, {
-        name: 'FOREIGN KEY(entropySource)',
-        type: 'REFERENCES MasterKeys(id)',
-        options: []
-      }]
-    }]
-
+    const tableData: TableOptions[] = dbHelper.defaultTables
     const db = await this.getDbInstance()
-    //TODO: error handling for opening DB
-    const creationStatuses = await Promise.all(
-      tableData.map(async t =>
-        await this.createTable(t, db)
-      )
-    )
 
-    const finished = creationStatuses.every(result => result)
-
-    //TODO: better error handling here
-    finished ? this.closeDB(db) : console.log ('error in provisioning database')
+    await Promise.all(tableData.map(async t => await this.createTable(t, db)))
+    await this.closeDB(db)
   }
 
-  private async createTable(options : TableOptions, db: any) : Promise<boolean> {
-    const query = this.assembleCreateTableQuery(options)
-    return await this.executeQuery(db, query)
+  private async createTable(options : TableOptions, db: SQLiteDatabase) : Promise<void> {
+    const query = dbHelper.assembleCreateTableQuery(options)
+    await this.executeQuery(db, query)
   }
 
-  private assembleCreateTableQuery(options: TableOptions) : string {
+  private async executeQuery(db: SQLiteDatabase, query: string) : Promise<ResultSet> {
+    return await db.transaction(async (tx: asyncTransaction) => {
+      const response = await tx.executeSql(query, [])
+      return response.result
+    })
+  }
+}
+
+const dbHelper = {
+  defaultTables: [{
+    name: 'Personas',
+    fields: [{
+      name: 'did',
+      type: 'VARCHAR(20)',
+      options: ['NOT NULL', 'UNIQUE', 'COLLATE NOCASE', 'PRIMARY KEY']
+    }, {
+      name: 'controllingKey',
+      type: 'INTEGER',
+      options: []
+    }, {
+      name: 'FOREIGN KEY(controllingKey)',
+      type: 'REFERENCES Keys(id)',
+      options: []
+    }]
+  }, {
+    name: 'Keys',
+    fields: [{
+      name: 'id',
+      type: 'INTEGER',
+      options: ['PRIMARY KEY', 'NOT NULL', 'UNIQUE']
+    }, {
+      name: 'wif',
+      type: 'VARCHAR(20)',
+      options: ['UNIQUE', 'NOT NULL']
+    }, {
+      name: 'path',
+      type: 'VARCHAR(10)',
+      options: ['NOT NULL']
+    }, {
+      name: 'entropySource',
+      type: 'INTEGER',
+      options: []
+    }, {
+      name: 'algorithm',
+      type: 'TEXT',
+      options: ['NOT NULL']
+    }, {
+      name: 'FOREIGN KEY(entropySource)',
+      type: 'REFERENCES MasterKeys(id)',
+      options: []
+    }]
+  }],
+
+  assembleCreateTableQuery: (options: TableOptions) : string => {
     const { name, fields } = options
     const st = `CREATE TABLE IF NOT EXISTS ${name}`
 
@@ -108,15 +124,5 @@ export class Storage {
     }).join(', ')
     return `${st} (${fieldSt})`
   }
-
-  private async executeQuery(db: any, query: string) : Promise<boolean> {
-    const successCB = () => true
-    const errorCB = (err: Error) => {
-      throw new Error(`Query execution failed: ${err.message}`)
-    }
-
-    return await db.transaction((tx: any) => 
-      tx.executeSql(query, errorCB, successCB)
-    )
-  }
 }
+
