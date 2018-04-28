da432e09181d10642453a2f1338fbf265e07decd
nataschaberg <natalja.wittenberg@gmx.de>
2018-04-28 15:55:41 +0200

    style sheet adjusted to new design & injected in app

--- src/styles/jolocom-theme.ts
+++ src/styles/jolocom-theme.ts
@@ -1,127 +1,164 @@
-const { lime, white, grey } = require('material-colors')
+const { white, grey } = require('material-colors')
 
 const palette = {
   primaryColor: '#942f51',
   primary1Color: '#b3c90f',
   primary2Color: '#b3c90f',
   primary3Color: '#b3c90f',
+  accentColor: '#05050d',
   accent1Color: '#942f51',
   accent2Color: '#942f51',
   accent3Color: '#942f51',
   textColor: '#4b132b',
-  textColor_grey: '#a4a4a3',
-  textColor_silverGrey: '#9ba0aa',
-  textColor_darkBrown75: '#633c38',
-  lighterTextColor: '#4b132b',
-  alternateTextColor: white,
   canvasColor: white,
   borderColor: grey['300'],
-  disabledColor: '#9ba0aa'
+  disabledColor: '#9ba0aa',
+  primaryColorBlack: '#05050d',
+  primaryColorSand: '#ffefdf',
+  primaryColorGrey: '#fafafa',
+  primaryColorWhite: 'white',
+  backgroundContainer: '#f5f5f5',
+  backgroundCard: '#fafafa',
+
+  // TODO: remove after ui refactor
+  textColor_grey: '#a4a4a3',
+  textColor_silverGrey: '#9ba0aa',
+  textColor_darkBrown75: '#633c38'
 }
 
+// dark: assumes dark background; light analog
 const textStyles = {
+  dark: {
+    appHeader: {
+      fontSize: 22,
+      color: palette.primaryColorWhite
+    },
+    tabHeader: {
+      active: {
+        fontSize: 20,
+        color: palette.primaryColorSand
+      },
+      inactive: {
+        fontSize: 20,
+        color: palette.primaryColorGrey
+      }
+    },
+    labelInputField: {
+      fontSize: 14,
+      color: palette.primaryColorGrey
+    },
+    textInputField: {
+      fontSize: 18,
+      color: palette.primaryColorWhite
+    },
+    header: {
+      fontSize: 34,
+      color: palette.primaryColorWhite
+    },
+    subheader: {
+      fontSize: 30,
+      color: palette.primaryColorSand
+    },
+    textL: {
+      fontSize: 20,
+      color: palette.primaryColorSand
+    },
+    textM: {
+      fontSize: 18,
+      color: palette.primaryColorSand
+    },
+    textS: {
+      fontSize: 14,
+      color: palette.primaryColorSand
+    }
+  },
+  light: {
+    labelDisplayField: {
+      fontSize: 16,
+      color: palette.primaryColor
+    },
+    textDisplayField: {
+      fontSize: 20,
+      color: palette.primaryColorBlack
+    }
+  },
+  // old styles TODO: remove when finished with refactor ui
   headline: {
-    fontSize: 24,
-    fontStyle: 'normal',
-    fontWeight: '200',
-    color: palette.textColor_grey
+     fontSize: 24,
+     fontStyle: 'normal',
+     fontWeight: '200',
+     color: palette.textColor_grey
   },
-  subheadline: {
-    fontSize: 20,
-    fontStyle: 'normal',
-    fontWeight: '200',
-    color: palette.textColor_grey
+   subheadline: {
+     fontSize: 20,
+     fontStyle: 'normal',
+     fontWeight: '200',
+     color: palette.textColor_grey
   },
-  sectionheader: {
-    fontSize: 20,
-    fontStyle: 'normal',
-    fontWeight: '400',
-    color: palette.textColor
-  },
-  labelInputFields: {
-    fontSize: 13,
-    fontStyle: 'normal',
-    fontWeight: '300',
-    color: palette.textColor_darkBrown75
-  },
-  inputFields: {
-    fontSize: 16,
-    fontStyle: 'normal',
-    fontWeight: '400',
-    color: palette.textColor_silverGrey
-  },
-  contentInputFields: {
-    fontSize: 16,
-    fontStyle: 'normal',
-    fontWeight: '400',
-    color: palette.textColor
-  },
-  textCopy: {
-    fontSize: 13,
-    fontStyle: 'normal',
-    fontWeight: '300',
-    color: palette.textColor_grey
-  },
-  labelButton: {
-    fontSize: '14px',
-    fontStyle: 'normal',
-    fontWeight: '500',
-    color: palette.accent1Color,
-    textTransform: 'uppercase'
-  },
-  screenHeader: {
-    fontSize: '20px',
-    fontStyle: 'normal',
-    fontWeight: '500',
-    color: palette.textColor
-  },
-  userName: {
-    fontSize: '28px',
-    fontStyle: 'normal',
-    fontWeight: '400',
-    color: palette.textColor
+   sectionheader: {
+     fontSize: 20,
+     fontStyle: 'normal',
+     fontWeight: '400',
+     color: palette.textColor
+   },
+   labelInputFields: {
+     fontSize: 13,
+     fontStyle: 'normal',
+     fontWeight: '300',
+     color: palette.textColor_darkBrown75
+   },
+   inputFields: {
+     fontSize: 16,
+     fontStyle: 'normal',
+     fontWeight: '400',
+     color: palette.textColor_silverGrey
+   },
+   contentInputFields: {
+     fontSize: 16,
+     fontStyle: 'normal',
+     fontWeight: '400',
+     color: palette.textColor
+   },
+   textCopy: {
+     fontSize: 13,
+     fontStyle: 'normal',
+     fontWeight: '300',
+     color: palette.textColor_grey
+   },
+   labelButton: {
+     fontSize: '14px',
+     fontStyle: 'normal',
+     fontWeight: '500',
+     color: palette.accent1Color,
+     textTransform: 'uppercase'
+   },
+   screenHeader: {
+     fontSize: '20px',
+     fontStyle: 'normal',
+     fontWeight: '500',
+     color: palette.textColor
+   },
+   userName: {
+     fontSize: '28px',
+     fontStyle: 'normal',
+     fontWeight: '400',
+     color: palette.textColor
   }
 }
 
 export const JolocomTheme = {
-
-  primaryColorBlack: 'black',
-  primaryColorSand: '#ffefdf',
-  primaryColorGrey: '#fafafa',
-  primaryColorWhite: 'white',
-
-  labelFontSize: 20,
-  headerFontSize: 22,
-
-
   contentFontFamily: 'Roboto, sans-serif',
   palette,
   textStyles,
-  appBar: {
-    color: '#f8f9fb',
-    textColor: '#4b132b'
-  },
-  // Used for actionable modals, like `add node`, `link node`
-  actionAppBar: {
-    color: palette.primary1Color,
-    textColor: '#ffffff'
-  },
-  inkBar: {
-    backgroundColor: '#b3c90f'
-  },
-  tabs: {
-    backgroundColor: '#f8f9fb',
-    selectedTextColor: '#4b132b',
-    textColor: '#4b132b'
-  },
-  textField: {
-    focusColor: lime['700']
-  },
-  jolocom: {
-    gray1: '#9ba0aa',
-    gray2: '#c3c6cc',
-    gray3: '#e1e2e5',
-    gray4: '#f8f9fb',
-    gray5: '#f7f7f7'
+  button: {
+    container: {
+      backgroundColor: palette.primaryColor,
+      borderRadius: 4,
+      margin: 0,
+      paddingLeft: 32,
+      paddingRight: 32,
+      paddingBottom: 16,
+      paddingTop: 12
+    }
   }
 }
