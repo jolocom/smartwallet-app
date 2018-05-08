8abc897eac0cf8142bb083a42f32c8841dd74bfd
Exulansis <rusu.eugeniu@gmx.de>
2018-05-08 13:44:06 +0200

    Creating the required tables, mvp.

--- src/lib/dbHelper.ts
+++ src/lib/dbHelper.ts
@@ -28,57 +28,162 @@ export interface AssembledQuery {
   values?: string[]
 }
 
+const PersonaTable = {
+  name: 'Personas',
+  fields: [{
+    name: 'did',
+    type: 'VARCHAR(75)',
+    options: ['PRIMARY KEY', 'NOT NULL']
+  }, {
+    name: 'controllingKey',
+    type: 'VARCHAR(110)',
+    options: ['NOT NULL', 'UNIQUE']
+  }, {
+    name: 'FOREIGN KEY(controllingKey)',
+    type: 'REFERENCES Keys(encryptedWif)',
+    options: []
+  }]
+}
+
+// TODO Unique on entr src + algo + path
+const KeysTable = {
+  name: 'Keys',
+  fields: [{
+    name: 'encryptedWif',
+    type: 'VARCHAR(110)',
+    options: ['PRIMARY KEY', 'NOT NULL']
+  }, {
+    name: 'path',
+    type: 'TEXT',
+    options: ['NOT NULL']
+  }, {
+    name: 'entropySource',
+    type: 'VARCHAR(100)',
+    options: ['NOT NULL']
+  }, {
+    name: 'keyType',
+    type: 'TEXT',
+    options: ['NOT NULL']
+  }, {
+    name: 'FOREIGN KEY(entropySource)',
+    type: 'REFERENCES MasterKeys(encryptedEntropy)',
+    options: []
+  }]
+}
+
+const MasterKeysTable = {
+  name: 'MasterKeys',
+  fields: [{
+    name: 'encryptedEntropy',
+    type: 'VARCHAR(100)',
+    options: ['PRIMARY KEY', 'NOT NULL']
+  }, {
+    name: 'timestamp',
+    type: 'INTEGER',
+    options: ['NOT NULL']
+  }]
+}
+
+const VerifiableCredentialsTable = {
+  name: 'VerifiableCredentials',
+  fields: [{
+    name: 'context',
+    type: 'TEXT',
+    options: ['NOT NULL']
+  }, {
+    name: 'id',
+    type: 'VARCHAR(50)',
+    options: ['PRIMARY KEY', 'NOT NULL']
+  }, {
+    name: 'type',
+    type: 'TEXT',
+    options: ['NOT NULL']
+  }, {
+    name: 'name',
+    type: 'VARCHAR(20)',
+    options: ['NOT NULL']
+  }, {
+    name: 'issuer',
+    type: 'VARCHAR(75)',
+    options: ['NOT NULL']
+  }, {
+    name: 'issued',
+    type: 'INTEGER',
+    options: ['NOT NULL']
+  }, {
+    name: 'expiry',
+    type: 'INTEGER',
+    options: []
+  }, {
+    name: 'subject',
+    type: 'VARCHAR(75)',
+    options: ['NOT NULL']
+  }, {
+    name: 'FOREIGN KEY(subject)',
+    type: 'REFERENCES Personas(did)',
+    options: []
+  }]
+}
+
+const ClaimsTable = {
+  name: 'Claims',
+  fields: [{
+    name: 'credentialId',
+    type: 'VARCHAR(50)',
+    options: ['NOT NULL']
+  }, {
+    name: 'propertyName',
+    type: 'VARCHAR(50)',
+    options: ['NOT NULL']
+  }, {
+    name: 'encryptedValue',
+    type: 'TEXT',
+    options: ['NOT NULL']
+  }, {
+    name: 'PRIMARY KEY (credentialId, propertyName)',
+    type: '',
+    options: []
+  }, {
+    name: 'UNIQUE (credentialId, propertyName)',
+    type: '',
+    options: []
+  }, {
+    name: 'FOREIGN KEY(credentialId)',
+    type: 'REFERENCES VerifiableCredentials(id)',
+    options: []
+  }]
+}
+
+const SignatureTable = {
+  name: 'Signatures',
+  fields: [{
+    name: 'signatureType',
+    type: 'VARCHAR(20)',
+    options: ['NOT NULL']
+  }, {
+    name: 'signatures',
+    type: 'TEXT',
+    options: ['NOT NULL']
+  }, {
+    name: 'credentialId',
+    type: 'VARCHAR(50)',
+    options: ['PRIMARY KEY', 'NOT NULL']
+  }, {
+    name: 'FOREIGN KEY(credentialId)',
+    type: 'REFERENCES VerifiableCredentials(id)',
+    options: []
+ }]
+}
+
 export const dbHelper = {
-  defaultTables: [{
-    name: 'Personas',
-    fields: [{
-      name: 'did',
-      type: 'VARCHAR(75)',
-      options: ['PRIMARY KEY']
-    }, {
-      name: 'controllingKey',
-      type: 'VARCHAR(110)',
-      options: ['NOT NULL', 'UNIQUE']
-    }, {
-      name: 'FOREIGN KEY(controllingKey)',
-      type: 'REFERENCES Keys(encryptedWif)',
-      options: []
-    }]
-  }, {
-    name: 'Keys',
-    fields: [{
-      name: 'encryptedWif',
-      type: 'VARCHAR(110)',
-      options: ['PRIMARY KEY']
-    }, {
-      name: 'path',
-      type: 'TEXT',
-      options: ['NOT NULL']
-    }, {
-      name: 'entropySource',
-      type: 'VARCHAR(100)',
-      options: ['NOT NULL']
-    }, {
-      name: 'keyType',
-      type: 'TEXT',
-      options: ['NOT NULL']
-    }, {
-      name: 'FOREIGN KEY(entropySource)',
-      type: 'REFERENCES MasterKeys(encryptedEntropy)',
-      options: []
-    }]
-  }, {
-    name: 'MasterKeys',
-    fields: [{
-      name: 'encryptedEntropy',
-      type: 'VARCHAR(100)',
-      options: ['PRIMARY KEY']
-    }, {
-      name: 'timestamp',
-      type: 'DATE',
-      options: ['NOT NULL']
-    }]
-  }],
+  defaultTables: [
+    PersonaTable,
+    KeysTable,
+    MasterKeysTable,
+    ClaimsTable,
+    VerifiableCredentialsTable,
+    SignatureTable
+  ],
 
   createTableQuery: (options: TableOptions) : AssembledQuery => {
     const { name, fields } = options
