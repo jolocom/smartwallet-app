769b262abeb027a4b23fc56b9823e08b95ccd482
Exulansis <rusu.eugeniu@gmx.de>
2018-05-18 14:47:58 +0200

    Remove evergreen tests, updated in accordance with new lib api.

--- tests/actions/account/index.test.ts
+++ tests/actions/account/index.test.ts
@@ -2,9 +2,9 @@ import { accountActions } from 'src/actions/'
 import configureStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 
-describe.only('Account action creators', () => {
+describe('Account action creators', () => {
   const mockStore = configureStore([thunk])({})
-  const mocKGetState = () => {}
+  const mockGetState = () => {}
 
   beforeEach(() => {
     mockStore.clearActions()
@@ -13,12 +13,14 @@ describe.only('Account action creators', () => {
   it('Should correctly handle one existing user identity', async () => {
     const backendMiddleware = {
       storageLib: {
-        getPersonas: jest.fn().mockResolvedValue([{did: 'did:jolo:mock'}])
+        get: {
+          persona: jest.fn().mockResolvedValue([{did: 'did:jolo:mock'}])
+        }
       }
     }
 
     const action = accountActions.checkIdentityExists()
-    await action(mockStore.dispatch, mocKGetState, backendMiddleware)
+    await action(mockStore.dispatch, mockGetState, backendMiddleware)
 
     expect(mockStore.getActions()).toMatchSnapshot()
   })
@@ -26,40 +28,31 @@ describe.only('Account action creators', () => {
   it('Should correctly handle more existing user identites', async () => {
     const backendMiddleware = {
       storageLib: {
-        getPersonas: jest.fn().mockResolvedValue([
-          { did: 'did:jolo:first' },
-          { did: 'did:jolo:second' }
-        ])
+        get: {
+          persona: jest.fn().mockResolvedValue([
+            { did: 'did:jolo:first' },
+            { did: 'did:jolo:second' }
+          ])
+        }
       }
     }
     
     const action = accountActions.checkIdentityExists()
-    await action(mockStore.dispatch, mocKGetState, backendMiddleware)
+    await action(mockStore.dispatch, mockGetState, backendMiddleware)
     expect(mockStore.getActions()).toMatchSnapshot()
   })
 
   it('Should correctly handle an empty personas table', async() => {
     const backendMiddleware = {
       storageLib: {
-        getPersonas: jest.fn().mockResolvedValue([])
+        get: {
+          persona: jest.fn().mockResolvedValue([])
+        }
       }
     }
 
     const action = accountActions.checkIdentityExists()
-    await action(mockStore.dispatch, mocKGetState, backendMiddleware)
-    expect(mockStore.getActions()).toMatchSnapshot()
-  })
-
-  it('Should correctly handle an "no such table" error being thrown', async () => {
-    const mockError = new Error('no such table')
-    const backendMiddleware = {
-      storageLib: {
-        getPersonas: jest.fn().mockRejectedValue(mockError)
-      }
-    }
-
-    const action = accountActions.checkIdentityExists()
-    await action(mockStore.dispatch, mocKGetState, backendMiddleware)
+    await action(mockStore.dispatch, mockGetState, backendMiddleware)
     expect(mockStore.getActions()).toMatchSnapshot()
   })
 
@@ -67,12 +60,14 @@ describe.only('Account action creators', () => {
     const mockError = { message: 'gamma rays have flipped our bits!' }
     const backendMiddleware = {
       storageLib: {
-        getPersonas: jest.fn().mockRejectedValue(mockError)
+        get: {
+          persona: jest.fn().mockRejectedValue(mockError)
+        }
       }
     }
 
     const action = accountActions.checkIdentityExists()
-    await action(mockStore.dispatch, mocKGetState, backendMiddleware)
+    await action(mockStore.dispatch, mockGetState, backendMiddleware)
     expect(mockStore.getActions()).toMatchSnapshot()
   })
 })
