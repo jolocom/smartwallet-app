6f6df3302ebb26541fd5699b65d05d235baf4d6d
charly <fei.charleen@gmail.com>
2018-06-04 14:34:58 +0200

    removed back arrow from UI

--- src/routes.ts
+++ src/routes.ts
@@ -20,7 +20,8 @@ export const HomeRoutes = TabNavigator(
           fontSize: JolocomTheme.labelFontSize 
         },
         headerStyle: { backgroundColor: JolocomTheme.primaryColorBlack },
-        headerTintColor: JolocomTheme.primaryColorWhite
+        headerTintColor: JolocomTheme.primaryColorWhite,
+        headerBackImage: null
       }
     },
     Interactions: {
@@ -34,7 +35,8 @@ export const HomeRoutes = TabNavigator(
         headerStyle: {
           backgroundColor: JolocomTheme.primaryColorBlack,
         },
-        headerTintColor: JolocomTheme.primaryColorWhite
+        headerTintColor: JolocomTheme.primaryColorWhite,
+        headerBackImage: null
       }
     }
   },
