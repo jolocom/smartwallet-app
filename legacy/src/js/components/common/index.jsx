4501ebc59c3c26e845f601977de25bc9a51a70b4
charly <fei.charleen@gmail.com>
2018-06-04 17:03:49 +0200

    removed legacy folder

--- legacy/src/js/components/common/index.jsx
+++
@@ -1,7 +0,0 @@
-export { default as IconServices } from './icon-services'
-export { default as Loading } from './loading'
-export { default as Error } from './error'
-export { default as Spinner } from './spinner'
-export { default as IconCheckmark } from './icon-checkmark'
-export { default as IconEther } from './icon-ether'
-export { default as IconBlockchain } from './icon-blockchain'
--- legacy/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const merge = require('webpack-merge')
-const webpack = require('webpack')
-
-const common = require('./webpack.config.common.js')
-module.exports = merge(common, {
-  devtool: '#source-map',
-  plugins: [ new webpack.HotModuleReplacementPlugin() ]
-})
