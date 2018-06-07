0416910d13576f36b3622821633dfaf217acf0a8
charly <ch4rly@riseup.net>
2018-06-07 14:46:29 +0200

    added SplashScreen.java and updated MainActivity.java, linked assets

--- android/app/src/main/java/com/smartwallet/MainApplication.java
+++ android/app/src/main/java/com/smartwallet/MainApplication.java
@@ -3,6 +3,7 @@ package com.smartwallet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.oblador.keychain.KeychainPackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SplashScreenReactPackage(),
             new VectorIconsPackage(),
             new SvgPackage(),
             new KeychainPackage(),
