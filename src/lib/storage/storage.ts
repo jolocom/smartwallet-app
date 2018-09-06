e531320fe94a4228ab32d65ca41fd0bf437d9311
nataschaberg <natalja.wittenberg@gmx.de>
2018-09-06 15:03:56 +0200

    remove console.log

--- src/lib/storage/storage.ts
+++ src/lib/storage/storage.ts
@@ -124,7 +124,6 @@ export class Storage {
   }
 
   private async storeVClaim(vCred: SignedCredential) : Promise<void> {
-    console.log('store verifiable credential agent', vCred)
     await this.createConnectionIfNeeded()
     const verifiableCredential = VerifiableCredentialEntity.fromVeriableCredential(vCred)
     const signature = SignatureEntity.fromLinkedDataSignature(vCred.getProofSection())
