91730642dc320d8b5affd6130476056679d62b49
nataschaberg <natalja.wittenberg@gmx.de>
2018-10-10 11:49:25 +0200

    tests expanded

--- tests/ui/components/claimDetails.test.tsx
+++ tests/ui/components/claimDetails.test.tsx
@@ -3,17 +3,35 @@ import { shallow } from 'enzyme'
 import { ClaimDetailsComponent } from 'src/ui/home/components/claimDetails'
 
 describe('ClaimDetails component', () => {
+  const COMMON_PROPS = {
+    handleClaimInput: () => {},
+    saveClaim: () => {}
+  }
+
   it('matches the snapshot on render', () => {
-    const props = {
-      handleClaimInput: () => {},
-      saveClaim: () => {},
+    const props = Object.assign({}, COMMON_PROPS {
       selectedClaim: {
         credentialType: 'Email',
         claimData: {
           email: 'test@test.com'
         }
       }
-    }
+    })
+
+    const rendered = shallow(<ClaimDetailsComponent {...props} />)
+    expect(rendered).toMatchSnapshot()
+  })
+
+  it('matches the snapshot on render with multi-line claim', () => {
+    const props = Object.assign({}, COMMON_PROPS {
+      selectedClaim: {
+        credentialType: 'Name',
+        claimData: {
+          givenName: 'natascha',
+          familyName: 'test'
+        }
+      }
+    })
 
     const rendered = shallow(<ClaimDetailsComponent {...props} />)
     expect(rendered).toMatchSnapshot()
