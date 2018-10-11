060c1deaf62cc404b82ad5b721cf38e55b7d9375
charly <ch4rly@riseup.net>
2018-10-11 13:35:54 +0200

    temporary reinsertion of haul config

--- webpack.haul.js
+++ webpack.haul.js
@@ -5,6 +5,11 @@ module.exports = ({ platform }, { module, resolve }) => ({
   module: {
     ...module,
     rules: [{
+        test: /\.js/,
+        exclude: /node_modules\/(?!(base64url|jsonld|rdf-canonize)\/).*/,	
+        loader: 'babel-loader'
+      },
+      {
         test: /\.svg$/,
         exclude: /node_modules/,
         use: [
