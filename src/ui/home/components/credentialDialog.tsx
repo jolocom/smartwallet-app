b5b43764b6fef24ba9e5cb983f60a3ac658469ab
charly <ch4rly@riseup.net>
2018-10-16 14:24:47 +0200

    initial commit on top card component

--- src/ui/home/components/credentialDialog.tsx
+++ src/ui/home/components/credentialDialog.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 import { Container } from 'src/ui/structure'
+import { JolocomTheme } from 'src/styles/jolocom-theme'
 import { DecoratedClaims } from 'src/reducers/account'
 import { ClaimCard } from 'src/ui/sso/components/claimCard'
 import { ReactNode } from 'react-redux'
@@ -12,6 +13,20 @@ interface Props {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#f5f5f5'
+  },
+  firstSection: {
+    flex: 0.3,
+    borderColor: JolocomTheme.secondaryColorSand,
+    borderRadius: 2,
+    backgroundColor: "purple"
+  },
+  firstSectionHeader: {
+    fontSize: JolocomTheme.landingHeaderFontSize,
+    color: JolocomTheme.primaryColorBlack
+  },
+  firstSectionSubheader: {
+    fontSize: JolocomTheme.headerFontSize,
+    color: JolocomTheme.secondaryColorGrey
   }
 })
 
@@ -23,5 +38,10 @@ export const CredentialDialogComponent: React.SFC<Props> = props => {
 }
 
 const renderFirstSection = (props: Props) : ReactNode => {
-  return <View/>
+  console.log(props, 'here are your props')
+  return <View style={styles.firstSection}>
+   <Text style={styles.firstSectionHeader}>{props.credentialToRender.credentialType}</Text>
+   <Text style={styles.firstSectionSubheader}>{props.credentialToRender.id}</Text>
+   {/* expiry date? */}
+  </View>
 }
