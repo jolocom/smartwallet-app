6d0cafcb66c229abb9e682ca04f5152585aaa849
charly <ch4rly@riseup.net>
2018-10-19 18:35:08 +0200

    updated snapshot for Exception Component testing

--- tests/ui/generic/exception.test.tsx
+++ tests/ui/generic/exception.test.tsx
@@ -1,28 +1,24 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { Exception } from 'src/ui/generic/'
+import { ExceptionComponent } from 'src/ui/generic/exception'
 import { StyleSheet } from 'react-native'
 
 describe('Exception screen component', () => {
-  it('Renders correctly when error object is provided', () => {
-    // const mockError = {
-    //   message: 'MOCK BAD ERROR',
-    //   stack: 'MOCK STACK TRACE'
-    // }
+  it('Renders correctly', () => {
 
-    // const props = {
-    //   navigation: {
-    //     state: {
-    //       params: {
-            // errorMessage: mockError.message,
-            // stackTrace: mockError.stack
-    //       }
-    //     }
-    //   }
-    // }
+    const props = {
+      navigation: {
+        state: {
+          // params: {
+          //   errorMessage: mockError.message,
+          //   stackTrace: mockError.stack
+          // }
+        }
+      }
+    }
 
-    // const rendered = shallow(<Exception {...props} />)
-    // expect(rendered).toMatchSnapshot()
+    const rendered = shallow(<ExceptionComponent {...props} />)
+    expect(rendered).toMatchSnapshot()
   })
 
   // it('Renders correctly when no error object is provided', () => {
