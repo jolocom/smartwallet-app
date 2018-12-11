aec411273b35008786942aa8c18ce74b774a9fa6
Daniel Siwek <danielsiwek95@gmail.com>
2018-12-11 16:35:02 +0100

    Add completed tests

--- tests/ui/components/layoutWithNavigationBar.test.tsx
+++ tests/ui/components/layoutWithNavigationBar.test.tsx
@@ -13,36 +13,55 @@ describe('LayoutWithNavigationBar component', () => {
     loading: false,
   }
 
-  it('should display qr code scanner based on state', () => {
-    const layoutComponent = shallow(<LayoutWithNavigationBar {...COMMON_PROPS} />);
-    layoutComponent.setState({ scanning: true });
+  it('should display only qr code scanner based on state', () => {
+    const layoutComponent = shallow((
+      <LayoutWithNavigationBar {...COMMON_PROPS}>
+        <Text>Sample test children</Text>
+      </LayoutWithNavigationBar>
+    ))
+    layoutComponent.setState({ scanning: true })
 
-    expect(layoutComponent.find(QRcodeScanner).length).toBe(1);
+    expect(layoutComponent.find(QRcodeScanner).length).toBe(1)
+    expect(layoutComponent.find(Text).length).toBe(0)
+    expect(layoutComponent).toMatchSnapshot()
   })
 
-  it('should display loading spinner based on state', () => {
-    const layoutComponent = shallow(<LayoutWithNavigationBar {...COMMON_PROPS} />);
-    layoutComponent.setState({ loading: true });
+  it('should display only loading spinner based on state', () => {
+    const layoutComponent = shallow((
+      <LayoutWithNavigationBar {...COMMON_PROPS}>
+        <Text>Sample test children</Text>
+      </LayoutWithNavigationBar>
+    ))
+    layoutComponent.setState({ loading: true })
 
-    expect(layoutComponent.find(LoadingSpinner).length).toBe(1);
+    expect(layoutComponent.find(LoadingSpinner).length).toBe(1)
+    expect(layoutComponent.find(Text).length).toBe(0)
+    expect(layoutComponent).toMatchSnapshot()
   })
 
-  it('should display loading spinner based on props', () => {
+  it('should display only loading spinner based on props', () => {
     const props = Object.assign({}, COMMON_PROPS, {
       loading: true
     })
-    const layoutComponent = shallow(<LayoutWithNavigationBar {...props} />);
+    const layoutComponent = shallow((
+      <LayoutWithNavigationBar {...props}>
+        <Text>Sample test children</Text>
+      </LayoutWithNavigationBar>
+    ))
 
-    expect(layoutComponent.find(LoadingSpinner).length).toBe(1);
+    expect(layoutComponent.find(LoadingSpinner).length).toBe(1)
+    expect(layoutComponent.find(Text).length).toBe(0)
+    expect(layoutComponent).toMatchSnapshot()
   })
 
-  // it('should render children with qr code button in a navigation if no loading and scanning state', () => {
-  //   const layoutComponent = shallow((
-  //     <LayoutWithNavigationBar>
-  //       <Text>Sample test children</Text>
-  //     </LayoutWithNavigationBar>
-  //   ));
+  it('should render children with qr code button in a navigation if no loading and scanning state', () => {
+    const layoutComponent = shallow((
+      <LayoutWithNavigationBar>
+        <Text>Sample test children</Text>
+      </LayoutWithNavigationBar>
+    ))
 
-  //   expect(layoutComponent.find(Text).props().children).toBe('Sample test children')
-  // })
+    expect(layoutComponent.find('Text').children().text()).toBe('Sample test children')
+    expect(layoutComponent).toMatchSnapshot()
+  })
 })
