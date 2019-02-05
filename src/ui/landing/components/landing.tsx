9da745a400429538c83b7610b34bd6def0eff9e5
Sean Li <li.saifahn@gmail.com>
2019-02-05 16:49:15 +0100

    Center landing button text on iOS

--- src/ui/landing/components/landing.tsx
+++ src/ui/landing/components/landing.tsx
@@ -90,6 +90,7 @@ const styles = StyleSheet.create({
     backgroundColor: JolocomTheme.primaryColorPurple
   },
   buttonText: {
+    paddingVertical: 15,
     fontFamily: JolocomTheme.contentFontFamily,
     color: JolocomTheme.primaryColorWhite,
     fontSize: JolocomTheme.headerFontSize,
