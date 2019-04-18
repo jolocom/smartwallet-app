65882836083f5be15a8b14871c61928ad7c16838
VolkerSchiewe <volker.s1994@gmail.com>
2019-04-18 15:52:06 +0200

    adapt loading stages to new step order

--- src/actions/registration/loadingStages.ts
+++ src/actions/registration/loadingStages.ts
@@ -1,16 +1,16 @@
 import I18n from 'src/locales/i18n'
 
 export const createIdentityStages = [
-  I18n.t('Encrypting and storing data locally'),
   I18n.t('Fueling with ether'),
   I18n.t('Registering decentralized identity'),
+  I18n.t('Encrypting and storing data locally'),
   I18n.t('Preparing launch')
 ]
 
 
 export const recoverIdentityStages = [
-  I18n.t('Encrypting and storing data locally'),
   I18n.t('Finding your decentralized identity'),
+  I18n.t('Encrypting and storing data locally'),
   I18n.t('Preparing launch')
 ]
 
