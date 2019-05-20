12e1261dbb6e74bfced4f6edf235bfee6a365e2b
Charles Cunningham <charles.cunningham@outlook.com>
2019-05-20 17:58:45 +0200

    added filtering by type

--- src/lib/filterCreds.ts
+++ src/lib/filterCreds.ts
@@ -14,9 +14,15 @@ const issuerFilter: (did: string) => Filter<SignedCredential> = (issuerDid: stri
     (cred: SignedCredential) =>
     cred.issuer === issuerDid
 
+const typeFilter: (typ: string) => Filter<SignedCredential> = (typ: string) =>
+    (cred: SignedCredential) =>
+    cred.type.includes(typ)
+
 const mostRecentOrder: Ordering<SignedCredential> = (c1: SignedCredential, c2: SignedCredential) =>
     c1.issued.valueOf() - c2.issued.valueOf()
 
+// These are some basic filters, if required they can be removed and the filtering/ordering construction functions can be exposed instead
 export const filters = { filterExpired: (creds: SignedCredential[]) => filterCreds(creds, [filterToTransformation(expiredFilter)]),
                          filterByIssuer: (did: string) => (creds: SignedCredential[]) => filterCreds(creds, [filterToTransformation(issuerFilter(did))]),
+                         filterByType: (typ: string) => (creds: SignedCredential[]) => filterCreds(creds, [filterToTransformation(typeFilter(typ))]),
                          orderByRecent: (creds: SignedCredential[]) => filterCreds(creds, [orderingToTransformation(mostRecentOrder)]) }
