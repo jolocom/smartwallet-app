5de2874e5572b9a1a7f7ea0f32588e47d79f824a
Mina Nagy Zaki <mnzaki@gmail.com>
2019-05-23 10:55:50 +0200

    entropy screen: some comments, and minor optimizations

--- src/ui/registration/components/maskedImage.tsx
+++ src/ui/registration/components/maskedImage.tsx
@@ -4,7 +4,7 @@ import {
   PanResponderInstance,
   GestureResponderEvent,
 } from 'react-native'
-import { Svg, Path, Circle } from 'react-native-svg'
+import { Svg, Path, Circle, CircleProps } from 'react-native-svg'
 import { JolocomTheme } from 'src/styles/jolocom-theme'
 
 interface Props {
@@ -13,12 +13,14 @@ interface Props {
 
 interface State {
   linesPts: number[]
+  linesPtsIdx: number
   prevX: number
   prevY: number
   curX: number
   curY: number
   pathD: string
-  circles: number[][]
+  circles: React.ReactElement<CircleProps>[]
+  circlesN: number
 }
 
 const MIN_DISTANCE_SQ = 50
@@ -30,12 +32,14 @@ export class MaskedImageComponent extends React.Component<Props, State> {
 
   state: State = {
     linesPts: [],
+    linesPtsIdx: 0,
     prevX: 0,
     prevY: 0,
     curX: 0,
     curY: 0,
     pathD: '',
     circles: [],
+    circlesN: 0,
   }
 
   componentWillMount() {
@@ -65,30 +69,60 @@ export class MaskedImageComponent extends React.Component<Props, State> {
   }
 
   private handleDraw = (e: GestureResponderEvent): void => {
-    const { prevX, prevY, linesPts, circles, pathD } = this.state
+    const { prevX, prevY, linesPts, linesPtsIdx, circles, pathD } = this.state
     const curX = Math.floor(e.nativeEvent.locationX), curY = Math.floor(e.nativeEvent.locationY)
 
     this.props.addPoint(curX, curY)
-    const dist_sq = Math.abs(curX - prevX) + Math.abs(curY - prevY)
 
+    const dist_sq = Math.abs(curX - prevX) + Math.abs(curY - prevY)
     if (dist_sq > MIN_DISTANCE_SQ) {
+      // if the new line segment is longer than the min, then
+      // check if it intersects with any previous segment
       for (let i = 0; i < linesPts.length; i += 4) {
-        const intsct = this.getIntersection(
-          linesPts[i],
-          linesPts[i + 1],
-          linesPts[i + 2],
-          linesPts[i + 3],
-          prevX,
-          prevY,
-          curX,
-          curY,
-        )
-        if (intsct) {
-          this.setState({ circles: circles.concat([intsct]) })
+        // get intersection
+        const
+          a = linesPts[i], b = linesPts[i+1],
+          c = linesPts[i+2], d = linesPts[i+3],
+          p = prevX, q = prevY, r = curX, s = curY
+
+        let det, gamma, lambda
+        det = (c - a) * (s - q) - (r - p) * (d - b)
+        if (det === 0) continue
+
+        lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det
+        gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det
+        if (0 < lambda && lambda < 1 && (0 < gamma && gamma < 1)) {
+          const x = a + lambda * (c - a)
+          const y = b + lambda * (d - b)
+
+          // they intersect at (x, y)
+          // so draw a circle at the intersection
+          this.state.circles.push(
+            <Circle
+              key={circles.length}
+              cx={x}
+              cy={y}
+              r="5"
+              fill={JolocomTheme.primaryColorSand}
+            />
+          )
+          this.setState({ circlesN: this.state.circles.length })
+
+          // only 1 intersection per segment
+          break
         }
       }
-      linesPts.push(prevX, prevY, curX, curY)
-      if (linesPts.length > MAX_LINE_PTS) linesPts.splice(0, 4)
+
+      // then add the new line and maintain the line list
+      if (linesPts.length >= MAX_LINE_PTS) {
+        linesPts.splice(linesPtsIdx, 4, prevX, prevY, curX, curY)
+        this.state.linesPtsIdx += 4
+      } else {
+        linesPts.push(prevX, prevY, curX, curY)
+      }
+
+
+      // and finally update state
       Object.assign(this.state, {
         curX,
         curY,
@@ -97,8 +131,11 @@ export class MaskedImageComponent extends React.Component<Props, State> {
         pathD: pathD + `L${curX},${curY} `,
         linesPts,
       })
+      // update the SVG path without re-rendering
       if (this._pathEl) this._pathEl.setNativeProps({ d: this.state.pathD })
     } else {
+      // if the line segment was too short, we don't commit it to state but we
+      // draw it anyway
       Object.assign(this.state, {
         curX,
         curY,
@@ -110,43 +147,18 @@ export class MaskedImageComponent extends React.Component<Props, State> {
     }
   }
 
-  // returns [x, y] for the intersection of (a,b)->(c,d) with (p,q)->(r,s),
-  // or undefined if they don't intersect
-  private getIntersection(
-    a: number,
-    b: number,
-    c: number,
-    d: number,
-    p: number,
-    q: number,
-    r: number,
-    s: number,
-  ): number[] | undefined {
-    let det, gamma, lambda
-    det = (c - a) * (s - q) - (r - p) * (d - b)
-    if (det === 0) return
-
-    lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det
-    gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det
-    if (0 < lambda && lambda < 1 && (0 < gamma && gamma < 1)) {
-      const x = a + lambda * (c - a)
-      const y = b + lambda * (d - b)
-      return [x, y]
-    }
-    return
-  }
-
   shouldComponentUpdate(nextProps: Props, nextState: State) {
-    return nextState.circles.length != this.state.circles.length
+    // optimization: rerender only if circles change
+    // path is set dynamically
+    return nextState.circlesN != this.state.circlesN
   }
 
   render() {
-    // {this.state.pathD + `L${this.state.curX},${this.state.curY}`}
     return (
       <Svg width="100%" height="100%" {...this.panResponder.panHandlers}>
         <Path
           ref={el => this._pathEl = el }
-          d=""
+          d={this.state.pathD}
           fill="none"
           stroke={JolocomTheme.primaryColorSandInactive}
           strokeLinecap="round"
@@ -154,15 +166,7 @@ export class MaskedImageComponent extends React.Component<Props, State> {
           strokeDasharray="1,4%"
           strokeWidth="1%"
         />
-        {this.state.circles.map((c, i) => (
-          <Circle
-            key={i}
-            cx={c[0]}
-            cy={c[1]}
-            r={5}
-            fill={JolocomTheme.primaryColorSand}
-          />
-        ))}
+        { this.state.circles }
       </Svg>
     )
   }
