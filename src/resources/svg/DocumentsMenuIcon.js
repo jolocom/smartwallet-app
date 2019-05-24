b4254b73d5b071b261bd490c50f4c60d06eb9f88
Sean Li <li.saifahn@gmail.com>
2019-05-24 12:18:39 +0200

    Update icon for cross-platform-support

--- src/resources/svg/DocumentsMenuIcon.js
+++ src/resources/svg/DocumentsMenuIcon.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Svg, { G, Rect } from 'react-native-svg'
+import { Platform } from 'react-native'
+
+const fillColor = Platform.OS === 'android' ? '#f5f5f5' : 'black'
 
 const SvgDocumentsMenuIcon = props => (
   <Svg width={28} height={28}>
@@ -7,6 +10,7 @@ const SvgDocumentsMenuIcon = props => (
       transform="translate(0 -2)"
       stroke={props.tintColor}
       strokeWidth={1.6}
+      fill={fillColor}
       fillRule="evenodd"
     >
       <Rect
