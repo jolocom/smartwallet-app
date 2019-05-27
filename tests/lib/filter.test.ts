3f1de75af41d784dea410ff423a3b5d3935c7cb9
Sean Li <li.saifahn@gmail.com>
2019-05-27 17:15:08 +0200

    Add tests

--- tests/lib/filter.test.ts
+++ tests/lib/filter.test.ts
@@ -1,6 +1,7 @@
 import { filters } from './../../src/lib/filterCreds'
-import { testCreds, issuers, types, getTestDecoratedClaims } from './testData/filterTestData'
+import { testCreds, issuers, types, getTestDecoratedClaims, decoratedTypes } from './testData/filterTestData'
 import * as filterDecoratedClaims from 'src/lib/filterDecoratedClaims'
+import { getUiCredentialTypeByType } from 'src/lib/util'
 
 describe('Filtering Credentials', () => {
   it('should filter expired creds', () => {
@@ -54,6 +55,27 @@ describe('Filtering Decorated Claims', () => {
     const now = new Date()
     const filtered = filterDecoratedClaims.filters.filterByExpired(testDecoratedClaims)
     expect(filtered.length).toEqual(testDecoratedClaims.length / 2)
-    expect(filtered.every(cred => cred.expires.valueOf() > now.valueOf()))
+    expect(filtered.every(claim => claim.expires.valueOf() > now.valueOf()))
+  })
+
+  it('should filter by issuer', () => {
+    issuers.forEach(issuer => {
+      expect(
+        filterDecoratedClaims.filters
+          .filterByIssuer(issuer)(testDecoratedClaims)
+          .every(claim => claim.issuer === issuer),
+      )
+    })
+  })
+
+  it('should filter by type', () => {
+    decoratedTypes.forEach(typ => {
+      const credentialType = getUiCredentialTypeByType(typ)
+      expect(
+        filterDecoratedClaims.filters
+          .filterByType(credentialType)(testDecoratedClaims)
+          .every(claim => claim.credentialType === credentialType),
+      )
+    })
   })
 })
