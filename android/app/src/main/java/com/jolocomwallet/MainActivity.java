3a5821f1016aeab99d2dce177146b90f8a005602
Mina Nagy Zaki <mnzaki@gmail.com>
2019-06-11 11:35:12 +0200

    More react-navigation deps
    - react-native-gesture-handler integration in android
    - react-native-reanimated

--- android/app/src/main/java/com/jolocomwallet/MainActivity.java
+++ android/app/src/main/java/com/jolocomwallet/MainActivity.java
@@ -6,6 +6,11 @@ import com.facebook.react.ReactFragmentActivity;
 import org.devio.rn.splashscreen.SplashScreen;
 import android.os.Bundle;
 
+// imports for react-native-gesture-handler
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 public class MainActivity extends ReactFragmentActivity {
 
     /**
@@ -21,5 +26,16 @@ public class MainActivity extends ReactFragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this);
         super.onCreate(savedInstanceState);
+    }
+
+    // override for react-native-gesture-handler
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+          return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
     }
 }
--- android/app/src/main/java/com/jolocomwallet/MainApplication.java
+++ android/app/src/main/java/com/jolocomwallet/MainApplication.java
@@ -3,6 +3,7 @@ package com.jolocomwallet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.apsl.versionnumber.RNVersionNumberPackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReanimatedPackage(),
             new RNScreensPackage(),
             new RNGestureHandlerPackage(),
             new RNVersionNumberPackage(),
