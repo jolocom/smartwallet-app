8cbcafd4ef875ec19d2b9255c1a17bfd5b171068
Exulansis <rusu.eugeniu@gmx.de>
2019-07-04 15:33:41 +0200

    Fixed typings on Landing container tests

--- tests/ui/containers/landing.test.tsx
+++ tests/ui/containers/landing.test.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
-import { LandingContainer, Landing } from 'src/ui/landing/containers/landing'
-import { LandingComponent } from 'src/ui/landing/components/landing'
+import { LandingContainer } from 'src/ui/landing/containers/landing'
 import { LoadingScreen } from 'src/ui/generic'
 import { shallow } from 'enzyme'
-
-import configureStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
 
 describe('landing container', () => {
   it('mounts correctly and matches snapshot if loading', () => {
@@ -23,7 +19,7 @@ describe('landing container', () => {
   })
 
   it('mounts correctly and matches snapshot if loading is finished', () => {
-    const props = {
+    const props: LandingContainer['props'] = {
       startRegistration: jest.fn(),
       loading: false,
     }
@@ -31,25 +27,5 @@ describe('landing container', () => {
     const rendered = shallow(<LandingContainer {...props} />)
     expect(rendered).toMatchSnapshot()
     expect(props.startRegistration).not.toHaveBeenCalled()
-
-    const childWrapper = rendered.find(LandingComponent).dive()
-    childWrapper.instance().props.handleButtonTap()
-    expect(childWrapper).toHaveLength(1)
-  })
-
-  it('correctly connects to redux', () => {
-    const initialState = {
-      account: {
-        loading: {
-          loading: true,
-        },
-      },
-    }
-
-    const mockStore = configureStore([thunk])(initialState)
-    const rendered = shallow(<Landing store={mockStore} />).find(
-      LandingContainer,
-    )
-    expect(rendered.props()).toMatchSnapshot()
   })
 })
