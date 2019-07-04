dd1ac0dcfef5daa8523712c15c5e2d781307bde9
Mina Nagy Zaki <mnzaki@gmail.com>
2019-07-04 11:37:31 +0200

    polyfill for Object.setPrototypeOf
    which is not available in react native js env
    and needed by typeorm and probably others

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
+/**
+ * Object.setPrototypeOf polyfill because typeorm (and possibly others) use it
+ */
+
+// @ts-ignore
+if (!Object.setPrototypeOf) {
+  // @ts-ignore
+  Object.setPrototypeOf = function(obj, proto) {
+    obj.__proto__ = proto
+    return obj
+  }
+}
+
 import { AppRegistry } from 'react-native'
 
 import App from 'src/App'
