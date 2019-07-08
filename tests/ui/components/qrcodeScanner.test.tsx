aca6bf381dffc9b327290f4c3cdb50aeb4534171
Mina Nagy Zaki <mnzaki@gmail.com>
2019-07-08 17:57:04 +0200

    tests/ui/components/qrcodeScanner: use createMockStore; cleanup; snapshots

--- tests/ui/components/qrcodeScanner.test.tsx
+++ tests/ui/components/qrcodeScanner.test.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import { QRcodeScanner } from 'src/ui/generic/qrcodeScanner'
 import { shallow } from 'enzyme'
-import mockCamera from '../../__mocks__/react-native-camera'
 
 describe('QRCodeScanner component', () => {
-  // TODO Is this needed?
-  jest.mock('react-native-camera', () => mockCamera)
-
   it('matches the snapshot with back handler', () => {
     const props: QRcodeScanner['props'] = {
       onScannerSuccess: jest.fn(),
       onScannerCancel: jest.fn(),
-      loading: false
+      loading: false,
+      // @ts-ignore navigation param not needed
+      navigation: null
     }
 
     const rendered = shallow(<QRcodeScanner {...props} />)
