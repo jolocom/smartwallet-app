8edb9b7c455d668cce9bff850edd821442e1d7aa
Exulansis <rusu.eugeniu@gmx.de>
2019-07-11 10:34:36 +0200

    Added usage examples and note about composing

--- src/actions/modifiers.ts
+++ src/actions/modifiers.ts
@@ -3,10 +3,11 @@ import { AnyAction, ThunkAction } from 'src/store'
 import { AppError } from '../lib/errors'
 
 /**
- * Wraps a thunk action with two actions created with loadingAction action creator which are dispatched before and after,
- * regardless of errors
+ * Curried function that wraps a {@link ThunkAction} with two calls to the provided loadingAction
+ * {@link ActionCreator} (called with "true" before the wrapped action is dispatched, and with "false" after)
  * @param loadingAction - An action creator to create the actions to be dispatched before and after the wrappedAction-
  * @param wrappedAction - The thunkAction to be wrapped
+ * @example dispatch(withLoading(toggleLoading)(saveClaims))
  */
 export const withLoading = (loadingAction: ActionCreator<AnyAction>) => (
   wrappedAction: ThunkAction,
@@ -20,10 +21,13 @@ export const withLoading = (loadingAction: ActionCreator<AnyAction>) => (
 }
 
 /**
- * Wraps a thunk action with an error handler to be dispatched on thrown error
- * @param errorHandler - An action creator to be dispatched with the error if an error is thrown
+ * Curried function that wraps a {@link ThunkAction} with the provided error handler to be dispatched on thrown error
+ * @param errorHandler - An {@link ActionCreator} to be dispatched with the error if an error is thrown
  * @param errorModifier - An optional modifier for the error which is thrown, is Identity function by default
- * @param wrappedAction - The thunkaction to be wrapped. If dispatching it throws an error, the errorhandler will be dispatched
+ * @param wrappedAction - The {@link ThunkAction} to be wrapped. If dispatching it throws an error, the errorhandler will be dispatched
+ * @example dispatch(withErrorHandling(showErrorScreen)(saveClaims))
+ * @dev The return value from this modifier can be passed to other modifiers, i.e. modifiers can be composed
+ * e.g. dispatch(withLoading(toggleLoading)(withErrorHandling(showErrorScreen)(saveClaims)))
  */
 export const withErrorHandling = (
   errorHandler: ActionCreator<AnyAction>,
