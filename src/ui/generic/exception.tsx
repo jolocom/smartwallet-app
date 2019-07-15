563febbcc0e47dcae1901721b19879b8f0b803ce
Mina Nagy Zaki <mnzaki@gmail.com>
2019-07-15 12:57:10 +0200

    update exception screen and its test
    use new patters for prop types
    fix some minor issues
    use new createMockStore

--- src/ui/generic/exception.tsx
+++ src/ui/generic/exception.tsx
@@ -4,29 +4,18 @@ import { Button } from 'react-native-material-ui'
 import { navigationActions } from 'src/actions/'
 import { Text, StyleSheet, View, Image } from 'react-native'
 import { JolocomTheme } from 'src/styles/jolocom-theme'
-import { routeList } from 'src/routeList'
 import I18n from 'src/locales/i18n'
-import { AppError, errorTitleMessages } from 'src/lib/errors'
+import { errorTitleMessages } from 'src/lib/errors'
 import { getRandomStringFromArray } from 'src/utils/getRandomStringFromArray'
 import strings from 'src/locales/strings'
 import { ThunkDispatch } from '../../store'
+import { NavigationScreenProps } from 'react-navigation';
 const errorImage = require('src/resources/img/error_image.png')
 
-interface ConnectProps {
-  navigateBack: (
-    routeName: string,
-  ) => ReturnType<typeof navigationActions.navigatorReset>
-}
-
-interface Props extends ConnectProps {
-  navigation: {
-    state: {
-      params: {
-        returnTo: routeList
-        error: Error & AppError
-      }
-    }
-  }
+interface Props
+  extends ReturnType<typeof mapDispatchToProps>,
+    ReturnType<typeof mapStateToProps>,
+    NavigationScreenProps {
   errorTitle?: string
 }
 
@@ -85,44 +74,53 @@ const styles = StyleSheet.create({
   },
 })
 
-export const ExceptionComponent: React.SFC<Props> = (props): JSX.Element => {
-  // TODO: display error code
-  const err = props.navigation.state.params.error
-  const errorTitle =
-    props.errorTitle || getRandomStringFromArray(errorTitleMessages)
-  let errorText = err
-    ? err.message
-    : strings.THERE_WAS_AN_ERROR_WITH_YOUR_REQUEST
-  errorText = I18n.t(errorText) + '.'
-  console.error(err && err.origError ? err.origError : err)
+export class ExceptionComponent extends React.PureComponent<Props> {
+  goBack() {
+    this.props.navigation && this.props.navigation.goBack()
+  }
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.upperContainer}>
-        <Image source={errorImage} style={{ width: 160, height: 160 }} />
-        <View style={styles.textBlock}>
-          <Text style={styles.errorTextHeader}>{I18n.t(errorTitle) + '.'}</Text>
-          <Text numberOfLines={5} style={styles.errorText}>
-            {errorText}
-          </Text>
+  render() {
+    const stateParams = this.props.navigation && this.props.navigation.state && this.props.navigation.state.params;
+    if (!stateParams) return null
+
+    // TODO: display error code
+    const err = stateParams.error
+    const errorTitle =
+      this.props.errorTitle || getRandomStringFromArray(errorTitleMessages)
+    let errorText = err
+      ? err.message
+      : strings.THERE_WAS_AN_ERROR_WITH_YOUR_REQUEST
+    errorText = I18n.t(errorText) + '.'
+    console.error(err && err.origError ? err.origError : err)
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.upperContainer}>
+          <Image source={errorImage} style={{ width: 160, height: 160 }} />
+          <View style={styles.textBlock}>
+            <Text style={styles.errorTextHeader}>
+              {I18n.t(errorTitle) + '.'}
+            </Text>
+            <Text numberOfLines={5} style={styles.errorText}>
+              {errorText}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.buttonBlock}>
+          <Button
+            raised
+            onPress={this.goBack.bind(this)}
+            style={{
+              container: styles.buttonContainer,
+              text: styles.buttonText,
+            }}
+            upperCase={false}
+            text={I18n.t(strings.GO_BACK)}
+          />
         </View>
       </View>
-      <View style={styles.buttonBlock}>
-        <Button
-          raised
-          onPress={() =>
-            props.navigateBack(props.navigation.state.params.returnTo)
-          }
-          style={{
-            container: styles.buttonContainer,
-            text: styles.buttonText,
-          }}
-          upperCase={false}
-          text={I18n.t(strings.GO_BACK)}
-        />
-      </View>
-    </View>
-  )
+    )
+  }
 }
 
 const mapStateToProps = (): {} => ({})
