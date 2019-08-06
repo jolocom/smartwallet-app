df86e5547e180fe8b79e29fc8fb324f9a974fcb8
Sean Li <li.saifahn@gmail.com>
2019-08-06 13:11:20 +0200

    #1397 Add initial colors file

---
+++ src/styles/colors.ts
@@ -0,0 +1,37 @@
+// COLOR NAMES
+
+export const black = 'black' // rgb(0, 0, 0)
+export const black030 = 'rgba(0, 0, 0, 0.3)'
+export const black040 = 'rgba(0, 0, 0, 0.4)'
+export const white = 'white' // rgb(255, 255, 255)
+export const blackMain = 'rgb(5, 5, 13)'
+export const blackMain040 = 'rgba(5, 5, 13, 0.4)'
+export const blackMain050 = 'rgba(5, 5, 13, 0.5)'
+export const sand = 'rgb(255, 222, 188)'
+export const sand025 = 'rgba(255, 222, 188, 0.25)'
+export const sand090 = 'rgb(255, 222, 188, 0.9)'
+export const sandLight = 'rgb(255, 239, 223)'
+export const sandLight040 = 'rgba(255, 239, 223)'
+export const purpleMain = 'rgb(148, 47, 81)'
+export const lightGreyLightest = 'rgb(250, 250, 250)'
+export const lightGreyLighter = 'rgb(245, 245, 245)'
+export const lightGreyLight = 'rgb(242, 242, 242)'
+export const lightGrey = 'rgb(236, 236, 236)'
+export const greyLighter = 'rgb(155, 155, 158)'
+export const greyLight = 'rgb(149, 149, 149)'
+export const grey = 'grey' // rgb(128, 128, 128)
+export const golden = 'rgb(241, 161, 7)'
+
+export const greenMain = 'rgb(40, 165, 45)'
+export const greenFaded060 = 'rgb(233, 239, 221, 0.6)'
+
+// PRESETS
+
+export const mainBackground = blackMain
+export const dotColorActive = sand
+export const dotColorInactive = 'rgb(255, 254, 252)'
+export const spinnerColor = sand090
+export const disabledButtonBackground = lightGreyLighter
+export const disabledButtonText = blackMain050
+export const backUpWarningBg = golden
+export const validTextValid = greenMain
---
+++ src/styles/index.ts
@@ -0,0 +1,3 @@
+import * as Colors from './colors'
+
+export { Colors }
