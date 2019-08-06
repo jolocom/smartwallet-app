42f2b5e4e1eb743ca0ad881b7f880215eb71095c
Sean Li <li.saifahn@gmail.com>
2019-08-06 16:54:22 +0200

    Update seedPhrase screen styling

--- src/ui/registration/components/seedPhrase.tsx
+++ src/ui/registration/components/seedPhrase.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { StyleSheet, View, Text } from 'react-native'
-import { JolocomTheme } from 'src/styles/jolocom-theme'
 import { Container } from 'src/ui/structure/'
 import I18n from 'src/locales/i18n'
 import strings from '../../../locales/strings'
+import { Colors, Typography, Buttons } from 'src/styles'
 
 const { Button } = require('react-native-material-ui')
 
@@ -15,7 +15,7 @@ interface Props {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: JolocomTheme.primaryColorBlack,
+    backgroundColor: Colors.blackMain,
   },
   noteSection: {
     marginTop: 20,
@@ -23,37 +23,29 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   note: {
-    textAlign: 'center',
-    lineHeight: 26,
-    color: JolocomTheme.primaryColorSand,
-    fontSize: JolocomTheme.labelFontSize,
-    fontFamily: JolocomTheme.contentFontFamily,
+    ...Typography.subMainText,
+    ...Typography.centeredText,
+    lineHeight: Typography.subMainText.fontSize + 2,
+    color: Colors.sandLight,
   },
   phraseSection: {
     flex: 1,
   },
   seedPhrase: {
-    textAlign: 'center',
-    color: JolocomTheme.primaryColorWhite,
-    fontSize: 34,
-    fontFamily: JolocomTheme.contentFontFamily,
-    lineHeight: 38,
+    ...Typography.baseFontStyles,
+    ...Typography.centeredText,
+    fontSize: Typography.text3XL,
+    lineHeight: Typography.text3XL + 4,
+    color: Colors.white,
   },
   buttonSection: {
     marginTop: 'auto',
   },
   buttonContainer: {
-    borderRadius: 4,
-    height: 48,
-    paddingHorizontal: 25,
-    backgroundColor: JolocomTheme.primaryColorPurple,
+    ...Buttons.buttonStandardContainer,
   },
   buttonText: {
-    paddingVertical: 15,
-    fontWeight: '100',
-    fontSize: JolocomTheme.headerFontSize,
-    color: JolocomTheme.primaryColorWhite,
-    fontFamily: JolocomTheme.contentFontFamily,
+    ...Buttons.buttonStandardText,
   },
 })
 
