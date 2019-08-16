d1a5eb9eb337d0f3b1a59ccc862f4b395e13f3d3
Sean Li <li.saifahn@gmail.com>
2019-08-16 11:25:00 +0200

    Separate stories into files by components
    Separate files makes it easier to see what each file does. Having
    all components in one file can be a bit messy.

---
+++ src/storybook/stories/issuerCard.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react-native'
+import { IssuerCard } from 'src/ui/documents/components/issuerCard'
+import { IdentitySummary } from 'src/actions/sso/types'
+import { Container } from 'src/ui/structure'
+
+const noPublicProfileSummary: IdentitySummary = {
+  did: 'no-public-profile-summary',
+}
+
+const publicProfileSummary: IdentitySummary = {
+  did: 'public-profile-summary',
+  publicProfile: {
+    name:
+      'This is my name which happens to be really long and tries to overflow!',
+    description: 'This is a description!',
+  },
+}
+
+const completePublicProfileSummary: IdentitySummary = {
+  did: 'public-profile-with-image',
+  publicProfile: {
+    name: 'public-profile-with-image',
+    description: 'where does this description go?',
+    image: 'https://miro.medium.com/fit/c/256/256/1*jbb5WdcAvaY1uVdCjX1XVg.png',
+    url: 'https://jolocom.io',
+  },
+}
+
+storiesOf('IssuerCard', module)
+  .add('with no public profile', () => (
+    <Container>{IssuerCard(noPublicProfileSummary)}</Container>
+  ))
+  .add('with a public profile', () => (
+    <Container>{IssuerCard(publicProfileSummary)}</Container>
+  ))
+  .add('complete public profile', () => (
+    <Container>{IssuerCard(completePublicProfileSummary)}</Container>
+  ))
