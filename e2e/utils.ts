02cacad9b78582775f00f50035dbc208ed5ba6c6
Mina Nagy Zaki <mnzaki@gmail.com>
2019-08-20 19:01:01 +0200

    chore(e2e): add utils.readVisibleText
    until https://github.com/wix/detox/issues/445 is handled

---
+++ e2e/utils.ts
@@ -0,0 +1,28 @@
+import { expect } from 'detox'
+
+// courtsey of https://github.com/wix/detox/issues/445#issuecomment-514801808
+export const readVisibleText = async (testID: string) => {
+  try {
+    await expect(element(by.id(testID))).toHaveText('_you_cant_possible_have_this_text_')
+    throw 'are you kidding me?'
+  } catch (error) {
+    if (device.getPlatform() === 'ios') {
+      const start = `accessibilityLabel was "`
+      const end = '" on '
+      const errorMessage = error.message.toString()
+      const [, restMessage] = errorMessage.split(start)
+      const [label] = restMessage.split(end)
+      return label
+    } else {
+      const start = 'Got:';
+      const end = '}"';
+      const errorMessage = error.message.toString();
+      const [, restMessage] = errorMessage.split(start);
+      const [label] = restMessage.split(end);
+      const value = label.split(',');
+      var combineText = value.find((i: string) => i.includes('text=')).trim();
+      const [, elementText] = combineText.split('=');
+      return elementText;
+    }
+  }
+}
