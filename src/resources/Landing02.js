563d33b25256973c57b045e93d5062e6da106adc
Sean Li <li.saifahn@gmail.com>
2019-08-28 16:32:24 +0200

    Update Landing image files
    These JS files are where the properties of the landing images are defined.
    A preset file has been made to provide the properties so that they can be
    changed in one place instead of manually across each landing file. The
    first image has also been updated to fit SE better.

--- src/resources/Landing02.js
+++ src/resources/Landing02.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { ImageBackground } from 'react-native'
-import I18n, { getI18nImage } from 'src/locales/i18n';
+import { getI18nImage } from 'src/locales/i18n'
+import { Presets } from 'src/styles'
 const image = getI18nImage('02.jpg')
 
-
-export default class Landing00 extends React.PureComponent {
+export default class Landing02 extends React.PureComponent {
   render() {
-    return(
-        <ImageBackground
-          source={ image }
-          style={{width: '100%', height: '100%', position: 'absolute'}}
-        />
+    return (
+      <ImageBackground
+        source={image}
+        style={Presets.landingBackground}
+        imageStyle={Presets.landingBackgroundImage}
+      />
     )
   }
 }
