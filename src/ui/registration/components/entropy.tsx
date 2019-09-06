69f6df4c27a8e1688aedf5c24b390f87d9fa928a
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-09-06 12:13:48 +0200

    POC pulsing animation

--- src/ui/registration/components/entropy.tsx
+++ src/ui/registration/components/entropy.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import React, {useEffect, useRef} from 'react'
+import { StyleSheet, View, Text, Animated } from 'react-native'
 import { Container, JolocomButton } from 'src/ui/structure/'
 import { MaskedImageComponent } from 'src/ui/registration/components/maskedImage'
 import I18n from 'src/locales/i18n'
 import strings from '../../../locales/strings'
 import { Typography, Colors } from 'src/styles'
+const image = require('src/resources/img/splashScreen.png')
 
 interface Props {
   addPoint: (x: number, y: number) => void
@@ -14,10 +15,6 @@ interface Props {
 const styles = StyleSheet.create({
   mainContainer: {
     backgroundColor: Colors.blackMain,
-  },
-  footerButton: {
-    position: 'absolute',
-    bottom: '5%',
   },
   text: {
     ...Typography.subMainText,
@@ -32,15 +29,32 @@ const styles = StyleSheet.create({
   },
 })
 
+const usePulse = () => {
+  const opacity = useRef(new Animated.Value(0)).current
+  
+  const pulse = () => Animated.sequence([
+    Animated.timing(opacity, { toValue: 1 }),
+    Animated.timing(opacity, { toValue: 0 })
+  ]).start(pulse)
+
+  useEffect(() => {
+    const timeout = setTimeout(pulse, 0)
+    return () => clearTimeout(timeout)
+  })
+
+  return opacity
+}
+
 export const EntropyComponent: React.SFC<Props> = props => {
   const { progress, addPoint } = props
+  const opacity = usePulse()
 
   const msg =
     progress === 0
-      ? I18n.t(strings.FOR_SECURITY_PURPOSES_WE_NEED_SOME_RANDOMNESS) +
-        '. ' +
-        I18n.t(strings.PLEASE_TAP_THE_SCREEN_AND_DRAW_ON_IT_RANDOMLY)
-      : `${Math.trunc(progress * 100)} %`
+    ? I18n.t(strings.FOR_SECURITY_PURPOSES_WE_NEED_SOME_RANDOMNESS) +
+      '. ' +
+      I18n.t(strings.PLEASE_TAP_THE_SCREEN_AND_DRAW_ON_IT_RANDOMLY)
+    : `${Math.trunc(progress * 100)} %`
 
   const textStyle = progress === 0 ? styles.text : [styles.text, styles.bigFont]
 
@@ -48,7 +62,11 @@ export const EntropyComponent: React.SFC<Props> = props => {
     <Container style={styles.mainContainer}>
       <Text style={textStyle}>{msg}</Text>
       <View style={{ width: '100%' }}>
-        <MaskedImageComponent disabled={progress === 1} addPoint={addPoint} />
+        {
+          progress === 0
+          ? <Animated.Image style={{ opacity }} source={image} />
+          : <MaskedImageComponent disabled={progress === 1} addPoint={addPoint} />
+        }
       </View>
     </Container>
   )
