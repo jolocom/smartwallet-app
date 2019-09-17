ce37399f30d74bc3d184bffe9f145e9b351315b5
Mina Nagy Zaki <mnzaki@gmail.com>
2019-09-17 13:34:58 +0200

    test(e2e/creation): manual sychronization around hand animation
    because it's an infinite loop animation using callbacks, always keeping
    the UI module busy, so detox need to be told to ignore that.
    
    Spoilers: it still doesn't work because the android
    disableSynchronization support is broken

--- e2e/01_new_user/02_creation.spec.ts
+++ e2e/01_new_user/02_creation.spec.ts
@@ -4,16 +4,21 @@ import { readVisibleText } from 'e2e/utils'
 
 describe('Identity Creation', () => {
   describe('Entropy Screen', () => {
-    let entropyMsg: Detox.DetoxAny
     let progressRegexp = /(\d+) %/
 
     beforeAll(async () => {
+      // we must disable automatic synchronization because of the inifinite loop
+      // animation on the entropy screen
+      await device.disableSynchronization()
       const getStarted = element(by.id('getStarted'))
       await getStarted.tap()
-      entropyMsg = element(by.id('entropyMsg'))
+      // and manually synchronize by waiting for entropyMessage to be visible
+      // NOTE: waitFor polls
+      await waitFor(element(by.id('entropyMsg'))).toBeVisible().withTimeout(2000)
     })
 
     it('should show an entropyMsg help text at first', async () => {
+      const entropyMsg = element(by.id('entropyMsg'))
       await expect(entropyMsg).toBeVisible()
       const text = await readVisibleText('entropyMsg')
       jestExpect(text).not.toMatch(progressRegexp)
@@ -22,17 +27,33 @@ describe('Identity Creation', () => {
     it('should show a percentage of entropy collected on swipe in scratchArea', async () => {
       const scratchArea = element(by.id('scratchArea'))
       await scratchArea.swipe('right', 'slow')
+
+      // at this point the animation is hidden
+      await device.enableSynchronization()
+
       const text = await readVisibleText('entropyMsg')
       jestExpect(text).toMatch(progressRegexp)
 
       const swipeDirs: Detox.Direction[] = ['up', 'down', 'left', 'right']
       for (let progress = 0; progress < 100; ) {
-        const text = await readVisibleText('entropyMsg')
+        let text;
+        try {
+          text = await readVisibleText('entropyMsg')
+        } catch (err) {
+          // if we made enough progress then screen will have changed and
+          // entropyMsg will not be visible
+          if (progress > 90) break
+          else throw err
+        }
         const match = progressRegexp.exec(text)
         jestExpect(match).toHaveLength(2)
         progress = parseInt((match as Array<string>)[1])
         await scratchArea.swipe(swipeDirs[progress % 4], 'fast')
       }
+    })
+
+    it('should navigate home after successful creation', async () => {
+      await waitFor(element(by.id('claimsScreen'))).toBeVisible().withTimeout(30000)
     })
   })
 })
