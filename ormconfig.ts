421d29a0dedd60778397c12d5127b1e5f45e00a8
Mina Nagy Zaki <mnzaki@gmail.com>
2019-09-26 00:40:35 +0200

    refactor(storage): load entities outside of config
    so that we don't have to load typeorm in config.ts,
    and so in tests

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,14 +1,10 @@
-import { entityList } from './src/lib/storage/entities'
-import { Initial1565886000404 } from './src/lib/storage/migration/1565886000404-initial'
-import { ReencryptSeed1567674609659 } from './src/lib/storage/migration/1567674609659-reencrypt-seed'
-
 export default {
   type: 'react-native',
   database: 'LocalSmartWalletData',
   location: 'default',
   logging: ['error', 'query', 'schema'],
-  entities: entityList,
-  migrations: [Initial1565886000404, ReencryptSeed1567674609659],
+  entities: './src/lib/storage/entities/',
+  migrations: './src/lib/storage/migration/',
   migrationsRun: true,
   synchronize: false,
   cli: {
