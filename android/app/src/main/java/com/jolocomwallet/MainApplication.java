13640eeb50d2b173fab8706f847a7899ee507cd5
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-10-09 15:09:04 +0200

    feat(package.json): react native ble module install

--- android/app/src/main/java/com/jolocomwallet/MainApplication.java
+++ android/app/src/main/java/com/jolocomwallet/MainApplication.java
@@ -3,6 +3,7 @@ package com.jolocomwallet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.polidea.reactnativeble.BlePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactcommunity.rnlocalize.RNLocalizePackage;
 import com.swmansion.reanimated.ReanimatedPackage;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BlePackage(),
             new VectorIconsPackage(),
             new RNLocalizePackage(),
             new ReanimatedPackage(),
