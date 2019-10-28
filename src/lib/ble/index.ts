488c4144eee191244607083621c468c37d19627e
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-10-28 17:59:08 +0100

    feat(ble): use generator to capture input, rearrange how dispatch is called
    dispatch is now provided to the connection as a callback to be used when the token is complete

--- src/lib/ble/index.ts
+++ src/lib/ble/index.ts
@@ -2,55 +2,64 @@ import { Device, BleManager } from 'react-native-ble-plx';
 import { JSONWebToken, JWTEncodable } from 'jolocom-lib/js/interactionTokens/JSONWebToken';
 
 export type BleSerialConnectionConfig = {
-    serviceUUID: string,
-    rxUUID: string,
-    txUUID: string,
+  serviceUUID: string,
+  rxUUID: string,
+  txUUID: string,
 }
 
 export interface SerialConnection {
   write: (toWrite: string) => Promise<any>
-  listen: (callback: (line: string) => void) => Subscription
-  close: () => Promise<void>,
-  respond: (token: JSONWebToken<JWTEncodable>) => Promise<any>
 }
 
 // a generator function to be passed in to the listen function as the callback
 // This function will reduce together a token until delimiter and then call a callback function
 // with the result
 const waitForToken = (delimiter: string) =>
-    (callback: (jwt: string) => void) =>
-        function*(received: string) {
-            while (!received.includes(delimiter, -1)) {
-                console.log(`received: ${received}`)
-                received += yield
-            }
-            callback(received.slice(0, received.indexOf(delimiter)))
-        }
+  (callback: (jwt: string) => void) =>
+    function*(received: string) {
+      while (!received.includes(delimiter, -1)) {
+        console.log(`received: ${received}`)
+        received += yield
+      }
+      callback(received.slice(0, received.indexOf(delimiter)))
+    }
 
 export const openSerialConnection = (manager: BleManager) => (
-    d: Device,
-    serialUUIDs: BleSerialConnectionConfig
-): Promise<SerialConnection> => d.isConnected().then(connected => connected
-    ? {
-        write: (toWrite: string) => d.writeCharacteristicWithResponseForService(
-            serialUUIDs.serviceUUID,
-            serialUUIDs.rxUUID,
-            toWrite).catch(console.log),
-        listen: (callback: (line: string) => void) => d.monitorCharacteristicForService(
-            serialUUIDs.serviceUUID,
-            serialUUIDs.txUUID,
-            (error, characteristic) => {
-                if (characteristic && characteristic.value) {
-                    callback(characteristic.value)
-                }
-            }
-        ),
-        close: () => d.cancelConnection().then(_ => manager.destroy()),
-        respond: (token: JSONWebToken<JWTEncodable>) => d.writeCharacteristicWithResponseForService(
-            serialUUIDs.serviceUUID,
-            serialUUIDs.rxUUID,
-            token.encode()
-        ).then(value => d.cancelConnection().then(_ => manager.destroy()))
-    }
-    : reject("Device Not Connected"))
+  d: Device,
+  serialUUIDs: BleSerialConnectionConfig
+) => (onRxDispatch: (send: (token: JSONWebToken<JWTEncodable>) => Promise<any>) =>
+  (e: string) => Promise<any>) =>
+    d.isConnected().then(async connected => {
+      if (!connected) throw new Error("Device not connected")
 
+      const b = waitForToken('\n')(
+        onRxDispatch(
+          (token: JSONWebToken<JWTEncodable>) => d.writeCharacteristicWithResponseForService(
+            serialUUIDs.serviceUUID,
+            serialUUIDs.rxUUID,
+            'henlo\n')//token.encode() + '\n')
+            .then(value => d.cancelConnection())
+            .then(_ => manager.destroy())
+        )
+      )(await d.readCharacteristicForService(
+        serialUUIDs.serviceUUID,
+        serialUUIDs.txUUID
+      ).then(ch => ch.value && Buffer.from(ch.value, 'base64').toString('ascii')) || '')
+
+      d.monitorCharacteristicForService(
+        serialUUIDs.serviceUUID,
+        serialUUIDs.txUUID,
+        (err, characteristic) => {
+          if (err) console.log(err)
+          if (characteristic && characteristic.value)
+            b.next(Buffer.from(characteristic.value, 'base64').toString('ascii'))
+        }
+      )
+
+      return {
+        write: (toWrite: string) => d.writeCharacteristicWithResponseForService(
+          serialUUIDs.serviceUUID,
+          serialUUIDs.rxUUID,
+          toWrite).catch(console.error)
+      }
+    })
--- src/ui/generic/bleScanner.tsx
+++ src/ui/generic/bleScanner.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
-import { StyleSheet, FlatList, Text } from 'react-native'
+import { StyleSheet, FlatList } from 'react-native'
 import { connect } from 'react-redux'
 import { Container, JolocomButton } from 'src/ui/structure'
 import { ThunkDispatch } from 'src/store'
 import { NavigationScreenProps } from 'react-navigation'
 import { Colors } from 'src/styles'
 import { BleManager } from 'react-native-ble-plx'
-import { openSerialConnection, SerialConnection, BleSerialConnectionConfig } from 'src/lib/ble'
+import {
+  openSerialConnection,
+  BleSerialConnectionConfig,
+} from 'src/lib/ble'
 import { showErrorScreen } from 'src/actions/generic'
 import { JolocomLib } from 'jolocom-lib'
 import { AppError, ErrorCode } from 'src/lib/errors'
 import { interactionHandlers } from 'src/lib/storage/interactionTokens'
 import { withLoading, withErrorScreen } from 'src/actions/modifiers'
-import { JSONWebToken, JWTEncodable } from 'jolocom-lib/js/interactionTokens/JSONWebToken';
+import {
+  JSONWebToken,
+  JWTEncodable,
+} from 'jolocom-lib/js/interactionTokens/JSONWebToken'
 
 interface Props
   extends ReturnType<typeof mapDispatchToProps>,
@@ -22,14 +28,12 @@ interface State {
   devices: {
     [id: string]: string
   }
-  connected: SerialConnection | null,
-  rx: string
 }
 
 const serialUUIDs: BleSerialConnectionConfig = {
-  serviceUUID: "6e400001-b5a3-f393-e0a9-e50e24dcca9e",
-  rxUUID: "6e400002-b5a3-f393-e0a9-e50e24dcca9e",
-  txUUID: "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
+  serviceUUID: '6e400001-b5a3-f393-e0a9-e50e24dcca9e',
+  rxUUID: '6e400002-b5a3-f393-e0a9-e50e24dcca9e',
+  txUUID: '6e400003-b5a3-f393-e0a9-e50e24dcca9e',
 }
 
 const styles = StyleSheet.create({
@@ -49,29 +53,28 @@ export class BLECodeScanner extends React.Component<Props, State> {
     super(props)
     this.state = {
       devices: {},
-      connected: null,
-      rx: '',
     }
     this.ble = new BleManager()
-    this.ble.startDeviceScan([serialUUIDs.serviceUUID], null, (error, device) => {
-      if (error) console.log(error.toString())
+    this.ble.startDeviceScan(
+      [serialUUIDs.serviceUUID],
+      null,
+      (error, device) => {
+        if (error) console.log(error.toString())
 
-      if (device && device.name) {
-        this.setState({
-          devices: {
-            ...this.state.devices,
-            [device.id]: device.name,
-          },
-        })
-      }
-    })
+        if (device && device.name) {
+          this.setState({
+            devices: {
+              ...this.state.devices,
+              [device.id]: device.name,
+            },
+          })
+        }
+      },
+    )
   }
 
   componentWillUnmount() {
     if (this.removeFocusListener) this.removeFocusListener()
-    this.state.connected
-      ? this.state.connected.close()
-      : null
   }
 
   onScannerCancel() {
@@ -79,59 +82,38 @@ export class BLECodeScanner extends React.Component<Props, State> {
   }
 
   render() {
-    const rx = this.state.rx
-    if (rx[rx.length - 1] === '\n' && this.state.connected) {
-      const connection = this.state.connected
-      this.props.onScannerSuccess(connection.respond)(rx)
-    }
-
-    if (!this.state.connected) {
-      const devices = this.state.devices
-      return (
-        <React.Fragment>
-          <Container style={styles.container}>
-            <FlatList
-              data={Object.keys(devices).map(id => ({
-                name: devices[id],
-                id,
-              }))}
-              renderItem={({ item }) => (
-                <JolocomButton
-                  text={item.name}
-                  onPress={async () =>
-                    this.ble
-                      .connectToDevice(item.id, { requestMTU: 512 })
-                      .then(device =>
-                        device.discoverAllServicesAndCharacteristics(),
-                      )
-                      .then(device =>
-                        openSerialConnection(this.ble)(device, serialUUIDs),
-                      )
-                      .then(serial => {
-                        this.ble.stopDeviceScan()
-                        this.setState({ connected: serial })
-                        serial.listen(line =>
-                          this.setState({
-                            rx:
-                              this.state.rx +
-                              Buffer.from(line, 'Base64').toString('ascii'),
-                          }),
-                        )
-                      })
-                  }
-                />
-              )}
-            />
-          </Container>
-        </React.Fragment>
-      )
-    } else {
-      return (
-        <React.Fragment>
-          <Text>{this.state.rx}</Text>
-        </React.Fragment>
-      )
-    }
+    const devices = this.state.devices
+    return (
+      <React.Fragment>
+        <Container style={styles.container}>
+          <FlatList
+            data={Object.keys(devices).map(id => ({
+              name: devices[id],
+              id,
+            }))}
+            renderItem={({ item }) => (
+              <JolocomButton
+                text={item.name}
+                onPress={async () =>
+                  this.ble
+                    .connectToDevice(item.id, { requestMTU: 512 })
+                    .then(device => {
+                      this.ble.stopDeviceScan()
+                      return device.discoverAllServicesAndCharacteristics()
+                    })
+                    .then(device =>
+                      openSerialConnection(this.ble)(device, serialUUIDs)(this.props.onScannerSuccess),
+                    )
+                    .then(serial => {
+                      console.log('connected')
+                    })
+                }
+              />
+            )}
+          />
+        </Container>
+      </React.Fragment>
+    )
   }
 }
 
@@ -139,7 +121,7 @@ const mapDispatchToProps = (dispatch: ThunkDispatch) => ({
   onScannerSuccess: (
     send: (token: JSONWebToken<JWTEncodable>) => Promise<any>,
   ) => async (e: string) => {
-    console.log(e)
+    console.log(`dispatching: ${e}`)
     let interactionToken
     try {
       interactionToken = JolocomLib.parse.interactionToken.fromJWT(e)
@@ -152,10 +134,10 @@ const mapDispatchToProps = (dispatch: ThunkDispatch) => ({
     return handler
       ? dispatch(withLoading(withErrorScreen(handler(interactionToken))))
       : dispatch(
-          showErrorScreen(
-            new AppError(ErrorCode.Unknown, new Error('No handler found')),
-          ),
-        )
+        showErrorScreen(
+          new AppError(ErrorCode.Unknown, new Error('No handler found')),
+        ),
+      )
   },
 })
 
