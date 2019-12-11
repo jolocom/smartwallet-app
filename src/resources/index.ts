f542b0b54423ac8a1f02ee9f6041bb056ce8b05f
Cristian Lungu <cristian.lungu@hsrw.org>
2019-12-11 16:34:44 +0100

    refactor(icons): removed old menu icons

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -2,16 +2,11 @@ export const Landing00 = require('src/resources/Landing00.js').default
 export const Landing01 = require('src/resources/Landing01.js').default
 export const Landing02 = require('src/resources/Landing02.js').default
 export const Landing03 = require('src/resources/Landing03.js').default
-export const MoreIcon = require('src/resources/svg/MoreIcon').default
 export const NameIcon = require('src/resources/svg/NameIcon').default
 export const EmailIcon = require('src/resources/svg/EmailIcon').default
 export const PhoneIcon = require('src/resources/svg/PhoneIcon').default
 export const AccessibilityIcon = require('src/resources/svg/AccessibilityIcon')
   .default
-export const IdentityMenuIcon = require('./svg/IdentityMenuIcon').default
-export const DocumentsMenuIcon = require('./svg/DocumentsMenuIcon').default
-export const RecordsMenuIcon = require('./svg/RecordsMenuIcon').default
-export const SettingsMenuIcon = require('./svg/SettingsMenuIcon').default
 export const HandIcon = require('./svg/HandIcon').default
 export const SplashIcon = require('./svg/SplashIcon').default
 export const CheckMarkIcon = require('./svg/CheckmarkIcon').default
