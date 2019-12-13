e92c12fdc5a0e6381bdb1b1a41c4015798b5f064
Mina Nagy Zaki <mnzaki@gmail.com>
2019-12-13 16:58:37 +0100

    fix(actions/consumeInteractionToken): wrap library errors

--- src/actions/sso/consumeInteractionToken.ts
+++ src/actions/sso/consumeInteractionToken.ts
@@ -6,14 +6,48 @@ import { AppError } from '../../lib/errors'
 import ErrorCode from '../../lib/errorCodes'
 import { ThunkAction } from '../../store'
 
+const callHandler = handler => {
+  try {
+    return handler()
+  } catch (e) {
+    if (e.message === 'Signature on token is invalid') {
+      throw new AppError(ErrorCode.InvalidSignature, e)
+    } else if (
+      e.message === 'You are not the intended audience of received token'
+    ) {
+      throw new AppError(ErrorCode.WrongDID, e)
+    } else if (e.message === 'The token nonce does not match the request') {
+      throw new AppError(ErrorCode.WrongNonce, e)
+    } else {
+      throw new AppError(ErrorCode.Unknown, e)
+    }
+  }
+}
+
 export const consumeInteractionToken = (
   jwt: string,
 ): ThunkAction => async dispatch => {
-  const interactionToken = JolocomLib.parse.interactionToken.fromJWT(jwt)
+  let interactionToken
+  try {
+    interactionToken = JolocomLib.parse.interactionToken.fromJWT(jwt)
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      throw new AppError(ErrorCode.ParseJWTFailed, e)
+    } else if (e.message === 'Token expired') {
+      throw new AppError(ErrorCode.TokenExpired, e)
+    } else {
+      throw new AppError(ErrorCode.Unknown, e)
+    }
+  }
+
   const handler = interactionHandlers[interactionToken.interactionType]
 
   return handler
-    ? dispatch(withLoading(withErrorScreen(handler(interactionToken))))
+    ? dispatch(
+        withLoading(
+          withErrorScreen(callHandler(() => handler(interactionToken))),
+        ),
+      )
     : dispatch(
         showErrorScreen(
           new AppError(ErrorCode.Unknown, new Error('No handler found')),
