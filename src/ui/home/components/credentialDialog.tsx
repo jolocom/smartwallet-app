e7ca8aad765631521c9917da3b6a6b802d03fbe8
Cristian Lungu <cristian.lungu@hsrw.org>
2020-01-25 19:51:29 +0100

    feat(credentialreceive): changed the credential receive container

--- src/ui/home/components/credentialDialog.tsx
+++ src/ui/home/components/credentialDialog.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
-import { StyleSheet, ScrollView, View } from 'react-native'
-import { DecoratedClaims } from 'src/reducers/account'
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+} from 'react-native'
 import { DocumentCard } from '../../documents/components/documentCard'
 import { IdentitySummary } from '../../../actions/sso/types'
 import { Colors, Spacing } from 'src/styles'
-import { DocumentDetailsComponent } from 'src/ui/documents/components/documentDetails'
+import { CredentialMetadataSummary } from '../../../lib/storage/storage'
+import { centeredText, fontMain } from '../../../styles/typography'
+import { black065, overflowBlack } from '../../../styles/colors'
 
 interface Props {
-  credentialToRender: DecoratedClaims
-  requester: IdentitySummary
+  requesterSummary: IdentitySummary | null
+  offerMetadata: CredentialMetadataSummary[]
+  onPressDocument: (doc: CredentialMetadataSummary) => void
+  selectedCredentials: CredentialMetadataSummary[]
 }
 
 const styles = StyleSheet.create({
@@ -20,21 +30,73 @@ const styles = StyleSheet.create({
     paddingVertical: Spacing.MD,
     alignItems: 'center',
   },
+  logo: {
+    borderRadius: 35,
+    width: 70,
+    height: 70,
+    margin: 10,
+  },
+  serviceName: {
+    fontFamily: fontMain,
+    fontSize: 28,
+    color: overflowBlack,
+  },
+  description: {
+    fontFamily: fontMain,
+    fontSize: 16,
+    color: black065,
+    marginTop: 10,
+    marginBottom: 4,
+    marginHorizontal: '10%',
+    ...centeredText,
+  },
+  documentWrapper: {
+    alignItems: 'center',
+    padding: 10,
+  },
 })
 
 export const CredentialDialogComponent: React.FC<Props> = (
   props: Props,
 ): JSX.Element => {
-  const { credentialToRender } = props
+  const {
+    requesterSummary,
+    offerMetadata,
+    onPressDocument,
+    selectedCredentials,
+  } = props
+  const publicProfile = requesterSummary && requesterSummary.publicProfile
 
   return (
     <View style={styles.container}>
-      <View style={styles.topSection}>
-        <DocumentCard document={credentialToRender} />
-      </View>
-
-      <ScrollView>
-        <DocumentDetailsComponent document={credentialToRender} />
+      <ScrollView style={{ width: '100%' }}>
+        {publicProfile && (
+          <View style={styles.topSection}>
+            <Image style={styles.logo} source={{ uri: publicProfile.image }} />
+            <Text style={styles.serviceName}>{publicProfile.name}</Text>
+            <Text style={styles.description}>
+              Choose one or more documents provided by this service and we will
+              generate them for you
+            </Text>
+          </View>
+        )}
+        {offerMetadata.map(summary => {
+          const { type, renderInfo } = summary
+          const isSelected = selectedCredentials.includes(summary)
+          return (
+            <TouchableOpacity
+              onPress={() => onPressDocument(summary)}
+              activeOpacity={1}
+              style={styles.documentWrapper}
+            >
+              <DocumentCard
+                selected={isSelected}
+                credentialType={type}
+                renderInfo={renderInfo}
+              />
+            </TouchableOpacity>
+          )
+        })}
       </ScrollView>
     </View>
   )
--- src/ui/home/containers/credentialReceive.tsx
+++ src/ui/home/containers/credentialReceive.tsx
@@ -1,58 +1,119 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
-import { RootState } from 'src/reducers/'
-import { DecoratedClaims } from 'src/reducers/account'
-
-import { saveExternalCredentials } from 'src/actions/account'
 import { CredentialDialogComponent } from '../components/credentialDialog'
-import { cancelReceiving } from 'src/actions/sso'
 import { ButtonSection } from 'src/ui/structure/buttonSectionBottom'
 import { View } from 'react-native'
 import { ThunkDispatch } from '../../../store'
+import { acceptSelectedCredentials } from '../../../actions/sso/credentialOfferRequest'
+import { CredentialMetadataSummary } from '../../../lib/storage/storage'
+import { CredentialOfferResponseSelection } from 'jolocom-lib/js/interactionTokens/interactionTokens.types'
+import { NavigationScreenProp, NavigationState } from 'react-navigation'
+import { CredentialOfferRequest } from 'jolocom-lib/js/interactionTokens/credentialOfferRequest'
+import { IdentitySummary } from '../../../actions/sso/types'
+import { JSONWebToken } from 'jolocom-lib/js/interactionTokens/JSONWebToken'
+import { withErrorScreen, withLoading } from '../../../actions/modifiers'
+import { navigationActions } from '../../../actions'
+import { routeList } from '../../../routeList'
 
-interface Props
-  extends ReturnType<typeof mapDispatchToProps>,
-    ReturnType<typeof mapStateToProps> {}
-
-interface State {
-  toRender: DecoratedClaims
+interface CredentialNavigationParams {
+  credentialOfferRequest: JSONWebToken<CredentialOfferRequest>
+  callbackURL: string
+  requesterSummary: IdentitySummary
+  offerMetadata: CredentialMetadataSummary[]
+  isDeepLink: boolean
 }
 
-export class CredentialsReceiveContainer extends React.Component<Props, State> {
-  public render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <View style={{ flex: 0.9 }}>
-          <CredentialDialogComponent
-            requester={this.props.offeror}
-            credentialToRender={this.props.offer}
-          />
-        </View>
-        <View style={{ flex: 0.1 }}>
-          <ButtonSection
-            confirmText={'Accept'}
-            denyText={'Deny'}
-            handleConfirm={this.props.saveExternalCredentials}
-            handleDeny={this.props.goBack}
-            disabled={false}
-          />
-        </View>
-      </View>
-    )
+interface Props extends ReturnType<typeof mapDispatchToProps> {
+  navigation: NavigationScreenProp<NavigationState, CredentialNavigationParams>
+}
+
+export const CredentialsReceiveContainer = (props: Props) => {
+  const [selected, setSelected] = useState<CredentialMetadataSummary[]>([])
+  const { navigation, acceptSelectedCredentials, goBack } = props
+  const {
+    state: {
+      params: {
+        credentialOfferRequest,
+        requesterSummary,
+        isDeepLink,
+        offerMetadata,
+      },
+    },
+  } = navigation
+
+  const handleConfirm = () => {
+    if (selected.length) {
+      const responseSelection: CredentialOfferResponseSelection[] = selected.map(
+        credential => {
+          return {
+            type: credential.type,
+            // providedInput ???
+          }
+        },
+      )
+      acceptSelectedCredentials(
+        responseSelection,
+        credentialOfferRequest,
+        isDeepLink,
+      )
+    }
   }
-}
 
-const mapStateToProps = (state: RootState) => ({
-  offer: state.account.claims.pendingExternal.offer[0].decoratedClaim,
-  offeror: state.account.claims.pendingExternal.offeror,
-})
+  const onPressDocument = (cred: CredentialMetadataSummary) => {
+    if (selected.includes(cred)) {
+      setSelected(selected.filter(current => current !== cred))
+    } else {
+      setSelected([...selected, cred])
+    }
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 0.9 }}>
+        <CredentialDialogComponent
+          requesterSummary={requesterSummary}
+          offerMetadata={offerMetadata}
+          onPressDocument={onPressDocument}
+          selectedCredentials={selected}
+        />
+      </View>
+      <View style={{ flex: 0.1 }}>
+        <ButtonSection
+          confirmText={'Accept'}
+          denyText={'Deny'}
+          handleConfirm={handleConfirm}
+          handleDeny={goBack}
+          disabled={false}
+        />
+      </View>
+    </View>
+  )
+}
 
 const mapDispatchToProps = (dispatch: ThunkDispatch) => ({
-  saveExternalCredentials: () => dispatch(saveExternalCredentials),
-  goBack: () => dispatch(cancelReceiving),
+  acceptSelectedCredentials: (
+    selected: CredentialOfferResponseSelection[],
+    credentialOfferRequest: JSONWebToken<CredentialOfferRequest>,
+    isDeepLink: boolean,
+  ) =>
+    dispatch(
+      withErrorScreen(
+        withLoading(
+          acceptSelectedCredentials(
+            selected,
+            credentialOfferRequest,
+            isDeepLink,
+          ),
+        ),
+      ),
+    ),
+  goBack: () =>
+    dispatch(
+      navigationActions.navigate({ routeName: routeList.InteractionScreen }),
+    ),
 })
 
 export const CredentialReceive = connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps,
 )(CredentialsReceiveContainer)
