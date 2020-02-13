098392255746bb9d13fd58d9a8876b30b8b4754e
Exulansis <rusu.eugeniu@gmx.de>
2020-02-13 14:21:50 +0200

    tests(databaseMigrations): asserting the DID is unchanged after migrations

--- tests/test_databases/test_migrations.sh
+++ tests/test_databases/test_migrations.sh
@@ -8,7 +8,9 @@
 # As part of it's init code, the repl file will instantiate the Storage class,
 # which will attempt to migrate the test database (by applying all local migrations) 
 # to the database.
+#
+# After that, the ./assert_correct_did.ts code fires.
 
 cp ./tests/test_databases/1.6.empty.sqlite3 ./db.sqlite3;
 cp ./tests/test_databases/local_identity.password.txt ./local_identity.password.txt;
-SMARTWALLET_PASSWORD=$(cat ./local_identity.password.txt) yarn repl 
+yarn repl ./tests/test_databases/assert_correct_did.ts;
