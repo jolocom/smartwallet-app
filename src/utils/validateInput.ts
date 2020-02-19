28a0f8842c8eb24292bba2b14c6493ee0d95cb4c
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-02-19 12:07:58 +0100

    feat(validateinput): add naive phone number validation (only digits and +)

--- src/utils/validateInput.ts
+++ src/utils/validateInput.ts
@@ -1,12 +1,18 @@
 export type InputValidator<T> = (input: T) => boolean
 
+const passesAll = <T>(iv: InputValidator<T>[]) => (value: T) =>
+  iv.map(fn => fn(value)).reduce((p, c) => p && c, true)
+
 const isStringLengthValid = (maxLen: number): InputValidator<string> => (
   input: string,
 ): boolean => input.length < maxLen && input.length >= 0
 
+const isOnlyNumbers: InputValidator<string> = input =>
+  /^\+?(0|[1-9]\d*)$/.test(input)
+
 const isAddressFieldValid = isStringLengthValid(100)
 const isNameFieldValid = isStringLengthValid(100)
-const isPhoneNumberValid = isStringLengthValid(100)
+const isPhoneNumberValid = passesAll([isStringLengthValid(100), isOnlyNumbers])
 const isEmailAddressValid = isStringLengthValid(100)
 
 export const inputFieldValidators: {
