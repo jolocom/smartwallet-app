9cd9aef2ed29ed60b2c68f07deb8bf98f5fc0f86
Exulansis <rusu.eugeniu@gmx.de>
2020-02-19 16:44:52 +0200

    fix(InputSeedPhrase): Moved mnemonic out of container state, using clojure.
    Should make it more difficult for rogue dependencies to read the value at runtime.
    Further commits should devise a POC attack to test against, and add documentation.

--- src/ui/recovery/container/inputSeedPhrase.tsx
+++ src/ui/recovery/container/inputSeedPhrase.tsx
@@ -24,141 +24,140 @@ interface Props
 
 interface State {
   inputValue: string
-  mnemonic: string[]
   isMnemonicValid: boolean
   suggestions: string[]
   markedWord: number
   inputState: WordState
 }
 
-export class InputSeedPhraseContainer extends React.Component<Props, State> {
-  private textInput: TextInput | undefined
-  public state = {
-    inputValue: '',
-    isMnemonicValid: false,
-    suggestions: [] as string[],
-    mnemonic: [] as string[],
-    markedWord: 0, // editing first word
-    inputState: WordState.editing,
-  }
 
-  public componentDidMount(): void {}
-
-  private handleInputChange = (text: string): void => {
-    let matches = [] as string[]
-    if (text.length >= 2) {
-      matches = wordlists.EN.filter((word: string): boolean =>
-        word.startsWith(text.trim()),
-      )
+const ClosureInputSeedPhrase = (mnemonic: string[] = []) => {
+  return class InputSeedPhraseContainer extends React.Component<Props, State> {
+    private textInput: TextInput | undefined
+    public state = {
+      inputValue: '',
+      isMnemonicValid: false,
+      suggestions: [] as string[],
+      markedWord: 0, // editing first word
+      inputState: WordState.editing,
     }
 
-    this.setState({
-      inputValue: text,
-      suggestions: matches.slice(0, 10),
-    })
-  }
+    public componentDidMount(): void {}
 
-  private onDoneButton = async () => {
-    const { suggestions, inputValue } = this.state
-    this.setState({
-      inputState: WordState.loading,
-    })
-    await timeout(500)
-    const matchingWord = suggestions.find(e => e === inputValue.trim())
+    private handleInputChange = (text: string): void => {
+      let matches = [] as string[]
+      if (text.length >= 2) {
+        matches = wordlists.EN.filter((word: string): boolean =>
+          word.startsWith(text.trim()),
+        )
+      }
 
-    if (matchingWord) {
       this.setState({
-        inputState: WordState.valid,
+        inputValue: text,
+        suggestions: matches.slice(0, 10),
+      })
+    }
+
+    private onDoneButton = async () => {
+      const { suggestions, inputValue } = this.state
+      this.setState({
+        inputState: WordState.loading,
       })
       await timeout(500)
-      this.selectWord(matchingWord)
-    } else {
+      const matchingWord = suggestions.find(e => e === inputValue.trim())
+
+      if (matchingWord) {
+        this.setState({
+          inputState: WordState.valid,
+        })
+        await timeout(500)
+        this.selectWord(matchingWord)
+      } else {
+        this.setState({
+          inputState: WordState.wrong,
+        })
+      }
+    }
+
+    private selectWord = (word: string): void => {
+      const { markedWord } = this.state
+      const isLastWord = markedWord === mnemonic.length
+      mnemonic[markedWord] = word
+      const mnemonicValid =
+        mnemonic.length === 12 && validateMnemonic(mnemonic.join(' '))
+      if (mnemonicValid && this.textInput) {
+        this.textInput.blur()
+      }
       this.setState({
-        inputState: WordState.wrong,
+        inputValue: isLastWord ? '' : word,
+        markedWord: isLastWord ? mnemonic.length : markedWord,
+        isMnemonicValid: mnemonicValid,
+        suggestions: [],
+        inputState: WordState.editing,
       })
     }
-  }
 
-  private selectWord = (word: string): void => {
-    const { mnemonic, markedWord } = this.state
-    const isLastWord = markedWord === mnemonic.length
-    mnemonic[markedWord] = word
-    const mnemonicValid =
-      mnemonic.length === 12 && validateMnemonic(mnemonic.join(' '))
-    if (mnemonicValid && this.textInput) {
-      this.textInput.blur()
-    }
-    this.setState({
-      inputValue: isLastWord ? '' : word,
-      mnemonic,
-      markedWord: isLastWord ? mnemonic.length : markedWord,
-      isMnemonicValid: mnemonicValid,
-      suggestions: [],
-      inputState: WordState.editing,
-    })
-  }
+    private changeMarkedWord(next: boolean) {
+      const { markedWord, inputValue } = this.state
 
-  private changeMarkedWord(next: boolean) {
-    const { markedWord, mnemonic, inputValue } = this.state
-
-    let nextWord = markedWord
-    let newValue = inputValue
-    if (newValue && newValue !== mnemonic[markedWord]) newValue = ''
-    else {
-      if (next) {
-        nextWord =
-          markedWord !== mnemonic.length ? markedWord + 1 : mnemonic.length
-      } else {
-        nextWord = markedWord !== 0 ? markedWord - 1 : 0
+      let nextWord = markedWord
+      let newValue = inputValue
+      if (newValue && newValue !== mnemonic[markedWord]) newValue = ''
+      else {
+        if (next) {
+          nextWord =
+            markedWord !== mnemonic.length ? markedWord + 1 : mnemonic.length
+        } else {
+          nextWord = markedWord !== 0 ? markedWord - 1 : 0
+        }
+        newValue = nextWord === mnemonic.length ? '' : mnemonic[nextWord]
       }
-      newValue = nextWord === mnemonic.length ? '' : mnemonic[nextWord]
+      this.setState({
+        inputValue: newValue,
+        markedWord: nextWord,
+      })
+      this.handleInputChange(newValue)
     }
-    this.setState({
-      inputValue: newValue,
-      markedWord: nextWord,
-    })
-    this.handleInputChange(newValue)
-  }
 
-  private nextWord = () => this.changeMarkedWord(true)
-  private previousWord = () => this.changeMarkedWord(false)
+    private nextWord = () => this.changeMarkedWord(true)
+    private previousWord = () => this.changeMarkedWord(false)
 
-  public render(): JSX.Element {
-    const {
-      inputValue,
-      mnemonic,
-      isMnemonicValid,
-      suggestions,
-      markedWord,
-      inputState,
-    } = this.state
+    public render(): JSX.Element {
+      const {
+        inputValue,
+        isMnemonicValid,
+        suggestions,
+        markedWord,
+        inputState,
+      } = this.state
 
-    return (
-      <React.Fragment>
-        <StatusBar />
-        <InputSeedPhraseComponent
-          inputValue={inputValue}
-          mnemonic={mnemonic}
-          isMnemonicValid={isMnemonicValid}
-          suggestions={suggestions}
-          markedWord={markedWord}
-          inputState={inputState}
-          selectWord={this.selectWord}
-          handleTextInput={this.handleInputChange}
-          handleButtonPress={() =>
-            this.props.recoverIdentity(mnemonic.join(' '))
-          }
-          inputRef={ref => {
-            this.textInput = ref
-          }}
-          handleDoneButton={this.onDoneButton}
-          handleNextWord={this.nextWord}
-          handlePreviousWord={this.previousWord}
-          handleBackButton={this.props.goBack}
-          isLoading={this.props.isLoading}
-        />
-      </React.Fragment>
-    )
+      return (
+        <React.Fragment>
+          <StatusBar />
+          <InputSeedPhraseComponent
+            inputValue={inputValue}
+            mnemonic={mnemonic}
+            isMnemonicValid={isMnemonicValid}
+            suggestions={suggestions}
+            markedWord={markedWord}
+            inputState={inputState}
+            selectWord={this.selectWord}
+            handleTextInput={this.handleInputChange}
+            handleButtonPress={() =>
+              this.props.recoverIdentity(mnemonic.join(' '))
+            }
+            inputRef={ref => {
+              this.textInput = ref
+            }}
+            handleDoneButton={this.onDoneButton}
+            handleNextWord={this.nextWord}
+            handlePreviousWord={this.previousWord}
+            handleBackButton={this.props.goBack}
+            isLoading={this.props.isLoading}
+          />
+        </React.Fragment>
+      )
+    }
   }
 }
 
@@ -175,4 +174,4 @@ const mapDispatchToProps = (dispatch: ThunkDispatch) => ({
 export const InputSeedPhrase = connect(
   mapStateToProps,
   mapDispatchToProps,
-)(InputSeedPhraseContainer)
+)(ClosureInputSeedPhrase())
