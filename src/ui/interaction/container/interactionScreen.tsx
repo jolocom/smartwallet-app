jolocom/little-sister

feat(loading): smoother loading, with fades and lots of patience
it waits for things to make sense before pulling the curtain away

also now loading state is moved to the new "generic" reducer, which is a
slightly uncomfortable name for "common" or "app wide" or whatever

src/ui/interaction/container/interactionScreen.tsx
@@ -12,7 +12,7 @@ import { withErrorScreen, withLoading } from '../../../actions/modifiers'
 import { connect } from 'react-redux'
 import { CloseIcon } from '../../../resources'
 import { fontMain, textXXS } from '../../../styles/typography'
-import { navigatorResetHome } from '../../../actions/navigation'
+import { navigateBack } from '../../../actions/navigation'
 import {
   JSONWebToken,
   JWTEncodable,
@@ -53,49 +53,21 @@ interface Props
     NavigationScreenProps {}
 
 const InteractionContainer = (props: Props) => {
-  const [AnimatedOpacity] = useState(new Animated.Value(0))
-
-  /*
-   * NOTE: (Android only) When navigating to the @InteractionScreen, the homepage
-   * (together with the @BottomBar) jumps about 20px up and disrupts
-   * the navigation transition. This happens due to the hiding of the
-   * @StatusBar. One workaround is delaying it, but the "flicker" is still
-   * noticeable.
-   */
-
-  useEffect(() => {
-    Animated.timing(AnimatedOpacity, {
-      toValue: 1,
-      duration: 200,
-      useNativeDriver: true,
-    }).start()
-  }, [])
-
   return (
-    <React.Fragment>
-      <Animated.View
-        style={{
-          backgroundColor: Colors.greyDark,
-          opacity: AnimatedOpacity,
-          flex: 1,
-        }}
-      >
-        <Wrapper withoutSafeArea style={{ backgroundColor: Colors.greyDark }}>
-          {IS_IOS && (
-            <TouchableOpacity
-              onPress={props.navigateHome}
-              style={styles.closeButton}
-            >
-              <CloseIcon />
-            </TouchableOpacity>
-          )}
-          <ScannerContainer
-            navigation={props.navigation}
-            onScannerSuccess={props.onScannerSuccess}
-          />
-        </Wrapper>
-      </Animated.View>
-    </React.Fragment>
+    <Wrapper dark withoutSafeArea>
+      {IS_IOS && (
+        <TouchableOpacity
+          onPress={props.navigateHome}
+          style={styles.closeButton}
+        >
+          <CloseIcon />
+        </TouchableOpacity>
+      )}
+      <ScannerContainer
+        navigation={props.navigation}
+        onScannerSuccess={props.onScannerSuccess}
+      />
+    </Wrapper>
   )
 }
 
@@ -111,7 +83,7 @@ const mapDispatchToProps = (dispatch: ThunkDispatch) => ({
           ),
         )
   },
-  navigateHome: () => dispatch(navigatorResetHome()),
+  navigateHome: () => dispatch(navigateBack()),
 })
 
 export const InteractionScreen = connect(

jolocom/smartwallet-app

src/ui/interaction/container/interactionScreen.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Wrapper } from '../../structure'
-import { Animated, Platform, StyleSheet, TouchableOpacity } from 'react-native'
+import { Platform, StyleSheet, TouchableOpacity } from 'react-native'
 import { ScannerContainer } from './scanner'
-import { NavigationScreenProps } from 'react-navigation'
+import { NavigationInjectedProps } from 'react-navigation'
 import { white } from '../../../styles/colors'
 import { ThunkDispatch } from '../../../store'
 import { connect } from 'react-redux'
 import { CloseIcon } from '../../../resources'
 import { fontMain, textXXS } from '../../../styles/typography'
-import { navigatorResetHome } from '../../../actions/navigation'
+
 import { consumeInteractionToken } from 'src/actions/sso/consumeInteractionToken'
 import { ErrorCode, AppError } from 'src/lib/errors'
 import { showErrorScreen } from 'src/actions/generic'
-import { Colors } from 'src/styles'
+import { navigateBack } from '../../../actions/navigation'
 
 const IS_IOS = Platform.OS === 'ios'
 
@@ -45,52 +45,24 @@ const styles = StyleSheet.create({
 
 interface Props
   extends ReturnType<typeof mapDispatchToProps>,
-    NavigationScreenProps {}
+    NavigationInjectedProps {}
 
 const InteractionContainer = (props: Props) => {
-  const [AnimatedOpacity] = useState(new Animated.Value(0))
-
-  /*
-   * NOTE: (Android only) When navigating to the @InteractionScreen, the homepage
-   * (together with the @BottomBar) jumps about 20px up and disrupts
-   * the navigation transition. This happens due to the hiding of the
-   * @StatusBar. One workaround is delaying it, but the "flicker" is still
-   * noticeable.
-   */
-
-  useEffect(() => {
-    Animated.timing(AnimatedOpacity, {
-      toValue: 1,
-      duration: 200,
-      useNativeDriver: true,
-    }).start()
-  }, [])
-
   return (
-    <React.Fragment>
-      <Animated.View
-        style={{
-          backgroundColor: Colors.greyDark,
-          opacity: AnimatedOpacity,
-          flex: 1,
-        }}
-      >
-        <Wrapper withoutSafeArea style={{ backgroundColor: Colors.greyDark }}>
-          {IS_IOS && (
-            <TouchableOpacity
-              onPress={props.navigateHome}
-              style={styles.closeButton}
-            >
-              <CloseIcon />
-            </TouchableOpacity>
-          )}
-          <ScannerContainer
-            navigation={props.navigation}
-            consumeToken={props.consumeToken}
-          />
-        </Wrapper>
-      </Animated.View>
-    </React.Fragment>
+    <Wrapper dark withoutSafeArea>
+      {IS_IOS && (
+        <TouchableOpacity
+          onPress={props.navigateHome}
+          style={styles.closeButton}
+        >
+          <CloseIcon />
+        </TouchableOpacity>
+      )}
+      <ScannerContainer
+        navigation={props.navigation}
+        onScannerSuccess={props.consumeToken}
+      />
+    </Wrapper>
   )
 }
 
@@ -108,7 +80,6 @@ const localNotificationErrors = [
 ]
 
 const mapDispatchToProps = (dispatch: ThunkDispatch) => ({
-  navigateHome: () => dispatch(navigatorResetHome()),
   consumeToken: async (jwt: string) => {
     try {
       return dispatch(consumeInteractionToken(jwt))
@@ -120,6 +91,7 @@ const mapDispatchToProps = (dispatch: ThunkDispatch) => ({
       }
     }
   },
+  navigateHome: () => dispatch(navigateBack()),
 })
 
 export const InteractionScreen = connect(