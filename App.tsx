8169e532a60c2116d42280d414937e455f51a61f
sbub <svetaibuben@gmail.com>
2020-04-20 20:27:27 +0200

    Initial commit: setting up react native app

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Welcome to the Smart Wallet!</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
