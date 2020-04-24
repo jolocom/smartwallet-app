05f647273146572ee83931ba411ac94c325d5083
mnzaki <mnzaki@gmail.com>
2020-04-24 20:11:57 +0200

    tests(claims): remove outdated tests

--- tests/ui/containers/claims.test.tsx
+++ tests/ui/containers/claims.test.tsx
@@ -10,38 +10,11 @@ describe('Claims container', () => {
   const COMMON_PROPS: ClaimsContainerProps = {
     claimsState: initialState,
     openClaimDetails: jest.fn(),
-    setClaimsForDid: jest.fn(),
     did: '',
   }
 
   it('mounts correctly and matches the snapshot', () => {
-    const setClaimsForDid = jest.fn()
-
-    const props = Object.assign({}, COMMON_PROPS, {
-      setClaimsForDid,
-    })
-
-    const rendered = shallow(<ClaimsContainer {...props} />)
-
-    expect(rendered).toMatchSnapshot()
-    expect(setClaimsForDid).toHaveBeenCalledTimes(1)
-  })
-
-  it('correctly changes scanning to true when qr code scanner is started', () => {
-    const props = Object.assign({}, COMMON_PROPS, {
-      setClaimsForDid: () => {},
-    })
-
-    const rendered = shallow(<ClaimsContainer {...props} />)
-    expect(rendered).toMatchSnapshot()
-  })
-
-  it('correctly changes scanning to false when qr code scanner is canceled', () => {
-    const props = Object.assign({}, COMMON_PROPS, {
-      setClaimsForDid: () => {},
-    })
-
-    const rendered = shallow(<ClaimsContainer {...props} />)
+    const rendered = shallow(<ClaimsContainer {...COMMON_PROPS} />)
     expect(rendered).toMatchSnapshot()
   })
 })
