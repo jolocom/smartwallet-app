4ba15c634eb5ad0cb474de3b3a3ca080f2be8d5a
Cristian Lungu <cristian.lungu@hsrw.org>
2020-04-28 17:54:55 +0200

    feat(entropy): added first section with animations

---
+++ src/screens/LoggedOut/entropy/HandAnimation.tsx
@@ -0,0 +1,63 @@
+import React, { useRef, useEffect } from 'react'
+import { StyleSheet, View, Animated } from 'react-native'
+import { HandIcon, SplashIcon } from '~/assets/svg'
+
+const usePulseForBoth = () => {
+  const handPV = useRef(new Animated.Value(0)).current
+  const splashPV = useRef(new Animated.Value(0)).current
+  const isPulsingRef = useRef(true)
+
+  const pulse = () =>
+    Animated.parallel([
+      //NOTE: these sequences are made of individual time frames, here I'm just
+      // making a list of timings which each move to the next value
+      Animated.sequence(
+        [1, 0, 1, 0, 0, 1, 0].map((n) =>
+          Animated.timing(splashPV, { toValue: n, useNativeDriver: true }),
+        ),
+      ),
+      Animated.sequence(
+        [1, 1, 1, 0, 0, 1, 1].map((n) =>
+          Animated.timing(handPV, { toValue: n, useNativeDriver: true }),
+        ),
+      ),
+    ]).start(() => {
+      isPulsingRef.current && pulse()
+    })
+
+  useEffect(() => {
+    pulse()
+    return () => {
+      isPulsingRef.current = false
+    }
+  })
+
+  return { handPV, splashPV }
+}
+
+export const HandAnimation: React.FC = () => {
+  const { handPV, splashPV } = usePulseForBoth()
+
+  return (
+    <View>
+      <Animated.View style={{ opacity: splashPV, ...styles.splashPosition }}>
+        <SplashIcon />
+      </Animated.View>
+      <Animated.View style={{ opacity: handPV, ...styles.handPosition }}>
+        <HandIcon />
+      </Animated.View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  handPosition: {
+    //NOTE: these values are to place the finger of the hand inside the 'splash'
+    top: -29,
+    left: 8,
+  },
+  splashPosition: {
+    top: -2,
+    left: -8,
+  },
+})
