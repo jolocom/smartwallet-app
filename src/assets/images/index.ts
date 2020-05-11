b5f382625640ee498792d1e1965e5757dd147bdb
Cristian Lungu <cristian.lungu@hsrw.org>
2020-05-11 21:08:23 +0200

    feat(walk): added image resources

---
+++ src/assets/images/index.ts
@@ -0,0 +1,4 @@
+export { default as Walkthrough1 } from './walkthrough1.png'
+export { default as Walkthrough2 } from './walkthrough2.png'
+export { default as Walkthrough3 } from './walkthrough3.png'
+export { default as Walkthrough4 } from './walkthrough4.png'
