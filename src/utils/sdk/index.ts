c8c8760c70cf7beb0eac87b11117fd02d68adbea
Cristian Lungu <cristian.lungu@hsrw.org>
2020-05-19 17:03:55 +0200

    fix(sdk): fixed sdk error where table is not found

--- src/utils/sdk/index.ts
+++ src/utils/sdk/index.ts
@@ -6,6 +6,7 @@ import { KeyChain } from './keychain'
 
 export const initSDK = async () => {
   const connection = await createConnection(typeormConfig)
+  await connection.synchronize()
   const storage = new JolocomTypeormStorage(connection)
   const passwordStore = new KeyChain()
   return new JolocomSDK({ storage, passwordStore })
