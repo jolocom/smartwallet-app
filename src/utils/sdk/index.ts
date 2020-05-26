ce76380838d7a8500e32f8b4550c2e161545452c
Cristian Lungu <cristian.lungu@hsrw.org>
2020-05-26 13:01:28 +0200

    fix(storage): fixed storage when connection already exists

--- src/utils/sdk/index.ts
+++ src/utils/sdk/index.ts
@@ -1,11 +1,21 @@
 import { JolocomSDK } from '@jolocom/sdk'
 import { JolocomTypeormStorage } from '@jolocom/sdk-storage-typeorm'
-import { createConnection } from 'typeorm'
+import { createConnection, getConnection } from 'typeorm'
 import typeormConfig from './ormconfig'
 import { KeyChain } from './keychain'
 
+const initConnection = async () => {
+  let connection
+  try {
+    connection = getConnection()
+  } catch (e) {
+    connection = await createConnection(typeormConfig)
+  }
+  return connection
+}
+
 export const initSDK = async () => {
-  const connection = await createConnection(typeormConfig)
+  const connection = await initConnection()
   await connection.synchronize()
   const storage = new JolocomTypeormStorage(connection)
   const passwordStore = new KeyChain()
