f02878d6df6686f17136b969045d1c7b7f6955e3
Cristian Lungu <cristian.lungu@hsrw.org>
2020-06-18 10:21:36 +0200

    fix(scanner): fixed header on camera screen

--- src/screens/Modals/Interactions/scanner/index.tsx
+++ src/screens/Modals/Interactions/scanner/index.tsx
@@ -9,43 +9,45 @@ import Paragraph from '~/components/Paragraph'
 import Header from '~/components/Header'
 import { Colors } from '~/utils/colors'
 import { strings } from '~/translations/strings'
+import NavigationHeader from '~/components/NavigationHeader'
 
 const Scanner: React.FC = () => {
   const { height } = useWindowDimensions()
   const { permission, handlePlatformPermissions } = useCameraPermissions()
 
-  return (
-    <ScreenContainer isFullscreen navigationHeader>
-      {permission === Results.GRANTED ? (
-        <View style={styles.scannerContainer}>
-          <QRCodeScanner
-            containerStyle={{ position: 'absolute' }}
-            onRead={(e) => console.log(e)}
-            cameraStyle={{ height }}
-          />
+  return permission === Results.GRANTED ? (
+    <ScreenContainer isFullscreen>
+      <View style={styles.scannerContainer}>
+        <View style={{ position: 'absolute', top: 0, zIndex: 10 }}>
+          <NavigationHeader />
         </View>
-      ) : (
-        <View style={styles.permissionContainer}>
-          <Header customStyles={styles.permissionText}>
-            {strings.CAMERA_PERMISSION}
-          </Header>
-          <Paragraph
-            customStyles={{
-              ...styles.permissionParagraph,
-              ...styles.permissionText,
-            }}
-          >
-            {strings.YOU_CANT_SCAN_WITHOUT_MAIN_FUNCTION}
-          </Paragraph>
-          <Btn
-            type={BtnTypes.secondary}
-            customTextStyles={{ color: Colors.activity }}
-            onPress={handlePlatformPermissions}
-          >
-            {strings.TAP_TO_ACTIVATE_CAMERA}
-          </Btn>
-        </View>
-      )}
+        <QRCodeScanner
+          containerStyle={{ position: 'absolute' }}
+          onRead={(e) => console.log(e)}
+          cameraStyle={{ height }}
+        />
+      </View>
+    </ScreenContainer>
+  ) : (
+    <ScreenContainer navigationHeader>
+      <Header customStyles={styles.permissionText}>
+        {strings.CAMERA_PERMISSION}
+      </Header>
+      <Paragraph
+        customStyles={{
+          ...styles.permissionParagraph,
+          ...styles.permissionText,
+        }}
+      >
+        {strings.YOU_CANT_SCAN_WITHOUT_MAIN_FUNCTION}
+      </Paragraph>
+      <Btn
+        type={BtnTypes.secondary}
+        customTextStyles={{ color: Colors.activity }}
+        onPress={handlePlatformPermissions}
+      >
+        {strings.TAP_TO_ACTIVATE_CAMERA}
+      </Btn>
     </ScreenContainer>
   )
 }
@@ -54,10 +56,6 @@ const styles = StyleSheet.create({
   scannerContainer: {
     width: '100%',
     flex: 1,
-  },
-  permissionContainer: {
-    paddingHorizontal: '5%',
-    width: '100%',
   },
   permissionParagraph: {
     paddingVertical: 18,
