e9da549bdb0d29084ebab34ff40310d98dc82e07
Cristian Lungu <cristian.lungu@hsrw.org>
2020-06-22 11:14:40 +0200

    feat(scanner): added torch

--- src/screens/Modals/Interactions/scanner/camera.tsx
+++ src/screens/Modals/Interactions/scanner/camera.tsx
@@ -1,27 +1,54 @@
-import React from 'react'
-import { View, useWindowDimensions, Dimensions, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import {
+  View,
+  useWindowDimensions,
+  Dimensions,
+  StyleSheet,
+  StatusBar,
+  TouchableHighlight,
+} from 'react-native'
 import QRCodeScanner from 'react-native-qrcode-scanner'
+import { RNCamera } from 'react-native-camera'
 
 import ScreenContainer from '~/components/ScreenContainer'
 import Paragraph from '~/components/Paragraph'
 import { Colors } from '~/utils/colors'
 import NavigationHeader from '~/components/NavigationHeader'
 import BP from '~/utils/breakpoints'
+import useDelay from '~/hooks/useDelay'
+import { TorchOnIcon, TorchOffIcon } from '~/assets/svg'
 
 const Camera = () => {
   const { height } = useWindowDimensions()
+  const [renderCamera, setRenderCamera] = useState(false)
+  const [isTorchPressed, setTorchPressed] = useState(false)
+
+  //FIXME: While the camera renders, the Modal transition freezes for a moment.
+  //       Delaying as a temporary fix.
+  useEffect(() => {
+    useDelay(() => setRenderCamera(true), 200)
+  }, [])
 
   return (
     <ScreenContainer isFullscreen>
+      <StatusBar hidden />
       <View style={styles.scannerContainer}>
         <View style={styles.navigationContainer}>
           <NavigationHeader />
         </View>
-        <QRCodeScanner
-          containerStyle={{ position: 'absolute' }}
-          onRead={(e) => console.log(e)}
-          cameraStyle={{ height }}
-        />
+        {renderCamera && (
+          <QRCodeScanner
+            containerStyle={{ position: 'absolute' }}
+            onRead={(e) => console.log(e)}
+            cameraStyle={{ height }}
+            cameraProps={{
+              captureAudio: false,
+              flashMode: isTorchPressed
+                ? RNCamera.Constants.FlashMode.torch
+                : RNCamera.Constants.FlashMode.off,
+            }}
+          />
+        )}
         <View style={styles.topOverlay} />
         <View
           style={{
@@ -41,6 +68,15 @@ const Camera = () => {
           <Paragraph customStyles={{ width: MARKER_SIZE }}>
             It’s all automatic, just place your phone above the code
           </Paragraph>
+          <TouchableHighlight
+            onPressIn={() => setTorchPressed(true)}
+            onPressOut={() => setTorchPressed(false)}
+            activeOpacity={1}
+            underlayColor={'transparent'}
+            style={styles.torch}
+          >
+            {isTorchPressed ? <TorchOnIcon /> : <TorchOffIcon />}
+          </TouchableHighlight>
         </View>
       </View>
     </ScreenContainer>
@@ -92,6 +128,22 @@ const styles = StyleSheet.create({
     height: MARKER_SIZE,
     width: (SCREEN_WIDTH - MARKER_SIZE) / 2,
     backgroundColor: Colors.black65,
+  },
+  torchWrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  torch: {
+    width: 69,
+    height: 69,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: BP({
+      large: 60,
+      medium: 40,
+      small: 20,
+    }),
   },
 })
 
