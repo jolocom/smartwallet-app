f91d599e6d5bd822903be8fa14329c5396134252
Cristian Lungu <cristian.lungu@hsrw.org>
2020-07-10 12:27:00 +0200

    fix(errorboundary): fixed error boundary by wrapping it with SafeAreaProvider

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler'
 import 'crypto'
 import React from 'react'
+import { StatusBar } from 'react-native'
 import { Provider } from 'react-redux'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
@@ -9,28 +10,26 @@ import { ErrorBoundary } from '~/ErrorBoundary'
 import Loader from '~/modals/Loader'
 import Lock from '~/modals/Lock'
 import { SDKContextProvider } from '~/utils/sdk/context'
-
 import configureStore from './configureStore'
 import ActionSheetContainer from '~/components/ActionSheetContainer'
-import { StatusBar } from 'react-native'
 
 const store = configureStore()
 
 const App = () => {
   return (
-    <ErrorBoundary>
-      <Provider store={store}>
-        <StatusBar barStyle="light-content" />
-        <SafeAreaProvider>
+    <SafeAreaProvider>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <StatusBar barStyle="light-content" />
           <SDKContextProvider>
             <ActionSheetContainer />
             <RootNavigation />
           </SDKContextProvider>
           <Loader />
           <Lock />
-        </SafeAreaProvider>
-      </Provider>
-    </ErrorBoundary>
+        </Provider>
+      </ErrorBoundary>
+    </SafeAreaProvider>
   )
 }
 
