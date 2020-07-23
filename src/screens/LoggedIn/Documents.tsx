898c9bf363972d66fc73c95b733fadf4d2edbe1a
sbub <svetaibuben@gmail.com>
2020-07-23 09:45:00 +0200

    Adding hyperlink for interaction description

--- src/screens/LoggedIn/Documents.tsx
+++ src/screens/LoggedIn/Documents.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from 'react'
 
-import ScreenContainer from '~/components/ScreenContainer';
-import Header from '~/components/Header';
+import ScreenContainer from '~/components/ScreenContainer'
+import Header from '~/components/Header'
+import Link from '~/components/Link'
+
+const text =
+  'The https://www.google.com/ is ready to share a scooter with you, unlock to start your ride'
 
 const Documents: React.FC = () => {
   return (
     <ScreenContainer>
-      <Header>Documents</Header>
+      <Link text={text} />
     </ScreenContainer>
-  );
-};
+  )
+}
 
-export default Documents;
+export default Documents
