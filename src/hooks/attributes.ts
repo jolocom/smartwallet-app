34a7ea03a3cf7cc02f0ab2886fb6e4cfd2240070
sbub <svetaibuben@gmail.com>
2020-07-24 13:58:14 +0200

    Getting user attributes from sdk instead of using a mocked data

--- src/hooks/attributes.ts
+++ src/hooks/attributes.ts
@@ -6,21 +6,72 @@ import {
 } from '~/modules/interaction/actions'
 import { AttrKeys } from '~/types/attributes'
 import { getAttributes } from '~/modules/attributes/selectors'
+import { useSDK } from './sdk'
+import { AttrsState, AttributeI } from '~/modules/attributes/types'
+import { CredentialOfferRequest } from 'jolocom-lib/js/interactionTokens/credentialOfferRequest'
+import { Credential } from 'jolocom-lib/js/credentials/credential/credential'
+
+enum AttrTypes {
+  ProofOfEmailCredential = 'ProofOfEmailCredential',
+  ProofOfMobilePhoneNumberCredential = 'ProofOfMobilePhoneNumberCredential',
+  ProofOfNameCredential = 'ProofOfNameCredential',
+}
+
+const ATTR_TYPES = {
+  ProofOfEmailCredential: AttrKeys.email,
+  ProofOfMobilePhoneNumberCredential: AttrKeys.number,
+  ProofOfNameCredential: AttrKeys.name,
+}
+
+type InitialEntryValueT = undefined | AttributeI[]
+interface CredentialI {
+  id: string
+  claim: {
+    [key: string]: string
+  }
+}
+
+const makeAttrEntry = (
+  attrKey: AttrKeys,
+  initialValue: InitialEntryValueT,
+  v: CredentialI,
+) => {
+  let entry: AttributeI = { id: v.id, value: '' }
+  if (attrKey === AttrKeys.name) {
+    entry.value = `${v.claim.givenName} ${v.claim.familyName}`
+  } else if (attrKey === AttrKeys.email) {
+    entry.value = v.claim.email
+  } else if (attrKey === AttrKeys.number) {
+    entry.value = v.claim.number
+  }
+
+  return Array.isArray(initialValue) ? [...initialValue, entry] : [entry]
+}
 
 export const useGetAllAttributes = () => {
   const dispatch = useDispatch()
-  const getAttributes = () => {
+  const sdk = useSDK()
+  const getAttributes = async () => {
     try {
-      const attributes = {
-        name: [
-          { id: 'abc1', value: 'John Smith' },
-          { id: 'abc2', value: 'JSmith' },
-        ],
-        email: [{ id: 'abc3', value: 'johnsmith@example.com' }],
-      }
+      const verifiableCredentials = await sdk.bemw.storageLib.get.verifiableCredential()
+      const attributes = verifiableCredentials.reduce((acc, v) => {
+        if (
+          v.type[1] === AttrTypes.ProofOfNameCredential ||
+          v.type[1] === AttrTypes.ProofOfMobilePhoneNumberCredential ||
+          v.type[1] === AttrTypes.ProofOfEmailCredential
+        ) {
+          const attrType = v.type[1] as AttrTypes
+          const attrKey: AttrKeys = ATTR_TYPES[attrType]
+          const entry = makeAttrEntry(attrKey, acc[attrKey], v)
+
+          acc[attrKey] = entry
+        }
+        return acc
+      }, {} as AttrsState<AttributeI>)
+
       dispatch(setAttrs(attributes))
     } catch (err) {
-      console.warn('Failed getting verifiable credentials')
+      console.warn('Failed getting verifiable credentials', err)
     }
   }
 
