de582a252282adf7492f04dc2942959fd44ae407
Cristian Lungu <cristian.lungu@hsrw.org>
2020-07-30 17:00:32 +0200

    refactor(share): removed unnecessary code

--- src/screens/Modals/Interactions/CredentialShare.tsx
+++ src/screens/Modals/Interactions/CredentialShare.tsx
@@ -1,33 +1,8 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { View } from 'react-native'
-
-import {
-  setIntermediaryState,
-  setAttributeInputKey,
-} from '~/modules/interaction/actions'
-import { IntermediaryState } from '~/modules/interaction/types'
-import InteractionFooter from './InteractionFooter'
-import InteractionHeader from './InteractionHeader'
-import { InteractionSummary } from '@jolocom/sdk/js/src/lib/interactionManager/types'
-import { getInteractionSummary } from '~/modules/interaction/selectors'
-
-const FAS: React.FC = ({ children }) => {
-  const { initiator }: InteractionSummary = useSelector(getInteractionSummary)
-
-  return (
-    <>
-      <InteractionHeader />
-      <View style={{ flex: 1 }}>{children}</View>
-      <InteractionFooter />
-    </>
-  )
-}
+import FasWrapper from '~/components/ActionSheet/FasWrapper'
 
 const CredentialShare = () => {
-  const dispatch = useDispatch()
-
-  return <FAS></FAS>
+  return <FasWrapper onSubmit={() => {}}></FasWrapper>
 }
 
 export default CredentialShare
