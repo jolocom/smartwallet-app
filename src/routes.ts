20e5c27ec5405cb946a975da79dc2420c856a66e
sbub <svetaibuben@gmail.com>
2020-08-20 12:33:15 +0200

    Disabling swiping out Change pin screen

--- src/routes.ts
+++ src/routes.ts
@@ -197,6 +197,9 @@ const MainStack = createStackNavigator(
     },
     [routeList.ChangePIN]: {
       screen: ChangePIN,
+      navigationOptions: {
+        gesturesEnabled: false,
+      },
     },
     [routeList.InteractionScreen]: {
       screen: InteractionScreen,
