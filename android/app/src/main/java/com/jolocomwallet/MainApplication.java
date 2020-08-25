c70e9d22c4c9ab6007095430a217c1410ef47883
sbub <svetaibuben@gmail.com>
2020-08-25 10:10:30 +0200

    Updating modules and linking

--- android/app/src/main/java/com/jolocomwallet/MainApplication.java
+++ android/app/src/main/java/com/jolocomwallet/MainApplication.java
@@ -3,6 +3,7 @@ package com.jolocomwallet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.hieuvp.fingerprint.ReactNativeFingerprintScannerPackage;
 
 import com.levelasquez.androidopensettings.AndroidOpenSettingsPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
@@ -42,6 +43,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeFingerprintScannerPackage(),
             new AndroidOpenSettingsPackage(),
             new AsyncStoragePackage(),
             new RNSentryPackage(),
