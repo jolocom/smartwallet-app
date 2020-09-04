0b1db1263ecf001d12c1dcdfb84706aa5c2c23f5
mnzaki <mnzaki@gmail.com>
2020-09-04 12:20:50 +0200

    fix(registration): change stages; add humanTimeout

--- src/actions/registration/loadingStages.ts
+++ src/actions/registration/loadingStages.ts
@@ -2,8 +2,8 @@ import I18n from 'src/locales/i18n'
 import strings from '../../locales/strings'
 
 export const loadingStages = [
-  I18n.t(strings.ENCRYPTING_AND_STORING_DATA_LOCALLY),
-  I18n.t(strings.FUELING_WITH_ETHER),
   I18n.t(strings.REGISTERING_DECENTRALIZED_IDENTITY),
+  I18n.t(strings.ENCRYPTING_AND_STORING_DATA_LOCALLY),
   I18n.t(strings.PREPARING_LAUNCH),
+  //I18n.t(strings.FUELING_WITH_ETHER),
 ]
