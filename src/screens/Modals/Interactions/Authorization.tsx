1c9251740de7f3fd4a6627d1e1c3fe73f5372fa1
sbub <svetaibuben@gmail.com>
2020-09-08 11:15:36 +0200

    Using guards in Authz flow

--- src/screens/Modals/Interactions/Authorization.tsx
+++ src/screens/Modals/Interactions/Authorization.tsx
@@ -4,26 +4,27 @@ import { Image, View, StyleSheet } from 'react-native'
 import BasWrapper from '~/components/ActionSheet/BasWrapper'
 import { useRootSelector } from '~/hooks/useRootSelector'
 import { getInteractionDetails } from '~/modules/interaction/selectors'
-import { AuthorizationDetailsI } from '~/modules/interaction/types'
+import { isAuthzDetails } from '~/modules/interaction/guards'
 
 const Authorization = () => {
-  const { imageURL } = useRootSelector<AuthorizationDetailsI>(
-    getInteractionDetails,
-  )
-
-  return (
-    <BasWrapper>
-      {imageURL && (
-        <View style={styles.imageWrapper}>
-          <Image
-            source={{ uri: imageURL }}
-            style={styles.image}
-            resizeMode="cover" // it will take max Dimension size (260 - width) and make height based on the aspect ration of actual image size
-          />
-        </View>
-      )}
-    </BasWrapper>
-  )
+  const details = useRootSelector(getInteractionDetails)
+  if (isAuthzDetails(details)) {
+    const { imageURL } = details
+    return (
+      <BasWrapper>
+        {imageURL && (
+          <View style={styles.imageWrapper}>
+            <Image
+              source={{ uri: imageURL }}
+              style={styles.image}
+              resizeMode="cover" // it will take max Dimension size (260 - width) and make height based on the aspect ration of actual image size
+            />
+          </View>
+        )}
+      </BasWrapper>
+    )
+  }
+  return null
 }
 
 const styles = StyleSheet.create({
