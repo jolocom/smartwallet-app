94efaa4d463463edfe71120b9887c431dab967c4
sbub <svetaibuben@gmail.com>
2020-09-10 16:53:59 +0200

    Adding missing backbutton on ClaimDetails screen for ios

--- src/ui/errors/components/navigationSection.tsx
+++ src/ui/errors/components/navigationSection.tsx
@@ -2,27 +2,27 @@ import React from 'react'
 import { TouchableOpacity, View } from 'react-native'
 import { styles } from '../styles'
 import { BackIcon, CrossIcon } from '../../../resources'
+import { Colors } from 'src/ui/deviceauth/colors'
 
 interface Props {
   onNavigation: () => void
   isBackButton: boolean
+  backButtonColor?: Colors
 }
 
 export const NavigationSection = (props: Props) => {
-  const { onNavigation, isBackButton } = props
+  const { onNavigation, isBackButton, backButtonColor } = props
   return (
     <View
       style={{
         ...styles.navigationWrapper,
-      }}
-    >
+      }}>
       {isBackButton ? (
         <TouchableOpacity
           onPress={onNavigation}
           activeOpacity={0.8}
-          style={styles.navigationButton}
-        >
-          <BackIcon />
+          style={styles.navigationButton}>
+          <BackIcon color={backButtonColor} />
         </TouchableOpacity>
       ) : (
         <View />
@@ -31,8 +31,7 @@ export const NavigationSection = (props: Props) => {
         <TouchableOpacity
           onPress={onNavigation}
           activeOpacity={0.8}
-          style={styles.navigationButton}
-        >
+          style={styles.navigationButton}>
           <CrossIcon />
         </TouchableOpacity>
       ) : (
