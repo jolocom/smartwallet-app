327495ced6e7c82b10e7edbe9865c3be85c986e4
sbub <svetaibuben@gmail.com>
2020-09-14 09:29:09 +0200

    Supporting different device resolution, positioning picture and header correctly

--- src/ui/deviceauth/HowToChangePIN.tsx
+++ src/ui/deviceauth/HowToChangePIN.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Image } from 'react-native'
+import { View } from 'react-native'
 import { connect } from 'react-redux'
 
 import I18n from 'src/locales/i18n'
 
 import ScreenContainer from './components/ScreenContainer'
 import Header from './components/Header'
-import Paragraph from './components/Paragraph'
+import Paragraph, { ParagraphSizes } from './components/Paragraph'
 import Btn, { BtnTypes } from './components/Btn'
 import AbsoluteBottom from './components/AbsoluteBottom'
 import strings from 'src/locales/strings'
@@ -17,8 +17,7 @@ import { RootState } from 'src/reducers'
 import { BP } from '../../styles/breakpoints'
 import { accountActions, navigationActions } from 'src/actions'
 import { routeList } from 'src/routeList'
-
-const recoveryInstruction = require('src/resources/img/recoveryInstructions.png')
+import RecoveryInstructions from 'src/resources/svg/ForgotPINInstructions'
 
 interface PropsI {
   handleGoBack: () => void
@@ -34,41 +33,58 @@ const HowToChangePIN: React.FC<PropsI> = ({
         customStyles={{
           backgroundColor: Colors.black,
           justifyContent: 'flex-start',
-          paddingTop: 30,
+          paddingTop: BP({ small: 30, medium: 50, large: 50 }),
           paddingHorizontal: 20,
         }}>
-        <Header color={Colors.white90} customStyles={{ textAlign: 'left' }}>
+        <Header
+          color={Colors.white90}
+          customStyles={{
+            fontSize: BP({ small: 24, medium: 28, large: 28 }),
+            alignSelf: 'flex-start',
+          }}>
           {I18n.t(strings.HOW_TO_CHANGE_PIN)}
         </Header>
         <Paragraph
           color={Colors.white80}
-          customStyles={{ alignSelf: 'flex-start', textAlign: 'left' }}>
+          customStyles={{
+            alignSelf: 'flex-start',
+            textAlign: 'left',
+            lineHeight: 17,
+          }}>
           {I18n.t(strings.WE_ARE_SORRY_THAT_YOU_FORGOT)}
         </Paragraph>
         <Paragraph
           color={Colors.white80}
-          customStyles={{ alignSelf: 'flex-start', textAlign: 'left' }}>
+          customStyles={{
+            alignSelf: 'flex-start',
+            textAlign: 'left',
+            lineHeight: 17,
+            marginTop: 10,
+          }}>
           {I18n.t(strings.YOU_CAN_CHANGE_PIN)}
         </Paragraph>
-
-        <AbsoluteBottom customStyles={{ alignSelf: 'center' }}>
-          <Image
-            source={recoveryInstruction}
-            style={{
-              marginBottom: -100,
-            }}
-          />
+        <View
+          style={{
+            transform: [{ scale: BP({ small: 0.75, medium: 1, large: 1 }) }],
+            position: 'absolute',
+            bottom: BP({ small: -70, medium: -50, large: -50 }),
+          }}>
+          <RecoveryInstructions />
+        </View>
+        <AbsoluteBottom customStyles={{ alignSelf: 'center', bottom: 0 }}>
           <Btn onPress={handleAccessRestore}>
             {I18n.t(strings.RESTORE_ACCESS)}
           </Btn>
           <Paragraph
+            size={ParagraphSizes.micro}
             color={Colors.white70}
             customStyles={{
               paddingHorizontal: BP({
-                small: 5,
-                medium: 10,
+                small: 10,
+                medium: 25,
                 large: 25,
               }),
+              lineHeight: 15,
             }}>
             {I18n.t(strings.STORING_NO_AFFECT_DATA)}
           </Paragraph>
