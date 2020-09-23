2bb1c368ae334ed3c5e4983e916b2199ab719e70
Cristian Lungu <cristian.lungu@hsrw.org>
2020-09-23 16:37:43 +0200

    fix(recovery): disabled scroll

--- src/screens/LoggedOut/Recovery/index.tsx
+++ src/screens/LoggedOut/Recovery/index.tsx
@@ -14,6 +14,7 @@ const Recovery: React.FC = () => {
     <ScrollView
       keyboardShouldPersistTaps="handled"
       contentContainerStyle={{ flex: 1 }}
+      scrollEnabled={false}
     >
       <ScreenContainer
         customStyles={{
