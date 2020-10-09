49db00b13fab04b85d6eba59f126ef187151be62
sbub <svetaibuben@gmail.com>
2020-10-09 11:33:35 +0200

    Removing  ununsed component

--- src/components/OptionalContainer.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactElement } from 'react'
-
-export const OptionalContainer: React.FC<{
-  container: (children: React.ReactNode) => ReactElement<any>
-  condition: boolean
-}> = ({ children, container, condition }) =>
-  condition ? container(children) : (children as ReactElement<any>)
