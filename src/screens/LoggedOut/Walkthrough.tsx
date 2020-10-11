8b484e4a9e828f14bf32770d1aa825e6043620a7
sbub <svetaibuben@gmail.com>
2020-10-11 20:42:50 +0200

    Correcting walkthrough

--- src/screens/LoggedOut/Walkthrough.tsx
+++ src/screens/LoggedOut/Walkthrough.tsx
@@ -19,7 +19,7 @@ import {
 import { strings } from '~/translations/strings'
 import { Colors } from '~/utils/colors'
 import ScreenContainer from '~/components/ScreenContainer'
-import JoloText, { JoloTextKind } from '~/components/JoloText'
+import JoloText, { JoloTextKind, JoloTextWeight } from '~/components/JoloText'
 import { JoloTextSizes } from '~/utils/fonts'
 
 const walkthroughData = [
@@ -93,7 +93,12 @@ const Walkthrough: React.FC = () => {
                 bottom: 235,
               }}
             >
-              <JoloText kind={JoloTextKind.title} size={JoloTextSizes.big}>
+              <JoloText
+                kind={JoloTextKind.title}
+                size={JoloTextSizes.big}
+                weight={JoloTextWeight.regular}
+                color={Colors.white90}
+              >
                 {walkthroughData[idx].header}
               </JoloText>
               <JoloText
