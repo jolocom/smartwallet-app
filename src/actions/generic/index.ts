e688b26486acf8907b1313ee5ef59462033fe733
mnzaki <mnzaki@gmail.com>
2020-10-14 18:30:46 +0200

    fix(actions/checkTermsOfService): don't pass wrong params

--- src/actions/generic/index.ts
+++ src/actions/generic/index.ts
@@ -64,12 +64,21 @@ export const checkTermsOfService = (
   const shouldShowTerms = storageHash !== currentHash
 
   if (!shouldShowTerms && onSubmit) onSubmit()
-  return dispatch(
-    navigationActions.navigate({
-      routeName: shouldShowTerms ? routeList.TermsOfServiceConsent : route,
-      params: { nextRoute: route, onSubmit },
-    }),
-  )
+
+  if (shouldShowTerms) {
+    return dispatch(
+      navigationActions.navigate({
+        routeName: routeList.TermsOfServiceConsent,
+        params: { nextRoute: route, onSubmit },
+      })
+    )
+  } else {
+    return dispatch(
+      navigationActions.navigate({
+        routeName: route,
+      })
+    )
+  }
 }
 
 export const storeTermsOfService = (
