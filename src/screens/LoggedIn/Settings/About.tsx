a2ebd9e0853b9573daf46fdc050cab05992d08ff
Cristian Lungu <cristian.lungu@hsrw.org>
2020-11-03 18:58:23 +0100

    feat(about): added About component

--- src/screens/LoggedIn/Settings/About.tsx
+++ src/screens/LoggedIn/Settings/About.tsx
@@ -2,13 +2,58 @@ import React from 'react'
 import JoloText, { JoloTextKind } from '~/components/JoloText'
 import ScreenContainer from '~/components/ScreenContainer'
 import { JoloTextSizes } from '~/utils/fonts'
+import { JolocomLogoBig } from '~/assets/svg'
+import { View } from 'react-native'
+import BtnGroup from '~/components/BtnGroup'
+import Btn, { BtnTypes } from '~/components/Btn'
+import { useRedirectTo } from '~/hooks/navigation'
+import { ScreenNames } from '~/types/screens'
+// @ts-ignore
+import packageJson from '~/../package.json'
 
-const About = () => (
-  <ScreenContainer hasHeaderBack>
-    <JoloText kind={JoloTextKind.title} size={JoloTextSizes.middle}>
-      About
-    </JoloText>
-  </ScreenContainer>
-)
+const About = () => {
+  const redirectToTerms = useRedirectTo(ScreenNames.TermsOfService)
+  const redirectToPrivacyPolicy = useRedirectTo(ScreenNames.PrivacyPolicy)
 
+  const version = packageJson.version
+
+  return (
+    <ScreenContainer hasHeaderBack>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <JoloText
+          customStyles={{
+            position: 'absolute',
+          }}
+          kind={JoloTextKind.subtitle}
+          size={JoloTextSizes.middle}
+        >
+          {`You are running version ${version}`}
+        </JoloText>
+        <View
+          style={{
+            transform: [{ scale: 0.8 }],
+          }}
+        >
+          <JolocomLogoBig />
+        </View>
+      </View>
+      <View style={{ flex: 1, width: '100%', justifyContent: 'center' }}>
+        <BtnGroup>
+          <Btn onPress={redirectToTerms} type={BtnTypes.secondary}>
+            Terms of Service
+          </Btn>
+          <Btn onPress={redirectToPrivacyPolicy} type={BtnTypes.secondary}>
+            Privacy Policy
+          </Btn>
+        </BtnGroup>
+      </View>
+    </ScreenContainer>
+  )
+}
 export default About
