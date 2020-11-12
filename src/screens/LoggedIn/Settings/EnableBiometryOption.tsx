49615cba1cfc258197a780807fd1257b171ee88a
sbub <svetaibuben@gmail.com>
2020-11-12 17:36:08 +0100

    Adding comments and re-organizing component for readability

--- src/screens/LoggedIn/Settings/EnableBiometryOption.tsx
+++ src/screens/LoggedIn/Settings/EnableBiometryOption.tsx
@@ -9,21 +9,16 @@ import { strings } from '~/translations/strings'
 import Option from './components/Option'
 
 const EnableBiometryOption = () => {
-  const [isOn, setIsOn] = useState(false)
-  const [isSwitchVisible, setSwitchVisibility] = useState(false)
+  /* State to define of this component is displayed: depends on if any biometrics were enrolled */
   const [isOptionVisible, setIsOptionVisible] = useState(false)
+  /* On state that is controlled and passed to ToggleSwitch */
+  const [isOn, setIsOn] = useState(false)
+  /* Based on this state we display switch or not to handle correct was on child useRef */
+  const [isSwitchVisible, setSwitchVisibility] = useState(false)
+  /* State represent what biometrics were enrolled */
   const [enrolledBiometry, setEnrolledBiometry] = useState(null)
+
   const { resetBiometry, getBiometry, updateBiometry } = useBiometry()
-
-  const getStoredBiometry = useCallback(async () => {
-    const biometry = await getBiometry()
-    setIsOn(!!biometry?.type)
-    setSwitchVisibility(true)
-  }, [])
-
-  useEffect(() => {
-    getStoredBiometry()
-  }, [])
 
   const checkIfBiometryIsEnrolled = async () => {
     try {
@@ -36,13 +31,27 @@ const EnableBiometryOption = () => {
     }
   }
 
+  /* check if we should display this component or not */
   useEffect(() => {
     checkIfBiometryIsEnrolled()
+  }, [])
+
+  /* check what has been stored biometry related in settings */
+  const getStoredBiometry = useCallback(async () => {
+    const biometry = await getBiometry()
+    setIsOn(!!biometry?.type)
+    setSwitchVisibility(true)
+  }, [])
+
+  useEffect(() => {
+    getStoredBiometry()
   }, [])
 
   const handleToggle = async () => {
     try {
       if (!isOn) {
+        /* if next state is on */
+
         // if user wants to activate biometry
         await FingerprintScanner.authenticate({
           fallbackEnabled: false,
@@ -50,8 +59,10 @@ const EnableBiometryOption = () => {
         updateBiometry(enrolledBiometry)
         setIsOn(true)
       } else {
+        /* if next state is off */
+
         // if user wants to switch biometry off
-        // - reset biometry value from storage as Lock screen keeps us with this value
+        // - reset biometry value from storage as Lock screen should use this value
         await resetBiometry()
         setIsOn(false)
       }
