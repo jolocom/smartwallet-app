d6c8bbe30d8c1f9920708071df63ccfd90974221
Exulansis <rusu.eugeniu@gmx.de>
2020-11-13 12:38:09 +0100

    tests(locale, recovery): Updated snapshots

--- tests/ui/components/inputSeedPhrase.test.tsx
+++ tests/ui/components/inputSeedPhrase.test.tsx
@@ -1,45 +1,33 @@
-// import React from 'react'
-// import { shallow } from 'enzyme'
-// import InputSeedPhraseComponent from '../../../src/ui/recovery/components/inputSeedPhrase'
-// import { WordState } from '../../../src/ui/recovery/container/inputSeedPhrase'
-// 
-// describe('inputSeedPhrase component', () => {
-//   const makeProps = (
-//     input: string = '',
-//     suggestions: string[] = [],
-//     isLoading: boolean = false,
-//   ) => ({
-//     inputValue: input,
-//     markedWord: 0,
-//     suggestions,
-//     isMnemonicValid: false,
-//     inputState: WordState.editing,
-//     mnemonic: [],
-//     isLoading,
-//     handleTextInput: jest.fn(),
-//     selectWord: jest.fn(),
-//     handleButtonPress: jest.fn(),
-//     inputRef: jest.fn(),
-//     handleDoneButton: jest.fn(),
-//     handleNextWord: jest.fn(),
-//     handlePreviousWord: jest.fn(),
-//     handleBackButton: jest.fn(),
-//   })
-// 
-//   it('matches the snapshot at start', () => {
-//     const props = makeProps()
-//     const rendered = shallow(<InputSeedPhraseComponent {...props} />)
-//     expect(rendered).toMatchSnapshot()
-//   })
-// 
-//   it('matches the snapshot while inputting', () => {
-//     const props = makeProps('in', ['insert', 'input'])
-//     const rendered = shallow(<InputSeedPhraseComponent {...props} />)
-//     expect(rendered).toMatchSnapshot()
-//   })
-//   it('matches the snapshot when loading', () => {
-//     const props = makeProps('', [], true)
-//     const rendered = shallow(<InputSeedPhraseComponent {...props} />)
-//     expect(rendered).toMatchSnapshot()
-//   })
-// })
+import React from 'react'
+import { shallow } from 'enzyme'
+import InputSeedPhraseComponent from '../../../src/ui/recovery/components/inputSeedPhrase'
+
+describe.only('inputSeedPhrase component', () => {
+  const makeProps = (
+    input: string = '',
+    suggestions: string[] = [],
+    isLoading: boolean = false,
+  ) => ({
+      handleSubmit: jest.fn(),
+      handleCancel: jest.fn(),
+      isLoading
+  })
+
+  it('matches the snapshot at start', () => {
+    const props = makeProps()
+    const rendered = shallow(<InputSeedPhraseComponent {...props} />)
+    expect(rendered).toMatchSnapshot()
+  })
+
+  // it('matches the snapshot while inputting', () => {
+  //   const props = makeProps('in', ['insert', 'input'])
+  //   const rendered = shallow(<InputSeedPhraseComponent {...props} />)
+  //   expect(rendered).toMatchSnapshot()
+  // })
+
+  it('matches the snapshot when loading', () => {
+    const props = makeProps('', [], true)
+    const rendered = shallow(<InputSeedPhraseComponent {...props} />)
+    expect(rendered).toMatchSnapshot()
+  })
+})
