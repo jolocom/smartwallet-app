a95aa8ce2716636787bbb5f3671c97461e6dca8c
Cristian Lungu <cristian.lungu@hsrw.org>
2020-11-16 12:13:59 +0100

    docs(navigation): added inline docs

--- src/NavigationProvider.tsx
+++ src/NavigationProvider.tsx
@@ -3,6 +3,11 @@ import { createContext, useContext } from 'react'
 import { NavigationContainerRef } from '@react-navigation/native'
 import { ScreenNames } from './types/screens'
 
+/* NOTE: exposing the navigation container through a context for components
+ *       which are rendered outside the RootNavigation component. This enables
+ *       use to navigate from any component wrapped with the context provider.
+ */
+
 const NavigationContext = createContext<RefObject<
   NavigationContainerRef
 > | null>(null)
@@ -17,6 +22,10 @@ export const NavigationContextProvider: React.FC<Props> = ({
 }) => {
   return <NavigationContext.Provider value={navRef} children={children} />
 }
+
+// NOTE: In case we need to navigate from outside the @RootNavigation routes
+// (e.g. Overlays), we should use the hooks below instead of the ones provided by
+// the navigation system and `~/hooks/navigation`.
 
 export const useOutsideNavigation = () => {
   const navRef = useContext(NavigationContext)
