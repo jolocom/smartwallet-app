cc7fe6b4ae1761d5541b4e976a3c9d6ff8e287cd
Cristian Lungu <cristian.lungu@hsrw.org>
2020-11-17 14:26:55 +0100

    feat(contactus): added input example

--- src/screens/LoggedIn/Settings/ContactUs.tsx
+++ src/screens/LoggedIn/Settings/ContactUs.tsx
@@ -1,10 +1,12 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 
 import JoloText, { JoloTextKind } from '~/components/JoloText'
 import ScreenContainer from '~/components/ScreenContainer'
 import { strings } from '~/translations/strings'
 import { JoloTextSizes } from '~/utils/fonts'
 import Dropdown from './components/Dropdown'
+import FieldInput from '~/components/FieldInput'
+import { InputValidation } from '~/utils/stringUtils'
 
 const INQUIRIES_LIST = [
   strings.POSSIBLE_PARTNERSHIP,
@@ -14,17 +16,30 @@ const INQUIRIES_LIST = [
 ]
 
 const ContactUs: React.FC = () => {
+  const [value, setValue] = useState('')
+
   const options = useMemo(
     () =>
       INQUIRIES_LIST.map((el) => ({ id: el.split(' ').join(''), value: el })),
     [],
   )
+
   return (
-    <ScreenContainer hasHeaderBack>
+    <ScreenContainer
+      hasHeaderBack
+      customStyles={{ justifyContent: 'flex-start' }}
+    >
       <JoloText kind={JoloTextKind.title} size={JoloTextSizes.middle}>
         Contact us
       </JoloText>
       <Dropdown options={options} />
+
+      <FieldInput
+        validation={InputValidation.email}
+        value={value}
+        onChangeText={setValue}
+        onValidation={console.log}
+      />
     </ScreenContainer>
   )
 }
