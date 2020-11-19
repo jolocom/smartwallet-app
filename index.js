7b5cb0f4ad8a6e12c06f00e40fbb0ff25c5a62c5
Cristian Lungu <cristian.lungu@hsrw.org>
2020-11-19 17:42:43 +0100

    feat(sentry): added sentry init

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
 
+import { initSentry } from '~/utils/sentry'
+
+initSentry()
+
 AppRegistry.registerComponent(appName, () => App)
---
+++ src/utils/sentry/config.ts
@@ -0,0 +1,2 @@
+export const SENTRY_DNS =
+  'https://c0ac34a2948e4bcc97415defc5e32a47@sentry.jolocom.io/3'
