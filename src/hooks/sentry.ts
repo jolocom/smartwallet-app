f9705dd510488e6853f5139e97ea717606030b1e
Cristian Lungu <cristian.lungu@hsrw.org>
2020-11-19 17:43:48 +0100

    feat(sentry): added basic hook

---
+++ src/hooks/sentry.ts
@@ -0,0 +1,33 @@
+import * as Sentry from '@sentry/react-native'
+
+interface UserReport {
+  issue?: string
+  details?: string
+  contact?: string
+}
+
+const useSentry = () => {
+  const error = false
+
+  const sendReport = (report: UserReport, sendPrivateData: boolean) => {
+    Sentry.withScope((scope) => {
+      scope.setExtras({ ...report, sendPrivateData })
+      if (!sendPrivateData) scope.setUser(null)
+
+      console.log({ report })
+      try {
+        if (error) {
+          Sentry.captureException(new Error('TEST_ERROR'))
+        } else {
+          Sentry.captureMessage('TEST_MESSAGE', scope)
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    })
+  }
+
+  return { sendReport }
+}
+
+export default useSentry
