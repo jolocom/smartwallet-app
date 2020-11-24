c75fb7a53c578916601333c58997903d65396d17
Cristian Lungu <cristian.lungu@hsrw.org>
2020-11-24 16:36:19 +0100

    feat(doc): showing the modal imperatively with refs

--- src/screens/LoggedIn/Documents/CardDetails.tsx
+++ src/screens/LoggedIn/Documents/CardDetails.tsx
@@ -1,84 +1,86 @@
-import React from 'react'
+import React, { useState, useImperativeHandle } from 'react'
+import { View, Image, StyleSheet, ScrollView } from 'react-native'
+
 import ActionSheet from '~/components/ActionSheet/ActionSheet'
 import NavigationHeader, { NavHeaderType } from '~/components/NavigationHeader'
-import { View, Image, StyleSheet, ScrollView } from 'react-native'
 import JoloText, { JoloTextKind } from '~/components/JoloText'
 import { JoloTextSizes } from '~/utils/fonts'
 import { Colors } from '~/utils/colors'
 import Block from '~/components/Block'
-import { debugView } from '~/utils/dev'
 
 interface Props {
   fields: Record<string, string>
   title: string
   image?: string
-  isVisible: boolean
-  onClose: () => void
 }
 
-const CardDetails: React.FC<Props> = ({
-  fields,
-  title,
-  image,
-  onClose,
-  isVisible,
-}) => {
-  return (
-    <ActionSheet isVisible={isVisible} onClose={onClose}>
-      <View style={styles.container}>
-        <NavigationHeader type={NavHeaderType.Close} onPress={onClose} />
-        <ScrollView
-          showsVerticalScrollIndicator={false}
-          overScrollMode="never"
-          style={{ paddingHorizontal: 20 }}
-          contentContainerStyle={{ paddingBottom: 50 }}
-        >
-          <View
-            style={[
-              styles.titleContainer,
-              {
-                paddingRight: image ? '40%' : 0,
-              },
-            ]}
+const CardDetails = React.forwardRef<{ show: () => void }, Props>(
+  ({ fields, title, image }, ref) => {
+    const [modalVisible, setModalVisible] = useState(false)
+
+    const handleClose = () => setModalVisible(false)
+
+    useImperativeHandle(ref, () => ({
+      show: () => setModalVisible(true),
+    }))
+
+    return (
+      <ActionSheet isVisible={modalVisible} onClose={handleClose}>
+        <View style={styles.container}>
+          <NavigationHeader type={NavHeaderType.Close} onPress={handleClose} />
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            overScrollMode="never"
+            style={{ paddingHorizontal: 20 }}
+            contentContainerStyle={{ paddingBottom: 50 }}
           >
-            <JoloText
-              customStyles={styles.fieldText}
-              kind={JoloTextKind.title}
-              size={JoloTextSizes.middle}
+            <View
+              style={[
+                styles.titleContainer,
+                {
+                  paddingRight: image ? '40%' : 0,
+                },
+              ]}
             >
-              {title}
-            </JoloText>
-          </View>
-          <Block customStyle={{ backgroundColor: Colors.white }}>
-            {image && <Image source={{ uri: image }} style={styles.image} />}
-            {Object.keys(fields).map((field, i) => (
-              <React.Fragment key={i}>
-                <View style={styles.fieldContainer}>
-                  <JoloText
-                    customStyles={styles.fieldText}
-                    size={JoloTextSizes.mini}
-                    color={Colors.osloGray}
-                  >
-                    {field}
-                  </JoloText>
-                  <JoloText
-                    color={Colors.black95}
-                    customStyles={[styles.fieldText, { marginTop: 8 }]}
-                  >
-                    {fields[field]}
-                  </JoloText>
-                </View>
-                {i !== Object.keys(fields).length - 1 && (
-                  <View style={styles.divider} />
-                )}
-              </React.Fragment>
-            ))}
-          </Block>
-        </ScrollView>
-      </View>
-    </ActionSheet>
-  )
-}
+              <JoloText
+                customStyles={styles.fieldText}
+                kind={JoloTextKind.title}
+                size={JoloTextSizes.middle}
+              >
+                {title}
+              </JoloText>
+            </View>
+            <Block customStyle={{ backgroundColor: Colors.white }}>
+              {image && <Image source={{ uri: image }} style={styles.image} />}
+              {Object.keys(fields).map((field, i) => (
+                <React.Fragment key={i}>
+                  <View style={styles.fieldContainer}>
+                    <JoloText
+                      customStyles={styles.fieldText}
+                      size={JoloTextSizes.mini}
+                      color={Colors.osloGray}
+                    >
+                      {field}
+                    </JoloText>
+                    <JoloText
+                      color={Colors.black95}
+                      customStyles={[styles.fieldText, { marginTop: 8 }]}
+                    >
+                      {fields[field]}
+                    </JoloText>
+                  </View>
+                  {i !== Object.keys(fields).length - 1 && (
+                    <View style={styles.divider} />
+                  )}
+                </React.Fragment>
+              ))}
+            </Block>
+          </ScrollView>
+        </View>
+      </ActionSheet>
+    )
+  },
+)
 
 const styles = StyleSheet.create({
   container: {
