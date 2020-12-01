6f7eaf96adb368f0815f70205aa1b26b0808abdb
sbub <svetaibuben@gmail.com>
2020-12-01 16:36:14 +0100

    Avoiding re-rendering of tab children when switching between tabs

--- src/screens/LoggedIn/Documents/index.tsx
+++ src/screens/LoggedIn/Documents/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo } from 'react'
 import { useSelector } from 'react-redux'
 import { ScrollView, View } from 'react-native'
 import { IClaimSection } from 'jolocom-lib/js/credentials/credential/types'
@@ -11,73 +11,100 @@ import DocumentTabs from '~/screens/LoggedIn/Documents/DocumentTabs'
 import OtherCard from '~/components/Card/OtherCard'
 import { DocumentTypes, DocumentFields } from '~/components/Card/types'
 import { capitalizeWord } from '~/utils/stringUtils'
+import { UICredential } from '~/types/credentials'
+
+const formatClaims = (claims: IClaimSection) =>
+  Object.keys(claims).map((key) => ({
+    name: capitalizeWord(key),
+    value: claims[key],
+  }))
+
+const getDocumentName = (claim: IClaimSection) => {
+  if (!!claim['givenName'] && !!claim['familyName']) {
+    return {
+      name: 'Subject name',
+      value: `${claim['givenName']} ${claim['familyName']}`,
+    }
+  }
+
+  return null
+}
+
+const getOptionalFields = (claim: IClaimSection) =>
+  Object.keys(claim)
+    .filter((k) => k !== 'id' && k !== 'familyName' && k !== 'givenName')
+    .map((key) => ({
+      name: capitalizeWord(key),
+      value: claim[key],
+    }))
+    .slice(0, 6)
+
+const DocumentCards: React.FC<{ documents: UICredential[] }> = memo(
+  ({ documents }) => {
+    return documents.map((document) => (
+      <DocumentCard
+        key={document.id}
+        id={document.id}
+        mandatoryFields={[
+          {
+            name: DocumentFields.DocumentName,
+            value: document.metadata.name,
+          },
+          getDocumentName(document.claim),
+        ]}
+        optionalFields={getOptionalFields(document.claim)}
+        highlight={document.id.slice(0, 14)}
+        image={document.renderInfo?.logo?.url}
+        claims={formatClaims(document.claim)}
+      />
+    ))
+  },
+  (prevProps, nextProps) =>
+    JSON.stringify(prevProps.documents) === JSON.stringify(nextProps.documents),
+)
+
+const OtherCards: React.FC<{ other: UICredential[] }> = memo(
+  ({ other }) => {
+    return other.map((otherDoc) => (
+      <OtherCard
+        id={otherDoc.id}
+        key={otherDoc.id}
+        mandatoryFields={[
+          {
+            name: DocumentFields.DocumentName,
+            value: otherDoc.metadata.name,
+          },
+        ]}
+        optionalFields={getOptionalFields(otherDoc.claim)}
+        image={otherDoc.renderInfo?.logo?.url}
+        claims={formatClaims(otherDoc.claim)}
+      />
+    ))
+  },
+  (prevProps, nextProps) =>
+    JSON.stringify(prevProps.other) === JSON.stringify(nextProps.other),
+)
 
 const DocumentList = () => {
   const { documents, other } = useSelector(getCredentialsBySection)
   const { activeTab, activeSubtab } = useTabs()
 
-  const getDocumentName = (claim: IClaimSection) => {
-    if (!!claim['givenName'] && !!claim['familyName']) {
-      return {
-        name: 'Subject name',
-        value: `${claim['givenName']} ${claim['familyName']}`,
-      }
-    }
-
-    return null
-  }
-
-  const formatClaims = (claims: IClaimSection) =>
-    Object.keys(claims).map((key) => ({
-      name: capitalizeWord(key),
-      value: claims[key],
-    }))
-
-  const getOptionalFields = (claim: IClaimSection) =>
-    Object.keys(claim)
-      .filter((k) => k !== 'id' && k !== 'familyName' && k !== 'givenName')
-      .map((key) => ({
-        name: capitalizeWord(key),
-        value: claim[key],
-      }))
-      .slice(0, 6)
-
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
-      {activeTab?.id === DocumentTypes.document &&
-        documents.map((document) => (
-          <DocumentCard
-            key={document.id}
-            id={document.id}
-            mandatoryFields={[
-              {
-                name: DocumentFields.DocumentName,
-                value: document.metadata.name,
-              },
-              getDocumentName(document.claim),
-            ]}
-            optionalFields={getOptionalFields(document.claim)}
-            highlight={document.id.slice(0, 14)}
-            image={document.renderInfo?.logo?.url}
-            claims={formatClaims(document.claim)}
-          />
-        ))}
-      {activeTab?.id === DocumentTypes.other &&
-        other.map((otherDoc) => (
-          <OtherCard
-            id={otherDoc.id}
-            key={otherDoc.id}
-            mandatoryFields={[
-              {
-                name: DocumentFields.DocumentName,
-                value: otherDoc.metadata.name,
-              },
-            ]}
-            optionalFields={getOptionalFields(otherDoc.claim)}
-            image={otherDoc.renderInfo?.logo?.url}
-            claims={formatClaims(otherDoc.claim)}
-          />
-        ))}
+      <View
+        style={{
+          display: activeTab?.id === DocumentTypes.document ? 'flex' : 'none',
+        }}
+      >
+        <DocumentCards documents={documents} />
+      </View>
+      <View
+        style={{
+          display: activeTab?.id === DocumentTypes.document ? 'none' : 'flex',
+        }}
+      >
+        <OtherCards other={other} />
+      </View>
       <View style={{ height: 100 }} />
     </ScrollView>
   )
