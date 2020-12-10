a3f390fddf62dba2f0fbd828ada3cf79a9acc8e4
Cristian Lungu <cristian.lungu@hsrw.org>
2020-12-10 14:20:40 +0100

    fix(identity): cleaned identity

--- src/screens/LoggedIn/Identity/index.tsx
+++ src/screens/LoggedIn/Identity/index.tsx
@@ -10,63 +10,7 @@ import FormField from './components/FormField'
 
 const Identity = () => {
   const attributes = useSelector(getAttributes)
-  return (
-    <ScreenContainer>
-      {/* <ScrollView
-            contentContainerStyle={{ paddingBottom: 100 }}
-            style={{ width: '100%' }}
-            showsVerticalScrollIndicator={false}
-            >
-            {Object.keys(attributes).map((attrKey) => (
-            <Widget>
-            <Widget.Header.Name value={attrKey} />
-            {attributes[attrKey].map((field) => (
-            <Widget.Field.Static value={field.value} />
-            ))}
-            <Widget>
-            <Widget.Header.Name value="companyName" />
-            <Widget.Field.Static value="Jolocom" />
-            </Widget>
-            </Widget>
-            ))}
-            <Form
-            config={{
-            id: 'name',
-            fields: [
-            {
-            id: 'givenName',
-            placeholder: 'Select a given name',
-            keyboardType: 'default',
-            },
-            {
-            id: 'lastName',
-            placeholder: 'Select a last name',
-            keyboardType: 'default',
-            },
-            ],
-            }}
-            onCancel={() => {}}
-            onSubmit={() => {}}
-            >
-            <Form.Header>
-            <Form.Header.Cancel />
-            <Form.Header.Done />
-            </Form.Header>
-            <Form.Body>
-            {({ fields, updateField }) =>
-            fields.map((field) => (
-            <FormField
-            key={field.id}
-            updateValue={(val: string) => updateField(field.id, val)}
-            value={field.value}
-            />
-            ))
-            }
-            </Form.Body>
-            </Form>
-            </ScrollView> */}
-    </ScreenContainer>
-  )
+  return <ScreenContainer></ScreenContainer>
 }
 
 export default Identity
