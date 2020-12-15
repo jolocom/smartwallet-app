7b52cc38b86eb891bba54181d527652a86359243
Cristian Lungu <cristian.lungu@hsrw.org>
2020-12-15 11:36:15 +0100

    feat(identity): added TopSheet to Identity screen

--- src/screens/LoggedIn/Identity/index.tsx
+++ src/screens/LoggedIn/Identity/index.tsx
@@ -1,33 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ScrollView } from 'react-native'
 import { useSelector } from 'react-redux'
 import ScreenContainer from '~/components/ScreenContainer'
 import Widget from '~/components/Widget'
 import { getAttributes } from '~/modules/attributes/selectors'
+import TopSheet from '~/components/TopSheet'
+import Btn, { BtnTypes } from '~/components/Btn'
 
 const Identity = () => {
   const attributes = useSelector(getAttributes)
+  const [showTopSheet, setShowTopSheet] = useState(true)
 
   return (
-    <ScreenContainer>
-      <ScrollView
-        contentContainerStyle={{ paddingBottom: 100 }}
-        style={{ width: '100%' }}
-        showsVerticalScrollIndicator={false}
-      >
-        {Object.keys(attributes).map((attrKey) => (
-          <Widget>
-            <Widget.Header.Name value={attrKey} />
-            {attributes[attrKey].map((field) => (
-              <Widget.Field.Static value={field.value} />
-            ))}
+    <ScreenContainer
+      isFullscreen={showTopSheet}
+      customStyles={{ paddingHorizontal: 0 }}
+    >
+      {showTopSheet ? (
+        <TopSheet isVisible={showTopSheet}>
+          <Btn onPress={() => setShowTopSheet(false)} type={BtnTypes.senary}>
+            Close
+          </Btn>
+        </TopSheet>
+      ) : (
+        <ScrollView
+          contentContainerStyle={{ paddingBottom: 100 }}
+          style={{ width: '100%', paddingHorizontal: '5%' }}
+          showsVerticalScrollIndicator={false}
+        >
+          {Object.keys(attributes).map((attrKey) => (
             <Widget>
-              <Widget.Header.Name value="companyName" />
-              <Widget.Field.Static value="Jolocom" />
+              <Widget.Header.Name value={attrKey} />
+              {attributes[attrKey].map((field) => (
+                <Widget.Field.Static value={field.value} />
+              ))}
+              <Widget>
+                <Widget.Header.Name value="companyName" />
+                <Widget.Field.Static value="Jolocom" />
+              </Widget>
             </Widget>
-          </Widget>
-        ))}
-      </ScrollView>
+          ))}
+        </ScrollView>
+      )}
     </ScreenContainer>
   )
 }
