e958143d5df223822b5c792919f5f29896b9c543
sbub <svetaibuben@gmail.com>
2020-12-16 11:16:31 +0100

    Removing svg folder from testing

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,5 @@ module.exports = {
     './node_modules/react-native-gesture-handler/jestSetup.js',
   ],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-  testPathIgnorePatterns: ['./node_modules/.*', './__tests__/utils/.*'],
+  testPathIgnorePatterns: ['./node_modules/.*', './__tests__/utils/.*', './src/assets/svg/'],
 }
