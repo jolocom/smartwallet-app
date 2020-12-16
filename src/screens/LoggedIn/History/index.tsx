1b209094f7d97a701f7f02279942b657ee82511b
Cristian Lungu <cristian.lungu@hsrw.org>
2020-12-16 14:25:09 +0100

    feat(history): added section header/footer styles

--- src/screens/LoggedIn/History/index.tsx
+++ src/screens/LoggedIn/History/index.tsx
@@ -6,7 +6,9 @@ import HistoryTabs from '~/components/Tabs/HistoryTabs'
 import useHistory from '~/hooks/history'
 import { HistoryFieldPlaceholder } from './HistoryField'
 import HistoryInteraction from './HistoryInteraction'
-import JoloText from '~/components/JoloText'
+import JoloText, { JoloTextKind } from '~/components/JoloText'
+import { JoloTextSizes } from '~/utils/fonts'
+import { Colors } from '~/utils/colors'
 
 const History: React.FC = () => {
   const {
@@ -33,8 +35,19 @@ const History: React.FC = () => {
             onEndReached={() => loadSections()}
             contentContainerStyle={{ marginTop: 32, paddingBottom: '40%' }}
             renderSectionHeader={({ section }) => (
-              <JoloText>{section.section}</JoloText>
+              <JoloText
+                kind={JoloTextKind.title}
+                size={JoloTextSizes.middle}
+                color={Colors.white85}
+                customStyles={{
+                  textAlign: 'left',
+                  marginBottom: 20,
+                }}
+              >
+                {section.section}
+              </JoloText>
             )}
+            renderSectionFooter={() => <View style={{ marginBottom: 36 }} />}
             renderItem={({ item, index }) => (
               <HistoryInteraction
                 key={index}
