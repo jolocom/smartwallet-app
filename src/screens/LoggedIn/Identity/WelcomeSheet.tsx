5a88fe0b23dba92dc779654330aef9c5bfb1c52f
Cristian Lungu <cristian.lungu@hsrw.org>
2020-12-17 13:46:24 +0100

    refactor(identity): extracted WelcomeSheet into a separate component and removed AnimateLayout issue

---
+++ src/screens/LoggedIn/Identity/WelcomeSheet.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react'
+import { Image, LayoutAnimation } from 'react-native'
+import { useSelector } from 'react-redux'
+
+import Btn, { BtnTypes } from '~/components/Btn'
+import JoloText, { JoloTextKind } from '~/components/JoloText'
+import TopSheet from '~/components/TopSheet'
+import BusinessCardWizard from '~/components/Wizard/BusinessCardWizard'
+import SingleCredentialWizard from '~/components/Wizard/SingleCredentialWizard'
+import { getAttributes } from '~/modules/attributes/selectors'
+import { strings } from '~/translations'
+import { Colors } from '~/utils/colors'
+
+enum IdentityForms {
+  SingleCredential = 'SingleCredential',
+  BusinessCard = 'BusinessCard',
+}
+
+const WelcomeSheet: React.FC = () => {
+  const attributes = useSelector(getAttributes)
+  const [isTopSheetVisible, setTopSheetVisibility] = useState(
+    Boolean(Object.keys(attributes).length),
+  )
+  const [activeForm, setActiveForm] = useState<IdentityForms | null>(null)
+
+  const animateSheet = () =>
+    LayoutAnimation.configureNext({
+      update: {
+        ...LayoutAnimation.Presets.linear.update,
+        duration: 200,
+      },
+      create: {
+        ...LayoutAnimation.Presets.linear.create,
+        duration: 200,
+      },
+      delete: {
+        ...LayoutAnimation.Presets.linear.delete,
+        duration: 100,
+      },
+      duration: 400,
+    })
+
+  const changeActiveForm = (form: IdentityForms | null) => {
+    if (form) {
+      animateSheet()
+      setActiveForm(form)
+    } else {
+      setTopSheetVisibility(false)
+    }
+  }
+
+  return (
+    <TopSheet
+      isVisible={isTopSheetVisible}
+      customStyles={{ alignItems: 'center' }}
+    >
+      {activeForm === IdentityForms.SingleCredential ? (
+        <SingleCredentialWizard onFormSubmit={() => changeActiveForm(null)} />
+      ) : activeForm === IdentityForms.BusinessCard ? (
+        <BusinessCardWizard onFormSubmit={() => changeActiveForm(null)} />
+      ) : (
+        <>
+          <Image source={require('~/assets/images/identityIntro.png')} />
+          <JoloText
+            kind={JoloTextKind.title}
+            color={Colors.white90}
+            customStyles={{ marginVertical: 10 }}
+          >
+            {strings.IT_IS_TIME_TO_CREATE}
+          </JoloText>
+
+          <Btn
+            onPress={() => changeActiveForm(IdentityForms.SingleCredential)}
+            type={BtnTypes.senary}
+          >
+            {strings.SINGLE_CREDENTIAL}
+          </Btn>
+          <Btn
+            onPress={() => changeActiveForm(IdentityForms.BusinessCard)}
+            type={BtnTypes.senary}
+          >
+            {strings.BUSINESS_CARD}
+          </Btn>
+        </>
+      )}
+    </TopSheet>
+  )
+}
+
+export default WelcomeSheet
