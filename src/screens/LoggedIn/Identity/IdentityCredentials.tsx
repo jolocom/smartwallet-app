e3c4d3cbdd1df95ee50eec0fa7cdf91dca7533e3
sbub <svetaibuben@gmail.com>
2021-01-11 13:16:30 +0100

    Listing all attrributes as empty on identitycredentials screen

--- src/screens/LoggedIn/Identity/IdentityCredentials.tsx
+++ src/screens/LoggedIn/Identity/IdentityCredentials.tsx
@@ -1,8 +1,41 @@
 import React from 'react'
-import { View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import { useSelector } from 'react-redux'
+import Widget from '~/components/Widget'
+import Field from '~/components/Widget/Field'
+import { attributeConfig } from '~/config/claims'
+import { getAttributes } from '~/modules/attributes/selectors'
+import { AttributeTypes } from '~/types/credentials'
 
 const IdentityCredentials = () => {
-  return <View testID="identity-credentials-present" />
+  const attributes = useSelector(getAttributes)
+
+  const handleOnCreate = (type: AttributeTypes) => {
+    console.log('Creating for an attribute type:', type)
+  }
+
+  return (
+    <View testID="identity-credentials-present" style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {Object.entries(attributeConfig).map(([aKey, aVal]) => (
+          <Widget onCreate={() => handleOnCreate(aKey)}>
+            <Widget.Header>
+              <Widget.Header.Name value={aVal.label} />
+              <Widget.Header.Action.CreateNew />
+            </Widget.Header>
+            <Field.Static value="pencil" />
+          </Widget>
+        ))}
+      </ScrollView>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+})
 
 export default IdentityCredentials
