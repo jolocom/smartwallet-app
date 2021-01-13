f46ad77b4d7e80e8c38c918c99b4753f911ca164
Cristian Lungu <cristian.lungu@hsrw.org>
2021-01-13 11:05:31 +0100

    feat(identity): added IdentityTabs scrollview

--- src/screens/LoggedIn/Identity/tabs/index.tsx
+++ src/screens/LoggedIn/Identity/tabs/index.tsx
@@ -3,6 +3,9 @@ import React, { useMemo, useState } from 'react'
 import IdentityTab, { IdentityTabType } from './IdentityTab'
 import { useCustomContext } from '~/hooks/context'
 import IdentityTabsHeader from './IdentityTabsHeader'
+import IdentityTabsContent from './IdentityTabsContent'
+import IdentityTabsPage from './IdentityTabsPage'
+import { ScrollView } from 'react-native'
 
 interface ITabsContext {
   activeTab: string | undefined
@@ -19,6 +22,8 @@ interface IIdentityTabs {
 interface ITabsComposition {
   Tab: IdentityTabType
   Header: React.FC
+  Content: React.FC
+  Page: React.FC<{ id: string }>
 }
 
 const IdentityTabs: React.FC<IIdentityTabs> & ITabsComposition = ({
@@ -35,11 +40,21 @@ const IdentityTabs: React.FC<IIdentityTabs> & ITabsComposition = ({
     [activeTab],
   )
 
-  return <TabsContext.Provider value={contextValue} children={children} />
+  return (
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      overScrollMode="never"
+      contentContainerStyle={{ paddingBottom: '40%' }}
+    >
+      <TabsContext.Provider value={contextValue} children={children} />
+    </ScrollView>
+  )
 }
 
 IdentityTabs.Header = IdentityTabsHeader
 IdentityTabs.Tab = IdentityTab
+IdentityTabs.Content = IdentityTabsContent
+IdentityTabs.Page = IdentityTabsPage
 
 export const useIdentityTabs = useCustomContext(TabsContext)
 
