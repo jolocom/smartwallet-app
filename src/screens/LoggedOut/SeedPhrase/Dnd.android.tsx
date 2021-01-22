1fd3836a6e7f5b04674b3cc51e946f6b865e5b76
Cristian Lungu <cristian.lungu@hsrw.org>
2021-01-22 11:31:13 +0100

    feat(seed-repeat): assembled the dnd

--- src/screens/LoggedOut/SeedPhrase/Dnd.android.tsx
+++ src/screens/LoggedOut/SeedPhrase/Dnd.android.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import JoloText from '~/components/JoloText';
-import { IDndProps } from './SeedPhraseRepeat';
+import React from 'react'
+import { View, TouchableOpacity, StyleSheet } from 'react-native'
+import DraggableFlatList from 'react-native-draggable-flatlist'
 
-const Dnd: React.FC<IDndProps> = ({tags}) => {
- return (
-  <JoloText>Coming soon ...</JoloText>
- )
+import { IDndProps } from './SeedPhraseRepeat'
+import WordPill from './components/WordPill'
+import { debugView } from '~/utils/dev'
+
+const Dnd: React.FC<IDndProps> = ({ tags, updateTags }) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <DraggableFlatList
+        data={tags}
+        // NOTE: the padding compensates for the active pill's border, which
+        // may distort the content of the pill otherwise
+        style={{ paddingHorizontal: 1.7 }}
+        // NOTE: allows the pill size to be based on the content, as opposed
+        // to being of fixed size
+        contentContainerStyle={{ alignItems: 'center' }}
+        renderItem={({ item, index, drag, isActive }) => (
+          <TouchableOpacity activeOpacity={1} onPressIn={drag}>
+            <WordPill.Repeat key={index} active={isActive}>
+              {item}
+            </WordPill.Repeat>
+          </TouchableOpacity>
+        )}
+        renderPlaceholder={() => <WordPill.Placeholder />}
+        keyExtractor={(item, index) => `pill-${item}-${index}`}
+        onDragEnd={({ data }) => updateTags(data)}
+      />
+    </View>
+  )
 }
 
-export default Dnd;
+export default Dnd
