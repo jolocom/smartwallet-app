0bfa1e16a27142f6990ad86ef549016277dd5085
Cristian Lungu <cristian.lungu@hsrw.org>
2021-01-22 12:27:11 +0100

    fix(identity-intro): fixed button width

--- src/components/Wizard/WizardBody.tsx
+++ src/components/Wizard/WizardBody.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { View } from 'react-native'
+
 import { IWizardBodyProps, useWizard } from '.'
 
 const WizardBody: React.FC<IWizardBodyProps> = ({ step, children }) => {
   const { activeStep } = useWizard()
   if (activeStep !== step) return null
-  return <>{children}</>
+  return <View>{children}</View>
 }
 
 export default WizardBody
