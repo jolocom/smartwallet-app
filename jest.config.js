31d3827b23575cdcb130d086ce903b8494382285
Cristian Lungu <cristian.lungu@hsrw.org>
2021-02-05 16:57:18 +0100

    test(container): fixed related tests (not all)

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,11 @@ module.exports = {
     './node_modules/react-native-gesture-handler/jestSetup.js',
   ],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-  testPathIgnorePatterns: ['./node_modules/.*', './__tests__/utils/.*', './src/assets/svg/', '/node_modules/(?!(react-native|@sentry/react-native)/)'],
+  testPathIgnorePatterns: [
+    './node_modules/.*',
+    './__tests__/utils/.*',
+    './src/assets/svg/',
+    '/node_modules/(?!(react-native|@sentry/react-native)/)',
+  ],
+  transformIgnorePatterns: ['node_modules/(?!static-container)/'],
 }
