2006e14a220be27756eb88481c79e7b561ce8687
Cristian Lungu <cristian.lungu@hsrw.org>
2021-02-08 13:59:40 +0100

    fix(identity-placeholder): fixed margin

--- src/screens/LoggedIn/Identity/tabs/IdentityTabPlaceholder.tsx
+++ src/screens/LoggedIn/Identity/tabs/IdentityTabPlaceholder.tsx
@@ -7,7 +7,7 @@ import { Colors } from '~/utils/colors'
 
 const IdentityTabsPlaceholder: React.FC<{ show: boolean }> = ({ show }) =>
   show ? (
-    <View style={{ marginBottom: 12, paddingHorizontal: '10%' }}>
+    <View style={{ marginBottom: 24, paddingHorizontal: '10%' }}>
       <JoloText color={Colors.white50} size={JoloTextSizes.mini}>
         {strings.YOUR_INFO_IS_QUITE_EMPTY}
       </JoloText>
