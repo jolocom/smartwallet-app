83adeaef939a9a03007c6e8bcedb3b2950e5f740
SBub <sveta@sbubs-mbp.localdomain>
2021-02-22 16:29:30 +0100

    feat(authentication flow): removing stale authentication flow

--- src/screens/Modals/Interaction/InteractionFlow/flows/Authentication.tsx
+++ src/screens/Modals/Interaction/InteractionFlow/flows/Authentication.tsx
@@ -4,20 +4,10 @@ import InteractionDescription from './components/InteractionDescription';
 import InteractionLogo from './components/InteractionLogo';
 import InteractionTitle from './components/InteractionTitle';
 import { ContainerBAS, LogoContainerBAS, Space } from './components/styled';
-
-/* --- Authentication API ---
-  <Interaction.BAS>
-    <Interaction.BAS.Logo />
-    <Interaction.BAS.Title />
-    <Interaction.BAS.Description />
-    <Interaction.BAS.Footer>
-      <Interaction.BAS.Submit label="Allow" />
-      <Interaction.BAS.Ignore />
-    </Interaction.BAS.Footer>
-  </Interaction.BAS>
-*/
+import useAuthSubmit from '~/hooks/interactions/useAuthSubmit';
 
 const Authentication = () => {
+  const handleSubmit = useAuthSubmit()
   return (
     <ContainerBAS>
       <LogoContainerBAS>
@@ -26,7 +16,7 @@ const Authentication = () => {
       <InteractionTitle />
       <InteractionDescription />
       <Space />
-      <InteractionFooter />
+      <InteractionFooter onSubmit={handleSubmit} />
     </ContainerBAS>
   )
 }
