12dd14ded6f2714d16cba6f446b6a7abcd69766f
Cristian Lungu <cristian.lungu@hsrw.org>
2021-03-09 11:22:11 +0100

    fix(offer-details): added correct types and mappings

--- src/screens/Modals/Interaction/InteractionFlow/components/card/types.ts
+++ src/screens/Modals/Interaction/InteractionFlow/components/card/types.ts
@@ -1,24 +1,20 @@
-interface IProperty {
-  key: string,
-  label: string,
-  value: string,
+export interface IProperty {
+  key: string
+  label: string
+  value: string
 }
 
 export interface IIncomingCard {
-  name: string,
-  properties: IProperty[],
+  name: string
+  properties: IProperty[]
 }
 
 export interface IIncomingRequestDocCardProps extends IIncomingCard {
-  holderName?: string,
-  highlight?: string,
+  holderName?: string
+  highlight?: string
   image?: string
 }
 
-export interface IIncomingRequestOtherProps extends IIncomingCard { }
-export interface IIncomingOfferDocProps extends IIncomingCard { }
-export interface IIncomingOfferOtherProps extends IIncomingCard { }
-
-export function isIncomingOfferCard(details: any): details is IIncomingOfferDocProps[] | IIncomingOfferOtherProps[] {
-  return typeof details.name && details.properties
-}
+export interface IIncomingRequestOtherProps extends IIncomingCard {}
+export interface IIncomingOfferDocProps extends IIncomingCard {}
+export interface IIncomingOfferOtherProps extends IIncomingCard {}
