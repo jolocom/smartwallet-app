6e5274a8d53e82f7796faaf70410a48bc6a20fd8
Cristian Lungu <cristian.lungu@hsrw.org>
2021-03-10 15:22:46 +0100

    feat(collapsible): added FlatList component

--- src/components/Collapsible/components/HidingTextContainer.tsx
+++ src/components/Collapsible/components/HidingTextContainer.tsx
@@ -2,13 +2,21 @@ import React from 'react'
 import { View, LayoutChangeEvent } from 'react-native'
 
 import { useCollapsible } from '../context'
+import { ICollapsibleComposite } from '../types'
 
 // TODO: add docs saying that this has to be a child of the scrollview
-export const HidingTextContainer: React.FC = ({ children }) => {
+export const HidingTextContainer: ICollapsibleComposite['HidingTextContainer'] = ({
+  children,
+  customStyles = {},
+}) => {
   const { setDistanceToText } = useCollapsible()
 
   const handleLayout = (e: LayoutChangeEvent) => {
     setDistanceToText(e.nativeEvent.layout.y)
   }
-  return <View onLayout={handleLayout}>{children}</View>
+  return (
+    <View style={[customStyles]} onLayout={handleLayout}>
+      {children}
+    </View>
+  )
 }
--- src/components/Collapsible/types.ts
+++ src/components/Collapsible/types.ts
@@ -1,20 +1,36 @@
-import { Animated, ScrollViewProps } from 'react-native'
+import {
+  Animated,
+  ScrollViewProps,
+  FlatListProps,
+  FlatList,
+} from 'react-native'
 import { IWithCustomStyle } from '../Card/types'
+import { ForwardRefExoticComponent, RefAttributes, ReactElement } from 'react'
 
 interface IHeaderProps extends IWithCustomStyle {
   height?: number
 }
 
-interface IScrollViewProps extends IWithCustomStyle, ScrollViewProps {
+interface IListProps {
   animatedHeader?: boolean
 }
+
+type IScrollViewProps = IWithCustomStyle & ScrollViewProps & IListProps
+
+export type IFlatListProps = IWithCustomStyle &
+  ForwardRefExoticComponent<FlatListProps<any>> &
+  RefAttributes<Animated.AnimatedComponent<typeof FlatList>> &
+  IListProps & {
+    renderHidingText: () => ReactElement
+  }
 
 export interface ICollapsibleComposite {
   Header: React.FC<IHeaderProps>
   AnimatedHeader: React.FC<IHeaderProps>
   ScrollView: React.FC<IScrollViewProps>
+  FlatList: React.FC<IFlatListProps>
   HeaderText: React.FC
-  HidingTextContainer: React.FC
+  HidingTextContainer: React.FC<IWithCustomStyle>
   HidingScale: React.FC
 }
 
