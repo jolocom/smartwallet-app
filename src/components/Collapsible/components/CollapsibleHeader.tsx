498c4fb614b10e6b814c9534c8f25f5d9cd6df25
Cristian Lungu <cristian.lungu@hsrw.org>
2021-03-12 13:04:24 +0100

    fix(collapsible): fixed not being able to tap on AnimatedHeader

--- src/components/Collapsible/components/CollapsibleHeader.tsx
+++ src/components/Collapsible/components/CollapsibleHeader.tsx
@@ -49,7 +49,6 @@ export const AnimatedHeader: ICollapsibleComposite['AnimatedHeader'] = ({
 
   return (
     <Animated.View
-      pointerEvents="none"
       style={[
         styles.container,
         styles.containerShadows,
