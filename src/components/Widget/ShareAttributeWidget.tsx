692f31c3d2a44bbe4dfd29fb1fff59adb74cdbe9
Cristian Lungu <cristian.lungu@hsrw.org>
2021-03-17 11:19:11 +0100

    fix(widget): replaced the key value from type to idx

--- src/components/Widget/ShareAttributeWidget.tsx
+++ src/components/Widget/ShareAttributeWidget.tsx
@@ -8,18 +8,17 @@ import { ScreenNames } from '~/types/screens'
 import { attributeConfig } from '~/config/claims'
 import { useCredentialShareFlow } from '~/hooks/interactions/useCredentialShareFlow'
 import { useSwitchScreens } from '~/hooks/navigation'
-
 import InteractionAttributesWidget from './InteractionAttributesWidget'
 
 const ShareAttributeWidget = () => {
-  const attributes = useSelector(getAvailableAttributesToShare);
-  
-  if (!Object.keys(attributes).length) return null;
+  const attributes = useSelector(getAvailableAttributesToShare)
 
-  const {
-    handleSelectCredential,
-  } = useCredentialShareFlow()
-  const handleScreenSwitch = useSwitchScreens(ScreenNames.InteractionAddCredential);
+  if (!Object.keys(attributes).length) return null
+
+  const { handleSelectCredential } = useCredentialShareFlow()
+  const handleScreenSwitch = useSwitchScreens(
+    ScreenNames.InteractionAddCredential,
+  )
 
   return (
     <>
@@ -31,10 +30,10 @@ const ShareAttributeWidget = () => {
           return (
             <>
               <InteractionAttributesWidget
-                key={attrType}
+                key={idx}
                 name={config.label}
                 type={attrType}
-                onAdd={() => handleScreenSwitch({type: attrType})}
+                onAdd={() => handleScreenSwitch({ type: attrType })}
                 onSelect={(attrType, id) =>
                   handleSelectCredential({ [attrType]: id })
                 }
@@ -48,7 +47,7 @@ const ShareAttributeWidget = () => {
               {idx !== arr.length - 1 ? <View style={{ height: 20 }} /> : null}
             </>
           )
-        }
+        } else return null
       })}
     </>
   )
