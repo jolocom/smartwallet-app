d9ed9b5075cd91282e82da09ca1df9b90d474f37
Cristian Lungu <cristian.lungu@hsrw.org>
2021-03-17 11:24:48 +0100

    fix(widget): added fragment key

--- src/components/Widget/ShareAttributeWidget.tsx
+++ src/components/Widget/ShareAttributeWidget.tsx
@@ -20,37 +20,33 @@ const ShareAttributeWidget = () => {
     ScreenNames.InteractionAddCredential,
   )
 
-  return (
-    <>
-      {Object.keys(attributes).map((credType, idx, arr) => {
-        const attrType = credType as AttributeTypes
-        const config = attributeConfig[attrType]
-        const attribute = attributes[attrType]
-        if (attribute) {
-          return (
-            <>
-              <InteractionAttributesWidget
-                key={idx}
-                name={config.label}
-                type={attrType}
-                onAdd={() => handleScreenSwitch({ type: attrType })}
-                onSelect={(attrType, id) =>
-                  handleSelectCredential({ [attrType]: id })
-                }
-                fields={attribute.map((attr) => ({
-                  id: attr.id,
-                  value: Object.values(attr.value).join(
-                    attrType === AttributeTypes.name ? ' ' : ', ',
-                  ),
-                }))}
-              />
-              {idx !== arr.length - 1 ? <View style={{ height: 20 }} /> : null}
-            </>
-          )
-        } else return null
-      })}
-    </>
-  )
+  return Object.keys(attributes).map((credType, idx, arr) => {
+    const attrType = credType as AttributeTypes
+    const config = attributeConfig[attrType]
+    const attribute = attributes[attrType]
+    if (attribute) {
+      return (
+        <React.Fragment key={idx}>
+          <InteractionAttributesWidget
+            key={idx}
+            name={config.label}
+            type={attrType}
+            onAdd={() => handleScreenSwitch({ type: attrType })}
+            onSelect={(attrType, id) =>
+              handleSelectCredential({ [attrType]: id })
+            }
+            fields={attribute.map((attr) => ({
+              id: attr.id,
+              value: Object.values(attr.value).join(
+                attrType === AttributeTypes.name ? ' ' : ', ',
+              ),
+            }))}
+          />
+          {idx !== arr.length - 1 ? <View style={{ height: 20 }} /> : null}
+        </React.Fragment>
+      )
+    } else return null
+  })
 }
 
 export default ShareAttributeWidget
