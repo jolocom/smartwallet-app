1f439ec825f82446cd0b35841df4e67e5ca2100c
Cristian Lungu <cristian.lungu@hsrw.org>
2021-03-23 15:00:27 +0100

    fix(cards): removed unnecessary prepareLabel and renamed util

--- src/screens/LoggedIn/Documents/utils.ts
+++ src/screens/LoggedIn/Documents/utils.ts
@@ -2,7 +2,6 @@ import { strings } from '~/translations'
 import moment from 'moment'
 
 import { ClaimKeys, DisplayCredential } from '~/types/credentials'
-import { prepareLabel } from '~/utils/stringUtils'
 
 export const filteredOptionalFields = [
   ClaimKeys.familyName,
@@ -31,9 +30,9 @@ export const getOptionalFields = <T extends DisplayCredential>(
   if (!credential.properties.length) return additionalFields
   return credential.properties
     .filter((p) => !filteredOptionalFields.includes(p.key as ClaimKeys))
-    .map((p) => ({
-      label: prepareLabel(p.label),
-      value: p.value,
+    .map(({ label, value }) => ({
+      label,
+      value,
     }))
     .concat(additionalFields)
     .slice(0, 3)
--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -3,7 +3,7 @@ export const truncateFirstWord = (text: string) => text.split(' ')[0]
 export const capitalizeWord = (word: string) =>
   word.charAt(0).toUpperCase() + word.slice(1)
 
-export const prepareLabel = (label: string): string => {
+export const camelCaseToFriendly = (label: string): string => {
   const words = label.split(/(?=[A-Z0-9])/)
   return words.length > 1
     ? words.map(capitalizeWord).join(' ')
