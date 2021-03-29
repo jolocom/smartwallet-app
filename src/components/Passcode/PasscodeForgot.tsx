0cbe9d4afd3b41a4cddba09abfa32027724099ba
Cristian Lungu <cristian.lungu@hsrw.org>
2021-03-29 16:48:48 +0200

    fix(passcode-forgot): adjusted position styles

--- src/components/Passcode/PasscodeForgot.tsx
+++ src/components/Passcode/PasscodeForgot.tsx
@@ -1,24 +1,19 @@
 import React from 'react'
-import { useKeyboard } from '@react-native-community/hooks'
 
 import { strings } from '~/translations'
-import AbsoluteBottom from '~/components/AbsoluteBottom'
 import Btn, { BtnTypes } from '~/components/Btn'
 import { useRedirectTo } from '~/hooks/navigation'
 import { ScreenNames } from '~/types/screens'
 
 const PasscodeForgot = () => {
-  const { keyboardHeight } = useKeyboard()
   const redirectToPinRecoveryInstruction = useRedirectTo(
     ScreenNames.PinRecoveryInstructions,
   )
 
   return (
-    <AbsoluteBottom customStyles={{ bottom: keyboardHeight }}>
-      <Btn type={BtnTypes.secondary} onPress={redirectToPinRecoveryInstruction}>
-        {strings.FORGOT_YOUR_PIN}
-      </Btn>
-    </AbsoluteBottom>
+    <Btn type={BtnTypes.secondary} onPress={redirectToPinRecoveryInstruction}>
+      {strings.FORGOT_YOUR_PIN}
+    </Btn>
   )
 }
 
