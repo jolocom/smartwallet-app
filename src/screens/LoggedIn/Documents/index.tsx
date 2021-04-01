e6c35ca5357fc1fb3e4bf3ce450bd1507c6b8d26
SBub <svetaibuben@gmail.com>
2021-04-01 15:11:19 +0200

    refactor(carousel docs): updating carousel styles/re-render carousel when credential change

--- src/screens/LoggedIn/Documents/index.tsx
+++ src/screens/LoggedIn/Documents/index.tsx
@@ -26,6 +26,10 @@ import { strings } from '~/translations'
 import { getOptionalFields } from './utils'
 import { CredentialRenderTypes } from 'jolocom-lib/js/interactionTokens/types'
 import { SCREEN_WIDTH } from '~/utils/dimensions'
+import JoloText from '~/components/JoloText'
+import { JoloTextSizes } from '~/utils/fonts'
+import { Colors } from '~/utils/colors'
+import BP from '~/utils/breakpoints'
 
 const CardList: React.FC = ({ children }) => {
   return (
@@ -34,7 +38,6 @@ const CardList: React.FC = ({ children }) => {
       overScrollMode={'never'}
       contentContainerStyle={{
         paddingBottom: '40%',
-        paddingHorizontal: 8,
         paddingTop: 32,
       }}
     >
@@ -44,7 +47,7 @@ const CardList: React.FC = ({ children }) => {
 }
 
 const DocumentList = () => {
-  const [cats, setCategories] = useState<
+  const [categories, setCategories] = useState<
     | CredentialsByType<DisplayCredentialDocument | DisplayCredentialOther>
     | CredentialsByIssuer<DisplayCredentialDocument | DisplayCredentialOther>
     | null
@@ -62,20 +65,23 @@ const DocumentList = () => {
     } else if (activeSubtab?.id === 'issuer') {
       setCategories(categoriesByIssuer)
     }
-  }, [activeSubtab?.id])
-
-  console.log({ cats })
+  }, [
+    activeSubtab?.id,
+    JSON.stringify(categoriesByType),
+    JSON.stringify(categoriesByIssuer),
+  ])
 
   const documents = useMemo(
-    () => (cats !== null ? cats[CredentialRenderTypes.document] : []),
-    [JSON.stringify(cats)],
+    () =>
+      categories !== null ? categories[CredentialRenderTypes.document] : [],
+    [JSON.stringify(categories)],
   )
   const other = useMemo(
-    () => (cats !== null ? cats[OtherCategory.other] : []),
-    [JSON.stringify(cats)],
+    () => (categories !== null ? categories[OtherCategory.other] : []),
+    [JSON.stringify(categories)],
   )
 
-  if (cats === null) return null
+  if (categories === null) return null
   return (
     <>
       <View
@@ -93,35 +99,54 @@ const DocumentList = () => {
         ) : (
           <CardList>
             {documents.map((d) => {
-              const { credentials } = d
+              const { credentials, value } = d
               return (
-                <Carousel
-                  activeSlideAlignment="center"
-                  data={credentials}
-                  layout="default"
-                  sliderWidth={SCREEN_WIDTH}
-                  itemWidth={SCREEN_WIDTH * 0.8}
-                  inactiveSlideOpacity={0.24}
-                  renderItem={({ item: c }) => (
-                    <DocumentCard
-                      key={c.id}
-                      id={c.id}
-                      mandatoryFields={[
-                        {
-                          label: DocumentFields.DocumentName,
-                          value: c.name ?? c.type,
-                        },
-                        {
-                          label: strings.SUBJECT_NAME,
-                          value: c.holderName,
-                        },
-                      ]}
-                      optionalFields={getOptionalFields(c)}
-                      highlight={c.id.slice(0, 14)}
-                      photo={c.photo}
-                    />
-                  )}
-                />
+                <>
+                  <ScreenContainer.Padding>
+                    {/* TODO: decide what we do with it, it does not look good */}
+                    {/* <JoloText
+                      size={JoloTextSizes.mini}
+                      color={Colors.white90}
+                      customStyles={{
+                        textAlign: 'left',
+                        marginBottom: BP({ default: 30, xsmall: 16 }),
+                      }}
+                    >
+                      {`${value}  • ${credentials.length}`}
+                    </JoloText> */}
+                  </ScreenContainer.Padding>
+                  <Carousel
+                    layoutCardOffset={0}
+                    contentContainerCustomStyle={{
+                      marginLeft: -4,
+                    }}
+                    activeSlideAlignment="center"
+                    data={credentials}
+                    layout="default"
+                    sliderWidth={SCREEN_WIDTH}
+                    itemWidth={SCREEN_WIDTH * 0.85}
+                    inactiveSlideOpacity={0.24}
+                    renderItem={({ item: c }) => (
+                      <DocumentCard
+                        key={c.id}
+                        id={c.id}
+                        mandatoryFields={[
+                          {
+                            label: DocumentFields.DocumentName,
+                            value: c.name ?? c.type,
+                          },
+                          {
+                            label: strings.SUBJECT_NAME,
+                            value: c.holderName,
+                          },
+                        ]}
+                        optionalFields={getOptionalFields(c)}
+                        highlight={c.id.slice(0, 14)}
+                        photo={c.photo}
+                      />
+                    )}
+                  />
+                </>
               )
             })}
           </CardList>
