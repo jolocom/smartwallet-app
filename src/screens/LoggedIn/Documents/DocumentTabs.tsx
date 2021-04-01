b0f1154c99acc2df4d2592f7eddc6fcb2dd0a285
Cristian Lungu <cristian.lungu@hsrw.org>
2021-04-01 16:48:31 +0200

    fix(documents): changing the active tab instead of the initialActiveTab

--- src/screens/LoggedIn/Documents/DocumentTabs.tsx
+++ src/screens/LoggedIn/Documents/DocumentTabs.tsx
@@ -1,38 +1,34 @@
 import React from 'react'
-import { useRoute, RouteProp } from '@react-navigation/native'
 
 import { strings } from '~/translations'
 import TabsContainer from '~/components/Tabs/Container'
 import { Tabs } from '~/components/Tabs/Tabs'
 import { DocumentTypes } from '~/types/credentials'
-import { MainTabsParamList } from '../MainTabs'
-import { ScreenNames } from '~/types/screens'
 
-const TABS = [
+export const documentTabs = [
   { id: DocumentTypes.document, value: strings.DOCUMENTS },
   { id: DocumentTypes.other, value: strings.OTHER },
 ]
 
-const SUBTABS = [
+export const documentSubtabs = [
   { id: 'type', value: strings.TYPE },
   { id: 'issuer', value: strings.ISSUER },
 ]
 
 const DocumentTabs: React.FC = ({ children }) => {
-  const route = useRoute<RouteProp<MainTabsParamList, ScreenNames.Documents>>()
-  const initialTabId = route.params.initialTab ?? DocumentTypes.document
-  const initialTab = TABS.find((t) => t.id === initialTabId)!
-
   return (
-    <Tabs initialActiveTab={initialTab} initialActiveSubtab={SUBTABS[0]}>
+    <Tabs
+      initialActiveTab={documentTabs[0]}
+      initialActiveSubtab={documentSubtabs[0]}
+    >
       <TabsContainer customStyles={{ marginBottom: 10 }}>
-        {TABS.map((t) => (
+        {documentTabs.map((t) => (
           <Tabs.Tab key={t.id} tab={t} />
         ))}
       </TabsContainer>
 
       <TabsContainer>
-        {SUBTABS.map((st) => (
+        {documentSubtabs.map((st) => (
           <Tabs.Subtab key={st.id} tab={st} />
         ))}
       </TabsContainer>
--- src/screens/LoggedIn/Documents/index.tsx
+++ src/screens/LoggedIn/Documents/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { ScrollView, View } from 'react-native'
+import { useRoute, RouteProp } from '@react-navigation/native'
 
 import ScreenContainer from '~/components/ScreenContainer'
 import DocumentCard from '~/components/Card/DocumentCard'
 import { useTabs } from '~/components/Tabs/context'
 import { getCustomCredentialsByCategories } from '~/modules/credentials/selectors'
-import DocumentTabs from '~/screens/LoggedIn/Documents/DocumentTabs'
+import DocumentTabs, {
+  documentTabs,
+} from '~/screens/LoggedIn/Documents/DocumentTabs'
 import OtherCard from '~/components/Card/OtherCard'
 import {
   DocumentTypes,
   DocumentFields,
-  OtherCategory,
   DisplayCredentialDocument,
   DisplayCredentialOther,
 } from '~/types/credentials'
@@ -38,15 +40,96 @@ const CardList: React.FC = ({ children }) => {
   )
 }
 
-const Documents: React.FC = () => {
+const DocumentList: React.FC = () => {
+  const route = useRoute<RouteProp<MainTabsParamList, ScreenNames.Documents>>()
+  const initialTabId = route.params.initialTab ?? DocumentTypes.document
+
   const categories = useSelector(getCustomCredentialsByCategories)
-  const { activeTab } = useTabs()
+  const { activeTab, setActiveTab } = useTabs()
+
+  useLayoutEffect(() => {
+    setActiveTab(documentTabs.find((t) => t.id === initialTabId)!)
+  }, [initialTabId])
 
   const documents = categories[
     CredentialRenderTypes.document
   ] as DisplayCredentialDocument[]
-  const other = categories[OtherCategory.other] as DisplayCredentialOther[]
+  const other = categories[DocumentTypes.other] as DisplayCredentialOther[]
 
+  return (
+    <>
+      <View
+        style={{
+          display: activeTab?.id === DocumentTypes.document ? 'flex' : 'none',
+          flex: 1,
+        }}
+        testID="document-cards-container"
+      >
+        {!documents.length ? (
+          <ScreenPlaceholder
+            title={strings.ITS_STILL_EMPTY}
+            description={strings.YOU_HAVENT_SAVED_ANY_DOCUMENTS_YET}
+          />
+        ) : (
+          <CardList>
+            {documents.map((d) => (
+              <DocumentCard
+                key={d.id}
+                id={d.id}
+                mandatoryFields={[
+                  {
+                    label: DocumentFields.DocumentName,
+                    value: d.name ?? d.type[1],
+                  },
+                  {
+                    label: strings.SUBJECT_NAME,
+                    value: d.holderName,
+                  },
+                ]}
+                optionalFields={getOptionalFields(d)}
+                highlight={d.id.slice(0, 14)}
+                photo={d.photo}
+              />
+            ))}
+          </CardList>
+        )}
+      </View>
+      <View
+        style={{
+          display: activeTab?.id === DocumentTypes.document ? 'none' : 'flex',
+          flex: 1,
+        }}
+        testID="other-cards-container"
+      >
+        {!other.length ? (
+          <ScreenPlaceholder
+            title={strings.NOTHING_HERE_YET}
+            description={strings.YOU_HAVENT_SAVED_ANYTHING_YET}
+          />
+        ) : (
+          <CardList>
+            {other.map((o) => (
+              <OtherCard
+                id={o.id}
+                key={o.id}
+                mandatoryFields={[
+                  {
+                    label: DocumentFields.DocumentName,
+                    value: o.name ?? o.type,
+                  },
+                ]}
+                optionalFields={getOptionalFields(o)}
+                photo={o.photo}
+              />
+            ))}
+          </CardList>
+        )}
+      </View>
+    </>
+  )
+}
+
+const Documents: React.FC = () => {
   return (
     <ScreenContainer
       customStyles={{
@@ -54,73 +137,7 @@ const Documents: React.FC = () => {
       }}
     >
       <DocumentTabs>
-        <View
-          style={{
-            display: activeTab?.id === DocumentTypes.document ? 'flex' : 'none',
-            flex: 1,
-          }}
-          testID="document-cards-container"
-        >
-          {!documents.length ? (
-            <ScreenPlaceholder
-              title={strings.ITS_STILL_EMPTY}
-              description={strings.YOU_HAVENT_SAVED_ANY_DOCUMENTS_YET}
-            />
-          ) : (
-            <CardList>
-              {documents.map((d) => (
-                <DocumentCard
-                  key={d.id}
-                  id={d.id}
-                  mandatoryFields={[
-                    {
-                      label: DocumentFields.DocumentName,
-                      value: d.name ?? d.type[1],
-                    },
-                    {
-                      label: strings.SUBJECT_NAME,
-                      value: d.holderName,
-                    },
-                  ]}
-                  optionalFields={getOptionalFields(d)}
-                  highlight={d.id.slice(0, 14)}
-                  photo={d.photo}
-                />
-              ))}
-            </CardList>
-          )}
-        </View>
-        <View
-          style={{
-            display: activeTab?.id === DocumentTypes.document ? 'none' : 'flex',
-            flex: 1,
-          }}
-          testID="other-cards-container"
-        >
-          {!other.length ? (
-            <ScreenPlaceholder
-              title={strings.NOTHING_HERE_YET}
-              description={strings.YOU_HAVENT_SAVED_ANYTHING_YET}
-            />
-          ) : (
-            <CardList>
-              {other.map((o) => (
-                <OtherCard
-                  id={o.id}
-                  key={o.id}
-                  mandatoryFields={[
-                    {
-                      label: DocumentFields.DocumentName,
-                      value: o.name ?? o.type,
-                    },
-                  ]}
-                  optionalFields={getOptionalFields(o)}
-                  photo={o.photo}
-                />
-              ))}
-            </CardList>
-          )}
-        </View>
+        <DocumentList />
       </DocumentTabs>
     </ScreenContainer>
   )
