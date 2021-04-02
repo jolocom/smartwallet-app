cc6bc91a3e4982442f76ab9b96d2bcc8a7943bcd
SBub <svetaibuben@gmail.com>
2021-04-02 14:18:06 +0200

    test(mocks): updating mocked values
    Updating mocked values to be able to test change pin as close as the app offers

--- __tests__/utils/setup.ts
+++ __tests__/utils/setup.ts
@@ -2,9 +2,6 @@ jest.mock('react-native-keychain', () => ({
   SECURITY_LEVEL_ANY: 'MOCK_SECURITY_LEVEL_ANY',
   SECURITY_LEVEL_SECURE_SOFTWARE: 'MOCK_SECURITY_LEVEL_SECURE_SOFTWARE',
   SECURITY_LEVEL_SECURE_HARDWARE: 'MOCK_SECURITY_LEVEL_SECURE_HARDWARE',
-  setGenericPassword: jest.fn().mockResolvedValue(true),
-  getGenericPassword: jest.fn().mockResolvedValue(true),
-  resetGenericPassword: jest.fn().mockResolvedValue(true),
 }))
 
 jest.mock('@react-navigation/native', () => ({
