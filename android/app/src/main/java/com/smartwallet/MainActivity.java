593b558a76b5580dd01edc0d5cbec669bbe54f0f
SBub <svetaibuben@gmail.com>
2021-04-13 16:26:45 +0200

    feat(splash screen): setting splash screen with boot splash

--- android/app/src/main/java/com/smartwallet/MainActivity.java
+++ android/app/src/main/java/com/smartwallet/MainActivity.java
@@ -1,6 +1,8 @@
 package com.smartwallet;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +13,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "SmartWallet";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this);
   }
 }
