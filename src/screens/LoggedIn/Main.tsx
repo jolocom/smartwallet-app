1c4046dc473f66fbfbddda26402fe4f9785cc0b9
Cristian Lungu <cristian.lungu@hsrw.org>
2021-04-13 15:26:22 +0200

    fix(dev-settings): added missing InteractionCardTest screen

--- src/screens/LoggedIn/Main.tsx
+++ src/screens/LoggedIn/Main.tsx
@@ -32,6 +32,7 @@ import CredentialForm from '../Modals/Forms/CredentialForm'
 import { PrimitiveAttributeTypes } from '~/types/credentials'
 import { IField } from '~/components/Card/types'
 import CardDetails from './Documents/CardDetails'
+import InteractionTest from './Settings/Development/InteractionCardsTest'
 
 export type MainStackParamList = {
   [ScreenNames.Interaction]: undefined
@@ -62,6 +63,7 @@ export type MainStackParamList = {
   [ScreenNames.InputTest]: undefined
   [ScreenNames.PasscodeTest]: undefined
   [ScreenNames.BusinessCardTest]: undefined
+  [ScreenNames.InteractionCardsTest]: undefined
 }
 
 const MainStack = createStackNavigator<MainStackParamList>()
@@ -176,6 +178,11 @@ const Main: React.FC = () => {
               <MainStack.Screen
                 name={ScreenNames.BusinessCardTest}
                 component={BusinessCardTest}
+                options={settingsScreenTransitionOptions}
+              />
+              <MainStack.Screen
+                name={ScreenNames.InteractionCardsTest}
+                component={InteractionTest}
                 options={settingsScreenTransitionOptions}
               />
             </>
