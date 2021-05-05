5d0752595eaa832498891b042254655d4eccc573
Cristian Lungu <cristian.lungu@hsrw.org>
2021-05-05 13:48:12 +0200

    feat(clear-identity): reset navigation and credentials/attributes stores

--- src/screens/LoggedIn/Settings/components/ClearIdentityBtn.tsx
+++ src/screens/LoggedIn/Settings/components/ClearIdentityBtn.tsx
@@ -1,37 +1,48 @@
 import React from 'react'
 import { Alert, StyleSheet, View } from 'react-native'
+import { useDispatch } from 'react-redux'
 
 import JoloText from '~/components/JoloText'
 import { useLoader } from '~/hooks/loader'
-import { useRedirect } from '~/hooks/navigation'
+import { useResetNavigation } from '~/hooks/navigation'
 import { useAgent } from '~/hooks/sdk'
 import { strings } from '~/translations'
-import { ScreenNames } from '~/types/screens'
 import { Colors } from '~/utils/colors'
 import { JoloTextSizes } from '~/utils/fonts'
+import { resetAttrs } from '~/modules/attributes/actions'
+import { resetCredentials } from '~/modules/credentials/actions'
 
 const ClearIdentityBtn = () => {
   const agent = useAgent()
   const loader = useLoader()
-  const redirect = useRedirect()
+  const resetNavigation = useResetNavigation()
+  const dispatch = useDispatch()
+
+  const clearIdentityData = () => {
+    loader(
+      async () => {
+        await agent.delete({ identity: false, encryptedWallet: false })
+      },
+      {},
+      (err) => {
+        if (!err) {
+          // NOTE: resetting navigation to unmount the @History tab, in case it
+          // was previously mounted
+          resetNavigation()
+
+          // NOTE: resetting the credentials and attributes stores
+          dispatch(resetAttrs())
+          dispatch(resetCredentials())
+        }
+      },
+    )
+  }
 
   const handlePress = async () => {
     Alert.alert(strings.CLEAR_IDENTITY_TITLE, strings.CLEAR_IDENTITY_MESSAGE, [
       {
         text: strings.CLEAR_IDENTITY_CONFIRM,
-        onPress: () => {
-          loader(
-            async () => {
-              await agent.delete({ identity: false, encryptedWallet: false })
-            },
-            {},
-            (err) => {
-              if (!err) {
-                redirect(ScreenNames.Identity)
-              }
-            },
-          )
-        },
+        onPress: clearIdentityData,
       },
       {
         text: strings.CLEAR_IDENTITY_CANCEL,
