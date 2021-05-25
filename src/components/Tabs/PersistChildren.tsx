dec8fef72299022ba9b6d8c98f2a3418e5b16404
Cristian Lungu <cristian.lungu@hsrw.org>
2021-05-25 14:11:03 +0200

    fix(history): removed debug

--- src/components/Tabs/PersistChildren.tsx
+++ src/components/Tabs/PersistChildren.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import { ITabPersistChildren } from './types'
-import { debugView } from '~/utils/dev'
 
 const PersistChildren: React.FC<ITabPersistChildren> = ({
   children,
