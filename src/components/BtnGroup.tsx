b807b6bff05a36771f989b86c78dd333098788d5
Cristian Lungu <cristian.lungu@hsrw.org>
2021-05-26 21:48:03 +0200

    refactor(btngroup): removed alignment props

--- src/components/BtnGroup.tsx
+++ src/components/BtnGroup.tsx
@@ -1,38 +1,14 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 
-export enum BtnsAlignment {
-  vertical,
-  horizontal,
-}
-
-interface PropsI {
-  alignment?: BtnsAlignment
-}
-
-const BtnGroup: React.FC<PropsI> = ({
-  alignment = BtnsAlignment.vertical,
-  children,
-}) => {
-  return (
-    <View
-      style={[
-        styles.container,
-        alignment === BtnsAlignment.horizontal && styles.horizontal,
-      ]}
-    >
-      {children}
-    </View>
-  )
+const BtnGroup: React.FC = ({ children }) => {
+  return <View style={styles.container}>{children}</View>
 }
 
 const styles = StyleSheet.create({
   container: {
     width: '100%',
     marginBottom: 16,
-  },
-  horizontal: {
-    flexDirection: 'row',
   },
 })
 
