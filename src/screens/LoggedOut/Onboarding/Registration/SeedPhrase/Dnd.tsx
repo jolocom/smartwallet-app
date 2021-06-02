e0747c261ef2800f659a758aa80bc4275c2ebde8
Cristian Lungu <cristian.lungu@hsrw.org>
2021-06-02 12:57:16 +0300

    fix(seed-repeat): align the ios dnd more centrally

--- src/screens/LoggedOut/Onboarding/Registration/SeedPhrase/Dnd.tsx
+++ src/screens/LoggedOut/Onboarding/Registration/SeedPhrase/Dnd.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { View } from 'react-native'
 import Pills from './draggable/ios/Pills'
 import { IDndProps } from './types'
 
 const Dnd: React.FC<IDndProps> = ({ tags, updateTags }) => {
-  return <Pills tags={tags} updateTags={updateTags} />
+  return (
+    <View style={{ flex: 1, paddingTop: '30%' }}>
+      <Pills tags={tags} updateTags={updateTags} />
+    </View>
+  )
 }
 
 export default Dnd
