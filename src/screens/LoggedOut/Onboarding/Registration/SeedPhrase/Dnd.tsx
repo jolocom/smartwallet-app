dba79c3b4fd3f69e3dc8b3dab0fdcdade4205c81
Cristian Lungu <cristian.lungu@hsrw.org>
2021-06-03 11:56:21 +0300

    fix(seed-repeat): phrase moving down if the order was wrong

--- src/screens/LoggedOut/Onboarding/Registration/SeedPhrase/Dnd.tsx
+++ src/screens/LoggedOut/Onboarding/Registration/SeedPhrase/Dnd.tsx
@@ -5,7 +5,13 @@ import { IDndProps } from './types'
 
 const Dnd: React.FC<IDndProps> = ({ tags, updateTags }) => {
   return (
-    <View style={{ flex: 1, paddingTop: '30%' }}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'flex-end',
+        paddingBottom: '60%',
+      }}
+    >
       <Pills tags={tags} updateTags={updateTags} />
     </View>
   )
