9a90eae3f2276c53a81bb09bb7d15d6c95ccafb7
Cristian Lungu <cristian.lungu@hsrw.org>
2021-06-04 12:55:56 +0300

    fix(seedphrase): disable gestures within the registration stack

--- src/screens/LoggedOut/Onboarding/Registration/index.tsx
+++ src/screens/LoggedOut/Onboarding/Registration/index.tsx
@@ -16,10 +16,12 @@ const Registration: React.FC = () => {
       <Stack.Screen
         name={ScreenNames.SeedPhraseWrite}
         component={SeedPhraseWrite}
+        options={{ gestureEnabled: false }}
       />
       <Stack.Screen
         name={ScreenNames.SeedPhraseRepeat}
         component={SeedPhraseRepeat}
+        options={{ gestureEnabled: false }}
       />
     </Stack.Navigator>
   )
