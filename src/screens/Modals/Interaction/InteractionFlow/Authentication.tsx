db31b8f14a2926f8d8c2ed7f39b949c756dbd323
Cristian Lungu <cristian.lungu@hsrw.org>
2021-06-15 15:17:48 +0200

    feat(auth): using the description field when present and not empty

--- src/screens/Modals/Interaction/InteractionFlow/Authentication.tsx
+++ src/screens/Modals/Interaction/InteractionFlow/Authentication.tsx
@@ -8,9 +8,13 @@ import InteractionTitle from './components/InteractionTitle'
 import { ContainerBAS, LogoContainerBAS } from './components/styled'
 import useAuthSubmit from '~/hooks/interactions/useAuthSubmit'
 import { strings } from '~/translations'
+import { useSelector } from 'react-redux'
+import { getAuthenticationDetails } from '~/modules/interaction/selectors'
 
 const Authentication = () => {
   const handleSubmit = useAuthSubmit()
+  const { description } = useSelector(getAuthenticationDetails)
+
   return (
     <ContainerBAS>
       <LogoContainerBAS>
@@ -18,7 +22,11 @@ const Authentication = () => {
       </LogoContainerBAS>
       <InteractionTitle label={strings.IS_IT_REALLY_YOU} />
       <InteractionDescription
-        label={strings.SERVICE_WOULD_LIKE_TO_CONFIRM_YOUR_DIGITAL_IDENTITY}
+        label={
+          description && !!description.length
+            ? description
+            : strings.SERVICE_WOULD_LIKE_TO_CONFIRM_YOUR_DIGITAL_IDENTITY
+        }
       />
       <Space />
       <InteractionFooter
