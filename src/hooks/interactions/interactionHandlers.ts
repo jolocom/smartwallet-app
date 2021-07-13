e3f959071e400b2f5fc3c0d125de3580fe53bdec
SBub <svetaibuben@gmail.com>
2021-07-13 12:20:52 +0200

    refactor(middleware): separating validation of credential constrains into its own file

--- src/hooks/interactions/interactionHandlers.ts
+++ src/hooks/interactions/interactionHandlers.ts
@@ -2,130 +2,18 @@ import {
   AuthenticationFlowState,
   AuthorizationFlowState,
   CredentialOfferFlowState,
-  CredentialRequestFlowState,
 } from '@jolocom/sdk/js/interactionManager/types'
 
-import { Agent, FlowType, Interaction } from 'react-native-jolocom'
+import { FlowType, Interaction } from 'react-native-jolocom'
 import {
   getCredentialCategory,
-  mapAttributesToDisplay,
-  mapCredentialsToDisplay,
-  separateCredentialsAndAttributes,
-  sortCredentialsByRecentIssueDate,
 } from '../signedCredentials/utils'
-import { CredentialRequest } from 'jolocom-lib/js/interactionTokens/credentialRequest'
-import { AttributeTypes } from '~/types/credentials'
-import { CredentialIssuer } from '@jolocom/sdk/js/credentials'
-import { SignedCredential } from 'jolocom-lib/js/credentials/signedCredential/signedCredential'
-import { SWErrorCodes } from '~/errors/codes'
 import { useAgent } from '../sdk'
 import useTranslation from '~/hooks/useTranslation'
 import { useToasts } from '../toasts'
 import { strings } from '~/translations'
 import truncateDid from '~/utils/truncateDid'
-
-class CredentialRequestHandler {
-  #requestedCredentials: SignedCredential[]
-  #validatedCredentials: SignedCredential[]
-  public missingCredentialTypes: string[] = []
-
-  constructor(
-    public interaction: Interaction,
-    public credentials: CredentialIssuer,
-  ) {
-    this.#requestedCredentials = []
-    this.#validatedCredentials = []
-  }
-
-  private get requestedCredentialTypes(): string[][] {
-    const state = this.interaction.getSummary()
-      .state as CredentialRequestFlowState
-    const { constraints } = state
-    const { requestedCredentialTypes } = constraints[0]
-    return requestedCredentialTypes
-  }
-
-  private get requestedTypes(): string[] {
-    return this.requestedCredentialTypes.map((t) => t[1])
-  }
-
-  async getStoredRequestedCredentials() {
-    const typesQuery = this.requestedCredentialTypes.map((t) => ({ type: t }))
-    // TODO: this returns not correct number of credentials
-    // because first types of constraint and actual credential
-    // do not match Credentials vs VerifiableCredential
-    const requestedCredentials = await this.credentials.query(typesQuery)
-
-    // TODO: remove after fixed issue with sdk credential query
-    const correctRequestedCredentials = requestedCredentials.filter((c) => {
-      if (this.requestedCredentialTypes.find((rt) => rt[1] === c.type[1])) {
-        return true
-      }
-      return false
-    })
-
-    this.#requestedCredentials = sortCredentialsByRecentIssueDate(
-      correctRequestedCredentials,
-    )
-    return this
-  }
-
-  validateAgainstConstrains() {
-    // FIX: this returns not correct number of credentials
-    // because first types of constraint and actual credential
-    // do not match Credentials vs VerifiableCredential
-
-    this.#validatedCredentials = (
-      this.interaction.getMessages()[0].interactionToken as CredentialRequest
-    )
-      .applyConstraints(this.#requestedCredentials.map((c) => c.toJSON()))
-      .map((c) => SignedCredential.fromJSON(c))
-
-    return this
-  }
-
-  checkForMissingServiceIssuedCredentials() {
-    const missingServiceIssuedCredentials = this.requestedTypes.filter((t) => {
-      if (Object.values(AttributeTypes).includes(t as AttributeTypes)) {
-        /**
-         * credential is a self issued credential -> exclude it
-         */
-        return false
-      } else {
-        /**
-         * credential is a service issued credential -> check its presence in validated credentials
-         */
-        return !Boolean(this.#validatedCredentials.find((c) => c.type[1] === t))
-      }
-    })
-
-    if (!!missingServiceIssuedCredentials.length) {
-      this.missingCredentialTypes = missingServiceIssuedCredentials
-    }
-
-    return this
-  }
-
-  async prepareCredentialsForUI(did: string) {
-    // TODO: there is a hook we can use useCredentials().separateSignedTransformToUI
-    const {
-      credentials: validatedServiceIssuedC,
-      selfIssuedCredentials: validatedSelfIssuedC,
-    } = separateCredentialsAndAttributes(this.#validatedCredentials, did)
-
-    const displayCredentials = await Promise.all(
-      validatedServiceIssuedC.map((c) =>
-        mapCredentialsToDisplay(this.credentials, c),
-      ),
-    )
-    return {
-      credentials: displayCredentials,
-      attributes: mapAttributesToDisplay(validatedSelfIssuedC),
-      requestedTypes: this.requestedTypes,
-      selectedCredentials: {},
-    }
-  }
-}
+import { CredentialRequestHandler } from '~/middleware/interaction/credentialRequestConstrains'
 
 const authenticationHandler = (state: AuthenticationFlowState) => ({
   description: state.description,
