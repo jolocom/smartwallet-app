a91388746c2e36b7ce95eba66ce135eb058fc2bd
Cristian Lungu <cristian.lungu@hsrw.org>
2021-07-14 10:24:32 +0200

    refactor(history): translated the record config

--- src/hooks/history/index.ts
+++ src/hooks/history/index.ts
@@ -4,9 +4,10 @@ import { useAgent } from '~/hooks/sdk'
 import { IRecordDetails, IPreLoadedInteraction } from '~/types/records'
 import { getDateSection } from './utils'
 import { RecordAssembler } from '~/middleware/records/recordAssembler'
-import { recordConfig } from '~/config/records'
+import useTranslation from '../useTranslation'
 
 export const useHistory = () => {
+  const { t } = useTranslation()
   const agent = useAgent()
 
   const getSectionDetails = (interaction: Interaction) => {
@@ -52,6 +53,72 @@ export const useHistory = () => {
     records: IPreLoadedInteraction[],
   ) => [getSectionDetails(interaction), ...records]
 
+  const getConfig = () => {
+    // NOTE: the first unfinished step will never be used, due to the fact
+    // that there is always a request i.e. first step.
+    const unfinishStep = (text: string) =>
+      t('History.notFinishedStepHeader', {
+        text: text.toLowerCase(),
+      })
+
+    return {
+      [FlowType.Authentication]: {
+        title: t('History.authenticationHeader'),
+        steps: {
+          finished: [
+            t('History.authenticationRequestStepHeader'),
+            t('History.authResponseStepHeader'),
+          ],
+          unfinished: [
+            unfinishStep(t('History.authenticationRequestStepHeader')),
+            unfinishStep(t('History.authResponseStepHeader')),
+          ],
+        },
+      },
+      [FlowType.Authorization]: {
+        title: t('History.authzHeader'),
+        steps: {
+          finished: [
+            t('History.authzRequestStepHeader'),
+            t('History.authzResponseStepHeader'),
+          ],
+          unfinished: [
+            unfinishStep(t('History.authzRequestStepHeader')),
+            unfinishStep(t('History.authzResponseStepHeader')),
+          ],
+        },
+      },
+      [FlowType.CredentialOffer]: {
+        title: t('History.credentialOfferHeader'),
+        steps: {
+          finished: [
+            t('History.offerRequestStepHeader'),
+            t('History.offerResponseStepHeader'),
+            t('History.offerReceiveStepHeader'),
+          ],
+          unfinished: [
+            unfinishStep(t('History.offerRequestStepHeader')),
+            unfinishStep(t('History.offerResponseStepHeader')),
+            unfinishStep(t('History.offerReceiveStepHeader')),
+          ],
+        },
+      },
+      [FlowType.CredentialShare]: {
+        title: t('History.credShareHeader'),
+        steps: {
+          finished: [
+            t('History.credShareRequestStepHeader'),
+            t('History.credShareResponseStepHeader'),
+          ],
+          unfinished: [
+            unfinishStep(t('History.credShareRequestStepHeader')),
+            unfinishStep(t('History.credShareResponseStepHeader')),
+          ],
+        },
+      },
+    }
+  }
+
   const assembleInteractionDetails = async (
     nonce: string,
   ): Promise<IRecordDetails> => {
@@ -64,7 +131,7 @@ export const useHistory = () => {
       summary: interaction.getSummary(),
       lastMessageDate: issued,
       expirationDate: expires,
-      config: recordConfig,
+      config: getConfig(),
     })
 
     return recordAssembler.getRecordDetails()
