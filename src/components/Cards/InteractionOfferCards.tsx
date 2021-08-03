d454b643ac70e0314626debf65e9599b84d8d02a
SBub <svetaibuben@gmail.com>
2021-08-03 02:48:00 +0200

    feat(offer-cards): adding document and other offer cards

--- src/components/Cards/InteractionOfferCards.tsx
+++ src/components/Cards/InteractionOfferCards.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { DisplayVal } from '@jolocom/sdk/js/credentials'
-import InteractionCardDoc from '~/assets/svg/InteractionCardDoc'
 import { Colors } from '~/utils/colors'
 import { strings } from '~/translations'
 import { sharedStyles } from './sharedStyle'
+import InteractionCardDoc from '~/assets/svg/InteractionCardDoc'
+import InteractionCardOther from '~/assets/svg/InteractionCardOther'
 
 type InteractionOfferDocumentCardProps = {
   credentialName: string
@@ -16,68 +17,113 @@ type InteractionOfferDocumentCardProps = {
  * - View with padding-bottom transform to Space component
  */
 export const InteractionOfferDocumentCard: React.FC<InteractionOfferDocumentCardProps> =
-  ({ credentialName, fields }) => {
-    return (
-      <InteractionCardDoc>
-        <View style={styles.documentBodyContainer}>
+  ({ credentialName, fields }) => (
+    <InteractionCardDoc>
+      <View style={styles.documentBodyContainer}>
+        <Text style={[sharedStyles.regularText, styles.credentialName]}>
+          {credentialName}
+        </Text>
+        <View style={{ paddingBottom: 16 }} />
+        <Text style={[sharedStyles.regularText, styles.fieldSectionTitle]}>
+          {strings.INCLUDED_INFO}:
+        </Text>
+        <View style={{ paddingBottom: 11 }} />
+        {fields.length ? (
+          fields.slice(0, 3).map((f, idx) => (
+            <>
+              {idx !== 0 && <View style={{ paddingBottom: 4 }} />}
+              <Text
+                style={[sharedStyles.regularText, sharedStyles.fieldLabelSmall]}
+              >
+                {f.label}
+              </Text>
+              <View style={{ paddingBottom: 0.5 }} />
+              <View style={styles.documentValuePlaceholder} />
+            </>
+          ))
+        ) : (
           <Text
-            style={[sharedStyles.regularText, styles.documentCredentialName]}
+            style={[sharedStyles.regularText, sharedStyles.fieldLabelSmall]}
           >
-            {credentialName}
+            {strings.NO_INFO_THAT_CAN_BE_PREVIEWED}
           </Text>
-          <View style={{ paddingBottom: 16 }} />
-          <Text style={[sharedStyles.regularText, styles.fieldSectionTitle]}>
-            {strings.INCLUDED_INFO}:
+        )}
+      </View>
+    </InteractionCardDoc>
+  )
+
+type InteractionOfferOtherCardProps = {
+  credentialName: string
+  fields: Array<Required<Pick<DisplayVal, 'label'>>>
+}
+
+// TODO: ask Liz to make available paddings for other card
+// label size is different in other and document is it intentional ?
+
+// TODO: this is very similar to document card - abstract
+export const InteractionOfferOtherCard: React.FC<InteractionOfferOtherCardProps> =
+  ({ credentialName, fields }) => (
+    <InteractionCardOther>
+      <View style={styles.otherBodyContainer}>
+        <Text style={[sharedStyles.regularText, styles.credentialName]}>
+          {credentialName}
+        </Text>
+        <View style={{ paddingBottom: 16 }} />
+        <Text style={[sharedStyles.regularText, styles.fieldSectionTitle]}>
+          {strings.INCLUDED_INFO}:
+        </Text>
+        <View style={{ paddingBottom: 11 }} />
+        {fields.length ? (
+          fields.slice(0, 3).map((f, idx) => (
+            <>
+              {idx !== 0 && <View style={{ paddingBottom: 4 }} />}
+              <Text
+                style={[sharedStyles.regularText, sharedStyles.fieldLabelSmall]}
+              >
+                {f.label}
+              </Text>
+              <View style={{ paddingBottom: 0.5 }} />
+              <View style={styles.documentValuePlaceholder} />
+            </>
+          ))
+        ) : (
+          <Text
+            style={[sharedStyles.regularText, sharedStyles.fieldLabelSmall]}
+          >
+            {strings.NO_INFO_THAT_CAN_BE_PREVIEWED}
           </Text>
-          <View style={{ paddingBottom: 11 }} />
-          {fields.length ? (
-            fields.slice(0, 3).map((f, idx) => (
-              <>
-                {idx !== 0 && <View style={{ paddingBottom: 4 }} />}
-                <Text
-                  style={[
-                    sharedStyles.regularText,
-                    sharedStyles.fieldLabelSmall,
-                  ]}
-                >
-                  {f.label}
-                </Text>
-                <View style={{ paddingBottom: 0.5 }} />
-                <View style={styles.documentValuePlaceholder} />
-              </>
-            ))
-          ) : (
-            <Text
-              style={[sharedStyles.regularText, sharedStyles.fieldLabelSmall]}
-            >
-              {strings.NO_INFO_THAT_CAN_BE_PREVIEWED}
-            </Text>
-          )}
-        </View>
-      </InteractionCardDoc>
-    )
-  }
+        )}
+      </View>
+    </InteractionCardOther>
+  )
 
 const styles = StyleSheet.create({
   fieldSectionTitle: {
     fontSize: 16,
+  },
+  credentialName: {
+    fontSize: 22,
+    lineHeight: 24,
+    letterSpacing: 0.15,
+    color: Colors.black80,
   },
   documentBodyContainer: {
     paddingBottom: 21,
     paddingTop: 16,
     paddingHorizontal: 14,
   },
-  documentCredentialName: {
-    fontSize: 22,
-    lineHeight: 24,
-    letterSpacing: 0.15,
-    color: Colors.black80,
-  },
+
   documentValuePlaceholder: {
     width: '45.8%',
     height: 20,
     borderRadius: 5,
     opacity: 0.57,
     backgroundColor: Colors.alto,
+  },
+  otherBodyContainer: {
+    // TODO: update once LIZ will provide values
+    paddingBottom: 21,
+    paddingTop: 16,
+    paddingHorizontal: 14,
   },
 })
