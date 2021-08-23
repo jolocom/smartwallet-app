1af3792f47075b6261cc035444ae2a94e1d4a45e
SBub <svetaibuben@gmail.com>
2021-08-23 11:48:01 +0200

    feat(use-settings): hook to store and retrieve setting keys

---
+++ src/hooks/settings.ts
@@ -0,0 +1,24 @@
+import { Agent } from 'react-native-jolocom'
+import { useAgent } from './sdk'
+
+export enum SettingKeys {
+  pinNrAttemptsLeft = 'pinNrAttemptsLeft',
+}
+
+const handleRetrieveSettingValue = (agent: Agent) => {
+  return agent.storage.get.setting
+}
+
+const handleSetSettingValue = (agent: Agent) => {
+  return async (key: SettingKeys, value: object) =>
+    await agent.storage.store.setting(key, value)
+}
+
+// TODO: use this hook in all places we set/get setting values
+export const useSettings = () => {
+  const agent = useAgent()
+  return {
+    get: handleRetrieveSettingValue(agent),
+    set: handleSetSettingValue(agent),
+  }
+}
