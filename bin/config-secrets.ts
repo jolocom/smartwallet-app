d006178ae3334f557ba584b372722df28add8a06
Cristian Lungu <cristian.lungu@hsrw.org>
2021-09-28 14:51:54 +0200

    build(config): script for generating sentry.properties

--- bin/config-secrets.ts
+++ bin/config-secrets.ts
@@ -1,5 +1,5 @@
 import fs from 'fs'
-import { stringify as stringifyEnv } from 'envfile'
+import { stringify as stringifyEnv, parse as parseEnv, Data } from 'envfile'
 import shell from 'shelljs'
 
 const cloneRepo = async (remote: string, repoName: string) => {
@@ -24,26 +24,76 @@ const catFile = async (path: string) => {
   })
 }
 
-const writeEnv = async (data: object) => {
+const writeEnv = async (path: string, data: object) => {
   const env = stringifyEnv(data)
 
   return new Promise((res, rej) => {
-    fs.writeFile('.env', env, (err) => {
+    fs.writeFile(path, env, (err) => {
       if (err) rej(err)
       else res(undefined)
     })
   })
 }
 
-const main = async () => {
+const readEnv = async (path: string) => {
+  return new Promise<Data>((res, rej) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        console.log(
+          '\x1b[4;31m%s\x1b[0m',
+          'Error! Could not find the environmental config (.env).',
+        )
+      } else res(parseEnv(data))
+    })
+  })
+}
+
+const configureEnv = async () => {
   const path = await cloneRepo('dev@hetz1.jolocom.io', 'common-secrets')
   const sentryDSN = await catFile(path + '/sentry/dsn.txt')
+  const sentryAuthToken = await catFile(path + '/sentry/auth-token.txt')
   const branchLiveKey = await catFile(path + '/branch/key-live.txt')
 
-  await writeEnv({
+  console.log('Writing secrets to the environmental config (.env)')
+  await writeEnv('.env', {
     SENTRY_DSN: sentryDSN,
+    SENTRY_AUTH_TOKEN: sentryAuthToken,
     BRANCH_LIVE_KEY: branchLiveKey,
   })
+}
+
+const configureSentry = async () => {
+  const { SENTRY_AUTH_TOKEN } = await readEnv('.env')
+
+  if (!SENTRY_AUTH_TOKEN || !SENTRY_AUTH_TOKEN.length) {
+    console.log(
+      '\x1b[4;31m%s\x1b[0m',
+      'Error! Could not find the Sentry Authentication token in the environmental config.',
+    )
+    return
+  }
+
+  const sentryProperties = {
+    'defaults.url': 'https://sentry.io/',
+    'defaults.org': 'jolocom',
+    'defaults.project': 'smartwallet',
+    'http.keepalive': 'false',
+    'auth.token': SENTRY_AUTH_TOKEN,
+  }
+
+  console.log('Writing the Sentry config (sentry.properties) for Android')
+  await writeEnv('./android/sentry.properties', sentryProperties)
+  console.log('Writing the Sentry config (sentry.properties) for iOS')
+  await writeEnv('./ios/sentry.properties', sentryProperties)
+}
+
+const main = async () => {
+  if (process.argv.length === 2) {
+    await configureEnv()
+    await configureSentry()
+  } else if (process.argv.includes('sentry')) {
+    await configureSentry()
+  }
 }
 
 main()
