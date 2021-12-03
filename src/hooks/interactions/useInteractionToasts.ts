49ee1e470aaeebf602334cd59afd80308ab37a95
Cristian Lungu <cristian.lungu@hsrw.org>
2021-12-03 13:44:38 +0100

    feat(interactions): added toast that redirects the user on success

--- src/hooks/interactions/useInteractionToasts.ts
+++ src/hooks/interactions/useInteractionToasts.ts
@@ -1,17 +1,38 @@
+import { Linking } from 'react-native'
+import { useSelector } from 'react-redux'
 import { useToasts } from '~/hooks/toasts'
+import { getRedirectUrl } from '~/modules/interaction/selectors'
 import { ToastBody } from '~/types/toasts'
 import useTranslation from '../useTranslation'
 
 const useInteractionToasts = () => {
   const { t } = useTranslation()
   const { scheduleInfo } = useToasts()
+  const redirectUrl = useSelector(getRedirectUrl)
 
-  const scheduleSuccessInteraction = (config?: Partial<ToastBody>) =>
-    scheduleInfo({
-      title: t('Toasts.successfulInteractionTitle'),
-      message: t('Toasts.successfulInteractionMsg'),
-      ...config,
-    })
+  const scheduleSuccessInteraction = (config?: Partial<ToastBody>) => {
+    if (redirectUrl) {
+      // TODO: should the config overwrite the redirect toast?
+      scheduleInfo({
+        title: t('Toasts.successfulInteractionTitle'),
+        message: 'You should return to back to the requester',
+        interact: {
+          label: 'Return',
+          onInteract: () => {
+            Linking.canOpenURL(redirectUrl).then((can) => {
+              if (can) Linking.openURL(redirectUrl)
+            })
+          },
+        },
+      })
+    } else {
+      scheduleInfo({
+        title: t('Toasts.successfulInteractionTitle'),
+        message: t('Toasts.successfulInteractionMsg'),
+        ...config,
+      })
+    }
+  }
 
   return {
     scheduleSuccessInteraction,
