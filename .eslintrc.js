bec7e0b65edefc4e50cbf19a754d824193ff620c
SBub <svetaibuben@gmail.com>
2022-01-10 11:30:52 +0100

    chore(eslintrc): overwriting config, removing rules
    config file is overwirtten by running `npx eslint --init`;
    rules were removed and will be added incrementally upon discussion;
    commented out lines are left for the research;

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,76 +1,50 @@
 module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended', // yes
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    // 'airbnb',
+    // Requires eslint-plugin-import package;
+    // /errors and /warnings are combined in one rule set: plugin:import/recommended
+    // 'plugin:import/errors',
+    // 'plugin:import/warnings',
+    // Below adds typescript support to eslint-plugin-import
+    // 'plugin:import/typescript',
+    'prettier',
+    // 'prettier/react',
+    // prettier/@typescript-eslint',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    // '__DEV__': 'readonly'
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
-    // allows imports
+    project: './tsconfig.json', // maybe
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'react'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    // turn on eslint-plugin-prettier and eslint-config-prettier
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
   rules: {
-    'arrow-body-style': ['warn', 'as-needed'],
-    // curly braces for if, else etc.
-    curly: ['error', 'multi-line'],
-    'default-case': ['error'],
-    eqeqeq: ['error', 'smart'],
-    // for...in will normally include properties in prototype chain, require if
-    'guard-for-in': ['error'],
-    // I don't think we even use labels
-    'no-extra-label': ['error'],
-    // not sure about this, but replaces tslint's `no-arg` rule
-    'no-caller': ['error'],
-    // transfered from tslint's `no-conditional-assignment` rule
-    'no-cond-assign': ['error'],
-    'no-debugger': ['error'],
-    // similar to tslint's `no-construct` rule
-    'no-new-wrappers': ['error'],
-    // replacement for `no-string-throw`
-    'no-throw-literal': ['error'],
-    'no-unused-expressions': ['error'],
-    'no-var': ['error'],
-    // this isn't a tslint thing, but have copied over
-    'new-parens': ['error'],
-    'object-shorthand': ['error'],
-    'prefer-arrow-callback': ['error'],
-    'prefer-const': ['error'],
-    semi: ['error', 'never'],
-    'use-isnan': ['error'],
-    '@typescript-eslint/array-type': ['error', 'array-simple'],
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        types: {
-          Object: 'Use {} instead.',
-          String: "Use 'string' instead.",
-          Number: "Use 'number' instead.",
-          Boolean: "Use 'boolean' instead.",
-        },
-      },
-    ],
-    'react/jsx-uses-vars': ['warn'],
-    // PascalCase for classes
-    '@typescript-eslint/class-name-casing': ['error'],
-    // don't prefix interface names with 'I'
-    '@typescript-eslint/interface-name-prefix': ['error', 'never'],
-    // don't conflict <Types> and JSX
-    '@typescript-eslint/no-angle-bracket-type-assertion': ['error'],
-    // lose out on typing benefits with any
-    '@typescript-eslint/no-explicit-any': ['error'],
-    '@typescript-eslint/no-empty-interface': ['off'],
-    '@typescript-eslint/no-inferrable-types': ['error'],
-    // namespaces and modules are outdated, use ES6 style
-    '@typescript-eslint/no-namespace': ['error'],
-    // use ES6-style imports instead
-    '@typescript-eslint/no-triple-slash-reference': ['error'],
-    '@typescript-eslint/no-var-requires': ['off'],
-    '@typescript-eslint/no-use-before-define': ['off'],
-    '@typescript-eslint/explicit-function-return-type': ['off'],
-    'import/no-duplicates': ['error'],
+    // Typescript
+    '@typescript-eslint/explicit-function-return-type': 'off',
+
+    // React
+    'react/prop-types': 'off',
   },
-  ignorePatterns: ['package.json', 'jest.config.js', 'app.json'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 }
