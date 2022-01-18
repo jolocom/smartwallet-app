977cb2488fae6309c39f1f1b9871d6097daefad2
SBub <svetaibuben@gmail.com>
2022-01-18 01:20:01 +0100

    test(ausweis-puk-info): testing ausweis puk info screen

---
+++ __tests__/suits/Ausweis/AusweisPukInfo.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useGoBack } from '~/hooks/navigation'
+import { AusweisPukInfo } from '~/screens/LoggedIn/eID/components'
+import { renderWithSafeArea } from '../../utils/renderWithSafeArea'
+
+jest.mock('../../../src/hooks/navigation')
+
+describe('Ausweis PUK info screen', () => {
+  beforeAll(() => {
+    ;(useGoBack as jest.Mock).mockReturnValue(jest.fn())
+  })
+  afterAll(() => {
+    jest.resetAllMocks()
+  })
+  test('is displayed according to the designs', () => {
+    const { toJSON } = renderWithSafeArea(<AusweisPukInfo />)
+    expect(toJSON()).toMatchSnapshot()
+  })
+})
