b9ba914078f0ede046af580f00b8ba6e00480c9d
SBub <svetaibuben@gmail.com>
2022-02-01 01:53:04 +0100

    style(seed-phrase): moving seed phrase positioned to the top on repeat and write screens

--- src/screens/LoggedOut/Onboarding/Registration/SeedPhrase/Dnd.tsx
+++ src/screens/LoggedOut/Onboarding/Registration/SeedPhrase/Dnd.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import { View } from 'react-native'
+import { debugView } from '~/utils/dev'
 import Pills from './draggable/ios/Pills'
 import { IDndProps } from './types'
 
 const Dnd: React.FC<IDndProps> = ({ tags, updateTags }) => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'flex-end',
-        paddingBottom: '60%',
-      }}
-    >
+    <View>
       <Pills tags={tags} updateTags={updateTags} />
     </View>
   )
