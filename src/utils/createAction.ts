2d175cf9b294683855ad69c9e7925dc49c86dd30
SBub <svetaibuben@gmail.com>
2022-02-21 23:03:49 +0100

    refactor(create-action): redefining fn types to tight action type and payload under the same type

--- src/utils/createAction.ts
+++ src/utils/createAction.ts
@@ -1,9 +1,14 @@
-const createAction = <T, P>(type: T) => {
-  const actionCreator = (payload: P) => ({
+type Action = {
+  type: unknown
+  payload: unknown
+}
+
+const createAction = <A extends Action>(type: A['type']) => {
+  const actionCreator = (payload: A['payload']) => ({
     type,
     payload,
   })
-
+  // TODO: check if type prop is being used anywhere
   actionCreator.type = type
 
   return actionCreator
