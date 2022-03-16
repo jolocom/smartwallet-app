360f1c6e396aabb57459a3b150aba108d8243617
SBub <svetaibuben@gmail.com>
2022-03-16 14:05:46 +0100

    chore(eslintrc): removing duplicate declarations

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,26 +3,14 @@ module.exports = {
     es6: true, // ???
     'react-native/react-native': true,
   },
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
-    ecmaVersion: 2018, // ???
-    sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'react-hooks',
-    'react-native',
-    '@typescript-eslint',
-    'import',
-    'prettier',
-  ],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react-native/all',
-    'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:import/recommended',
     'plugin:prettier/recommended',
@@ -33,10 +21,6 @@ module.exports = {
 
     // React-Native
     'react-native/no-raw-text': 'off',
-
-    // Prettier
-    // Report prettier errors as an ESLint error
-    'prettier/prettier': 'error',
 
     // Typescript
     '@typescript-eslint/explicit-function-return-type': 'off',
