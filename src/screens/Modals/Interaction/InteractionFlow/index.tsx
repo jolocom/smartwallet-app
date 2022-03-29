d72eb7a86153a8e0cec86257ee5f0679683b6465
SBub <svetaibuben@gmail.com>
2022-03-29 10:43:33 +0200

    refactor(interaction-flow): using redefined BottomSheet component in the interaction flow

--- src/screens/Modals/Interaction/InteractionFlow/index.tsx
+++ src/screens/Modals/Interaction/InteractionFlow/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
 import { useSelector } from 'react-redux'
 import { FlowType } from 'react-native-jolocom'
 import { useBackHandler } from '@react-native-community/hooks'
@@ -13,8 +12,7 @@ import CredentialOffer from './CredentialOffer'
 import { getInteractionType } from '~/modules/interaction/selectors'
 
 import { useFinishInteraction } from '~/hooks/interactions/handlers'
-import ScreenDismissArea from '~/components/ScreenDismissArea'
-import { Colors } from '~/utils/colors'
+import BottomSheet from '~/components/BottomSheet'
 
 const InteractionFlow: React.FC = () => {
   const interactionType = useSelector(getInteractionType)
@@ -50,27 +48,13 @@ const InteractionFlow: React.FC = () => {
     finishInteraction()
   }
 
+  // TODO: we are rendering the bottom sheet also for the full
+  // screen interactions
   return (
-    <View style={styles.fullScreen}>
-      <ScreenDismissArea onDismiss={handleDismissInteraction} />
-      <View style={styles.interactionBody}>{renderInteractionBody()}</View>
-    </View>
+    <BottomSheet onDismiss={handleDismissInteraction}>
+      {renderInteractionBody()}
+    </BottomSheet>
   )
 }
-
-const styles = StyleSheet.create({
-  fullScreen: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: Colors.black65,
-  },
-  tapArea: {
-    flex: 1,
-  },
-  interactionBody: {
-    flex: 0,
-    alignItems: 'center',
-  },
-})
 
 export default InteractionFlow
