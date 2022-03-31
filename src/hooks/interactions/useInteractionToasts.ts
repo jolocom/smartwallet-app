886c65c4ab9b033e9a69d4bf62ddc57d8fd34371
SBub <svetaibuben@gmail.com>
2022-03-31 09:19:57 +0200

    refactor(use-interaction-toast): using correct values for the redirect params

--- src/hooks/interactions/useInteractionToasts.ts
+++ src/hooks/interactions/useInteractionToasts.ts
@@ -1,13 +1,18 @@
 import { StackActions, useNavigation } from '@react-navigation/native'
 import { useEffect, useRef } from 'react'
 import { Linking } from 'react-native'
+import { IdentitySummary } from 'react-native-jolocom'
 import { useSelector } from 'react-redux'
 import { SWErrorCodes } from '~/errors/codes'
 import { useToasts } from '~/hooks/toasts'
-import { getRedirectUrl } from '~/modules/interaction/selectors'
+import {
+  getInteractionCounterparty,
+  getRedirectUrl,
+} from '~/modules/interaction/selectors'
 import { ScreenNames } from '~/types/screens'
 import { ToastBody } from '~/types/toasts'
 import { getCounterpartyName } from '~/utils/dataMapping'
+import truncateDid from '~/utils/truncateDid'
 import useTranslation from '../useTranslation'
 import { useFinishInteraction } from './handlers'
 
@@ -29,9 +34,18 @@ export const useCompleteInteraction = (
   const { t } = useTranslation()
   const { scheduleInfo, scheduleErrorWarning } = useToasts()
   const { closeInteraction, clearInteraction } = useFinishInteraction()
-  const redirectUrl = useSelector(getRedirectUrl)
-  const serviceName = useSelector(getCounterpartyName)
   const navigation = useNavigation()
+
+  // This value will be used to "remember" interaction counterparty before its reset in the store
+  const counterpartyInfo =
+    useRef<{ counterparty: IdentitySummary; redirectUrl: string | null }>()
+  const shadowRedirectUrl = useRef<string | null>(null)
+  const redirectUrl = useSelector(getRedirectUrl)
+  const counterparty = useSelector(getInteractionCounterparty)
+
+  useEffect(() => {
+    shadowRedirectUrl.current = redirectUrl
+  }, [redirectUrl])
 
   const completeInteractionObj = useRef<AsyncGenerator>()
 
@@ -47,6 +61,12 @@ export const useCompleteInteraction = (
     screenNavigateToAfterInteraction: CompleteInteraction['screenNavigateToAfterInteraction'],
   ) {
     try {
+      // Store counterparty information to be used in the redirct url
+      counterpartyInfo.current = {
+        counterparty,
+        redirectUrl: shadowRedirectUrl.current,
+      }
+
       const config = await handleInteraction()
 
       // NOTE: anywhere where the interaction has to pause before completing return from the interaction handler terminate property of value true,i.e. in "renegotiation" scenario
@@ -56,14 +76,17 @@ export const useCompleteInteraction = (
 
       clearInteraction()
 
-      //if (!!redirectUrl && (await Linking.canOpenURL(redirectUrl))) {
-      yield navigation.dispatch(
-        StackActions.replace(ScreenNames.InteractionRedirect, {
-          serviceName,
-          completeRedirect: completeInteraction,
-        }),
-      )
-      //}
+      if (
+        counterpartyInfo.current?.redirectUrl &&
+        (await Linking.canOpenURL(counterpartyInfo.current?.redirectUrl))
+      ) {
+        yield navigation.dispatch(
+          StackActions.replace(ScreenNames.InteractionRedirect, {
+            ...counterpartyInfo.current,
+            completeRedirect: completeInteraction,
+          }),
+        )
+      }
 
       scheduleInfo({
         title: t('Toasts.successfulInteractionTitle'),
@@ -75,7 +98,9 @@ export const useCompleteInteraction = (
         scheduleErrorWarning(e, {
           title: t('Toasts.offerInvalidDocsTitle'),
           message: t('Toasts.offerInvalidDocsMsg', {
-            serviceName,
+            serviceName: getCounterpartyName(
+              counterpartyInfo.current?.counterparty,
+            ),
           }),
         })
       } else {
