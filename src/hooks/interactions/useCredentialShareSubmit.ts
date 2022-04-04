b8f210fec1e56cc95a1b8937fd514231eb1a674d
Cristian Lungu <cristian.lungu@hsrw.org>
2022-04-04 17:03:23 +0200

    refactor(credential-share): adjusted complete interaction hook

--- src/hooks/interactions/useCredentialShareSubmit.ts
+++ src/hooks/interactions/useCredentialShareSubmit.ts
@@ -1,22 +1,15 @@
 import { useCredentialShareFlow } from './useCredentialShareFlow'
 import { useCompleteInteraction } from './useCompleteInteraction'
+import { ScreenNames } from '~/types/screens'
 
 const useCredentialShareSubmit = () => {
   const { assembleShareResponseToken } = useCredentialShareFlow()
   const { completeInteraction } = useCompleteInteraction(async () => {
     await assembleShareResponseToken()
 
-    /*
-     * NOTE:
-     * 1. in case you want to override a general
-     * interaction success toast, return an object
-     * with `successToast` property.
-     * 2. in case you choose to pause an execution of
-     * the interaction flow and inject custom logic
-     * to be run meanwhile return an object
-     * `{pause: true, pauseFn: () => void}`
-     */
-    return undefined
+    return {
+      screenToNavigate: ScreenNames.History,
+    }
   })
 
   return completeInteraction
