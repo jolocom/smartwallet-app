692f715816c15aa50a630e2a85c69298e7fb78ee
Cristian Lungu <cristian.lungu@hsrw.org>
2022-04-04 17:01:32 +0200

    refactor(usecompleteinteraction): removed unnecessary use of generators

--- src/hooks/interactions/useCompleteInteraction.ts
+++ src/hooks/interactions/useCompleteInteraction.ts
@@ -1,7 +1,6 @@
 import { StackActions, useNavigation } from '@react-navigation/native'
 import { useEffect, useRef } from 'react'
 import { Linking } from 'react-native'
-import { IdentitySummary } from 'react-native-jolocom'
 import { useSelector } from 'react-redux'
 import { SWErrorCodes } from '~/errors/codes'
 import { useToasts } from '~/hooks/toasts'
@@ -22,8 +21,7 @@ import { useFinishInteraction } from './handlers'
  */
 interface Config {
   successToast?: Partial<ToastBody>
-  pause?: boolean
-  pauseHandler?: () => void
+  screenToNavigate?: ScreenNames
 }
 
 /**
@@ -32,124 +30,62 @@ interface Config {
  * @param screenNavigateToAfterInteraction - after an interaction has completed user will be navigated to this screen is provided
  */
 interface CompleteInteraction {
-  handleInteraction: () => Promise<Config | undefined>
-  screenNavigateToAfterInteraction?: ScreenNames
+  handleInteraction: () => Promise<Config | void>
 }
 
 export const useCompleteInteraction = (
   handleInteraction: CompleteInteraction['handleInteraction'],
-  screenNavigateToAfterInteraction?: CompleteInteraction['screenNavigateToAfterInteraction'],
 ) => {
   const { t } = useTranslation()
   const { scheduleInfo, scheduleErrorWarning } = useToasts()
   const { closeInteraction, clearInteraction } = useFinishInteraction()
   const navigation = useNavigation()
 
-  // This value will be used to "remember" interaction counterparty before its reset in the store
-  const counterpartyInfo =
-    useRef<{ counterparty: IdentitySummary; redirectUrl: string | null }>()
-  const shadowRedirectUrl = useRef<string | null>(null)
   const redirectUrl = useSelector(getRedirectUrl)
   const counterparty = useSelector(getInteractionCounterparty)
 
-  useEffect(() => {
-    shadowRedirectUrl.current = redirectUrl
-  }, [redirectUrl])
-
-  /*
-   * Creating a generator object to iterate over the generator `makeCompleteInteraction`
-   */
-  const completeInteractionObj = useRef<AsyncGenerator>()
-
-  useEffect(() => {
-    completeInteractionObj.current = makeCompleteInteraction(
-      handleInteraction,
-      screenNavigateToAfterInteraction,
-    )
-  }, [])
-
-  const makeCompleteInteraction = async function* (
-    handleInteraction: CompleteInteraction['handleInteraction'],
-    screenNavigateToAfterInteraction: CompleteInteraction['screenNavigateToAfterInteraction'],
-  ) {
+  const completeInteraction = async () => {
     try {
-      // Store counterparty information to be used in the redirct url
-      counterpartyInfo.current = {
-        counterparty,
-        redirectUrl: shadowRedirectUrl.current,
-      }
-
-      /**
-       * returning object with `successToken` property will override the default success token;
-       * returning object with `pause` and `pauseHandler` will pause interaction complete execution
-       */
       const config = await handleInteraction()
 
-      /**
-       * NOTE: anywhere where the interaction has to pause before completing
-       * return from the interaction handler pause property of value true,i.e. in "renegotiation" scenario
-       */
-      if (config?.pause === true) {
-        yield config?.pauseHandler && config?.pauseHandler()
+      const cleanInteraction = () => {
+        clearInteraction()
+        closeInteraction(config?.screenToNavigate)
+
+        scheduleInfo({
+          title: t('Toasts.successfulInteractionTitle'),
+          message: t('Toasts.successfulInteractionMsg'),
+          ...config?.successToast,
+        })
       }
 
-      /**
-       * NOTE: interaction is reset. If you won't reset it here, the interaction sheet will
-       * always be rendered on top of any other screen if the interaction details values are still in the store
-       */
-      clearInteraction()
-
-      /**
-       * Pause interaction complete if the redirectUrl was provided by a counterparty
-       */
-      if (
-        counterpartyInfo.current?.redirectUrl &&
-        (await Linking.canOpenURL(counterpartyInfo.current?.redirectUrl))
-      ) {
-        yield navigation.dispatch(
+      if (redirectUrl && (await Linking.canOpenURL(redirectUrl))) {
+        navigation.dispatch(
           StackActions.replace(ScreenNames.InteractionRedirect, {
-            ...counterpartyInfo.current,
-            completeRedirect: completeInteraction,
+            redirectUrl,
+            counterparty,
+            completeRedirect: cleanInteraction,
           }),
         )
+      } else {
+        cleanInteraction()
       }
-
-      scheduleInfo({
-        title: t('Toasts.successfulInteractionTitle'),
-        message: t('Toasts.successfulInteractionMsg'),
-        ...config?.successToast,
-      })
     } catch (e) {
       clearInteraction()
 
-      // TODO: define custom wallet Error class to declaratively use toast body
-      if (e.message === SWErrorCodes.SWInteractionOfferAllInvalid) {
-        scheduleErrorWarning(e, {
-          title: t('Toasts.offerInvalidDocsTitle'),
-          message: t('Toasts.offerInvalidDocsMsg', {
-            serviceName: getCounterpartyName(
-              counterpartyInfo.current?.counterparty,
-            ),
-          }),
-        })
-      } else {
-        scheduleErrorWarning(e)
+      if (e instanceof Error) {
+        if (e.message === SWErrorCodes.SWInteractionOfferAllInvalid) {
+          scheduleErrorWarning(e, {
+            title: t('Toasts.offerInvalidDocsTitle'),
+            message: t('Toasts.offerInvalidDocsMsg', {
+              serviceName: getCounterpartyName(counterparty),
+            }),
+          })
+        } else {
+          scheduleErrorWarning(e)
+        }
       }
-    } finally {
-      // TODO: perhaps we don't want to navigate user to a different screen in an interaction failed
-      closeInteraction(screenNavigateToAfterInteraction)
     }
-  }
-
-  /**
-   * NOTE: this function will start an interaction flow or
-   * will continue an interaction complete flow if it was paused by `yield` keyword
-   */
-  const completeInteraction = async () => {
-    if (completeInteractionObj.current?.next === undefined) {
-      throw new Error('Generator object was not populated')
-    }
-    await completeInteractionObj.current.next()
   }
 
   return { completeInteraction }
