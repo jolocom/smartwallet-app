caa2819913bced2e7e0d2afe9c203461a37187b4
Thomas Wallner <thomas.wallner23@gmail.com>
2022-04-05 14:19:13 +0200

    fix(interactions/useauthzsubmit): adding scheduleErrorWarning to scheduleSuccessInteraction

--- src/hooks/interactions/useAuthzSubmit.ts
+++ src/hooks/interactions/useAuthzSubmit.ts
@@ -17,7 +17,7 @@ const useAuthzSubmit = () => {
       await agent.processJWT(authzResponse)
       await interaction.send(authzResponse)
 
-      scheduleSuccessInteraction()
+      scheduleSuccessInteraction().catch(scheduleErrorWarning)
     } catch (e) {
       scheduleErrorWarning(e)
     } finally {
