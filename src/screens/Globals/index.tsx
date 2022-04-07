80716407fffaa3edb0bcd8e2a440ba8e87f4a437
Thomas Wallner <thomas.wallner23@gmail.com>
2022-04-07 16:54:06 +0200

    fix(globals/index): created new folder for global stack

---
+++ src/screens/Globals/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const ModalStack = createStackNavigator()
+
+const GlobalModals = () => {
+  return (
+    // <ModalStack.Navigator>
+    //   <ModalStack.Screen />
+    //   <ModalStack.Screen />
+    //   <ModalStack.Screen />
+    // </ModalStack.Navigator>
+    <></>
+  )
+}
