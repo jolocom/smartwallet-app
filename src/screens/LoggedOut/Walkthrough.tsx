c1b96aa6a515090506dc44605bf03c5f4a7faec7
Thomas Wallner <thomas.wallner23@gmail.com>
2022-04-08 18:42:32 +0200

    fix(walkthrough): changing navigation from idle to TermsConsent
    in order for the user to get directed to the right screen to accept terms and privacy policy when
    initially setting up the wallet

--- src/screens/LoggedOut/Walkthrough.tsx
+++ src/screens/LoggedOut/Walkthrough.tsx
@@ -65,7 +65,9 @@ const useWalkthroughProceed = () => {
       if (isTermsConsentOutdated) {
         dispatch(setTermsConsentVisibility(true))
         // Idle screen serves as a background when terms consent is hiding, so we don't see for a fraction of a second the Walkthrough screen
-        navigation.navigate(ScreenNames.Idle)
+        navigation.navigate(ScreenNames.GlobalModals, {
+          screen: ScreenNames.TermsConsent,
+        })
         pendingActionRef.current = cb
       } else {
         cb()
