af51c888255a980bf1313ff2e395f71668b2af37
Thomas Wallner <thomas.wallner23@gmail.com>
2022-04-12 12:23:15 +0200

    refactor(termsconsent screens): adding new props to TermsTemplate component

--- src/screens/Globals/TermsConsent/PrivacyPolicy.tsx
+++ src/screens/Globals/TermsConsent/PrivacyPolicy.tsx
@@ -3,7 +3,11 @@ import { privacyPolicyEN, privacyPolicyDE } from '~/translations'
 import TermsTemplate from './components/TermsAndPrivacyDetails'
 
 const PrivacyPolicy: React.FC = () => (
-  <TermsTemplate title={'Privacy Policy'} text={privacyPolicyEN} />
+  <TermsTemplate
+    title={'Privacy Policy'}
+    deText={privacyPolicyDE}
+    enText={privacyPolicyEN}
+  />
 )
 
 export default PrivacyPolicy
--- src/screens/Globals/TermsConsent/TermsOfService.tsx
+++ src/screens/Globals/TermsConsent/TermsOfService.tsx
@@ -3,7 +3,11 @@ import { termsOfServiceEN, termsOfServiceDE } from '~/translations'
 import TermsTemplate from './components/TermsAndPrivacyDetails'
 
 const TermsOfService: React.FC = () => (
-  <TermsTemplate title={'Terms of Service'} text={termsOfServiceEN} />
+  <TermsTemplate
+    title={'Terms of Service'}
+    deText={termsOfServiceDE}
+    enText={termsOfServiceEN}
+  />
 )
 
 export default TermsOfService
