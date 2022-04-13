7ecd00b96e8fb0b0c704715a2f04fa6a77f8dcfb
Thomas Wallner <thomas.wallner23@gmail.com>
2022-04-13 16:32:28 +0200

    refactor(privacy-policy & terms od service): adding useTranslation hook for headers

--- src/screens/Globals/TermsConsent/PrivacyPolicy.tsx
+++ src/screens/Globals/TermsConsent/PrivacyPolicy.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { privacyPolicyEN, privacyPolicyDE } from '~/translations'
 import TermsTemplate from './components/TermsAndPrivacyDetails'
+import useTranslation from '~/hooks/useTranslation'
 
-const PrivacyPolicy: React.FC = () => (
-  <TermsTemplate
-    title={'Privacy Policy'}
-    deText={privacyPolicyDE}
-    enText={privacyPolicyEN}
-  />
-)
+const PrivacyPolicy: React.FC = () => {
+  const { t } = useTranslation()
+
+  return (
+    <TermsTemplate
+      title={t('PrivacyPolicy.header')}
+      deText={privacyPolicyDE}
+      enText={privacyPolicyEN}
+    />
+  )
+}
 
 export default PrivacyPolicy
--- src/screens/Globals/TermsConsent/TermsOfService.tsx
+++ src/screens/Globals/TermsConsent/TermsOfService.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { termsOfServiceEN, termsOfServiceDE } from '~/translations'
 import TermsTemplate from './components/TermsAndPrivacyDetails'
+import useTranslation from '~/hooks/useTranslation'
 
-const TermsOfService: React.FC = () => (
-  <TermsTemplate
-    title={'Terms of Service'}
-    deText={termsOfServiceDE}
-    enText={termsOfServiceEN}
-  />
-)
+const TermsOfService: React.FC = () => {
+  const { t } = useTranslation()
+
+  return (
+    <TermsTemplate
+      title={t('Terms of Service.header')}
+      deText={termsOfServiceDE}
+      enText={termsOfServiceEN}
+    />
+  )
+}
 
 export default TermsOfService
