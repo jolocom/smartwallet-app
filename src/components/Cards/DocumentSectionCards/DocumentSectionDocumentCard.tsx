3001534f3e8870f33be5ae5e9fabe1266a2e497c
Cristian Lungu <cristian.lungu@hsrw.org>
2022-05-13 18:56:14 +0200

    feat(document-card): adapted header to design

--- src/components/Cards/DocumentSectionCards/DocumentSectionDocumentCard.tsx
+++ src/components/Cards/DocumentSectionCards/DocumentSectionDocumentCard.tsx
@@ -1,153 +1,23 @@
-import React, { useState, StyleProp, ViewStyle } from 'react'
-import { View, Image, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet } from 'react-native'
 
-import DocumentCardMedium from '~/assets/svg/DocumentCardMedium'
-import ScaledCard, { ScaledText, ScaledView } from '../ScaledCard'
-import { useCredentialNameScale, usePruneFields } from '../hooks'
-import { Colors } from '~/utils/colors'
-import { Fonts } from '~/utils/fonts'
+import ScaledCard, { ScaledView } from '../ScaledCard'
+import { useCredentialNameScale } from '../hooks'
 import {
   ORIGINAL_DOCUMENT_CARD_HEIGHT,
   ORIGINAL_DOCUMENT_CARD_WIDTH,
   ORIGINAL_DOCUMENT_SCREEN_WIDTH,
 } from './consts'
-import { CardMoreBtn } from './components'
+import {
+  CardMoreBtn,
+  DocumentFields,
+  DocumentFooter,
+  DocumentHeader,
+  DocumentHolderName,
+  DocumentPhoto,
+} from './components'
 import { DocumentCardProps } from './types'
-import { FieldsCalculator } from '../InteractionShare/components'
 import { TextLayoutEvent } from '~/types/props'
-import { debugView } from '~/utils/dev'
-import { DisplayVal } from '@jolocom/sdk/js/credentials'
-
-const DocumentFooter: React.FC<{
-  rightIcons?: string[]
-  leftIcons?: string[]
-  style?: StyleProp<ViewStyle>
-}> = ({ rightIcons, leftIcons, style = {} }) => {
-  //TODO use icons
-  return (
-    <ScaledView
-      style={[styles.footerContainer, style]}
-      scaleStyle={styles.footerContainerScaled}
-    >
-      <View style={styles.footerBorder} />
-    </ScaledView>
-  )
-}
-
-const DocumentPhoto: React.FC<{ photo: string }> = ({ photo }) => (
-  <ScaledView
-    scaleStyle={styles.photoContainerScaled}
-    style={styles.photoContainer}
-  >
-    <Image resizeMode="cover" style={styles.photo} source={{ uri: photo }} />
-  </ScaledView>
-)
-
-const DocumentHeader: React.FC<{ name: string; icon?: string }> = ({
-  name,
-  icon,
-}) => {
-  const { isCredentialNameScaled, handleCredentialNameTextLayout } =
-    useCredentialNameScale()
-
-  return (
-    <View style={{ flexDirection: 'row', width: '100%' }}>
-      <ScaledText
-        // @ts-expect-error
-        onTextLayout={handleCredentialNameTextLayout}
-        numberOfLines={1}
-        scaleStyle={
-          isCredentialNameScaled
-            ? styles.credentialNameScaled
-            : styles.credentialName
-        }
-        style={[styles.regularText, { flex: 0.863 }]}
-      >
-        {name}
-      </ScaledText>
-    </View>
-  )
-}
-
-const DocumentHolderName: React.FC<{
-  name: string
-  onLayout: (e: TextLayoutEvent) => void
-}> = ({ name, onLayout }) => {
-  const { isCredentialNameScaled } = useCredentialNameScale()
-  return (
-    <>
-      <ScaledView
-        scaleStyle={{ paddingBottom: isCredentialNameScaled ? 22 : 16 }}
-      />
-      <ScaledView
-        scaleStyle={{
-          paddingHorizontal: 10,
-        }}
-      >
-        <ScaledText
-          // @ts-expect-error
-          onTextLayout={onLayout}
-          numberOfLines={2}
-          style={styles.mediumText}
-          scaleStyle={styles.holderName}
-        >
-          {name}
-        </ScaledText>
-      </ScaledView>
-    </>
-  )
-}
-
-const DocumentFields: React.FC<{
-  fields: Required<DisplayVal>[]
-  maxFields: number
-  maxLines: number
-}> = ({ fields, maxLines, maxFields }) => {
-  const {
-    displayedFields,
-    handleFieldValueLayout,
-    handleFieldValuesVisibility,
-  } = usePruneFields(fields, maxFields, maxLines)
-
-  return (
-    <FieldsCalculator cbFieldsVisibility={handleFieldValuesVisibility}>
-      {displayedFields.map((f, idx) => (
-        <React.Fragment key={f.key}>
-          {idx !== 0 && <ScaledView scaleStyle={{ paddingBottom: 14 }} />}
-          <ScaledText
-            numberOfLines={1}
-            style={[
-              styles.regularText,
-              {
-                width: '100%',
-              },
-            ]}
-            scaleStyle={styles.fieldLabel}
-          >
-            {f.label.trim()}:
-          </ScaledText>
-          <ScaledView scaleStyle={{ paddingBottom: 9 }} />
-          <ScaledText
-            numberOfLines={2}
-            //@ts-expect-error
-            onTextLayout={(e: TextLayoutEvent) =>
-              handleFieldValueLayout(e, idx)
-            }
-            scaleStyle={styles.fieldText}
-            style={[
-              styles.mediumText,
-              {
-                width: '100%',
-              },
-            ]}
-          >
-            {f.value}
-          </ScaledText>
-        </React.Fragment>
-      ))}
-    </FieldsCalculator>
-  )
-}
 
 const DocumentSectionDocumentCard: React.FC<DocumentCardProps> = ({
   credentialName,
@@ -173,6 +43,10 @@ const DocumentSectionDocumentCard: React.FC<DocumentCardProps> = ({
     originalScreenWidth: ORIGINAL_DOCUMENT_SCREEN_WIDTH,
   }
 
+  const MOCK_STYLES = {
+    icon: 'https://play-lh.googleusercontent.com/iPqyCoNDLdqRpykOWskqVynbgjPwcp-n8-HZjirdqq9VB39rCcPBneu3zMHL5Wadgmw',
+  }
+
   return (
     <ScaledCard
       originalHeight={scalingConfig.originalHeight}
@@ -195,7 +69,11 @@ const DocumentSectionDocumentCard: React.FC<DocumentCardProps> = ({
           }}
           scaleStyle={styles.bodyContainer}
         >
-          <DocumentHeader name={credentialName} />
+          <DocumentHeader
+            name={credentialName}
+            icon={MOCK_STYLES.icon}
+            onPressMenu={onHandleMore}
+          />
           {holderName && (
             <DocumentHolderName
               name={holderName}
@@ -212,83 +90,13 @@ const DocumentSectionDocumentCard: React.FC<DocumentCardProps> = ({
         <DocumentFooter />
       </View>
       {photo && <DocumentPhoto photo={photo} />}
-      <CardMoreBtn
-        onPress={onHandleMore}
-        positionStyles={{
-          top: 18,
-          right: 17,
-        }}
-      />
     </ScaledCard>
   )
 }
 
 const styles = StyleSheet.create({
   bodyContainer: {
-    paddingTop: 22,
-    paddingHorizontal: 14,
-    paddingBottom: 14,
-    //flex: 1,
-  },
-  credentialName: {
-    fontSize: 28,
-    lineHeight: 28,
-  },
-  credentialNameScaled: {
-    fontSize: 22,
-    lineHeight: 22,
-  },
-  holderName: {
-    fontSize: 20,
-    lineHeight: 20,
-  },
-  photoContainer: {
-    position: 'absolute',
-    overflow: 'hidden',
-    zIndex: 10,
-  },
-  photoContainerScaled: {
-    bottom: 27,
-    right: 14,
-    width: 82,
-    height: 82,
-    borderRadius: 41,
-  },
-  photo: {
-    width: '100%',
-    height: '100%',
-  },
-  footerContainerScaled: {
-    height: 60,
-    paddingHorizontal: 20,
-  },
-  footerContainer: {
-    width: '100%',
-    zIndex: 9,
-  },
-  footerBorder: {
-    width: '100%',
-    borderTopWidth: 1,
-    borderTopColor: '#D8D8D8',
-  },
-  regularText: {
-    fontFamily: Fonts.Regular,
-    fontSize: 22,
-    color: Colors.black,
-  },
-  mediumText: {
-    fontFamily: Fonts.Medium,
-    color: Colors.black,
-  },
-  fieldLabel: {
-    fontSize: 16,
-    lineHeight: 16,
-    color: Colors.slateGray,
-  },
-  fieldText: {
-    fontSize: 20,
-    lineHeight: 20,
-    letterSpacing: 0.14,
+    paddingHorizontal: 16,
   },
 })
 
--- src/components/Cards/DocumentSectionCards/components.tsx
+++ src/components/Cards/DocumentSectionCards/components.tsx
@@ -1,17 +1,30 @@
+import { DisplayVal } from '@jolocom/sdk/js/credentials'
 import React from 'react'
-import { StyleSheet, TouchableOpacity, ViewStyle } from 'react-native'
+import {
+  Image,
+  StyleProp,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from 'react-native'
+import { TextLayoutEvent } from '~/types/props'
 
 import { Colors } from '~/utils/colors'
-import { ScaledView } from '../ScaledCard'
+import { debugView } from '~/utils/dev'
+import { Fonts } from '~/utils/fonts'
+import { useCredentialNameScale, usePruneFields } from '../hooks'
+import { FieldsCalculator } from '../InteractionShare/components'
+import { ScaledText, ScaledView } from '../ScaledCard'
 
 interface Props {
   onPress: () => void
   positionStyles: Partial<Pick<ViewStyle, 'left' | 'right' | 'top' | 'bottom'>>
 }
 
-export const CardMoreBtn: React.FC<Props> = ({ onPress, positionStyles }) => (
+export const CardMoreBtn: React.FC<Props> = ({ onPress }) => (
   <ScaledView
-    scaleStyle={[styles.dotsContainerScaled, positionStyles]}
+    scaleStyle={styles.dotsContainerScaled}
     style={styles.dotsContainer}
   >
     <TouchableOpacity onPress={onPress} style={styles.dotsBtn}>
@@ -22,19 +35,172 @@ export const CardMoreBtn: React.FC<Props> = ({ onPress, positionStyles }) => (
   </ScaledView>
 )
 
+export const DocumentFooter: React.FC<{
+  rightIcons?: string[]
+  leftIcons?: string[]
+  style?: StyleProp<ViewStyle>
+}> = ({ rightIcons, leftIcons, style = {} }) => {
+  //TODO use icons
+  return (
+    <ScaledView
+      style={[styles.footerContainer, style]}
+      scaleStyle={styles.footerContainerScaled}
+    >
+      <View style={styles.footerBorder} />
+    </ScaledView>
+  )
+}
+
+export const DocumentPhoto: React.FC<{ photo: string }> = ({ photo }) => (
+  <ScaledView
+    scaleStyle={styles.photoContainerScaled}
+    style={styles.photoContainer}
+  >
+    <Image resizeMode="cover" style={styles.photo} source={{ uri: photo }} />
+  </ScaledView>
+)
+
+export const DocumentHeader: React.FC<{
+  name: string
+  icon?: string
+  onPressMenu: () => void
+}> = ({ name, icon, onPressMenu }) => {
+  const { handleCredentialNameTextLayout } = useCredentialNameScale()
+
+  return (
+    <View style={styles.headerContainer}>
+      {icon && (
+        <ScaledView
+          scaleStyle={{
+            width: 32,
+            height: 32,
+            marginRight: 10,
+          }}
+        >
+          <Image
+            resizeMode="cover"
+            style={[styles.photo, { borderRadius: 4.2 }]}
+            source={{ uri: icon }}
+          />
+        </ScaledView>
+      )}
+      <ScaledText
+        // @ts-expect-error
+        onTextLayout={handleCredentialNameTextLayout}
+        numberOfLines={1}
+        scaleStyle={styles.credentialName}
+        style={[styles.mediumText, { flex: 1 }]}
+      >
+        {name}
+      </ScaledText>
+      <CardMoreBtn
+        onPress={onPressMenu}
+        positionStyles={{
+          top: 18,
+          right: 17,
+        }}
+      />
+    </View>
+  )
+}
+
+export const DocumentHolderName: React.FC<{
+  name: string
+  onLayout: (e: TextLayoutEvent) => void
+}> = ({ name, onLayout }) => {
+  const { isCredentialNameScaled } = useCredentialNameScale()
+  return (
+    <>
+      <ScaledView
+        scaleStyle={{ paddingBottom: isCredentialNameScaled ? 22 : 16 }}
+      />
+      <ScaledView
+        scaleStyle={{
+          paddingHorizontal: 10,
+        }}
+      >
+        <ScaledText
+          // @ts-expect-error
+          onTextLayout={onLayout}
+          numberOfLines={2}
+          style={styles.mediumText}
+          scaleStyle={styles.holderName}
+        >
+          {name}
+        </ScaledText>
+      </ScaledView>
+    </>
+  )
+}
+
+export const DocumentFields: React.FC<{
+  fields: Required<DisplayVal>[]
+  maxFields: number
+  maxLines: number
+}> = ({ fields, maxLines, maxFields }) => {
+  const {
+    displayedFields,
+    handleFieldValueLayout,
+    handleFieldValuesVisibility,
+  } = usePruneFields(fields, maxFields, maxLines)
+
+  return (
+    <FieldsCalculator cbFieldsVisibility={handleFieldValuesVisibility}>
+      {displayedFields.map((f, idx) => (
+        <React.Fragment key={f.key}>
+          {idx !== 0 && <ScaledView scaleStyle={{ paddingBottom: 14 }} />}
+          <ScaledText
+            numberOfLines={1}
+            style={[
+              styles.regularText,
+              {
+                width: '100%',
+              },
+            ]}
+            scaleStyle={styles.fieldLabel}
+          >
+            {f.label.trim()}:
+          </ScaledText>
+          <ScaledView scaleStyle={{ paddingBottom: 9 }} />
+          <ScaledText
+            numberOfLines={2}
+            //@ts-expect-error
+            onTextLayout={(e: TextLayoutEvent) =>
+              handleFieldValueLayout(e, idx)
+            }
+            scaleStyle={styles.fieldText}
+            style={[
+              styles.mediumText,
+              {
+                width: '100%',
+              },
+            ]}
+          >
+            {f.value}
+          </ScaledText>
+        </React.Fragment>
+      ))}
+    </FieldsCalculator>
+  )
+}
+
 const styles = StyleSheet.create({
   dotsContainerScaled: {
-    paddingHorizontal: 3,
+    paddingVertical: 3,
+    width: 24,
+    height: 24,
   },
   dotsContainer: {
-    position: 'absolute',
-    zIndex: 100,
+    //position: 'absolute',
+    //zIndex: 100,
+    height: '100%',
   },
   dotsBtn: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-    paddingVertical: 10,
+    alignItems: 'stretch',
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    paddingHorizontal: 10,
+    flex: 1,
   },
   dot: {
     width: 4,
@@ -42,5 +208,69 @@ const styles = StyleSheet.create({
     borderRadius: 2,
     marginHorizontal: 2,
     backgroundColor: Colors.black,
+  },
+  credentialName: {
+    fontSize: 22,
+    lineHeight: 26,
+    fontWeight: '500',
+  },
+  holderName: {
+    fontSize: 20,
+    lineHeight: 20,
+  },
+  photoContainer: {
+    position: 'absolute',
+    overflow: 'hidden',
+    zIndex: 10,
+  },
+  photoContainerScaled: {
+    bottom: 27,
+    right: 14,
+    width: 82,
+    height: 82,
+    borderRadius: 41,
+  },
+  headerContainer: {
+    height: 68,
+    width: '100%',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingVertical: 16,
+  },
+  photo: {
+    width: '100%',
+    height: '100%',
+  },
+  footerContainerScaled: {
+    height: 60,
+    paddingHorizontal: 20,
+  },
+  footerContainer: {
+    width: '100%',
+    zIndex: 9,
+  },
+  footerBorder: {
+    width: '100%',
+    borderTopWidth: 1,
+    borderTopColor: '#D8D8D8',
+  },
+  regularText: {
+    fontFamily: Fonts.Regular,
+    fontSize: 22,
+    color: Colors.black,
+  },
+  mediumText: {
+    fontFamily: Fonts.Medium,
+    color: Colors.black,
+  },
+  fieldLabel: {
+    fontSize: 16,
+    lineHeight: 16,
+    color: Colors.slateGray,
+  },
+  fieldText: {
+    fontSize: 20,
+    lineHeight: 20,
+    letterSpacing: 0.14,
   },
 })
