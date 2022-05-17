425c5a59b16186bc1cc3cb0f5893f2729899e4ee
Cristian Lungu <cristian.lungu@hsrw.org>
2022-05-17 12:39:25 +0200

    feat(documents): sort documents by issuance date (newest)

--- src/modules/credentials/selectors.ts
+++ src/modules/credentials/selectors.ts
@@ -1,10 +1,18 @@
 import { createSelector } from 'reselect'
-import { mapDisplayToDocuments } from '~/hooks/signedCredentials/utils'
+import {
+  mapDisplayToDocuments,
+  sortCredentialsByRecentIssueDate,
+} from '~/hooks/signedCredentials/utils'
 import { RootReducerI } from '~/types/reducer'
 
 export const getAllCredentials = (state: RootReducerI) => state.credentials.all
 
 export const getAllDocuments = createSelector(
   [getAllCredentials],
-  mapDisplayToDocuments,
+  (documents) => {
+    const sorted = sortCredentialsByRecentIssueDate(documents)
+    const docs = mapDisplayToDocuments(sorted)
+
+    return docs
+  },
 )
