6952427bc047b2b25396056d6e996a91718b0b55
Thomas Wallner <thomas.wallner23@gmail.com>
2022-06-28 17:14:55 +0200

    refactor(userendericons): adding useRenderIcons hook

---
+++ src/hooks/useRenderIcons.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+import { Image } from 'react-native'
+
+const useRenderIcons = (issuerIcon: string | undefined) => {
+  const [renderIcon, setRenderIcon] = useState(false)
+
+  const shouldRenderIcon = async (icon: string) => {
+    let result
+    try {
+      result = (await Image.prefetch(icon)) as boolean
+    } catch (e) {
+      setRenderIcon(false)
+    }
+    setRenderIcon(result as boolean)
+  }
+
+  useEffect(() => {
+    issuerIcon && shouldRenderIcon(issuerIcon)
+  }, [])
+
+  return { renderIcon }
+}
+
+export default useRenderIcons
