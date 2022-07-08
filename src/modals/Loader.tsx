0cd8c57b94acf6c98d1d4907f03e812225f69ed5
Thomas Wallner <thomas.wallner23@gmail.com>
2022-07-08 12:22:13 +0200

    refactor(loader): applying loop to ripple instead of animateValueTo

--- src/modals/Loader.tsx
+++ src/modals/Loader.tsx
@@ -78,13 +78,11 @@ const Loader: React.FC<LoaderI> = ({ bgColor = Colors.black }) => {
     toValue: number,
     duration: number,
   ) => {
-    return Animated.loop(
-      Animated.timing(value, {
-        toValue: toValue,
-        duration: duration,
-        useNativeDriver: true,
-      }),
-    )
+    return Animated.timing(value, {
+      toValue: toValue,
+      duration: duration,
+      useNativeDriver: true,
+    })
   }
 
   const reset = Animated.parallel([
@@ -108,10 +106,9 @@ const Loader: React.FC<LoaderI> = ({ bgColor = Colors.black }) => {
     animateValueTo(animatedWidth3, SCALE_MAX, 2500),
   ])
 
-  const ripple = Animated.sequence([
-    Animated.parallel([fRipple, sRipple, tRipple]),
-    reset,
-  ])
+  const ripple = Animated.loop(
+    Animated.sequence([Animated.parallel([fRipple, sRipple, tRipple]), reset]),
+  )
 
   const bounceError = () => {
     Animated.parallel([
