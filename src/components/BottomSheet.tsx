f4ac7d4fd5126721c5ad40441f5a176200b7ea49
Cristian Lungu <cristian.lungu@hsrw.org>
2022-07-26 14:51:45 +0200

    feat(bottom-sheet): added customstyles prop

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
+import { IWithCustomStyle } from '~/types/props'
 import { Colors } from '~/utils/colors'
 import ScreenDismissArea from './ScreenDismissArea'
 
-interface BottomSheetProps {
+interface BottomSheetProps extends IWithCustomStyle {
   onDismiss?: () => void
 }
 
 // NOTE: the hight of the sheet will depend on the children
-const BottomSheet: React.FC<BottomSheetProps> = ({ children, onDismiss }) => {
+const BottomSheet: React.FC<BottomSheetProps> = ({
+  children,
+  onDismiss,
+  customStyles,
+}) => {
   return (
     <View style={styles.fullScreen}>
       <ScreenDismissArea onDismiss={onDismiss} />
-      <View style={styles.interactionBody}>{children}</View>
+      <View style={[styles.interactionBody, customStyles]}>{children}</View>
     </View>
   )
 }
@@ -22,9 +27,6 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     backgroundColor: Colors.black65,
-  },
-  tapArea: {
-    flex: 1,
   },
   interactionBody: {
     flex: 0,
