f01909ed5272cd7dc093260b241ddd7964ba82c0
Thomas Wallner <thomas.wallner23@gmail.com>
2022-08-09 18:03:23 +0200

    refactor(truncatedjolotext): making component dynamic and reworking ontextlayout logic

--- src/components/TruncatedJoloText.tsx
+++ src/components/TruncatedJoloText.tsx
@@ -1,37 +1,41 @@
 import React, { useState } from 'react'
-import JoloText, { JoloTextKind, JoloTextWeight } from './JoloText'
-import { Colors } from '~/utils/colors'
+import JoloText, { IJoloTextProps } from './JoloText'
 
-interface Props {
+interface Props extends IJoloTextProps {
   text: string
   numOfLines: number
 }
 
-const TruncatedJoloText: React.FC<Props> = ({ text, numOfLines }) => {
+const TruncatedJoloText: React.FC<Props> = ({
+  text,
+  numOfLines,
+  color,
+  kind,
+  weight,
+}) => {
   const [truncatedText, setTruncatedText] = useState('')
 
   const onTextLayout = (e) => {
     const { lines } = e.nativeEvent
-    let output: string =
-      lines.length <= numOfLines
-        ? lines
-            .splice(0, numOfLines)
-            .map((line) => line.text)
-            .join('')
-        : lines
-            .splice(0, numOfLines)
-            .map((line) => line.text)
-            .join('')
-            .slice(0, -4) + '...?'
-    setTruncatedText(output)
+
+    if (lines.length > numOfLines) {
+      const output =
+        lines
+          .splice(0, numOfLines)
+          .map((line) => line.text)
+          .join('')
+          .slice(0, -4) + '...?'
+
+      setTruncatedText(output)
+    }
   }
 
   return (
     <JoloText // @ts-expect-error
       onTextLayout={onTextLayout}
-      color={Colors.white90}
-      kind={JoloTextKind.title}
-      weight={JoloTextWeight.regular}
+      color={color}
+      kind={kind}
+      weight={weight}
     >
       {truncatedText ? truncatedText : text}
     </JoloText>
