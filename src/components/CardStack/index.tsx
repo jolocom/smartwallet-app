f4d797202ba1b8da6088772d5abd0984300957bf
Cristian Lungu <lungucristian95@gmail.com>
2022-08-26 14:06:14 +0200

    fix(documents): avoid scrolling when expanding first item

--- src/components/CardStack/index.tsx
+++ src/components/CardStack/index.tsx
@@ -59,7 +59,12 @@ export const StackScrollView = <T extends { id: string }, P extends {}>({
       return { changed, expanded, collapsed }
     },
     ({ changed, expanded }) => {
-      if (expanded) {
+      const isFirstItem =
+        expandValue.value?.stackId === data[0].stackId &&
+        expandValue.value?.itemId === data[0].data[0].id
+
+      console.log({ isFirstItem, expandState })
+      if (expanded && !isFirstItem) {
         scrollTo(scrollRef, 0, 150, true)
       }
 
