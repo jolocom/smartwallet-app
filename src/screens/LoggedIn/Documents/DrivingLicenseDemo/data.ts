4e21f9a18300f5360e435d401f0f6ff5899e941e
Cristian Lungu <cristian.lungu@hsrw.org>
2022-09-07 17:20:28 +0200

    feat(mdl): added metadata for mdl VC

---
+++ src/screens/LoggedIn/Documents/DrivingLicenseDemo/data.ts
@@ -0,0 +1,120 @@
+export const mdlMetadata = {
+  type: ['VerifiableCredential', 'DrivingLicenseCredential'],
+  name: 'Führerschein',
+  context: [
+    {
+      name: 'schema:name',
+      given_name: 'schema:given_name',
+      family_name: 'schema:family_name',
+      birth_date: 'schema:birth_date',
+      document_number: 'schema:document_number',
+      issuing_authority: 'schema:issuing_authority',
+      expiry_date: 'schema:expiry_date',
+      issue_date: 'schema:issue_date',
+      issuing_country: 'schema:issuing_country',
+      un_distinguishing_sign: 'schema:un_distinguishing_sign',
+      driving_privileges: 'schema:driving_privileges',
+      portrait: 'schema:portrait',
+    },
+  ],
+}
+
+export const mdlManifest = {
+  type: 'DrivingLicenseCredential',
+  credential: {
+    schema: 'https://schemas.jolocom.io/DrivingLicenseCredential',
+    name: 'Führerschein',
+    display: {
+      properties: [
+        {
+          path: ['$.given_name'],
+          label: 'Vorname',
+          mime_type: 'text/plain',
+          preview: false,
+        },
+        {
+          path: ['$.family_name'],
+          label: 'Familienname',
+          mime_type: 'text/plain',
+          preview: false,
+        },
+        {
+          path: ['$.birth_date'],
+          label: 'Geburtsdatum',
+          mime_type: 'text/plain',
+          preview: false,
+        },
+        {
+          path: ['$.document_number'],
+          label: 'Führerscheinnummer',
+          mime_type: 'text/plain',
+          preview: false,
+        },
+        {
+          path: ['$.issuing_authority'],
+          label: 'Ausstellende Behörde',
+          mime_type: 'text/plain',
+          preview: false,
+        },
+        {
+          path: ['$.expiry_date'],
+          label: 'Gültig bis',
+          mime_type: 'text/plain',
+          preview: false,
+        },
+        {
+          path: ['$.issue_date'],
+          label: 'Letztes Update',
+          mime_type: 'text/plain',
+          preview: false,
+        },
+        {
+          path: ['$.issuing_country'],
+          label: 'Ausstellungsland',
+          mime_type: 'text/plain',
+          preview: false,
+        },
+        {
+          path: ['$.un_distinguishing_sign'],
+          label: 'Länderkennzeichen',
+          mime_type: 'text/plain',
+          preview: false,
+        },
+        {
+          path: ['$.driving_privileges'],
+          label: 'Führerscheinrechte',
+          mime_type: 'text/plain',
+          preview: false,
+        },
+      ],
+    },
+    styles: {
+      thumbnail: {
+        uri: 'https://cdn.icon-icons.com/icons2/1694/PNG/512/eueuropeanunionflag_111740.png',
+        alt: '',
+      },
+      hero: {
+        uri: 'https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2014/11/german_flag/15079489-2-eng-GB/German_flag_pillars.png',
+        alt: '',
+      },
+      background: {
+        image_url: {
+          uri: 'https://i.ibb.co/p1Zz70C/istockphoto-1072694792-612x612.jpg',
+          alt: '',
+        },
+      },
+      text: {
+        color: '',
+      },
+    },
+  },
+  issuer: {
+    did: 'did:jun:Er6cXshrAxqwUvTyzmn0pKTkNbrHgaTTc5kTn8Z19hYE',
+    publicProfile: {
+        name: "Jolocom",
+        description: "Jolocom is a decentralized identity platform that enables users to own and control their own digital identity.",
+        image: "https://cloudsignatureconsortium.org/wp-content/uploads/2019/11/Logo_300dpi-300x160-1.png",
+        url: "https://jolocom.io"
+    }
+  },
+}
