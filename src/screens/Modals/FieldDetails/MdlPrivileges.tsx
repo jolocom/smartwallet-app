fb9fc0a856970f37636c945420bb992ab6f439fc
Thomas Wallner <thomas.wallner23@gmail.com>
2022-09-19 17:57:32 +0200

    refactor(mdlprivileges): extracting MdlPrivileges into separate component

---
+++ src/screens/Modals/FieldDetails/MdlPrivileges.tsx
@@ -0,0 +1,117 @@
+import React from 'react'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import JoloText, { JoloTextKind, JoloTextWeight } from '~/components/JoloText'
+import { handleLayout } from './Field'
+import BP from '~/utils/breakpoints'
+import { Colors } from '~/utils/colors'
+import { JoloTextSizes } from '~/utils/fonts'
+import { MotorCycleIcon, PassengerCarIcon, TractorIcon } from '~/assets/svg'
+import {
+  DrivingPrivilegesKeys,
+  VehicleTypes,
+} from '~/screens/LoggedIn/Documents/DrivingLicenseDemo/data'
+
+const GetVehicleIcon = (title: VehicleTypes) => (
+  <View style={styles.vehicleIconContainer}>
+    {title === VehicleTypes.MopedAndMotorcycle && <MotorCycleIcon />}
+    {title === VehicleTypes.PassengerCar && <PassengerCarIcon />}
+    {title === VehicleTypes.TractorAndForklift && <TractorIcon />}
+    {title === VehicleTypes.Truck && <TractorIcon />}
+  </View>
+)
+
+export const renderPrivileges = (categories) => {
+  return categories.map((field, i) => {
+    if (field.data[DrivingPrivilegesKeys.VehicleCode].length) {
+      return (
+        <React.Fragment key={i}>
+          <TouchableOpacity
+            style={styles.privilegesContainer}
+            onLayout={handleLayout}
+            activeOpacity={1}
+          >
+            {GetVehicleIcon(field.title)}
+            <JoloText
+              customStyles={styles.fieldText}
+              size={JoloTextSizes.mini}
+              color={Colors.osloGray}
+            >
+              {field.title}
+            </JoloText>
+            <View
+              style={{
+                width: '100%',
+              }}
+            >
+              {Object.keys(field.data).map((key, i) => (
+                <View
+                  style={{
+                    ...styles.vehicleFieldsContainer,
+                    ...(i !== Object.keys(field.data).length - 1 && {
+                      borderBottomColor: Colors.genevaGray15,
+                      borderBottomWidth: 1,
+                    }),
+                  }}
+                  key={i}
+                >
+                  <JoloText
+                    kind={JoloTextKind.subtitle}
+                    color={Colors.black}
+                    weight={JoloTextWeight.medium}
+                  >
+                    {key}
+                  </JoloText>
+                  <JoloText
+                    color={Colors.black}
+                    customStyles={styles.fieldText}
+                  >
+                    {key === DrivingPrivilegesKeys.VehicleCode
+                      ? field.data[key].join(', ')
+                      : field.data[key]}
+                  </JoloText>
+                </View>
+              ))}
+            </View>
+          </TouchableOpacity>
+          {i !== categories.length - 1 && (
+            <View style={styles.privilegesDivider} />
+          )}
+        </React.Fragment>
+      )
+    }
+  })
+}
+
+const styles = StyleSheet.create({
+  fieldText: {
+    textAlign: 'left',
+  },
+  privilegesDivider: {
+    height: 6,
+    backgroundColor: Colors.genevaGray,
+    width: '100%',
+    opacity: 0.15,
+  },
+  privilegesContainer: {
+    paddingVertical: BP({
+      default: 8,
+      xsmall: 4,
+    }),
+    paddingHorizontal: 16,
+    alignItems: 'flex-start',
+    position: 'relative',
+  },
+  vehicleFieldsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    position: 'relative',
+  },
+  vehicleIconContainer: {
+    position: 'absolute',
+    right: 16,
+    top: BP({
+      default: 8,
+      xsmall: 4,
+    }),
+  },
+})
--- src/screens/Modals/FieldDetails/index.tsx
+++ src/screens/Modals/FieldDetails/index.tsx
@@ -1,6 +1,6 @@
 import { RouteProp, useRoute } from '@react-navigation/native'
 import React, { useState } from 'react'
-import { Image, StyleSheet, TouchableOpacity, View } from 'react-native'
+import { Image, StyleSheet, View } from 'react-native'
 import { useSafeArea } from 'react-native-safe-area-context'
 import { useSelector } from 'react-redux'
 import Block from '~/components/Block'
@@ -10,7 +10,7 @@ import { NavHeaderType } from '~/components/NavigationHeader'
 import ScreenContainer from '~/components/ScreenContainer'
 import { useDocuments } from '~/hooks/documents'
 import useImagePrefetch from '~/hooks/useImagePrefetch'
-import { renderField, handleLayout } from './Field'
+import { renderField } from './Field'
 import { getDocumentById } from '~/modules/credentials/selectors'
 import { TextLayoutEvent } from '~/types/props'
 import { ScreenNames } from '~/types/screens'
@@ -18,12 +18,8 @@ import BP from '~/utils/breakpoints'
 import { Colors } from '~/utils/colors'
 import { JoloTextSizes } from '~/utils/fonts'
 import { MainStackParamList } from '../../LoggedIn/Main'
-import { MotorCycleIcon, PassengerCarIcon, TractorIcon } from '~/assets/svg'
-import {
-  DrivingPrivilegesKeys,
-  VehicleTypes,
-} from '~/screens/LoggedIn/Documents/DrivingLicenseDemo/data'
 import { useGoBack } from '~/hooks/navigation'
+import { renderPrivileges } from './MdlPrivileges'
 import useDrivingPrivileges from './hooks'
 
 const IMAGE_SIZE = BP({ large: 104, default: 90 })
@@ -77,77 +73,6 @@ const FieldDetails = () => {
   const getNumOfLines = (e: TextLayoutEvent) => {
     const { lines } = e.nativeEvent
     setNumOfLines(lines.length)
-  }
-
-  const GetVehicleIcon = (title: VehicleTypes) => (
-    <View style={styles.vehicleIconContainer}>
-      {title === VehicleTypes.MopedAndMotorcycle && <MotorCycleIcon />}
-      {title === VehicleTypes.PassengerCar && <PassengerCarIcon />}
-      {title === VehicleTypes.TractorAndForklift && <TractorIcon />}
-      {title === VehicleTypes.Truck && <TractorIcon />}
-    </View>
-  )
-
-  const renderPrivileges = (categories) => {
-    return categories.map((field, i) => {
-      if (field.data[DrivingPrivilegesKeys.VehicleCode].length) {
-        return (
-          <React.Fragment key={i}>
-            <TouchableOpacity
-              style={styles.privilegesContainer}
-              onLayout={handleLayout}
-              activeOpacity={1}
-            >
-              {GetVehicleIcon(field.title)}
-              <JoloText
-                customStyles={styles.fieldText}
-                size={JoloTextSizes.mini}
-                color={Colors.osloGray}
-              >
-                {field.title}
-              </JoloText>
-              <View
-                style={{
-                  width: '100%',
-                }}
-              >
-                {Object.keys(field.data).map((key, i) => (
-                  <View
-                    style={{
-                      ...styles.vehicleFieldsContainer,
-                      ...(i !== Object.keys(field.data).length - 1 && {
-                        borderBottomColor: Colors.genevaGray15,
-                        borderBottomWidth: 1,
-                      }),
-                    }}
-                    key={i}
-                  >
-                    <JoloText
-                      kind={JoloTextKind.subtitle}
-                      color={Colors.black}
-                      weight={JoloTextWeight.medium}
-                    >
-                      {key}
-                    </JoloText>
-                    <JoloText
-                      color={Colors.black}
-                      customStyles={styles.fieldText}
-                    >
-                      {key === DrivingPrivilegesKeys.VehicleCode
-                        ? field.data[key].join(', ')
-                        : field.data[key]}
-                    </JoloText>
-                  </View>
-                ))}
-              </View>
-            </TouchableOpacity>
-            {i !== categories.length - 1 && (
-              <View style={styles.privilegesDivider} />
-            )}
-          </React.Fragment>
-        )
-      }
-    })
   }
 
   return (
@@ -249,34 +174,6 @@ const styles = StyleSheet.create({
 
   fieldText: {
     textAlign: 'left',
-  },
-  privilegesDivider: {
-    height: 6,
-    backgroundColor: Colors.genevaGray,
-    width: '100%',
-    opacity: 0.15,
-  },
-  privilegesContainer: {
-    paddingVertical: BP({
-      default: 8,
-      xsmall: 4,
-    }),
-    paddingHorizontal: 16,
-    alignItems: 'flex-start',
-    position: 'relative',
-  },
-  vehicleFieldsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    position: 'relative',
-  },
-  vehicleIconContainer: {
-    position: 'absolute',
-    right: 16,
-    top: BP({
-      default: 8,
-      xsmall: 4,
-    }),
   },
 })
 
