e39290f5c9d02ef92bb017eb9a94edbea124b3bb
Cristian Lungu <cristian.lungu@hsrw.org>
2022-09-22 10:56:16 +0200

    fix(mdl): removed the mdl module and fixed mdl deletion

--- src/modules/mdl/actions.ts
+++
@@ -1,5 +0,0 @@
-import { DrivingLicenseData } from "react-native-mdl";
-import createAction from "~/utils/createAction";
-import { MdlActions } from "./types";
-
-export const setMdlDisplayData = createAction<DrivingLicenseData | null>(MdlActions.setDisplayData)
