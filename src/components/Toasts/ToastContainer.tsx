0128b0b44977b4e3e87016fc0c5707649cdfd4ff
Thomas Wallner <thomas.wallner23@gmail.com>
2022-10-14 11:00:05 +0200

    refactor(toastcontainer): adding new styling to ToastContainer

--- src/components/Toasts/ToastContainer.tsx
+++ src/components/Toasts/ToastContainer.tsx
@@ -1,19 +1,40 @@
 import React from 'react'
 import { View } from 'react-native'
 import { useSafeArea } from 'react-native-safe-area-context'
+import { ToastType } from '~/types/toasts'
 import { Colors } from '~/utils/colors'
+import { useToastToShow } from './context'
 
 export const BOTTOM_PADDING = 20
 
 const ToastContainer: React.FC = ({ children }) => {
   const { top } = useSafeArea()
+  const { toastToShow } = useToastToShow()
+
+  const getBorderColor = (type: ToastType | undefined) => {
+    switch (type) {
+      case ToastType.warning:
+        return Colors.error
+      case ToastType.info:
+        return Colors.white
+      default:
+        return Colors.success
+    }
+  }
   return (
     <View
       style={{
-        paddingTop: top + 20,
         backgroundColor: Colors.black,
-        paddingHorizontal: 25,
-        paddingBottom: BOTTOM_PADDING,
+        paddingVertical: 24,
+        paddingHorizontal: 21,
+        flexDirection: 'row',
+        width: '94%',
+        borderColor: getBorderColor(toastToShow?.type),
+        borderRadius: 16,
+        borderWidth: 2,
+        marginTop: top,
+        alignSelf: 'center',
+        alignItems: 'center',
       }}
     >
       {children}
