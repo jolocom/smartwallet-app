6891e76db7fa9382e219f331704b459558770219
Thomas Wallner <thomas.wallner23@gmail.com>
2022-10-14 11:00:46 +0200

    refactor(interactivetoast): adding icon to InteractiveToast

--- src/components/Toasts/InteractiveToast.tsx
+++ src/components/Toasts/InteractiveToast.tsx
@@ -14,7 +14,10 @@ import JoloText, { JoloTextKind } from '../JoloText'
 import { useToastToShow } from './context'
 import ToastContainer from './ToastContainer'
 import ToastDescription from './ToastDescription'
+import { ErrorToastIcon, InfoToastIcon } from '~/assets/svg'
 import ToastTitle from './ToastTitle'
+import { debugView } from '~/utils/dev'
+import { ToastType } from '~/types/toasts'
 
 interface IProps {
   handleInteractionBtnLayout: (e: LayoutChangeEvent) => void
@@ -24,38 +27,59 @@ const InteractiveToast = React.forwardRef<View, IProps>(
   ({ handleInteractionBtnLayout }, ref) => {
     const { toastToShow, toastColor, invokeInteract, isInteractive } =
       useToastToShow()
+
+    console.log({ toastToShow })
+
+    const getToastIcon = (type: ToastType | undefined) => {
+      switch (type) {
+        case ToastType.warning:
+          return <ErrorToastIcon />
+        case ToastType.info:
+          return <InfoToastIcon />
+        default:
+          break
+      }
+    }
+
     if (isInteractive) {
       return (
         <ToastContainer>
-          <ToastTitle customStyles={{ textAlign: 'left' }} />
-          <View style={styles.withInteractContainer}>
-            <View style={styles.withInteractText}>
-              <ToastDescription customStyles={{ textAlign: 'left' }} />
-            </View>
-            <View
-              style={styles.interactBtnContainer}
-              onLayout={handleInteractionBtnLayout}
-              ref={ref}
-            >
-              <TouchableOpacity
-                onPressIn={invokeInteract}
-                style={styles.interactBtn}
+          {getToastIcon(toastToShow?.type)}
+          <View style={{ flex: 1, marginLeft: 23 }}>
+            <ToastTitle
+              customStyles={{
+                textAlign: 'left',
+              }}
+            />
+            <View style={styles.withInteractContainer}>
+              <View style={styles.withInteractText}>
+                <ToastDescription customStyles={{ textAlign: 'left' }} />
+              </View>
+              <View
+                style={styles.interactBtnContainer}
+                onLayout={handleInteractionBtnLayout}
+                ref={ref}
               >
-                {toastToShow?.interact && (
-                  <JoloText
-                    kind={JoloTextKind.subtitle}
-                    size={JoloTextSizes.tiniest}
-                    color={toastColor}
-                    customStyles={{
-                      fontSize: 12,
-                      letterSpacing: 0.8,
-                      lineHeight: 12,
-                    }}
-                  >
-                    {toastToShow.interact.label}
-                  </JoloText>
-                )}
-              </TouchableOpacity>
+                {/* <TouchableOpacity
+                  onPressIn={invokeInteract}
+                  style={styles.interactBtn}
+                >
+                  {toastToShow?.interact && (
+                    <JoloText
+                      kind={JoloTextKind.subtitle}
+                      size={JoloTextSizes.tiniest}
+                      color={toastColor}
+                      customStyles={{
+                        fontSize: 12,
+                        letterSpacing: 0.8,
+                        lineHeight: 12,
+                      }}
+                    >
+                      {toastToShow.interact.label}
+                    </JoloText>
+                  )}
+                </TouchableOpacity> */}
+              </View>
             </View>
           </View>
         </ToastContainer>
@@ -67,8 +91,8 @@ const InteractiveToast = React.forwardRef<View, IProps>(
 
 const styles = StyleSheet.create({
   withInteractContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    // flexDirection: 'row',
+    // justifyContent: 'space-between',
   },
   interactBtnContainer: {
     flex: 0.3,
@@ -81,7 +105,7 @@ const styles = StyleSheet.create({
     borderColor: Colors.silverChalice,
     borderRadius: 6.4,
   },
-  withInteractText: { alignItems: 'flex-start', flex: 0.6 },
+  withInteractText: { alignItems: 'flex-start', flex: 1 },
 })
 
 export default InteractiveToast
