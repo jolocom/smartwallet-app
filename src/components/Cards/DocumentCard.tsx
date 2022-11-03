fdc98afe6a896d51666528ad4eea29f97a36db11
Thomas Wallner <thomas.wallner23@gmail.com>
2022-11-03 10:39:33 +0100

    refactor(documentcard): adding right color to animation

--- src/components/Cards/DocumentCard.tsx
+++ src/components/Cards/DocumentCard.tsx
@@ -329,7 +329,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgb(113, 114, 254)',
+    backgroundColor: Colors.success,
     zIndex: 1,
     borderRadius: 15,
   },
