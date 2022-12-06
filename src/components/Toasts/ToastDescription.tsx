efb44236b0ac1fc068a68579eaa6aaa241c02b17
Thomas Wallner <thomas.wallner23@gmail.com>
2022-12-06 17:06:43 +0100

    refactor(toastdescription): replacing TouachableOpacity from rn by react-native-gesture-handler

--- src/components/Toasts/ToastDescription.tsx
+++ src/components/Toasts/ToastDescription.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { StyleSheet, TouchableOpacity } from 'react-native'
+import { StyleSheet } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 import JoloText, { JoloTextKind } from '~/components/JoloText'
 import { IWithCustomStyle } from '~/types/props'
 import BP from '~/utils/breakpoints'
