b2dcc864923c60e5a3bfebe23f797bbcbe50edc7
Paul Tonga <paulxaviertonga@gmail.com>
2023-03-01 18:01:12 +0200

    build(70): build 70

--- src/screens/LoggedIn/Settings/index.tsx
+++ src/screens/LoggedIn/Settings/index.tsx
@@ -21,7 +21,6 @@ import Btn, { BtnTypes } from '~/components/Btn'
 import useTranslation from '~/hooks/useTranslation'
 import EnableScreenshotsOption from './EnableScreenshotsOption'
 import useSettings from '~/hooks/settings'
-import MnemonicPhraseWarning from './MnemonicPhraseWarning'
 
 const SettingsGeneral: React.FC = () => {
   const { t } = useTranslation()
